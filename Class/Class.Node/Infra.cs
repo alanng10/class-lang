3f957bd3f8fc45d22c359a4a29b90bba15465830
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-19 20:20:46 +0800

    Class

--- Class/Class.Node/Infra.cs
+++ Class/Class.Node/Infra.cs
@@ -1,78 +1,38 @@
 namespace Class.Node;
 
-
-
-
-
-public class Stat : Any
+public class Infra : Any
 {
-    public static Stat This { get; } = CreateShare();
+    public static Infra This { get; } = ShareCreate();
 
-
-
-
-    private static Stat CreateShare()
+    private static Infra ShareCreate()
     {
-        Stat share;
-
-
-        share = new Stat();
-
-
+        Infra share;
+        share = new Infra();
 
         Any a;
-
-
         a = share;
-
-
         a.Init();
-
-
 
         return share;
     }
-
-
-
-
 
 
     public override bool Init()
     {
         base.Init();
 
+        this.InfraInfra = InfraInfra.This;        
 
-
-
-        this.InfraStat = InfraStat.This;
-        
-
-
-
-        this.IntSignValueNegativeMax = this.InfraStat.IntCapValue / 2;
-
-
+        this.IntSignValueNegativeMax = this.InfraInfra.IntCapValue / 2;
 
         this.IntSignValuePositiveMax = this.IntSignValueNegativeMax - 1;
-
-
-        
 
         return true;
     }
 
-
-
-
-    protected virtual InfraStat InfraStat { get; set; }
-
-
-
+    protected virtual InfraInfra InfraInfra { get; set; }
 
     public virtual long IntSignValuePositiveMax { get; set; }
-
-
 
     public virtual long IntSignValueNegativeMax { get; set; }
 }
