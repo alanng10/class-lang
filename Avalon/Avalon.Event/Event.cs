6188d45ff325b74bcbb4650055f62ea1e4a7e1db
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-21 13:09:21 +0800

    Event

--- Avalon/Avalon.Event/Event.cs
+++ Avalon/Avalon.Event/Event.cs
@@ -1,82 +1,33 @@
 namespace Avalon.Event;
 
-
-
-
 public class Event : Any
 {
     public override bool Init()
     {
         base.Init();
-
-
-
         StateTable state;
-
         state = new StateTable();
-
         state.Init();
-
-
-
         this.State = state;
-
-
-
         this.StateIter = this.State.CreateIter();
-
-
-
         return true;
     }
     
-
-
-
-
     public virtual StateTable State { get; set; }
-
-
-
-
-
     protected virtual Iter StateIter { get; set; }
-
-
-
-
-
 
     public virtual bool Trigger(object arg)
     {
         Iter iter;
-
-
         iter = this.StateIter;
-
-
-
         this.State.SetIter(iter);
-
-
-
-
         while (iter.Next())
         {
             State state;
-
-
             state = (State)iter.Value;
-
-
             state.Arg = arg;
-
-
             state.Execute();
         }
-
-
-
         return true;
     }
 }
