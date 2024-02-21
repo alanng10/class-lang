4ce22313e81db1fdc5eaa939c9b3a544d04f6801
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-21 19:49:29 +0800

    Avalon

--- Script/Avalon/PushPackage.cmd
+++ Script/Avalon/PushPackage.cmd
@@ -1,22 +1,9 @@
 @echo off
 
-
-
-
-
 echo Push Package
-
-
 ..\nuget delete Avalon 1.0.0 -NonInteractive
-
 echo:
-
-
 ..\nuget locals all -clear
-
 echo:
-
-
 ..\nuget push Avalon.1.0.0.nupkg
-
 echo:
