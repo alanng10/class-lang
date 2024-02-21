d08cc8d7d3194745af1096d4439a7d55a468e55b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-21 13:04:00 +0800

    Comp

--- Avalon/Avalon.Comp/Change.cs
+++ Avalon/Avalon.Comp/Change.cs
@@ -1,15 +1,7 @@
 namespace Avalon.Comp;
 
-
-
-
-
-
 public class Change : Any
 {
     public virtual Comp Comp { get; set; }
-
-
-
     public virtual Field Field { get; set; }
 }
--- Avalon/Avalon.Comp/Comp.cs
+++ Avalon/Avalon.Comp/Comp.cs
@@ -1,99 +1,37 @@
 namespace Avalon.Comp;
 
-
-
-
-
 public class Comp : Any
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.TriggerArg = this.CreateTriggerArg();
-
-
-
-
         this.ChangeEvent = new EventEvent();
-
-
         this.ChangeEvent.Init();
-
-
-
-
-
         return true;
     }
-
-
-
-
-
 
     protected virtual Change CreateTriggerArg()
     {
         Change a;
-
-
         a = new Change();
-
-
         a.Init();
-
-
-
         return a;
     }
-
-
-
-
-
 
     public virtual bool Change(Field field, Change change)
     {
         return true;
     }
 
-
-
-
-
-
-
     protected virtual bool Trigger(Field field)
     {
         this.TriggerArg.Comp = this;
-
-
         this.TriggerArg.Field = field;
-
-
-
-
         this.ChangeEvent.Trigger(this.TriggerArg);
-        
-
-
-
         return true;
     }
 
-
-
-
-
-
     public virtual Change TriggerArg { get; set; }
-
-
-
-
     public virtual EventEvent ChangeEvent { get; set; }
 }
