b706a66e89e1db2e26a22d4b4f40901e2ceed615
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-21 19:50:18 +0800

    Avalon

--- Script/Avalon/Make.cmd
+++ Script/Avalon/Make.cmd
@@ -1,44 +1,20 @@
 @echo off
 
-
 echo Clean Module
-
 set CSharpOut=.\Out\net6.0
-
 del /F /Q %CSharpOut%\Avalon.*
-
 del /F /Q %CSharpOut%\MakeProject.*
-
 rmdir /S /Q %CSharpOut%\Lib
-
 echo:
-
 
 echo Clean Demo
-
 rmdir /S /Q .\Out\Demo
-
 echo:
-
-
-
 call Script\Avalon\MakeModule
-
 echo:
-
-
-
 call Script\Avalon\MakePackage
-
 echo:
-
-
 call Script\Avalon\PushPackage
-
 echo:
-
-
-
 call Script\Avalon\MakeDemo
-
 echo:
