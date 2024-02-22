bae0e3b1467a39df5169d963b6a2f038ca20f79e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 13:55:49 +0800

    Draw

--- Avalon/Avalon.Draw/Brush.cs
+++ Avalon/Avalon.Draw/Brush.cs
@@ -1,121 +1,52 @@
 namespace Avalon.Draw;
 
-
-
-
-
 public class Brush : Any
 {
     public override bool Init()
     {
         base.Init();
-
-
-
         BrushInfra infra;
-
         infra = BrushInfra.This;
-
-
-
         ulong kindU;
-
         kindU = this.Kind.Intern;
-
-
-
-
         ulong colorU;
-
         colorU = 0;
-
-
         if (!(this.Color == null))
         {
             colorU = infra.InternColor(this.Color);
         }
-
-
-
         ulong imageU;
-
         imageU = 0;
-
-
         if (!(this.Image == null))
         {
             imageU = this.Image.Video.Ident;
         }
-
-
-
         ulong gradientU;
-
         gradientU = 0;
-
-
         if (!(this.Gradient == null))
         {
             gradientU = this.Gradient.Intern;
         }
-
-
-
         this.Intern = Extern.Brush_New();
-
-
         Extern.Brush_SetKind(this.Intern, kindU);
-
         Extern.Brush_SetColor(this.Intern, colorU);
-
         Extern.Brush_SetImage(this.Intern, imageU);
-
         Extern.Brush_SetGradient(this.Intern, gradientU);
-
-        
         Extern.Brush_Init(this.Intern);
-
-
-
         return true;
     }
-
-
-
 
     public virtual bool Final()
     {
         Extern.Brush_Final(this.Intern);
-
-
         Extern.Brush_Delete(this.Intern);
-
-
-
         return true;
     }
 
-
-
-
     public virtual BrushKind Kind { get; set; }
-
-
     public virtual Color Color { get; set; }
-
-
     public virtual Image Image { get; set; }
-
-
     public virtual Gradient Gradient { get; set; }
 
-
-
-
-    internal virtual ulong Intern { get; set; }
-
-
-
-
-    
+    internal virtual ulong Intern { get; set; }    
 }
--- Avalon/Avalon.Draw/BrushInfra.cs
+++ Avalon/Avalon.Draw/BrushInfra.cs
@@ -1,59 +1,27 @@
 namespace Avalon.Draw;
 
-
-
-
-
 class BrushInfra : Any
 {
     public static BrushInfra This { get; } = ShareCreate();
 
-
-
-
     private static BrushInfra ShareCreate()
     {
         BrushInfra share;
-
         share = new BrushInfra();
-
-
-
         Any a;
-
         a = share;
-
         a.Init();
-
-
-
         return share;
     }
-
-
-
 
     internal virtual ulong InternColor(Color color)
     {
         ulong k;
-
         k = 0;
-
-
-
         k = k | ((((ulong)color.Blue) & 0xff) << (0 * 8));
-
-
         k = k | ((((ulong)color.Green) & 0xff) << (1 * 8));
-
-
         k = k | ((((ulong)color.Red) & 0xff) << (2 * 8));
-
-
         k = k | ((((ulong)color.Alpha) & 0xff) << (3 * 8));
-
-
-
         return k;
     }
 }
