4ce9ac955e83b3456dc9720da9cfce6207fff639
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 21:06:57 +0800

    Infra

--- Script/Infra/MakeModule.cmd
+++ Script/Infra/MakeModule.cmd
@@ -1,8 +1,14 @@
 @echo off
 set InfraProjectOutFold=.\Out\Infra-Windows-Release
+set InfraDemoProjectOutFold=.\Out\InfraDemo-Windows-Release
 
 mkdir %InfraProjectOutFold% 1>NUL 2>NUL
 pushd %InfraProjectOutFold%
+mingw32-make
+popd
+
+mkdir %InfraDemoProjectOutFold% 1>NUL 2>NUL
+pushd %InfraDemoProjectOutFold%
 mingw32-make
 popd
 
