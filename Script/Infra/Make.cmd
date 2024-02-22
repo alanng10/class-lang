24de043832cd9c2ede1865217665fd64eded1da2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 20:49:15 +0800

    Script

--- Script/Infra/Make.cmd
+++ Script/Infra/Make.cmd
@@ -1,8 +1,6 @@
 @echo off
 set InfraProjectOutFold=.\Out\Infra-Windows-Release
 
-%QtRoot%\6.6.1\mingw_64\bin\qtenv2.bat
-
 mkdir %InfraProjectOutFold% 1>NUL 2>NUL
 pushd %InfraProjectOutFold%
 qmake ../../Infra/Infra/Infra.pro
