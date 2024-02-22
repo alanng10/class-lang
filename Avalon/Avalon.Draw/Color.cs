84031075d59d38199588f629a63f1c2042402e0b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 15:15:20 +0800

    Draw

--- Avalon/Avalon.Draw/Color.cs
+++ Avalon/Avalon.Draw/Color.cs
@@ -1,24 +1,9 @@
 namespace Avalon.Draw;
 
-
-
-
 public class Color : Any
 {
     public virtual int Alpha { get; set; }
-
-
-
-
     public virtual int Red { get; set; }
-
-
-
-
     public virtual int Green { get; set; }
-
-
-
-
     public virtual int Blue { get; set; }
 }
