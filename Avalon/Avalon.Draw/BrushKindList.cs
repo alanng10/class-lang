56544551586ac01d0c874543423fee28c8508466
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 14:00:03 +0800

    Draw

--- Avalon/Avalon.Draw/BrushKindList.cs
+++ Avalon/Avalon.Draw/BrushKindList.cs
@@ -1,74 +1,30 @@
 namespace Avalon.Draw;
 
-
-
-
-
 public class BrushKindList : Any
 {
     public static BrushKindList This { get; } = ShareCreate();
 
-
-
-
     private static BrushKindList ShareCreate()
     {
         BrushKindList share;
-
-
         share = new BrushKindList();
-
-
-
         Any a;
-
-
         a = share;
-
-
         a.Init();
-
-
-
         return share;
     }
-
-
-
-
-
 
     public override bool Init()
     {
         base.Init();
-
-
-
         this.InitArray();
-
-
-
         this.Count = this.Array.Count;
-
-
-
         this.Index = 0;
 
-
-
-
         ulong share;
-
         share = Extern.Infra_Share();
-
-
-
         ulong stat;
-
         stat = Extern.Share_Stat(share);
-
-
-
 
         this.Solid = this.AddItem(Extern.Stat_BrushKindSolid(stat));
         this.Dense1 = this.AddItem(Extern.Stat_BrushKindDense1(stat));
@@ -89,16 +45,8 @@ public class BrushKindList : Any
         this.ConicalGradient = this.AddItem(Extern.Stat_BrushKindConicalGradient(stat));
         this.Texture = this.AddItem(Extern.Stat_BrushKindTexture(stat));
 
-
-
-
-
-
         return true;
     }
-
-
-
 
     public virtual BrushKind Solid { get; set; }
     public virtual BrushKind Dense1 { get; set; }
@@ -119,59 +67,27 @@ public class BrushKindList : Any
     public virtual BrushKind ConicalGradient { get; set; }
     public virtual BrushKind Texture { get; set; }
 
-
-
-
     protected virtual BrushKind AddItem(ulong o)
     {
         BrushKind item;
-
         item = new BrushKind();
-
         item.Init();
-
         item.Index = this.Index;
-
         item.Intern = o;
-
-
-
         this.Array.Set(item.Index, item);
-
-
-
         this.Index = this.Index + 1;
-
-
-
         return item;
     }
-
-
-
 
     protected virtual bool InitArray()
     {
         this.Array = new Array();
-
-
         this.Array.Count = this.ArrayCount;
-
-
         this.Array.Init();
-
-
-
         return true;
     }
 
-
-
-
-
     protected virtual Array Array { get; set; }
-
-
 
     protected virtual int ArrayCount
     { 
@@ -184,21 +100,12 @@ public class BrushKindList : Any
         }
     }
 
-
-
-
     public virtual int Count { get; set; }
-
-
-
 
     public virtual BrushKind Get(int index)
     {
         return (BrushKind)this.Array.Get(index);
     }
-
-
-
     
     protected virtual int Index { get; set; }
 
