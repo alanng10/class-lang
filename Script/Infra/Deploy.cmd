d66d4a7f3a2b1e204b4b358a1d7a745fe7627278
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 21:01:24 +0800

    Infra

--- Script/Infra/Deploy.cmd
+++ Script/Infra/Deploy.cmd
@@ -1,30 +1,9 @@
 @echo off
 
-
-
-
-set InfraOutFold=.\Out\Infra-Windows-Release\release
-
-
-set InfraDeployFold=.\Out\InfraDeploy
-
-
 set WinDeployQt=%QtRoot%\6.6.1\mingw_64\bin\windeployqt.exe
 
-
-
-mkdir %InfraDeployFold% 1>NUL 2>NUL
-
-
-
-copy /Y %InfraOutFold%\Infra.dll %InfraDeployFold%
-
-
+call Script\Infra\UpdateDeploy
 
 pushd %InfraDeployFold%
-
-
 %WinDeployQt% Infra.dll
-
-
 popd
