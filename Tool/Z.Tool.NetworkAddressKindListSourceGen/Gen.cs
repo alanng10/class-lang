fd583c4ce936a67bcf42b0a720dcf0a6e4ec307a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 11:26:08 +0800

    Tool

--- Tool/Z.Tool.NetworkAddressKindListSourceGen/Gen.cs
+++ Tool/Z.Tool.NetworkAddressKindListSourceGen/Gen.cs
@@ -1,52 +1,18 @@
 namespace Z.Tool.NetworkAddressKindListSourceGen;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Avalon.Network";
-
-
         this.ClassName = "AddressKindList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "AddressKind";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
         this.StatItemClassName = "NetworkAddressKind";
-        
-
-
-
         this.ItemListFileName = this.GetStatItemListFileName();
-
-
-
-
         return true;
     }
 }
