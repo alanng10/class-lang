b92279878c2209778e053043caa4d308815d2c90
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 11:52:30 +0800

    Network

--- Avalon/Avalon.Network/Server.cs
+++ Avalon/Avalon.Network/Server.cs
@@ -1,200 +1,82 @@
 namespace Avalon.Network;
 
-
-
-
-
-
-
 public class Server : Any
 {
     public override bool Init()
     {
         base.Init();
-
-
-
         this.InternIntern = InternIntern.This;
-
-
         this.InternInfra = InternInfra.This;
-
-
         this.NetworkInfra = Infra.This;
-
-
-
         this.InternHandle = new Handle();
-
         this.InternHandle.Any = this;
-
         this.InternHandle.Init();
 
-
-
-
         MaideAddress oa;
-
         oa = this.NetworkInfra.ServerNewPeerMaideAddress;
-
-
-
         ulong arg;
-
-
         arg = this.InternHandle.ULong();
-
-
-
-
         this.InternNewPeerState = this.InternInfra.StateCreate(oa, arg);
 
-
-
-
         this.Intern = Extern.NetworkServer_New();
-
-
         Extern.NetworkServer_Init(this.Intern);
-
-
-
         Extern.NetworkServer_SetNewPeerState(this.Intern, this.InternNewPeerState);
-
-
-
         return true;
     }
-
-
-
 
     public virtual bool Final()
     {
         Extern.NetworkServer_Final(this.Intern);
-
-
         Extern.NetworkServer_Delete(this.Intern);
-
-
 
         this.InternInfra.StateDelete(this.InternNewPeerState);
 
-
-
         this.InternHandle.Final();
-
-
-
         return true;
     }
 
-
-
-
     public virtual Address Address { get; set; }
-
-
     public virtual int Port { get; set; }
-
-
-
     public virtual State NewPeerState { get; set; }
 
-
-
-
     private InternIntern InternIntern { get; set; }
-
-
     private InternInfra InternInfra { get; set; }
-
-
     private Infra NetworkInfra { get; set; }
-
-
-
-
     private ulong Intern { get; set; }
-
-
     private ulong InternNewPeerState { get; set; }
-
-
     private Handle InternHandle { get; set; }
-
-
-
-
 
     public virtual bool Listen()
     {
         ulong addressU;
-
         addressU = this.Address.Intern;
-
-
-
         ulong portU;
-
         portU = (ulong)this.Port;
 
-
-
         Extern.NetworkServer_SetAddress(this.Intern, addressU);
-
-
         Extern.NetworkServer_SetPort(this.Intern, portU);
 
-
-
-
         ulong u;
-
         u = Extern.NetworkServer_Listen(this.Intern);
-
-
-
-
         bool a;
-
         a = (!(u == 0));
-
-
         return a;
     }
-
-
-
-
 
     public virtual bool Close()
     {
         Extern.NetworkServer_Close(this.Intern);
-
-
-
         return true;
     }
-
-
-
 
     public virtual bool IsListen
     {
         get
         {
             ulong u;
-
             u = Extern.NetworkServer_IsListen(this.Intern);
-
-
-
             bool a;
-
             a = (!(u == 0));
-
-
             return a;
         }
         set
@@ -202,28 +84,15 @@ public class Server : Any
         }
     }
 
-
-
-
-
     public virtual Network NextPendingPeer()
     {
         ulong networkU;
-
         networkU = Extern.NetworkServer_NextPendingPeer(this.Intern);
 
-
-
         Network a;
-
         a = new Network();
-
         a.SetIntern = networkU;
-
         a.Init();
-
-
-
         return a;
     }
 
@@ -233,26 +102,13 @@ public class Server : Any
     public virtual bool ClosePeer(Network network)
     {
         ulong u;
-
         u = network.SetIntern;
-
 
         network.Final();
 
-
-
-
         Extern.NetworkServer_ClosePeer(this.Intern, u);
-
-
-
-
         return true;
     }
-
-
-
-
 
     private bool ExecuteNewPeerState()
     {
@@ -260,44 +116,20 @@ public class Server : Any
         {
             this.NewPeerState.Execute();
         }
-
-
-
         return true;
     }
-
-
-
-
 
     internal static ulong InternNewPeer(ulong networkServer, ulong arg)
     {
         InternIntern internIntern;
-
         internIntern = InternIntern.This;
 
-
-
-
         object ao;
-
         ao = internIntern.HandleTarget(arg);
 
-
-
-
         Server a;
-
         a = (Server)ao;
-
-
-
-
         a.ExecuteNewPeerState();
-
-
-
-
 
         return 1;
     }
