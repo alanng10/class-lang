f4910c0694b79a1129be475877340c66e71c1590
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 15:17:36 +0800

    Tool

--- Tool/Z.Tool.CompositeListSourceGen/Gen.cs
+++ Tool/Z.Tool.CompositeListSourceGen/Gen.cs
@@ -1,53 +1,18 @@
 namespace Z.Tool.CompositeListSourceGen;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Avalon.Draw";
-
-
         this.ClassName = "CompositeList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "Composite";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
         this.StatItemClassName = "Composite";
-        
-
-
-
         this.ItemListFileName = this.GetStatItemListFileName();
-
-
-
-
-
         return true;
     }
 }
