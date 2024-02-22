641a0fceea38d4d975f5e80fb0c525667f6572f1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 11:21:46 +0800

    Avalon

--- Avalon/Avalon.Network/AddressKindList.cs
+++ Avalon/Avalon.Network/AddressKindList.cs
@@ -1,74 +1,30 @@
 namespace Avalon.Network;
 
-
-
-
-
 public class AddressKindList : Any
 {
     public static AddressKindList This { get; } = ShareCreate();
 
-
-
-
     private static AddressKindList ShareCreate()
     {
         AddressKindList share;
-
-
         share = new AddressKindList();
-
-
-
         Any a;
-
-
         a = share;
-
-
         a.Init();
-
-
-
         return share;
     }
-
-
-
-
-
 
     public override bool Init()
     {
         base.Init();
-
-
-
         this.InitArray();
-
-
-
         this.Count = this.Array.Count;
-
-
-
         this.Index = 0;
 
-
-
-
         ulong share;
-
         share = Extern.Infra_Share();
-
-
-
         ulong stat;
-
         stat = Extern.Share_Stat(share);
-
-
-
 
         this.IPv6 = this.AddItem(Extern.Stat_NetworkAddressKindIPv6(stat));
         this.IPv4 = this.AddItem(Extern.Stat_NetworkAddressKindIPv4(stat));
@@ -79,15 +35,8 @@ public class AddressKindList : Any
         this.AnyIPv6 = this.AddItem(Extern.Stat_NetworkAddressKindAnyIPv6(stat));
         this.AnyIPv4 = this.AddItem(Extern.Stat_NetworkAddressKindAnyIPv4(stat));
 
-
-
-
-
-
         return true;
     }
-
-
 
 
     public virtual AddressKind IPv6 { get; set; }
@@ -100,58 +49,28 @@ public class AddressKindList : Any
     public virtual AddressKind AnyIPv4 { get; set; }
 
 
-
-
     protected virtual AddressKind AddItem(ulong o)
     {
         AddressKind item;
-
         item = new AddressKind();
-
         item.Init();
-
         item.Index = this.Index;
-
         item.Intern = o;
-
-
-
         this.Array.Set(item.Index, item);
-
-
-
         this.Index = this.Index + 1;
-
-
-
         return item;
     }
-
-
 
 
     protected virtual bool InitArray()
     {
         this.Array = new Array();
-
-
         this.Array.Count = this.ArrayCount;
-
-
         this.Array.Init();
-
-
-
         return true;
     }
 
-
-
-
-
     protected virtual Array Array { get; set; }
-
-
 
     protected virtual int ArrayCount
     { 
@@ -164,21 +83,12 @@ public class AddressKindList : Any
         }
     }
 
-
-
-
     public virtual int Count { get; set; }
-
-
-
 
     public virtual AddressKind Get(int index)
     {
         return (AddressKind)this.Array.Get(index);
     }
-
-
-
     
     protected virtual int Index { get; set; }
 
