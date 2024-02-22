4d3e25087e79962ee9469dbc2188ff3441001bb9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 22:01:02 +0800

    Script

--- Script/Infra/MakeDemoProject.cmd
+++ Script/Infra/MakeDemoProject.cmd
@@ -1,12 +1,7 @@
 @echo off
 
-set InfraProjectOutFold=.\Out\Infra-Windows-Release
 set InfraDemoProjectOutFold=.\Out\InfraDemo-Windows-Release
 
-mkdir %InfraProjectOutFold% 1>NUL 2>NUL
-pushd %InfraProjectOutFold%
-qmake ../../Infra/Infra/Infra.pro
-popd
 mkdir %InfraDemoProjectOutFold% 1>NUL 2>NUL
 pushd %InfraDemoProjectOutFold%
 qmake ../../Infra/InfraDemo/InfraDemo.pro
---
+++ Script/Infra/MakeModuleProject.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set InfraProjectOutFold=.\Out\Infra-Windows-Release
+
+mkdir %InfraProjectOutFold% 1>NUL 2>NUL
+pushd %InfraProjectOutFold%
+qmake ../../Infra/Infra/Infra.pro
+popd
