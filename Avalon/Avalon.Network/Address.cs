2cd74ab1ce93fd75d8b5ab54d098745f0476c43f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 11:11:24 +0800

    Network

--- Avalon/Avalon.Network/Address.cs
+++ Avalon/Avalon.Network/Address.cs
@@ -1,109 +1,45 @@
 namespace Avalon.Network;
 
-
-
-
-
 public class Address : Any
 {
     public override bool Init()
     {
         base.Init();
-
-
-
         this.Intern = Extern.NetworkAddress_New();
-
-
         Extern.NetworkAddress_Init(this.Intern);
-
-
-
         return true;
     }
-
-
-
 
     public virtual bool Final()
     {
         Extern.NetworkAddress_Final(this.Intern);
-
-
         Extern.NetworkAddress_Delete(this.Intern);
-
-
-
         return true;
     }
 
-
-
-
-
     public virtual AddressKind Kind { get; set; }
-
-
-
     public virtual long ValueA { get; set; }
-
-
     public virtual long ValueB { get; set; }
-
-
     public virtual long ValueC { get; set; }
 
-
-
-
     internal virtual ulong Intern { get; set; }
-
-
-
-
 
     public virtual bool Set()
     {
         ulong kindU;
-
-
         kindU = this.Kind.Intern;
-
-
-
         ulong valueAU;
-
         ulong valueBU;
-
         ulong valueCU;
-
-
         valueAU = (ulong)this.ValueA;
-
         valueBU = (ulong)this.ValueB;
-
         valueCU = (ulong)this.ValueC;
 
-
-
         Extern.NetworkAddress_SetKind(this.Intern, kindU);
-
-
         Extern.NetworkAddress_SetValueA(this.Intern, valueAU);
-
-
         Extern.NetworkAddress_SetValueB(this.Intern, valueBU);
-
-
         Extern.NetworkAddress_SetValueC(this.Intern, valueCU);
-
-
-
         Extern.NetworkAddress_Set(this.Intern);
-
-
-
-
         return true;
     }
 }
--- Avalon/Avalon.Network/AddressKind.cs
+++ Avalon/Avalon.Network/AddressKind.cs
@@ -1,13 +1,8 @@
 namespace Avalon.Network;
 
-
-
-
 public class AddressKind : Any
 {
     public virtual int Index { get; set; }
-
-
-
+    
     internal virtual ulong Intern { get; set; }
 }
