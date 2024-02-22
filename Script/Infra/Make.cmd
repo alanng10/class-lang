e8cceca677c44a591f2a29f7c01679a78cc2cc5b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 07:09:07 +0800

    Infra

--- Script/Infra/Make.cmd
+++ Script/Infra/Make.cmd
@@ -1,5 +1,10 @@
 @echo off 
 
 call Script\Infra\CleanModule
-
-
+call Script\Infra\CleanDemo
+call Script\Infra\CleanDeploy
+call Script\Infra\MakeModuleProject
+call Script\Infra\MakeModule
+call Script\Infra\MakeDemoProject
+call Script\Infra\MakeDemo
+call Script\Infra\Deploy
