540fcb8215b72f9d6182497846d296c2372d1357
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 20:36:05 +0800

    Script

--- Script/Infra/UpdateDeploy.cmd
+++ Script/Infra/UpdateDeploy.cmd
@@ -1,12 +1,6 @@
 @echo off
 
-
-
 set InfraOutFold=.\Out\Infra-Windows-Release\release
-
-
 set InfraDeployFold=.\Out\InfraDeploy
-
-
-
+mkdir %InfraDeployFold% 1>NUL 2>NUL
 copy /Y %InfraOutFold%\Infra.dll %InfraDeployFold%
