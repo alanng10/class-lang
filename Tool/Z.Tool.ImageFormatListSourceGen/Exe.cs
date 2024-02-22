2d16e97025f84e50f2d018cfc7bfae7584c81bc7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 07:35:14 +0800

    Tool

--- Tool/Z.Tool.ImageFormatListSourceGen/Exe.cs
+++ Tool/Z.Tool.ImageFormatListSourceGen/Exe.cs
@@ -1,45 +1,24 @@
 namespace Z.Tool.ImageFormatListSourceGen;
 
-
-
-
 class Exe : ExeExe
 {
     public override int Execute()
     {
         Gen gen;
-
         gen = new Gen();
-
         gen.Init();
-
-
-
         int o;
-
         o = gen.Execute();
-
-
         return o;
     }
-
-
-
 
     static int Main()
     {
         Exe exe;
-
         exe = new Exe();
-
         exe.Init();
-
-
         int o;
-
         o = exe.Execute();
-
-
         return o;
     }
 }
