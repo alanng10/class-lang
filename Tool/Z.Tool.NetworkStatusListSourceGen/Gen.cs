23a11decacc6d9f8514ab6ef470bc02626d229d6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 11:55:20 +0800

    Tool

--- Tool/Z.Tool.NetworkStatusListSourceGen/Gen.cs
+++ Tool/Z.Tool.NetworkStatusListSourceGen/Gen.cs
@@ -1,75 +1,26 @@
 namespace Z.Tool.NetworkStatusListSourceGen;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Avalon.Network";
-
-
         this.ClassName = "StatusList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "Status";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
         this.StatItemClassName = "NetworkStatus";
-        
-
-
-
         this.ItemListFileName = this.GetStatItemListFileName();
-
-
-
-
-
         this.AddMethodFileName = "AddMethodNetworkStatus.txt";
-
-
-
         this.InitMethodFileName = "InitMethod.txt";
-
-
-
-
-
         return true;
     }
-
-
-
-
 
     protected override bool AppendInitFieldAddItem(StringBuilder sb, string index, object value)
     {
         sb.Append("AddItem").Append("(").Append(")");
-
-
         return true;
     }
 }
