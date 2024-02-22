0abee9087d222d9711890f615040d233f0ddb9da
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-22 21:58:31 +0800

    Infra

--- Script/Infra/Execute.cmd
+++ Script/Infra/Execute.cmd
@@ -1,6 +1,12 @@
 @echo off
 
 set InfraDemoProjectOutFold=.\Out\InfraDemo-Windows-Release
+set InfraProjectOutFold=.\Out\Infra-Windows-Release
+
 pushd %InfraDemoProjectOutFold%
-release\InfraDemo
+
+setlocal 
+set "PATH=%WorkingFold%\%InfraProjectOutFold%\release;%PATH%" && release\InfraDemo
+endlocal
+
 popd
