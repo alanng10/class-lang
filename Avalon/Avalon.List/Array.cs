ba19c75f5e7c09e2f6e145c1dbfd1a532bd18cb6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 17:46:49 +0800

    List

--- Avalon/Avalon.List/Array.cs
+++ Avalon/Avalon.List/Array.cs
@@ -1,161 +1,74 @@
 namespace Avalon.List;
 
-
-
-
 public class Array : List
 {
-
-
-
     public override bool Init()
     {
         this.InfraInfra = InfraInfra.This;
-
-
-
         this.Comparer = new Comparer();
-
         this.Comparer.Init();
 
-
-
         this.Value = new object[this.Count];
-
-
-
         return true;
     }
 
-
-
-
     protected virtual InfraInfra InfraInfra { get; set; }
 
-
-
     private object[] Value { get; set; }
-
-
     private Comparer Comparer { get; set; }
-
-
-
-
 
     public override object Add(object item)
     {
         return null;
     }
 
-
-
-
-
     public override object Insert(object index, object item)
     {
         return null;
     }
-
-
-
-
 
     public override bool Remove(object index)
     {
         return false;
     }
 
-
-
-
-
-
-
     public override bool Clear()
     {
         return false;
     }
 
-
-
-
-
-
-
     public override bool Contain(object index)
     {
         int u;
-
-
         u = this.IntIndex(index);
-
-
-
         if (u == -1)
         {
             return false;
         }
 
-
-
-
         int intIndex;
-
-
         intIndex = u;
-
-
-
         return this.Contain(intIndex);
     }
-
-
-
-
-
 
     public virtual bool Contain(int index)
     {
         return !(index < 0) & (index < this.Count);
     }
 
-
-
-
-
-
     public override object Get(object index)
     {
         int u;
-
-
         u = this.IntIndex(index);
-
-
-
         if (u == -1)
         {
             return null;
         }
 
-
-
-
         int intIndex;
-
-
         intIndex = u;
-
-
-
         return this.Get(intIndex);
     }
-
-
-
-
-
 
     public virtual object Get(int index)
     {
@@ -163,48 +76,22 @@ public class Array : List
         {
             return null;
         }
-
-
-
         return this.Value[index];
     }
-
-
-
-
-
 
     public override bool Set(object index, object value)
     {
         int u;
-
-
         u = this.IntIndex(index);
-
-
-
         if (u == -1)
         {
             return false;
         }
 
-
-
-
         int intIndex;
-
-
         intIndex = u;
-
-
-
         return this.Set(intIndex, value);
     }
-
-
-
-
-
 
     public virtual bool Set(int index, object value)
     {
@@ -212,102 +99,42 @@ public class Array : List
         {
             return false;
         }
-
-
-
         this.Value[index] = value;
-
-
-
-
         return true;
     }
-
-
-
-
-
 
     public virtual bool Sort(Range range, Compare compare)
     {
         int index;
-
         index = range.Start;
-
-
         int count;
-
         count = this.InfraInfra.Count(range);
-
-
 
         this.Comparer.CompareAny = compare;
 
-
-
         SystemArray.Sort<object>(this.Value, index, count, this.Comparer);
         
-
-
         this.Comparer.CompareAny = null;
-
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     public override Iter CreateIter()
     {
         Iter a;
-
-
         a = new ArrayIter();
-
-
         a.Init();
-
-
-
         return a;
     }
-
-
-
-
 
     public override bool SetIter(Iter iter)
     {
         ArrayIter a;
-
         a = (ArrayIter)iter;
-
-
-
         a.Array = this;
-
-
         a.IntIndex = 0;
-
-
         a.CurrentIndex = -1;
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     private int IntIndex(object index)
     {
@@ -316,18 +143,10 @@ public class Array : List
             return -1;
         }
 
-
-
         int t;
-
         t = (int)index;
-
-
-
         int ret;
-
         ret = t;
-
         return ret;
     }
 }
