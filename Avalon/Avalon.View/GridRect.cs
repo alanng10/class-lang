5281b811a82a0cb976bb0b5a5010cb5a13c969d9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-24 06:50:02 +0800

    View

--- Avalon/Avalon.View/GridRect.cs
+++ Avalon/Avalon.View/GridRect.cs
@@ -1,108 +1,44 @@
 namespace Avalon.View;
 
-
-
-
 public class GridRect : Comp
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.PosField = this.CreatePosField();
-
-
-
-
-
         this.SizeField = this.CreateSizeField();
-
-
-
-
-
-
         this.Pos = this.CreatePos();
-
-
-
-
-
         this.Size = this.CreateSize();
-
-
-
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     protected virtual Field CreatePosField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
 
-
-
-
-
     protected virtual Field CreateSizeField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
 
-
-
-
-
-
-
     protected virtual GridPos CreatePos()
     {
         GridPos a;
-
         a = new GridPos();
-
         a.Init();
-
-
         return a;
     }
-
-
-
 
     protected virtual GridSize CreateSize()
     {
         GridSize a;
-
         a = new GridSize();
-
         a.Init();
-
-
         return a;
     }
 
-
-
-
-
-
     public virtual Field PosField { get; set; }
-
-
 
     public virtual GridPos Pos
     {
@@ -117,26 +53,13 @@ public class GridRect : Comp
         }
     }
 
-
-
     protected virtual bool ChangePos(Change change)
     {
         this.Trigger(this.PosField);
-
-
-
         return true;
     }
 
-
-
-
-
-
-
     public virtual Field SizeField { get; set; }
-
-
 
     public virtual GridSize Size
     {
@@ -151,22 +74,11 @@ public class GridRect : Comp
         }
     }
 
-
-
     protected virtual bool ChangeSize(Change change)
     {
         this.Trigger(this.SizeField);
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     public override bool Change(Field field, Change change)
     {
@@ -174,17 +86,10 @@ public class GridRect : Comp
         {
             this.ChangePos(change);
         }
-
-
-
         if (this.SizeField == field)
         {
             this.ChangeSize(change);
         }
-        
-
-
-
         return true;
     }
 }
