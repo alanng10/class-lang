e0c464abe56aace40625eae9fe618342c95b075b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 17:27:35 +0800

    Draw

--- Avalon/Avalon.Draw/PenKindList.cs
+++ Avalon/Avalon.Draw/PenKindList.cs
@@ -1,74 +1,30 @@
 namespace Avalon.Draw;
 
-
-
-
-
 public class PenKindList : Any
 {
     public static PenKindList This { get; } = ShareCreate();
 
-
-
-
     private static PenKindList ShareCreate()
     {
         PenKindList share;
-
-
         share = new PenKindList();
-
-
-
         Any a;
-
-
         a = share;
-
-
         a.Init();
-
-
-
         return share;
     }
-
-
-
-
-
 
     public override bool Init()
     {
         base.Init();
-
-
-
         this.InitArray();
-
-
-
         this.Count = this.Array.Count;
-
-
-
         this.Index = 0;
 
-
-
-
         ulong share;
-
         share = Extern.Infra_Share();
-
-
-
         ulong stat;
-
         stat = Extern.Share_Stat(share);
-
-
-
 
         this.Solid = this.AddItem(Extern.Stat_PenKindSolid(stat));
         this.Dash = this.AddItem(Extern.Stat_PenKindDash(stat));
@@ -77,16 +33,8 @@ public class PenKindList : Any
         this.DashDotDot = this.AddItem(Extern.Stat_PenKindDashDotDot(stat));
         this.CustomDash = this.AddItem(Extern.Stat_PenKindCustomDash(stat));
 
-
-
-
-
-
         return true;
     }
-
-
-
 
     public virtual PenKind Solid { get; set; }
     public virtual PenKind Dash { get; set; }
@@ -95,59 +43,27 @@ public class PenKindList : Any
     public virtual PenKind DashDotDot { get; set; }
     public virtual PenKind CustomDash { get; set; }
 
-
-
-
     protected virtual PenKind AddItem(ulong o)
     {
         PenKind item;
-
         item = new PenKind();
-
         item.Init();
-
         item.Index = this.Index;
-
         item.Intern = o;
-
-
-
         this.Array.Set(item.Index, item);
-
-
-
         this.Index = this.Index + 1;
-
-
-
         return item;
     }
-
-
-
 
     protected virtual bool InitArray()
     {
         this.Array = new Array();
-
-
         this.Array.Count = this.ArrayCount;
-
-
         this.Array.Init();
-
-
-
         return true;
     }
 
-
-
-
-
     protected virtual Array Array { get; set; }
-
-
 
     protected virtual int ArrayCount
     { 
@@ -160,22 +76,12 @@ public class PenKindList : Any
         }
     }
 
-
-
-
     public virtual int Count { get; set; }
-
-
-
 
     public virtual PenKind Get(int index)
     {
         return (PenKind)this.Array.Get(index);
     }
-
-
-
     
     protected virtual int Index { get; set; }
-
 }
