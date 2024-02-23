9b1dda2c9273723e7c09ed58842ac605b66ad50b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-24 06:44:58 +0800

    View

--- Avalon/Avalon.View/GridChild.cs
+++ Avalon/Avalon.View/GridChild.cs
@@ -1,99 +1,35 @@
 namespace Avalon.View;
 
-
-
-
-
-
-
 public class GridChild : Comp
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
-
         this.ViewField = this.CreateViewField();
-
-
-
-
-
         this.RectField = this.CreateRectField();
-
-
-
-
-
-
-
         this.Rect = this.CreateRect();
-
-
-
-
-
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     protected virtual Field CreateViewField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
 
-
-
-
-
     protected virtual Field CreateRectField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
 
-
-
-
-
-
-
     protected virtual GridRect CreateRect()
     {
         GridRect a;
-
-
         a = new GridRect();
-
-
         a.Init();
-
-
-
         return a;
     }
 
-
-
-
-
-
-
-
     public virtual Field ViewField { get; set; }
-
-
 
     public virtual View View
     {
@@ -107,25 +43,13 @@ public class GridChild : Comp
         }
     }
 
-
-
     protected virtual bool ChangeView(Change change)
     {
         this.Trigger(this.ViewField);
-
-
-
         return true;
     }
 
-
-
-
-
-
     public virtual Field RectField { get; set; }
-
-
 
     public virtual GridRect Rect
     {
@@ -139,21 +63,11 @@ public class GridChild : Comp
         }
     }
 
-
-
-
     protected virtual bool ChangeRect(Change change)
     {
         this.Trigger(this.RectField);
-
-
-
         return true;
     }
-
-
-
-
 
     public override bool Change(Field field, Change change)
     {
@@ -161,17 +75,10 @@ public class GridChild : Comp
         {
             this.ChangeView(change);
         }
-
-
-
         if (this.RectField == field)
         {
             this.ChangeRect(change);
         }
-        
-
-
-
         return true;
     }
 }
