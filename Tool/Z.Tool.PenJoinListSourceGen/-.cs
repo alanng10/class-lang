2f49646eaea110547a5224b72423c1d1a18555dd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 17:23:31 +0800

    Tool

--- Tool/Z.Tool.PenJoinListSourceGen/-.cs
+++ Tool/Z.Tool.PenJoinListSourceGen/-.cs
@@ -1,3 +1,2 @@
-global using Any = Avalon.Infra.Any;
 global using ExeExe = Avalon.Exe.Exe;
 global using SourceGen = Z.Infra.StatItemListSourceGen.Gen;
--- Tool/Z.Tool.PenJoinListSourceGen/Gen.cs
+++ Tool/Z.Tool.PenJoinListSourceGen/Gen.cs
@@ -1,52 +1,18 @@
 namespace Z.Tool.PenJoinListSourceGen;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Avalon.Draw";
-
-
         this.ClassName = "PenJoinList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "PenJoin";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
         this.StatItemClassName = "PenJoin";
-        
-
-
-
         this.ItemListFileName = this.GetStatItemListFileName();
-
-
-
-
         return true;
     }
 }
