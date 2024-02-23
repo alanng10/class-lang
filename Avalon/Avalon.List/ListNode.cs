28735c9c1bf2e810dd473aacf43aeb22746fc4b9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 18:03:37 +0800

    List

--- Avalon/Avalon.List/ListNode.cs
+++ Avalon/Avalon.List/ListNode.cs
@@ -1,31 +1,9 @@
 namespace Avalon.List;
 
-
-
-
-
 public class ListNode : Any
 {
     public ListNode Previous { get; set; }
-
-
-
-
-
-
     public ListNode Next { get; set; }
-
-
-
-
-
-
     public ListNodeRef Ref { get; set; }
-
-
-
-
-
-
     public object Value { get; set; }
 }
