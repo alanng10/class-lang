ae93c77cd3308a2860cc19376c1d42fe01915ed0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 17:49:25 +0800

    List

--- Avalon/Avalon.List/Comparer.cs
+++ Avalon/Avalon.List/Comparer.cs
@@ -1,13 +1,8 @@
 namespace Avalon.List;
 
-
-
-
 class Comparer : Any, IComparerObject
 {
     public virtual Compare CompareAny { get; set; }
-
-
 
     public int Compare(object x, object y)
     {
