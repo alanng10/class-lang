45e72cbb0133962b5204a0d9fd41c6240e0795bf
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 17:26:41 +0800

    Tool

--- Tool/Z.Tool.PenKindListSourceGen/Gen.cs
+++ Tool/Z.Tool.PenKindListSourceGen/Gen.cs
@@ -1,52 +1,18 @@
 namespace Z.Tool.PenKindListSourceGen;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Avalon.Draw";
-
-
         this.ClassName = "PenKindList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "PenKind";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
         this.StatItemClassName = "PenKind";
-        
-
-
-
         this.ItemListFileName = this.GetStatItemListFileName();
-
-
-
-
         return true;
     }
 }
