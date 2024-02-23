91381647f4b2e9c8a19717607406ff763e164e4d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-23 17:51:45 +0800

    List

--- Avalon/Avalon.List/Infra.cs
+++ Avalon/Avalon.List/Infra.cs
@@ -1,95 +1,42 @@
 namespace Avalon.List;
 
-
-
-
-
 public class Infra : Any
 {
     public static Infra This { get; } = ShareCreate();
 
-
-
-
     private static Infra ShareCreate()
     {
         Infra share;
-
         share = new Infra();
-
-
-
         Any a;
-
         a = share;
-
         a.Init();
-        
-
-
         return share;
     }
-
-
-
-
-
 
     public virtual Array CreateArray(List list)
     {
         Array a;
-
         a = new Array();
-
         a.Count = list.Count;
-
         a.Init();
         
-
-
-
         Iter iter;
-
         iter = list.CreateIter();
-
-
-
         list.SetIter(iter);
 
-
-
-
         int count;
-
         count = a.Count;
-
-
         int i;
-
         i = 0;
-
-
         while (i < count)
         {
             iter.Next();
-
-
             object aa;
-
             aa = iter.Value;
-
-
-
             a.Set(i, aa);
-
-
-
             i = i + 1;
         }
-
-
-
-
         return a;
     }
 }
