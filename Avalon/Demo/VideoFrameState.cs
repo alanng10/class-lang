4c5590dc9476738a6db7323d86f6a556ce5bc174
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-24 10:26:59 +0800

    Demo

--- Avalon/Demo/VideoFrameState.cs
+++ Avalon/Demo/VideoFrameState.cs
@@ -1,64 +1,30 @@
 namespace Demo;
 
-
-
-
-
-
 class VideoFrameState : State
 {
     public override bool Init()
     {
         base.Init();
-
-
-
         this.DrawInfra = DrawInfra.This;
-
-
         this.ViewInfra = ViewInfra.This;
-
-
-
         return true;
     }
 
-
-
     public Demo Demo { get; set; }
 
-
-
     protected DrawInfra DrawInfra { get; set; }
-
-
     protected ViewInfra ViewInfra { get; set; }
-
-
-
 
     public override bool Execute()
     {
-        VideoFrame frame;
-        
+        VideoFrame frame;        
         frame = this.Demo.Play.VideoOut.Frame;
-
-
 
         this.DrawInfra.VideoFrameGetImage(frame, this.Demo.PlayImage);
 
-
-
-
-
         this.ViewInfra.AssignDrawRectValue(this.Demo.UpdateRect, this.Demo.ViewA.Area);
 
-
-
         this.Demo.Frame.Update(this.Demo.UpdateRect);
-
-
-
         return true;
     }
 }
