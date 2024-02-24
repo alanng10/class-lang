e6bf8200fe89a8826f94df8529d896671705e6b0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-24 10:33:34 +0800

    Demo

--- Avalon/Demo/ThreadPostState.cs
+++ Avalon/Demo/ThreadPostState.cs
@@ -1,80 +1,35 @@
 namespace Demo;
 
-
-
-
-
-
 class ThreadPostState : ThreadExecuteState
 {
     public State PostState { get; set; }
-
-
-
     public ThreadSemaphore Semaphore { get; set; }
-
-
-
     public ThreadPost Post { get; set; }
-
-
-
 
     public override bool Execute()
     {
         ThreadCurrent current;
-
         current = new ThreadCurrent();
-
         current.Init();
 
-
-
-
         ThreadThread thread;
-
         thread = current.Thread;
 
-
-
-
         ThreadPost post;
-
         post = new ThreadPost();
-
         post.Init();
-
-
         post.State = this.PostState;
-
-
-
 
         this.Post = post;
 
-
-
         this.Semaphore.Release();
 
-
-
-
         int o;
-
         o = thread.ExecuteEventLoop();
-
-
-
 
         post.Final();
 
-
-
-
         this.Result = o;
-
-
-
         return true;
     }
 }
