29b3d79fea3849c960080da22240f2dae9da6dec
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-24 10:36:24 +0800

    Demo

--- Avalon/Demo/ElapseState.cs
+++ Avalon/Demo/ElapseState.cs
@@ -1,54 +1,23 @@
 namespace Demo;
 
-
-
-
 class ElapseState : State
 {
     public TimeInterval Interval { get; set; }
-
-
-
     public ThreadThread Thread { get; set; }
-    
-
-
     public int ElapseCount { get; set; }
-
-
-
     public int ExitCode { get; set; }
-
-
 
     private int Count { get; set; }
     
-
-
-
     public override bool Execute()
     {
         Console console;
-
         console = Console.This;
-
-
-        
         console.Write("ElapseState.Execute START\n");
-
-
-
 
         this.Count = this.Count + 1;
 
-
-
-
         console.Write("Elapse Count: " + this.Count + "\n");
-
-
-
-  
          
         if (!(this.Count < this.ElapseCount))
         {
@@ -56,21 +25,13 @@ class ElapseState : State
             {
                 this.Interval.Stop();
 
-
                 console.Write("ElapseState.Execute Interval Stop\n");
             }
-
-
 
             this.Thread.ExitEventLoop(this.ExitCode);
         }
 
-
-
-
         console.Write("ElapseState.Execute END\n");
-
-
 
         return true;
     }
