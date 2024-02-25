4d1d71d5eb2d6814fe6614b19d7276764a7baa82
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:31:02 +0800

    Node

--- Class/Class.Node/PartItemRangeState.cs
+++ Class/Class.Node/PartItemRangeState.cs
@@ -1,27 +1,12 @@
 namespace Class.Node;
 
-
-
-
-
-
 public class PartItemRangeState : RangeState
 {
     public override bool Execute()
     {
         Range a;
-
-
-
         a = this.Create.ExecuteCompRange(this.Arg.Result, this.Arg.Range);
-
-
-
-
         this.Result = a;
-
-
-
         return true;
     }
 }
