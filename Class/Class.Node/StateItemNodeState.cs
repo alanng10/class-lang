bece65ed9da62819b73dab7cbec3aa6768cc84a5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:29:12 +0800

    Node

--- Class/Class.Node/StateItemNodeState.cs
+++ Class/Class.Node/StateItemNodeState.cs
@@ -1,27 +1,12 @@
 namespace Class.Node;
 
-
-
-
-
-
 public class StateItemNodeState : NodeState
 {
     public override bool Execute()
     {
         Node a;
-
-
-
         a = this.Create.ExecuteExecute(this.Arg);
-
-
-
-
         this.Result = a;
-
-
-
         return true;
     }
 }
--- Class/Class.Node/StateItemRangeState.cs
+++ Class/Class.Node/StateItemRangeState.cs
@@ -1,27 +1,12 @@
 namespace Class.Node;
 
-
-
-
-
-
 public class StateItemRangeState : RangeState
 {
     public override bool Execute()
     {
         Range a;
-
-
-
         a = this.Create.ExecuteExecuteRange(this.Arg.Result, this.Arg.Range);
-
-
-
-
         this.Result = a;
-
-
-
         return true;
     }
 }
