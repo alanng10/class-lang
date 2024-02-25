ff6f8d9d17f18eee70b48ee80611f7f040c49aa4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:31:55 +0800

    Node

--- Class/Class.Node/ParamItemNodeState.cs
+++ Class/Class.Node/ParamItemNodeState.cs
@@ -1,27 +1,12 @@
 namespace Class.Node;
 
-
-
-
-
-
 public class ParamItemNodeState : NodeState
 {
     public override bool Execute()
     {
         Node a;
-
-
-
         a = this.Create.ExecuteVar(this.Arg);
-
-
-
-
         this.Result = a;
-
-
-
         return true;
     }
 }
--- Class/Class.Node/ParamItemRangeState.cs
+++ Class/Class.Node/ParamItemRangeState.cs
@@ -1,27 +1,12 @@
 namespace Class.Node;
 
-
-
-
-
-
 public class ParamItemRangeState : RangeState
 {
     public override bool Execute()
     {
         Range a;
-
-
-
         a = this.Create.ExecuteParamItemRange(this.Arg.Result, this.Arg.Range);
-
-
-
-
         this.Result = a;
-
-
-
         return true;
     }
 }
