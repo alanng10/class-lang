38f36348e3e7761fe721fd7c0825693ebcc5148a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:33:36 +0800

    Node

--- Class/Class.Node/CreateOperateState.cs
+++ Class/Class.Node/CreateOperateState.cs
@@ -1,14 +1,7 @@
 namespace Class.Node;
 
-
-
-
-
 public class CreateOperateState : InfraState
 {
     public new virtual CreateOperateArg Arg { get; set; }
-
-
-
     public virtual Node Node { get; set; }
 }
