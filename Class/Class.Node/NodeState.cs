996ea679c675d13bf19cd7faf74ae000fa5c9cd6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:26:30 +0800

    Node

--- Class/Class.Node/NodeState.cs
+++ Class/Class.Node/NodeState.cs
@@ -1,19 +1,8 @@
 namespace Class.Node;
 
-
-
-
-
 public class NodeState : InfraState
 {
     public virtual Create Create { get; set; }
-
-    
-
-
     public new virtual Node Result { get; set; }
-
-
-
     public new virtual Range Arg { get; set; }
 }
