1f064d58b06772ba0be2c1958bb4b4e606f22073
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:36:54 +0800

    Tool

--- Tool/Z.Tool.ErrorKindListSourceGen/Exe.cs
+++ Tool/Z.Tool.ErrorKindListSourceGen/Exe.cs
@@ -1,25 +1,14 @@
 namespace Z.Tool.ErrorKindListSourceGen;
 
-
-
-
 class Exe : Any
 {
     static int Main()
     {
         Gen gen;
-
         gen = new Gen();
-
         gen.Init();
-
-
-
         int o;
-
         o = gen.Execute();
-
-
         return o;
     }
 }
--- Tool/Z.Tool.ErrorKindListSourceGen/Gen.cs
+++ Tool/Z.Tool.ErrorKindListSourceGen/Gen.cs
@@ -1,75 +1,26 @@
 namespace Z.Tool.ErrorKindListSourceGen;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override int Execute()
     {
         this.ExecuteOne("Node");
-
-
-
         this.ExecuteOne("Check");
-
-
-
-
         return 0;
     }
-
-
-
-
-
 
     protected virtual bool ExecuteOne(string name)
     {
         this.Namespace = "Class." + name;
-
-
         this.ClassName = "ErrorKindList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "ErrorKind";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
-
         this.ItemListFileName = "ItemListErrorKind" + name + ".txt";
-
-
-
         this.AddMethodFileName = "AddMethodErrorKind.txt";
-
-
-
         this.OutputFilePath = "../../Class/Class." + name + "/ErrorKindList.cs";
-
-
-
-
-
         base.Execute();
-
-
-
-
         return true;
     }
 
