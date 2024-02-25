6884d4e2e2009aee1fe3bdb431a1f6465734330b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 18:57:13 +0800

    Node

--- Class/Class.Node/Create.cs
+++ Class/Class.Node/Create.cs
@@ -7912,43 +7912,20 @@ public class Create : InfraCreate
     public virtual Range ExecuteCompRange(Range result, Range range)
     {
         int start;
-
-
         int end;
-
-
         start = range.Start;
-
-
         end = range.End;
 
-
-
-
-
         Range a;
-
-
         a = null;
-
-
-
-
         if (a == null)
         {
             a = this.ExecuteFieldRange(result, this.Range(this.RangeA, start, end));
         }
-
-
-
         if (a == null)
         {
             a = this.ExecuteMaideRange(result, this.Range(this.RangeA, start, end));
         }
-
-
-
-
         return a;
     }
 
