993121a00c6ead84d8665e1eac2519f777f192ca
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:33:00 +0800

    Node

--- Class/Class.Node/ArgueItemRangeState.cs
+++ Class/Class.Node/ArgueItemRangeState.cs
@@ -1,27 +1,12 @@
 namespace Class.Node;
 
-
-
-
-
-
 public class ArgueItemRangeState : RangeState
 {
     public override bool Execute()
     {
         Range a;
-
-
-
         a = this.Create.ExecuteArgueItemRange(this.Arg.Result, this.Arg.Range);
-
-
-
-
         this.Result = a;
-
-
-
         return true;
     }
 }
