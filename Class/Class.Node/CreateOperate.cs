7e7801b2f664bfc8b909c6a4734a6a4fefeb7aa5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-25 16:14:00 +0800

    Node

--- Class/Class.Node/CreateOperate.cs
+++ Class/Class.Node/CreateOperate.cs
@@ -1,55 +1,34 @@
 namespace Class.Node;
 
-
-
-
-
 public class CreateOperate : Any
 {
     public virtual Create Create { get; set; }
-
-
 
     public virtual Node Execute()
     {
         return null;
     }
 
-
-
-
-
     public virtual int ExecuteListNew()
     {
         return 0;
     }
-
-
-
 
     public virtual Array ExecuteListGet(int index)
     {
         return null;
     }
 
-
-
-
     public virtual bool ExecuteListSetItem(int index, int itemIndex, object item)
     {
         return true;
     }
 
-
-
     public virtual bool ExecuteListCount(int index, int count)
     {
         return true;
     }
-
-
-
-
+    
     public virtual bool ExecuteError(ErrorKind kind, int start, int end)
     {
         return true;
