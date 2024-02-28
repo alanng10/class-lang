3a50062bee3b905645dffec468d67d7e9d19ba2f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-28 15:17:08 +0800

    Node

--- Class/Class.Node/Infra.cs
+++ Class/Class.Node/Infra.cs
@@ -8,31 +8,24 @@ public class Infra : Any
     {
         Infra share;
         share = new Infra();
-
         Any a;
         a = share;
         a.Init();
-
         return share;
     }
-
 
     public override bool Init()
     {
         base.Init();
-
         this.InfraInfra = InfraInfra.This;        
 
         this.IntSignValueNegativeMax = this.InfraInfra.IntCapValue / 2;
-
         this.IntSignValuePositiveMax = this.IntSignValueNegativeMax - 1;
-
         return true;
     }
 
     protected virtual InfraInfra InfraInfra { get; set; }
 
     public virtual long IntSignValuePositiveMax { get; set; }
-
     public virtual long IntSignValueNegativeMax { get; set; }
 }
