ab2d57c8a3fa0ef25ce9b8b04226d6403e3c2f35
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-28 11:06:44 +0800

    Text

--- Avalon/Avalon.Text/Infra.cs
+++ Avalon/Avalon.Text/Infra.cs
@@ -1,300 +1,127 @@
 namespace Avalon.Text;
 
-
-
-
 public class Infra : Any
 {
     public static Infra This { get; } = ShareCreate();
 
-
-
-
     private static Infra ShareCreate()
     {
         Infra share;
-
-
         share = new Infra();
-
-
-
         Any a;
-
-
         a = share;
-
-
         a.Init();
-        
-
-
         return share;
     }
-
-
-
-
-
-
 
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.InfraInfra = InfraInfra.This;
-
-
-
-
         this.IntHexFormat = "x15";
-
-
-
-
-
         this.IntHexDigitCount = 15;
-
-
-
-
-
-
         return true;
     }
 
-
-
-
-
     protected virtual InfraInfra InfraInfra { get; set; }
-
-
-
-
 
     private string IntHexFormat { get; set; }
 
-
-
-
-
     public virtual int IntHexDigitCount { get; set; }
-
-
-
-
-
-
 
     public virtual bool IsDigit(char o)
     {
         return !((o < '0') | ('9' < o));
     }
 
-
-
-
-
     public virtual bool IsHexLetter(char o)
     {
         return !((o < 'a') | ('f' < o));
     }
-
-
-
 
     public virtual bool IsLowerLetter(char o)
     {
         return !((o < 'a') | ('z' < o));
     }
 
-
-
     public virtual bool IsUpperLetter(char o)
     {
         return !((o < 'A') | ('Z' < o));
     }
 
-
-
-
-
-
     public virtual char GetChar(Text text, Pos pos)
     {
-        char oc;
-        
+        char oc;        
         oc = text.GetLine(pos.Row).Value[pos.Col];
-
-
         return oc;
     }
-
-
-
-
-
 
     public virtual bool Equal(Text text, Range range, string o)
     {
         Line line;
-
-
         line = text.GetLine(range.Row);
 
-
-
-
         int k;
-
-
         k = this.InfraInfra.Count(range.Col);
-
-
-
-
         int count;
-
         count = k;
-
-
-
         if (!(count == o.Length))
         {
             return false;
         }
 
-
-
         int index;
-
-
-
         char oca;
-
-
         char ocb;
-
-
-
         int i;
-
-
         i = 0;
-
-
-
         while (i < count)
         {
             index = range.Col.Start + i;
 
-
-
             oca = line.Value[index];
-
-
             ocb = o[i];
-
-
-
             if (!(oca == ocb))
             {
                 return false;
             }
-
-
-
             i = i + 1;
         }
-
-
-
-
-
         return true;
     }
-
-    
-
-
-
 
     public virtual Span SpanCreate(int count)
     {
         Span span;
-
         span = new Span();
-
         span.Init();
-
-
         span.Data = new char[count];
-
-
         span.Range = new InfraRange();
-
         span.Range.Init();
-
         span.Range.End = count;
-
-
         return span;
     }
-
-
-
-
 
     public virtual Span SpanCreateString(string a)
     {
         Span span;
-
         span = new Span();
-
         span.Init();
-
-
         span.Data = a.ToCharArray();
-
-
         span.Range = new InfraRange();
-
         span.Range.Init();
-
         span.Range.End = span.Data.Length;
-
-
         return span;
     }
-
-
-
-
 
     public virtual string StringCreate(Span o)
     {
         InfraRange range;
-
         range = o.Range;
-
-
-
         int count;
-
         count = this.InfraInfra.Count(range);
-
-
-
         string a;
-
         a = new string(o.Data, range.Start, count);
-
-
         return a;
     }
 
