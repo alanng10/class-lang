f74ef95a5bfa9ace3c557ca1ce9f2e1c53ddb07d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-02-29 17:54:58 +0800

    Infra

--- Infra/Infra/Entry.c
+++ Infra/Infra/Entry.c
@@ -1,89 +1,43 @@
 #include "Entry.h"
 
-
-
-
 InfraClassNew(Entry)
-
-
-
-
 
 Int Entry_Init(Int o)
 {
     return true;
 }
 
-
-
-
 Int Entry_Final(Int o)
 {
     return true;
 }
 
-
-
-
-
 Int Entry_GetIndex(Int o)
 {
     Entry* m;
-
     m = CastPointer(o);
-
-
     return m->Index;
 }
-
-
-
-
 
 Int Entry_SetIndex(Int o, Int value)
 {
     Entry* m;
-
     m = CastPointer(o);
-
-
-
     m->Index = value;
-
-
-
     return true;
 }
-
-
-
-
 
 Int Entry_GetValue(Int o)
 {
     Entry* m;
-
     m = CastPointer(o);
-
-
     return m->Value;
 }
-
-
-
-
 
 Int Entry_SetValue(Int o, Int value)
 {
     Entry* m;
-
     m = CastPointer(o);
-
-
-
     m->Value = value;
-
-
-
     return true;
 }
