ce08ced130eb4be5bffc65cd57ef9ff7d0f9c139
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 15:11:30 +0800

    Infra

--- Infra/Infra/Data.c
+++ Infra/Infra/Data.c
@@ -1,89 +1,43 @@
 #include "Data.h"
 
-
-
-
 InfraClassNew(Data)
 
-
-
-
-
-Bool Data_Init(Int o)
+Int Data_Init(Int o)
 {
     return true;
 }
 
-
-
-
-Bool Data_Final(Int o)
+Int Data_Final(Int o)
 {
     return true;
 }
 
-
-
-
-
-Int Data_GetCount(Int o)
+Int Data_CountGet(Int o)
 {
     Data* m;
-
     m = CastPointer(o);
-
-
     return m->Count;
 }
 
-
-
-
-
-Bool Data_SetCount(Int o, Int value)
+Int Data_CountSet(Int o, Int value)
 {
     Data* m;
-
     m = CastPointer(o);
-
-
-
     m->Count = value;
-
-
-
     return true;
 }
 
-
-
-
-
-Int Data_GetValue(Int o)
+Int Data_ValueGet(Int o)
 {
     Data* m;
-
     m = CastPointer(o);
-
-
     return m->Value;
 }
 
-
-
-
-
-Bool Data_SetValue(Int o, Int value)
+Int Data_ValueSet(Int o, Int value)
 {
     Data* m;
-
     m = CastPointer(o);
-
-
-
     m->Value = value;
-
-
-
     return true;
 }
