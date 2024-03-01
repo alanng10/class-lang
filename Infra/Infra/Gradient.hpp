038177b2742d500dcb2ce366bf5f8735562b0733
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 22:31:17 +0800

    Infra

--- Infra/Infra/Gradient.hpp
+++ Infra/Infra/Gradient.hpp
@@ -1,51 +1,20 @@
 #pragma once
 
-
-
-
 #include <QGradient>
 
-
-
-
 #include "Probate.hpp"
-
-
-
-
 
 struct Gradient
 {
     Int Kind;
-
-
     Int Value;
-
-
     Int Stop;
-
-
     Int Spread;
-
-
-
-
     QGradient* Intern;
 };
 
-
-
-
 #define CP(a) ((Gradient*)(a))
 
+Int Gradient_SetInternStop(Int result, Int stop);
 
-
-
-
-
-Bool Gradient_SetInternStop(Int result, Int stop);
-
-
-
-Bool Gradient_SetInternStopPoint(Int result, Int pos, Int color);
-
+Int Gradient_SetInternStopPoint(Int result, Int pos, Int color);
