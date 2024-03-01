145d0a477e19ee7169a401a41371d0b3bb6c591b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 15:17:58 +0800

    Infra

--- Infra/Infra/Array.c
+++ Infra/Infra/Array.c
@@ -1,165 +1,67 @@
 #include "Array.h"
 
-
-
-
 InfraClassNew(Array)
 
-
-
-
-Bool Array_Init(Int o)
+Int Array_Init(Int o)
 {
     Array* m;
-
     m = CastPointer(o);
-
-
 
     Int count;
-
     count = m->Count;
-
-
-
-
     Int byteCount;
-
     byteCount = count * Constant_IntByteCount();
 
-
-
     Int u;
-
     u = New(byteCount);
-
-
     m->Item = u;
-
-
-
-
     return true;
 }
 
-
-
-
-
-
-Bool Array_Final(Int o)
+Int Array_Final(Int o)
 {
     Array* m;
-
     m = CastPointer(o);
-
-
-
     Delete(m->Item);
-
-
-
     return true;
 }
 
-
-
-
-
-
-
-Int Array_GetCount(Int o)
+Int Array_CountGet(Int o)
 {
     Array* m;
-
     m = CastPointer(o);
-
-
     return m->Count;
 }
 
-
-
-
-
-Bool Array_SetCount(Int o, Int value)
+Int Array_CountSet(Int o, Int value)
 {
     Array* m;
-
     m = CastPointer(o);
-
-
-
     m->Count = value;
-
-
-
     return true;
 }
 
-
-
-
-
-Int Array_GetItem(Int o, Int index)
+Int Array_ItemGet(Int o, Int index)
 {
     Array* m;
-
     m = CastPointer(o);
-
-
-
     Int u;
-
     u = m->Item;
-
-
-
     Int* p;
-
     p = CastPointer(u);
-
-
-
     Int a;
-
     a = p[index];
-
-
-
     return a;
 }
 
-
-
-
-
-Bool Array_SetItem(Int o, Int index, Int value)
+Int Array_ItemSet(Int o, Int index, Int value)
 {
     Array* m;
-
     m = CastPointer(o);
-
-
-
     Int u;
-
     u = m->Item;
-
-
-
     Int* p;
-
     p = CastPointer(u);
-
-
-
     p[index] = value;
-
-
-
     return true;
 }
-
-
-
