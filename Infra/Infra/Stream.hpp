c8cf959160c76ab3e9c414048623f313d4a2e1a3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 16:41:27 +0800

    Infra

--- Infra/Infra/Stream.hpp
+++ Infra/Infra/Stream.hpp
@@ -1,65 +1,25 @@
 #pragma once
 
-
-
-
 #include <QIODevice>
 
-
-
-
 #include "Probate.hpp"
-
-
-
 
 struct Stream
 {
     Int Kind;
-
-
     Int Value;
-
-
     Int Status;
-
-
-
-
     Int HasPos;
-
-
     Int HasCount;
-
-
-
     Int CanRead;
-
-
     Int CanWrite;
-
-
-
     QIODevice* Intern;
 };
 
-
-
 #define CP(a) ((Stream*)(a))
-
-
 
 typedef Bool (*Stream_Flush_Maide)(Int device);
 
-
-
-
 Bool Stream_CheckRange(Int dataCount, Int start, Int end);
 
-
-
-
-
-
 Int Stream_InternFlush(Int o);
-
