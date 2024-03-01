6dad005d3a67bf21268abe7ad3f7db104dc6b268
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 15:29:40 +0800

    Infra

--- Infra/Infra/State.c
+++ Infra/Infra/State.c
@@ -1,91 +1,16 @@
 #include "State.h"
 
-
-
 InfraClassNew(State)
-
-
-
-
-
 
 Int State_Init(Int o)
 {
     return true;
 }
 
-
-
-
 Int State_Final(Int o)
 {
     return true;
 }
 
-
-
-
-
-Int State_GetMaide(Int o)
-{
-    State* m;
-
-    m = CastPointer(o);
-
-
-    return m->Maide;
-}
-
-
-
-
-
-Int State_SetMaide(Int o, Int value)
-{
-    State* m;
-
-    m = CastPointer(o);
-
-
-
-    m->Maide = value;
-
-
-
-    return true;
-}
-
-
-
-
-
-
-Int State_GetArg(Int o)
-{
-    State* m;
-
-    m = CastPointer(o);
-
-
-    return m->Arg;
-}
-
-
-
-
-
-Int State_SetArg(Int o, Int value)
-{
-    State* m;
-
-    m = CastPointer(o);
-
-
-
-    m->Arg = value;
-
-
-
-    return true;
-}
-
+Field(State, Maide)
+Field(State, Arg)
--- Infra/Infra/State.h
+++ Infra/Infra/State.h
@@ -1,17 +1,10 @@
 #pragma once
 
-
-
 #include "Probate.h"
-
-
-
 
 typedef struct
 {
     Int Maide;
-
-
     Int Arg;
 }
 State;
