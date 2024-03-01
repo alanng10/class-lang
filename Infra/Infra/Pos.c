49679c6d3bbff8ccc3d77dfeafcd945eab2e954e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 15:37:08 +0800

    Infra

--- Infra/Infra/Pos.c
+++ Infra/Infra/Pos.c
@@ -1,118 +1,49 @@
 #include "Pos.h"
 
-
-
 InfraClassNew(Pos)
 
-
-
-
-
-
-Bool Pos_Init(Int o)
+Int Pos_Init(Int o)
 {
     return true;
 }
 
-
-
-
-Bool Pos_Final(Int o)
+Int Pos_Final(Int o)
 {
     return true;
 }
 
+FieldGet(Pos, Left)
 
-
-
-Int Pos_GetLeft(Int o)
+Int Pos_LeftSet(Int o, Int value)
 {
     Pos* m;
-
     m = CastPointer(o);
-
-
-    return m->Left;
-}
-
-
-
-
-
-Bool Pos_SetLeft(Int o, Int value)
-{
-    Pos* m;
-
-    m = CastPointer(o);
-
-
 
     SInt oa;
-
     oa = value;
-
     oa = oa << 4;
-
     oa = oa >> 4;
 
-
-
     Int ob;
-
     ob = oa;
-
-
     m->Left = ob;
-
-
-
     return true;
 }
 
+FieldGet(Pos, Up)
 
-
-
-
-Int Pos_GetUp(Int o)
+Int Pos_UpSet(Int o, Int value)
 {
     Pos* m;
-
     m = CastPointer(o);
-
-
-    return m->Up;
-}
-
-
-
-
-
-Bool Pos_SetUp(Int o, Int value)
-{
-    Pos* m;
-
-    m = CastPointer(o);
-
-
 
     SInt oa;
-
     oa = value;
-
     oa = oa << 4;
-
     oa = oa >> 4;
 
-
-
     Int ob;
-
     ob = oa;
-
-
     m->Up = ob;
-
-
-
     return true;
 }
