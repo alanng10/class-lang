2afc494a611886ad176753db748e7179734f9434
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 22:20:56 +0800

    Infra

--- Infra/Infra/TextEncode.hpp
+++ Infra/Infra/TextEncode.hpp
@@ -1,36 +1,18 @@
 #pragma once
 
-
-
 #include <QStringEncoder>
 #include <QStringDecoder>
 #include <QByteArrayView>
 #include <QStringView>
 
-
-
-
 #include "Probate.hpp"
-
-
-
-
 
 struct TextEncode
 {
     Int Kind;
-
     Int WriteBom;
-
-
     QStringEncoder* InternEncode;
-
     QStringDecoder* InternDecode;
 };
-
-
-
-
-
 
 #define CP(a) ((TextEncode*)(a))
