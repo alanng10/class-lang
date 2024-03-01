fc1edd6e2bdee2deb82abebdaf1e9429e40f1bdb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 15:27:21 +0800

    Infra

--- Infra/Infra/Data.c
+++ Infra/Infra/Data.c
@@ -12,32 +12,5 @@ Int Data_Final(Int o)
     return true;
 }
 
-Int Data_CountGet(Int o)
-{
-    Data* m;
-    m = CastPointer(o);
-    return m->Count;
-}
-
-Int Data_CountSet(Int o, Int value)
-{
-    Data* m;
-    m = CastPointer(o);
-    m->Count = value;
-    return true;
-}
-
-Int Data_ValueGet(Int o)
-{
-    Data* m;
-    m = CastPointer(o);
-    return m->Value;
-}
-
-Int Data_ValueSet(Int o, Int value)
-{
-    Data* m;
-    m = CastPointer(o);
-    m->Value = value;
-    return true;
-}
+Field(Data, Count)
+Field(Data, Value)
