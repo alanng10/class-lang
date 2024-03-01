e892d0d93528da6804dbf838251ebf63349e480a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 16:22:03 +0800

    Infra

--- Infra/Infra/Post.hpp
+++ Infra/Infra/Post.hpp
@@ -1,35 +1,16 @@
 #pragma once
 
-
-
-
-
 #include <QEvent>
 #include <QCoreApplication>
 
-
-
 #include "PostIntern.hpp"
 
-
-
 #include "Probate.hpp"
-
-
-
-
 
 struct Post
 {
     Int ExecuteState;
-
-
-
     PostIntern* Intern;
 };
-
-
-
-
 
 #define CP(a) ((Post*)(a))
