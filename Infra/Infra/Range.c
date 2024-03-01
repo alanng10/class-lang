07223dc32aaeeedcd6ccc17e69af871887c98794
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-01 15:24:50 +0800

    Infra

--- Infra/Infra/Range.c
+++ Infra/Infra/Range.c
@@ -1,89 +1,16 @@
 #include "Range.h"
 
-
-
-
 InfraClassNew(Range)
 
-
-
-
-
-
-Bool Range_Init(Int o)
+Int Range_Init(Int o)
 {
     return true;
 }
 
-
-
-
-Bool Range_Final(Int o)
+Int Range_Final(Int o)
 {
     return true;
 }
 
-
-
-
-Int Range_GetStart(Int o)
-{
-    Range* m;
-
-    m = CastPointer(o);
-
-
-    return m->Start;
-}
-
-
-
-
-
-Bool Range_SetStart(Int o, Int value)
-{
-    Range* m;
-
-    m = CastPointer(o);
-
-
-
-    m->Start = value;
-
-
-
-    return true;
-}
-
-
-
-
-
-Int Range_GetEnd(Int o)
-{
-    Range* m;
-
-    m = CastPointer(o);
-
-
-    return m->End;
-}
-
-
-
-
-
-Bool Range_SetEnd(Int o, Int value)
-{
-    Range* m;
-
-    m = CastPointer(o);
-
-
-
-    m->End = value;
-
-
-
-    return true;
-}
+Field(Range, Start)
+Field(Range, End)
