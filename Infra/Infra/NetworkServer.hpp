31663cb0ea78bfad3eafd3b4ad92e69b631d3fbe
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-02 10:35:45 +0800

    Infra

--- Infra/Infra/NetworkServer.hpp
+++ Infra/Infra/NetworkServer.hpp
@@ -1,41 +1,17 @@
 #pragma once
 
-
-
-
-
-#include "NetworkServerIntern.hpp"
 #include <QHostAddress>
 
-
-
+#include "NetworkServerIntern.hpp"
 
 #include "Probate.hpp"
-
-
-
-
 
 struct NetworkServer
 {
     Int Address;
-
-
     Int Port;
-
-
-
     Int NewPeerState;
-
-
-
     NetworkServerIntern* Intern;
 };
 
-
-
-
-
-
 #define CP(a) ((NetworkServer*)(a))
-
