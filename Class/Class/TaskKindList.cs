ae9677a7c49ef07539ff8abd0d97406afe222951
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-05 17:08:36 +0800

    Class

--- Class/Class/TaskKindList.cs
+++ Class/Class/TaskKindList.cs
@@ -1,78 +1,33 @@
 namespace Class;
 
-
-
-
-
 public class TaskKindList : Any
 {
-    public static TaskKindList This { get; } = CreateShare();
+    public static TaskKindList This { get; } = ShareCreate();
 
-
-
-
-    private static TaskKindList CreateShare()
+    private static TaskKindList ShareCreate()
     {
         TaskKindList share;
-
-
         share = new TaskKindList();
-
-
-
         Any a;
-
-
         a = share;
-
-
         a.Init();
-
-
-
         return share;
     }
-
-
-
-
-
 
     public override bool Init()
     {
         base.Init();
-
-
-
         this.InitArray();
-
-
-
         this.Count = this.Array.Count;
-
-
-
         this.Index = 0;
-
-
-
 
         this.Port = this.AddItem();
         this.Token = this.AddItem();
         this.Node = this.AddItem();
         this.Check = this.AddItem();
         this.Module = this.AddItem();
-
-
-
-
-
-
         return true;
     }
-
-
-
 
     public virtual TaskKind Port { get; set; }
     public virtual TaskKind Token { get; set; }
@@ -80,56 +35,26 @@ public class TaskKindList : Any
     public virtual TaskKind Check { get; set; }
     public virtual TaskKind Module { get; set; }
 
-
-
-
     protected virtual TaskKind AddItem()
     {
         TaskKind item;
-
         item = new TaskKind();
-
         item.Init();
-
         item.Index = this.Index;
-
-
-
-        this.Array.Set(this.Index, item);
-
-
+        this.Array.Set(item.Index, item);
         this.Index = this.Index + 1;
-
-
-
         return item;
     }
-
-
-
 
     protected virtual bool InitArray()
     {
         this.Array = new Array();
-
-
         this.Array.Count = this.ArrayCount;
-
-
         this.Array.Init();
-
-
-
         return true;
     }
 
-
-
-
-
     protected virtual Array Array { get; set; }
-
-
 
     protected virtual int ArrayCount
     { 
@@ -142,22 +67,12 @@ public class TaskKindList : Any
         }
     }
 
-
-
-
     public virtual int Count { get; set; }
-
-
-
 
     public virtual TaskKind Get(int index)
     {
         return (TaskKind)this.Array.Get(index);
     }
-
-
-
     
     protected virtual int Index { get; set; }
-
 }
