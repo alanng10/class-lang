93bfdbaf86d996b1538676bc0c7af69ef6853cdb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-13 21:05:54 +0800

    Stream

--- Avalon/Avalon.Stream/MemoryStream.cs
+++ Avalon/Avalon.Stream/MemoryStream.cs
@@ -121,11 +121,11 @@ class MemoryStream : Stream
 
     public override bool Read(Data data, Range range)
     {
-        return this.Intern.Read(data.Value, data.Count, range.Start, range.End);
+        return this.Intern.Read(data.Value, data.Count, range.Index, range.Index + range.Count);
     }
 
     public override bool Write(Data data, Range range)
     {
-        return this.Intern.Write(data.Value, data.Count, range.Start, range.End);
+        return this.Intern.Write(data.Value, data.Count, range.Index, range.Index + range.Count);
     }
 }
