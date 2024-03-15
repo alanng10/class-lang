55b0f455a245467b1416bca865b685f14732ef50
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-15 15:55:44 +0800

    Binary

--- Class/Class.Binary/-.cs
+++
@@ -1,7 +0,0 @@
-global using Any = Avalon.Infra.Any;
-global using Data = Avalon.Infra.Data;
-global using DataRead = Avalon.Infra.DataRead;
-global using InfraInfra = Avalon.Infra.Infra;
-global using Array = Avalon.List.Array;
-global using ModuleRef = Class.Infra.ModuleRef;
-global using ModuleInt = Class.Infra.ModuleInt;
