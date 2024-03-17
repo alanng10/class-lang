0340924f58392a5f9274f13b4108667ffb6596b9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-17 15:07:40 +0800

    Class

--- Class/Class.Infra/KeywordList.cs
+++ Class/Class.Infra/KeywordList.cs
@@ -1,61 +1,25 @@
 namespace Class.Infra;
 
-
-
-
-
 public class KeywordList : Any
 {
-    public static KeywordList This { get; } = CreateShare();
+    public static KeywordList This { get; } = ShareCreate();
 
-
-
-
-    private static KeywordList CreateShare()
+    private static KeywordList ShareCreate()
     {
         KeywordList share;
-
-
         share = new KeywordList();
-
-
-
         Any a;
-
-
         a = share;
-
-
         a.Init();
-
-
-
         return share;
     }
-
-
-
-
-
 
     public override bool Init()
     {
         base.Init();
-
-
-
         this.InitArray();
-
-
-
         this.Count = this.Array.Count;
-
-
-
         this.Index = 0;
-
-
-
 
         this.Class = this.AddItem("class");
         this.ItemGet = this.AddItem("get");
@@ -77,17 +41,8 @@ public class KeywordList : Any
         this.Probate = this.AddItem("probate");
         this.Precate = this.AddItem("precate");
         this.Private = this.AddItem("private");
-
-
-
-
-
-
         return true;
     }
-
-
-
 
     public virtual Keyword Class { get; set; }
     public virtual Keyword ItemGet { get; set; }
@@ -110,58 +65,27 @@ public class KeywordList : Any
     public virtual Keyword Precate { get; set; }
     public virtual Keyword Private { get; set; }
 
-
-
-
     protected virtual Keyword AddItem(string text)
     {
         Keyword item;
-
         item = new Keyword();
-
         item.Init();
-
         item.Index = this.Index;
-
         item.Text = text;
-
-
-
-        this.Array.Set(this.Index, item);
-
-
+        this.Array.Set(item.Index, item);
         this.Index = this.Index + 1;
-
-
-
         return item;
     }
-
-
-
 
     protected virtual bool InitArray()
     {
         this.Array = new Array();
-
-
         this.Array.Count = this.ArrayCount;
-
-
         this.Array.Init();
-
-
-
         return true;
     }
 
-
-
-
-
     protected virtual Array Array { get; set; }
-
-
 
     protected virtual int ArrayCount
     { 
@@ -174,22 +98,12 @@ public class KeywordList : Any
         }
     }
 
-
-
-
     public virtual int Count { get; set; }
-
-
-
 
     public virtual Keyword Get(int index)
     {
         return (Keyword)this.Array.Get(index);
     }
-
-
-
     
     protected virtual int Index { get; set; }
-
 }
