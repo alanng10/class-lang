a525e9ef845c2280b4b75c84ad1c87ef427ccecb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-23 15:10:13 +0800

    Infra

---
+++ Avalon/Avalon.Infra/CharData.cs
@@ -0,0 +1,39 @@
+namespace Avalon.Infra;
+
+public class CharData : Any
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Value = new char[this.Count];
+        return true;
+    }
+
+    public virtual int Count { get; set; }
+
+    private char[] Value { get; set; }
+
+    public virtual int Get(int index)
+    {
+        if (!this.Contain(index))
+        {
+            return -1;
+        }
+        return this.Value[index];
+    }
+
+    public virtual bool Set(int index, int value)
+    {
+        if (!this.Contain(index))
+        {
+            return false;
+        }
+        this.Value[index] = (char)value;
+        return true;
+    }
+
+    public virtual bool Contain(int index)
+    {
+        return ((!(index < 0)) | index < this.Count);
+    }
+}
