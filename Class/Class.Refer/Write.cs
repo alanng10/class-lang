e41fe6e5c516486745c919b0366212557f8d5510
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-23 17:09:49 +0800

    Refer

--- Class/Class.Refer/Write.cs
+++ Class/Class.Refer/Write.cs
@@ -14,11 +14,40 @@ public class Write : Any
         return true;
     }
 
+    public virtual Refer Refer { get; set; }
+    public virtual Data Data { get; set; }
+    public virtual int Index { get; set; }
+
     protected virtual CountWriteOperate CountOperate { get; set; }
     protected virtual SetWriteOperate SetOperate { get; set; }
     protected virtual WriteOperate Operate { get; set; }
 
-    public virtual Data Data { get; set; }
+    public virtual bool Execute()
+    {
+        this.Operate = this.CountOperate;
+        this.Index = 0;
 
-    public virtual int Index { get; set; }
+        this.ExecuteStage();
+
+        int count;
+        count = this.Index;
+        this.Data = new Data();
+        this.Data.Count = count;
+        this.Data.Init();
+
+        this.Operate = this.SetOperate;
+        this.Index = 0;
+
+        this.ExecuteStage();
+
+        this.Operate = null;
+        this.Index = 0;
+
+        return true;
+    }
+
+    protected virtual bool ExecuteStage()
+    {
+        return true;
+    }
 }
