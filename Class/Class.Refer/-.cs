1f85e8809db64f71917887ea591b57aa3a9ca492
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-23 16:46:12 +0800

    Refer

--- Class/Class.Refer/-.cs
+++ Class/Class.Refer/-.cs
@@ -1,3 +1,4 @@
 global using Any = Avalon.Infra.Any;
+global using Data = Avalon.Infra.Data;
 global using Array = Avalon.List.Array;
 global using ModuleRef = Class.Infra.ModuleRef;
---
+++ Class/Class.Refer/Write.cs
@@ -0,0 +1,6 @@
+namespace Class.Refer;
+
+public class Write : Any
+{
+    public virtual Data Data { get; set; }
+}
