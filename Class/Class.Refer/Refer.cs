1570f3665bef91619a10dd9c8f3a6ca082488807
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-23 16:06:33 +0800

    Refer

--- Class/Class.Refer/Refer.cs
+++ Class/Class.Refer/Refer.cs
@@ -2,5 +2,15 @@ namespace Class.Refer;
 
 public class Refer : Any
 {
-   
+    public virtual ModuleRef Ref { get; set; }
+
+    public virtual Array Class { get; set; }
+
+    public virtual Array Import { get; set; }
+
+    public virtual Array Export { get; set; }
+
+    public virtual Array Base { get; set; }
+
+    public virtual Array Part { get; set; }
 }
