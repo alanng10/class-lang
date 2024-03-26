ed10dc40ee39c6ac2f43a2aa77b60f4b06218e6e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-26 15:34:20 +0800

    Tool

--- Tool/Z.Tool.RefKindListSourceGen/-.cs
+++
@@ -1,4 +0,0 @@
-global using Any = Avalon.Infra.Any;
-global using TableEntry = Avalon.List.Entry;
-global using SourceGen = Z.Infra.ListSourceGen.Gen;
-global using StringBuilder = System.Text.StringBuilder;
--- Tool/Z.Tool.StateKindListSourceGen/Value.cs
+++
@@ -1,9 +0,0 @@
-namespace Z.Tool.StateKindListSourceGen;
-
-
-
-
-class Value : Any
-{
-    public virtual string Text { get; set; }
-}
