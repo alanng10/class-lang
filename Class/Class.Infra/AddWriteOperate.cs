e5761d60c4c963a5a5757ef8b513589f5e8eb519
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-26 11:05:52 +0800

    Class

--- Class/Class.Infra/AddWriteOperate.cs
+++ Class/Class.Infra/AddWriteOperate.cs
@@ -2,14 +2,24 @@ namespace Class.Infra;
 
 public class AddWriteOperate : WriteOperate
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.TextInfra = TextInfra.This;
+        return true;
+    }
+
     public virtual StringValueWrite Write { get; set; }
+    protected virtual TextInfra TextInfra { get; set; }
 
     public override bool ExecuteChar(char oc)
     {
         int index;
         index = this.Write.Index;
 
-        this.Write.Array[index] = oc;
+        Data data;
+        data = this.Write.Data;
+        this.TextInfra.CharSet(data, index, oc);
         
         index = index + 1;
 
