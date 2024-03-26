32725e09d4ff0fa2235ee32998a4a4f58b6df8b9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-26 19:03:27 +0800

    Script

--- Script/Deploy.cmd
+++ Script/Deploy.cmd
@@ -2,7 +2,6 @@
 
 set DotNetModuleOutFold=.\Out\net6.0
 set DeployFold=.\Out\Deploy
-set AvalonInternData=Avalon.Intern.-
 
 rmdir /S /Q %DeployFold% 2>NUL
 mkdir %DeployFold% 1>NUL 2>NUL
@@ -12,7 +11,7 @@ copy /Y %DotNetModuleOutFold%\Class.*.dll %DeployFold% 1>NUL 2>NUL
 
 pushd %DotNetModuleOutFold%
 for /d %%a in ("*.-") do ( 
-    mkdir "%DeployFold%\%%~nxa" 1>NUL 2>NUL
-    xcopy /S /E "%DotNetModuleOutFold%\%%~nxa" "%DeployFold%\%%~nxa" 1>NUL 2>NUL
+    mkdir "..\..\%DeployFold%\%%~nxa" 1>NUL 2>NUL
+    xcopy /S /E ".\%%~nxa" "..\..\%DeployFold%\%%~nxa" 1>NUL 2>NUL
 )
 popd
