2819d3d900a13c2f78dc0653dd5576b45b94f022
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-27 12:33:27 +0800

    Tool

--- Tool/Z.Tool.ReferBinaryGen/Class.cs
+++ Tool/Z.Tool.ReferBinaryGen/Class.cs
@@ -2,6 +2,10 @@ namespace Z.Tool.ReferBinaryGen;
 
 public class Class : Any
 {
+    public virtual int Index { get; set; }
+
+    public virtual string Name { get; set; }
+
     public virtual SystemType Type { get; set; }
 
     public virtual Array Property { get; set; }
