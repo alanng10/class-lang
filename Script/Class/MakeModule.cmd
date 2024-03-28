6d5d046c5831f55b553997270ba0466ba72dba3f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-28 15:53:23 +0800

    Script

--- Script/Class/MakeModule.cmd
+++ Script/Class/MakeModule.cmd
@@ -2,6 +2,11 @@
 
 echo Make Module
 echo:
+set DotNetModuleOutFold=.\Out\net6.0
 pushd Class\ClassExe
 dotnet build -v quiet
+popd
+pushd %DotNetModuleOutFold%
+del /F /Q Class.exe 2>NUL
+rename ClassExe.exe Class.exe
 popd
