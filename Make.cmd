f483359f1ea6dfc7e6d3e04c9f55044ecacb89a9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-28 16:48:43 +0800

    Cmd

--- Make.cmd
+++ Make.cmd
@@ -14,5 +14,5 @@ echo Make Class
 call Script\Class\MakeModule
 echo:
 echo Make Refer
-call Script\Tool\Make ReferBinaryGen
+call Script\Tool\Make ReferBinaryGen 1>NUL
 call Script\Tool\Execute ReferBinaryGen
