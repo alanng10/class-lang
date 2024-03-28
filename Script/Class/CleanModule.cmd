5cf562331c991ca12c195333b8435d16f599639d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-28 15:54:42 +0800

    Script

--- Script/Class/CleanModule.cmd
+++ Script/Class/CleanModule.cmd
@@ -4,3 +4,4 @@ echo Clean Module
 set DotNetModuleOutFold=.\Out\net6.0
 del /F /Q %DotNetModuleOutFold%\Class.*.dll 2>NUL
 del /F /Q %DotNetModuleOutFold%\ClassExe* 2>NUL
+del /F /Q %DotNetModuleOutFold%\Class.exe 2>NUL
