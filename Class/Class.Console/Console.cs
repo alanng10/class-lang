bef0e41c63e01b13fe4d227245d4f8d2faf4889f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-28 15:33:44 +0800

    Class

---
+++ Class/Class.Console/Console.cs
@@ -0,0 +1,11 @@
+namespace Class.Console;
+
+public class Console : Any
+{
+    public virtual Array Arg { get; set; }
+
+    public virtual int Execute()
+    {
+        return 0;
+    }
+}
--- Class/Class.Console/Entry.cs
+++ Class/Class.Console/Entry.cs
@@ -2,5 +2,12 @@ namespace Class.Console;
 
 public class Entry : EntryEntry
 {
-    
+    public override int Execute()
+    {
+        Console console;
+        console = new Console();
+        console.Init();
+        console.Arg = this.Arg;
+        return console.Execute();
+    }
 }
