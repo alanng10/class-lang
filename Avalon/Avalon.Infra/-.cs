360ddde9782ccb058e53ee61b16b826b9143cd80
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-03-28 12:21:42 +0800

    Avalon

--- Avalon/Avalon.Infra/-.cs
+++ Avalon/Avalon.Infra/-.cs
@@ -1,3 +1,3 @@
 global using InternIntern = Avalon.Intern.Intern;
-global using IComparerObject = System.Collections.Generic.IComparer<object>;
 global using ReferenceEqualityComparer = System.Collections.Generic.ReferenceEqualityComparer;
+global using StringBuilder = System.Text.StringBuilder;
---
+++ Avalon/Avalon.Infra/StringJoin.cs
@@ -0,0 +1,30 @@
+namespace Avalon.Infra;
+
+public class StringJoin : Any
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Builder = new StringBuilder();
+        return true;
+    }
+
+    private StringBuilder Builder { get; set; }
+
+    public virtual string Result()
+    {
+        return this.Builder.ToString();
+    }
+
+    public virtual bool Clear()
+    {
+        this.Builder.Clear();
+        return true;
+    }
+
+    public virtual bool Append(string a)
+    {
+        this.Builder.Append(a);
+        return true;
+    }
+}
