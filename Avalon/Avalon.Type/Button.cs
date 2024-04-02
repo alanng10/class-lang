e1f4b256e6532c22fe079dea0254ec47d88747d8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-02 17:23:31 +0800

    Avalon

--- Avalon/Avalon.Type/Button.cs
+++ Avalon/Avalon.Type/Button.cs
@@ -3,5 +3,5 @@ namespace Avalon.Type;
 public class Button : Any
 {
     public virtual int Index { get; set; }
-    public virtual char Char { get; set; }
+    public virtual ButtonChar Char { get; set; }
 }
