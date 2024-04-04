4cd6bbb4040206f5dd021ee325b554759feecf5a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-04 15:23:47 +0800

    Class

--- Class/Class.Refer/SetReadOperate.cs
+++ Class/Class.Refer/SetReadOperate.cs
@@ -2,9 +2,28 @@ namespace Class.Refer;
 
 public class SetReadOperate : Any
 {
+    public virtual Read Read { get; set; }
+
     public virtual string ExecuteString()
     {
-        return null;
+        Read read;
+        read = this.Read;
+        int o;
+        o = read.ExecuteCount();
+        if (o == -1)
+        {
+            return null;
+        }
+
+        int oa;
+        oa = read.StringIndex;
+        string a;
+        a = (string)read.StringArray.Get(oa);
+
+        read.Index = read.Index + o;
+        read.StringIndex = oa + 1;
+        read.StringDataIndex = read.StringDataIndex + o;
+        return a;
     }
 
     public virtual Array ExecuteArray()
