adc7f53c2f0c010bb43ff89913923b6c573f8833
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-04 14:34:26 +0800

    Class

--- Class/Class.Refer/Read.cs
+++ Class/Class.Refer/Read.cs
@@ -13,19 +13,43 @@ public class Read : Any
     public virtual int Index { get; set; }
     protected virtual InfraInfra InfraInfra { get; set; }
 
-    protected virtual int ExecuteByte()
+    protected virtual long ExecuteInt()
     {
-        Data data;
-        data = this.Data;
         int index;
         index = this.Index;
+        Data data;
+        data = this.Data;
+        int dataCount;
+        dataCount = (int)data.Count;
+        int count;
+        count = sizeof(long);
+        if (!((index < dataCount) & !(dataCount < (index + count))))
+        {
+            return -1;
+        }
+
+        long a;
+        a = this.InfraInfra.DataIntGet(data, index);
+        index = index + count;
+        this.Index = index;
+        return a;
+    }
+
+    protected virtual int ExecuteByte()
+    {
+        int index;
+        index = this.Index;
+        Data data;
+        data = this.Data;
         if (!(index < data.Count))
         {
             return -1;
         }
 
         int a;
-        a = this.Data.Get(index);
+        a = data.Get(index);
+        index = index + 1;
+        this.Index = index;
         return a;
     }
 }
