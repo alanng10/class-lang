c5ba928236be15ee2d9e1b6cf3a4d6ac3fa4e467
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-05 22:15:30 +0800

    Infra

--- Infra/Infra/Random.cpp
+++ Infra/Infra/Random.cpp
@@ -4,12 +4,49 @@ CppClassNew(Random)
 
 Int Random_Init(Int o)
 {
+    Random* m;
+    m = CP(o);
+
+    m->Intern = new QRandomGenerator;
+
     return true;
 }
 
 Int Random_Final(Int o)
 {
+    Random* m;
+    m = CP(o);
+
+    delete m->Intern;
+
     return true;
 }
 
-CppField(Random, Seed)
+CppFieldGet(Random, Seed)
+
+Int Random_SeedSet(Int o, Int value)
+{
+    Random* m;
+    m = CP(o);
+
+    m->Seed = value;
+
+    quint32 u;
+    u = (quint32)(m->Seed);
+
+    m->Intern->seed(u);
+    return true;
+}
+
+Int Random_Execute(Int o)
+{
+    Random* m;
+    m = CP(o);
+
+    quint32 u;
+    u = m->Intern->generate();
+
+    Int a;
+    a = u;
+    return a;
+}
--- Infra/Infra/Random.hpp
+++ Infra/Infra/Random.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <QRandomGenerator>
+
 #include "Probate.hpp"
 
 struct Random
 {
     Int Seed;
+    QRandomGenerator* Intern;
 };
 
 #define CP(a) ((Random*)(a))
