a20cef44956c42f12165329bb6856fdf189f431d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-05 21:07:45 +0800

    Infra

--- Infra/Infra/Probate.hpp
+++ Infra/Infra/Probate.hpp
@@ -1,71 +1,36 @@
 #pragma once
 
-
-
 extern "C"
 {
 #include "Probate.h"
 }
 
-
-
-
-
-#define CppClassNew(c) \
-Int c##_New()\
+#define CppClassNew(varClass) \
+Int varClass##_New()\
 {\
     Int o;\
-\
     o = (Int)(new c());\
-\
-\
     return o;\
 }\
-\
-\
-\
-Int c##_Delete(Int o)\
+Int varClass##_Delete(Int o)\
 {\
-    c* k;\
-\
-    k = (c*)(o);\
-\
-\
+    varClass* k;\
+    k = (varClass*)(o);\
     delete k;\
-\
-\
     return true;\
 }\
 
 
-
-
-
-
-
 #define CastDoubleToInt(a)   (*((Int*)&a))
-
-
-
 #define CastIntToDouble(a)   (*((double*)&a))
 
-
-
-
 #define CastInt32ToFloat(a)   (*((float*)&a))
-
-
-
-
 
 #define InternQReal(a) \
     SInt a##_u;\
     a##_u = a;\
-\
     a##_u = a##_u << 4;\
-\
     a##_u = a##_u >> 4;\
-\
     qreal a##U;\
     a##U = a##_u;\
 
