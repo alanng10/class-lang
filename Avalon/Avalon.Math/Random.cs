d07fe044bad0e85def0b78d179760fb95cecdf45
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-05 22:25:51 +0800

    Avalon

---
+++ Avalon/Avalon.Math/Random.cs
@@ -0,0 +1,48 @@
+namespace Avalon.Math;
+
+public class Random : Any
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Intern = Extern.Random_New();
+        Extern.Random_Init(this.Intern);
+        return true;
+    }
+
+    public virtual bool Final()
+    {
+        Extern.Random_Final(this.Intern);
+        Extern.Random_Delete(this.Intern);
+        return true;
+    }
+
+    private ulong Intern { get; set; }
+
+    public virtual long Seed
+    {
+        get
+        {
+            ulong u;
+            u = Extern.Random_SeedGet(this.Intern);
+            long a;
+            a = (long)u;
+            return a;
+        }
+        set
+        {
+            ulong u;
+            u = (ulong)value;
+            Extern.Random_SeedSet(this.Intern, u);
+        }
+    }
+
+    public virtual long Execute()
+    {
+        ulong u;
+        u = Extern.Random_Execute(this.Intern);
+        long a;
+        a = (long)u;
+        return a;
+    }
+}
