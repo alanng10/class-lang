3d39622e4338dc92b7ed9d9b65543c79186db26e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-06 11:03:30 +0800

    Infra

--- Infra/Infra/Image.cpp
+++ Infra/Infra/Image.cpp
@@ -69,104 +69,42 @@ Int Image_DataCreate(Int o)
 Int Image_SetReadIntern(Int o, Int value)
 {
     Image* m;
-
     m = CP(o);
 
-
-
-
     QImage* u;
-
     u = (QImage*)value;
-
-
-
 
     (*(m->Intern)) = *u;
 
-
-
-
     int widthU;
-
     int heightU;
-
-
     widthU = m->Intern->width();
-
-
     heightU = m->Intern->height();
 
-
-
-
     const uchar* dataValueU;
-
     dataValueU = m->Intern->constBits();
 
-
-
     qsizetype dataCountU;
-
     dataCountU = m->Intern->sizeInBytes();
 
-
-
-
     Int width;
-
     Int height;
-
-
     width = widthU;
-
     height = heightU;
-
-
-
     Int size;
-
     size = m->Size;
-
-
     Size_WidthSet(size, width);
-
-
     Size_HeightSet(size, height);
 
-
-
-
     Int dataCount;
-
     dataCount = dataCountU;
-
-
-
     Int dataValue;
-
     dataValue = CastInt(dataValueU);
 
-
-
-
     Int data;
-
     data = m->Data;
-
-
-
-
     Data_CountSet(data, dataCount);
-
-
-
     Data_ValueSet(data, dataValue);
-
-
-
-
-
     return true;
 }
 
@@ -176,33 +114,18 @@ CppField(Image, Data)
 Int Image_VideoOut(Int o)
 {
     Image* m;
-
     m = CP(o);
-
-
-
     return m->VideoOut;
 }
 
 Int Image_RowByteCountGet(Int o)
 {
     Image* m;
-
     m = CP(o);
-
-
-
     qsizetype u;
-
     u = m->Intern->bytesPerLine();
-
-
-
     Int a;
-
     a = u;
-
-
     return a;
 }
 
@@ -211,69 +134,30 @@ Int Image_RowByteCountSet(Int o, Int value)
     return true;
 }
 
-
-
-
-
 Int Image_VideoOutSet(Int o)
 {
     Image* m;
-
     m = CP(o);
-
-
-
     QPaintDevice* uu;
-
     uu = m->Intern;
-
-
-
     Int ua;
-
     ua = CastInt(uu);
 
-
-
-
     Int alphaFlag;
-
     alphaFlag = 1;
-
     alphaFlag = alphaFlag << 59;
-
-
 
     ua = ua | alphaFlag;
 
-
-
     m->VideoOut = ua;
-
-
-
-
     return true;
 }
-
-
-
-
 
 Int Image_Intern(Int o)
 {
     Image* m;
-
     m = CP(o);
-
-
-
-    Int u;
-
-    u = CastInt(m->Intern);
-
-
-
-    return u;
+    Int a;
+    a = CastInt(m->Intern);
+    return a;
 }
-
