b036b1c3fc3b4bb5453f38a34a33a8a8a361fcb8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-06 11:07:43 +0800

    Infra

--- Infra/Infra/Interval.cpp
+++ Infra/Infra/Interval.cpp
@@ -5,38 +5,19 @@ CppClassNew(Interval)
 Int Interval_Init(Int o)
 {
     Interval* m;
-
     m = CP(o);
-
-
-
-
     m->Intern = new IntervalIntern();
-
-
     m->Intern->Interval = o;
-
-
     m->Intern->Init();
-
-
-
-
     return true;
 }
 
 Int Interval_Final(Int o)
 {
     Interval* m;
-
     m = CP(o);
 
-
-
     delete m->Intern;
-
-
-
     return true;
 }
 
@@ -46,22 +27,11 @@ CppField(Interval, SingleShot)
 Int Interval_ActiveGet(Int o)
 {
     Interval* m;
-
     m = CP(o);
-
-
-
     bool bu;
-
     bu = m->Intern->isActive();
-
-
-
     Bool b;
-
     b = bu;
-
-
     return b;
 }
 
@@ -75,119 +45,48 @@ CppField(Interval, ElapseState)
 Int Interval_Start(Int o)
 {
     Interval* m;
-
     m = CP(o);
-
-
-
-
     Int time;
-
     time = m->Time;
-
-
     Int singleShot;
-
     singleShot = m->SingleShot;
 
-
-
-
     int timeU;
-
     timeU = time;
-
-
-
     bool singleShotU;
-
-    singleShotU = (!(singleShot == 0));
-
-
-
-
+    singleShotU = singleShot;
 
     m->Intern->setInterval(timeU);
-
-
     m->Intern->setSingleShot(singleShotU);
-
-
-
-
     m->Intern->start();
-
-
-
-
-
     return true;
 }
-
-
-
-
 
 Int Interval_Stop(Int o)
 {
     Interval* m;
-
     m = CP(o);
-
-
-
     m->Intern->stop();
-
-
-
     return true;
 }
-
-
-
-
-
 
 Int Interval_Elapse(Int o)
 {
     Interval* m;
-
     m = CP(o);
 
-
-
-
     Int state;
-
     state = m->ElapseState;
-
-
-
     Int aa;
-
     aa = State_MaideGet(state);
-
-
     Int arg;
-
     arg = State_ArgGet(state);
 
-
-
-
     Interval_Elapse_Maide maide;
-
     maide = (Interval_Elapse_Maide)aa;
-
-
-
     if (!(maide == null))
     {
         maide(o, arg);
     }
-
-
-
-
     return true;
 }
