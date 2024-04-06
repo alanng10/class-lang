367486f3c806e7309a79567fbe298263cbabddf5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-06 16:09:37 +0800

    Infra

--- Infra/Infra/Play.cpp
+++ Infra/Infra/Play.cpp
@@ -5,30 +5,18 @@ CppClassNew(Play)
 Int Play_Init(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     m->Intern = new QMediaPlayer;
-
-
-
     return true;
 }
 
 Int Play_Final(Int o)
 {
     Play* m;
-
     m = CP(o);
 
-
-
     delete m->Intern;
-
-
-
+    
     return true;
 }
 
@@ -37,104 +25,46 @@ CppField(Play, Source)
 Int Play_SourceThisSet(Int o)
 {
     Play* m;
-
     m = CP(o);
 
-
-
-
-
     QString urlString;
-
-
     Int ua;
-
     ua = CastInt(&urlString);
-
-
 
     String_QStringSet(ua, m->Source);
 
-
-
-
-
     QUrl url(urlString, QUrl::StrictMode);
-
-
 
     if (!url.isValid())
     {
         return false;
     }
 
-
-
-
     m->Intern->setSource(url);
-
-
-
-
-
     return true;
 }
-
-
-
-
-
 
 Int Play_Execute(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     m->Intern->play();
-
-
-
     return true;
 }
-
-
-
-
 
 Int Play_Pause(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     m->Intern->pause();
-
-
-
     return true;
 }
-
-
-
-
 
 Int Play_Stop(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     m->Intern->stop();
-
-
-
     return true;
 }
 
@@ -143,53 +73,24 @@ CppFieldGet(Play, VideoOut)
 Int Play_VideoOutSet(Int o, Int value)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     m->VideoOut = value;
-
-
-
 
     if (m->VideoOut == null)
     {
         m->Intern->setVideoOutput(null);
-
-
         return true;
     }
 
-
-
-
     Int oa;
-
     oa = VideoOut_Intern(m->VideoOut);
-
-
-
-
     VideoOutIntern* ua;
-
     ua = (VideoOutIntern*)oa;
 
-
-
-
     QVideoSink* u;
-
     u = ua;
 
-
-
-
     m->Intern->setVideoOutput(u);
-
-
-
-
     return true;
 }
 
@@ -198,105 +99,55 @@ CppFieldGet(Play, AudioOut)
 Int Play_AudioOutSet(Int o, Int value)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     m->AudioOut = value;
-
-
 
     if (m->AudioOut == null)
     {
         m->Intern->setAudioOutput(null);
-
-
         return true;
     }
 
-
-
     Int u;
-
     u = AudioOut_Intern(m->AudioOut);
 
-
-
-
     QAudioOutput* audioOutput;
-
     audioOutput = (QAudioOutput*)u;
 
-
-
     m->Intern->setAudioOutput(audioOutput);
-
-
-
     return true;
 }
 
 Int Play_HasVideo(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     bool bu;
-
     bu = m->Intern->hasVideo();
-
-
-
     Bool a;
-
     a = bu;
-
     return a;
 }
 
 Int Play_HasAudio(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     bool bu;
-
     bu = m->Intern->hasAudio();
-
-
-
     Bool a;
-
     a = bu;
-
     return a;
 }
 
 Int Play_TimeGet(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     qint64 u;
-
     u = m->Intern->duration();
-
-
-
     Int a;
-
     a = u;
-
     return a;
 }
 
@@ -308,40 +159,20 @@ Int Play_TimeSet(Int o, Int value)
 Int Play_PosGet(Int o)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     qint64 u;
-
     u = m->Intern->position();
-
-
-
     Int a;
-
     a = u;
-
     return a;
 }
 
 Int Play_PosSet(Int o, Int value)
 {
     Play* m;
-
     m = CP(o);
-
-
-
     qint64 u;
-
     u = value;
-
-
     m->Intern->setPosition(u);
-
-
-
     return true;
 }
--- Infra/Infra/Play.hpp
+++ Infra/Infra/Play.hpp
@@ -1,41 +1,18 @@
 #pragma once
 
-
-
-
-
 #include <QMediaPlayer>
 #include <QAudioOutput>
 
-
-
 #include "VideoOutIntern.hpp"
 
-
-
 #include "Probate.hpp"
-
-
-
-
 
 struct Play
 {
     Int Source;
-
-
     Int VideoOut;
-
-
     Int AudioOut;
-
-
-
     QMediaPlayer* Intern;
 };
-
-
-
-
 
 #define CP(a) ((Play*)(a))
