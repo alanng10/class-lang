0e28e7024265d7b657527b8dcdc367fc68fa31e2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-06 11:08:45 +0800

    Infra

--- Infra/Infra/IntervalIntern.cpp
+++ Infra/Infra/IntervalIntern.cpp
@@ -1,37 +1,16 @@
 #include "IntervalIntern.hpp"
 
-
-
-
-
-
 Bool IntervalIntern::Init()
 {
     this->setTimerType(Qt::PreciseTimer);
 
-
-
-
     connect(this, &QTimer::timeout, this, &IntervalIntern::TimeOutHandle);
-
-
-
-
-
     return true;
 }
-
-
-
-
 
 void IntervalIntern::TimeOutHandle()
 {
     Int interval;
-
     interval = this->Interval;
-
-
-
     Interval_Elapse(interval);
 }
--- Infra/Infra/IntervalIntern.hpp
+++ Infra/Infra/IntervalIntern.hpp
@@ -1,35 +1,17 @@
 #pragma once
 
-
-
-
-
 #include <QTimer>
 
-
-
 #include "Probate.hpp"
-
-
-
-
-
 
 class IntervalIntern : public QTimer
 {
     Q_OBJECT
 
-
 public:
-
     Bool Init();
-
-
     Int Interval;
 
-
 private slots:
-
     void TimeOutHandle();
 };
-
