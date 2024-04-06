8c38351ed66197e320b006acd1acebc12808f5d6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-06 15:54:06 +0800

    Infra

--- Infra/Infra/Memory.cpp
+++ Infra/Infra/Memory.cpp
@@ -17,175 +17,62 @@ CppField(Memory, Stream)
 Int Memory_Open(Int o)
 {
     Memory* m;
-
     m = CP(o);
 
-
-
-
-
     Int stream;
-
     stream = m->Stream;
 
-
-
-
-
     QIODeviceBase::OpenModeFlag modeU;
-
     modeU = QIODeviceBase::ReadWrite;
 
-
-
-
-
     QBuffer* buffer;
-
-
     buffer = new QBuffer();
-
-
-
-
-
     buffer->open(modeU);
 
-
-
-
-
-
     QIODevice* oo;
-
     oo = buffer;
-
-
-
-
+    
     Int uo;
-
     uo = CastInt(oo);
 
-
-
-
-
-
     Int aaa;
-
-
     aaa = Infra_Share();
-
-
-
     Int stat;
-
     stat = Share_Stat(aaa);
-
-
-
-
     Int kind;
-
     kind = Stat_StreamKindMemory(stat);
 
-
-
-
     Bool canRead;
-
     canRead = true;
-
-
-
     Bool canWrite;
-
     canWrite = true;
 
-
-
-
-
     Stream_KindSet(stream, kind);
-
-
     Stream_ValueSet(stream, uo);
-
-
-
-
     Stream_CanReadSet(stream, canRead);
-
-
     Stream_CanWriteSet(stream, canWrite);
-
-
-
-
-
     return true;
 }
 
 Int Memory_Close(Int o)
 {
     Memory* m;
-
     m = CP(o);
-
-
-
-
     Int stream;
-
     stream = m->Stream;
-
-
-
-
     Int value;
-
-
     value = Stream_ValueGet(stream);
 
-
-
-
     QIODevice* oo;
-
-
     oo = (QIODevice*)value;
 
-
-
-
     QBuffer* buffer;
-
-
     buffer = (QBuffer*)oo;
-
-
-
     buffer->close();
-
-
-
 
     delete buffer;
 
-
-
-
-
     Stream_KindSet(stream, null);
-
-
-
     Stream_ValueSet(stream, null);
-
-
-
-
-
     return true;
 }
