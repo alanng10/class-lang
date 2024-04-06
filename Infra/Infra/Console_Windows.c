88a822ab39a27d0f01575daacc8f2295878b92c3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-06 10:44:16 +0800

    Infra

--- Infra/Infra/Console_Windows.c
+++ Infra/Infra/Console_Windows.c
@@ -1,17 +1,9 @@
 #include "Console_Windows.h"
 
-
-
-
-
 Int Console_OS_Init()
 {
     SetConsoleOutputCP(CP_UTF8);
 
-
     SetConsoleCP(CP_UTF8);
-
-
-
     return true;
 }
--- Infra/Infra/Console_Windows.h
+++ Infra/Infra/Console_Windows.h
@@ -1,11 +1,5 @@
 #pragma once
 
-
-
-
 #include <windows.h>
-
-
-
 
 #include "Probate.h"
