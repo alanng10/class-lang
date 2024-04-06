988a8d8636c4e12d8274f7f5d5657026f94f328a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-06 10:57:43 +0800

    Infra

--- Infra/Infra/GradientLinear.cpp
+++ Infra/Infra/GradientLinear.cpp
@@ -5,90 +5,40 @@ CppClassNew(GradientLinear)
 Int GradientLinear_Init(Int o)
 {
     GradientLinear* m;
-
     m = CP(o);
 
-
-
-
     Int startPos;
-
     startPos = m->StartPos;
-
-
-
     Int endPos;
-
     endPos = m->EndPos;
 
-
-
-
     Int startLeft;
-
     Int startUp;
-
-
     startLeft = Pos_LeftGet(startPos);
-
     startUp = Pos_UpGet(startPos);
 
-
-
-
     Int endLeft;
-
     Int endUp;
-
-
     endLeft = Pos_LeftGet(endPos);
-
     endUp = Pos_UpGet(endPos);
 
-
-
-
     InternQReal(startLeft)
-
     InternQReal(startUp)
-
     InternQReal(endLeft)
-
     InternQReal(endUp)
 
-
-
-
     QLinearGradient* linearGradient;
-
-
     linearGradient = new QLinearGradient(startLeftU, startUpU, endLeftU, endUpU);
-
-
-
-
     m->Intern = linearGradient;
-
-
-
-
     return true;
 }
 
 Int GradientLinear_Final(Int o)
 {
     GradientLinear* m;
-
     m = CP(o);
 
-
-
-
     delete m->Intern;
-
-
-
-
     return true;
 }
 
@@ -98,16 +48,8 @@ CppField(GradientLinear, EndPos)
 Int GradientLinear_Intern(Int o)
 {
     GradientLinear* m;
-
     m = CP(o);
-
-
-
-    Int u;
-
-    u = CastInt(m->Intern);
-
-
-
-    return u;
+    Int a;
+    a = CastInt(m->Intern);
+    return a;
 }
