5b884727ccbab95db3fcb84def1ce3f5acc3199f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-07 19:36:36 +0800

    Tool

--- Tool/Z.Infra.Class/ModuleInfo.cs
+++ Tool/Z.Infra.Class/ModuleInfo.cs
@@ -2,7 +2,7 @@ namespace Z.Infra.Class;
 
 public class ModuleInfo
 {
-    public string C_RefString(object o, [CallerFilePath] string filePath = null)
+    public virtual string C_RefString(object o, [CallerFilePath] string filePath = null)
     {
         return null;
     }
--- Tool/Z.Infra.Class/String.cs
+++ Tool/Z.Infra.Class/String.cs
@@ -2,17 +2,17 @@ namespace Z.Infra.Class;
 
 public class String
 {
-    public int G_Count(string o)
+    public virtual int G_Count(string o)
     {
         return o.Length;
     }
 
-    public int C_Char(string o, int index)
+    public virtual int C_Char(string o, int index)
     {
         return o[index];
     }
 
-    public string C_SubString(string o, Range range)
+    public virtual string C_SubString(string o, Range range)
     {
         return o.Substring(range.Index, range.Count);
     }
