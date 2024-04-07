c5b8b5cc21794b22c991787e7d8c2e73a1de3759
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-07 19:41:36 +0800

    Tool

--- Tool/Z.Infra.Class/-.cs
+++ Tool/Z.Infra.Class/-.cs
@@ -1,2 +1,4 @@
+global using Any = Avalon.Infra.Any;
 global using Range = Avalon.Infra.Range;
+global using InfraInfra = Avalon.Infra.Infra;
 global using CallerFilePathAttribute = System.Runtime.CompilerServices.CallerFilePathAttribute;
--- Tool/Z.Infra.Class/String.cs
+++ Tool/Z.Infra.Class/String.cs
@@ -1,7 +1,16 @@
 namespace Z.Infra.Class;
 
-public class String
+public class String : Any
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.InfraInfra = InfraInfra.This;
+        return true;
+    }
+
+    protected virtual InfraInfra InfraInfra { get; set; }
+
     public virtual int G_Count(string o)
     {
         return o.Length;
@@ -9,11 +18,19 @@ public class String
 
     public virtual int C_Char(string o, int index)
     {
+        if (!(index < 0) & (index < o.Length))
+        {
+            return -1;
+        }
         return o[index];
     }
 
     public virtual string C_SubString(string o, Range range)
     {
+        if (!this.InfraInfra.CheckRange(o.Length, range))
+        {
+            return null;
+        }
         return o.Substring(range.Index, range.Count);
     }
 }
