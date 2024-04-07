55b9e03a4dac133e5c69849be51c6e0469ff81b5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-08 07:15:03 +0800

    Infra

--- Infra/Infra/Post.cpp
+++ Infra/Infra/Post.cpp
@@ -1,56 +1,24 @@
 #include "Post.hpp"
 
-
-
-
 CppClassNew(Post)
-
-
-
-
-
 
 Int Post_Init(Int o)
 {
     Post* m;
-
     m = CP(o);
-
-
-
-
-    m->Intern = new PostIntern();
-
-
+    m->Intern = new PostIntern;
     m->Intern->Post = o;
-
-
     m->Intern->Init();
-
-
-
-
     return true;
 }
-
-
-
-
 
 Int Post_Final(Int o)
 {
     Post* m;
-
     m = CP(o);
 
-
-
-
     delete m->Intern;
-
-
-
-
+    
     return true;
 }
 
@@ -59,69 +27,30 @@ CppField(Post, ExecuteState)
 Int Post_Execute(Int o)
 {
     Post* m;
-
     m = CP(o);
-
-
-
-
     QEvent* u;
-
     u = new QEvent(QEvent::User);
 
-
-
     QCoreApplication::postEvent(m->Intern, u);
-
-
-
-
     return true;
 }
-
-
-
-
 
 Int Post_ExecuteHandle(Int o)
 {
     Post* m;
-
     m = CP(o);
-
-
-
     Int state;
-
     state = m->ExecuteState;
-
-
-
     Int aa;
-
     aa = State_MaideGet(state);
-
-
     Int arg;
-
     arg = State_ArgGet(state);
 
-
-
-
     Post_Execute_Maide maide;
-
     maide = (Post_Execute_Maide)aa;
-
-
-
     if (!(maide == null))
     {
         maide(o, arg);
     }
-
-
-
-
     return true;
 }
