66ce4c8219c83b74d473dc154d863e67eb9e5fb8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-07 19:19:50 +0800

    Tool

--- Tool/Z.Infra.Class/ModuleInfo.cs
+++ Tool/Z.Infra.Class/ModuleInfo.cs
@@ -2,7 +2,7 @@ namespace Z.Infra.Class;
 
 public class ModuleInfo
 {
-    public static string C_RefString(object o, [CallerFilePath] string filePath = null)
+    public string C_RefString(object o, [CallerFilePath] string filePath = null)
     {
         return null;
     }
