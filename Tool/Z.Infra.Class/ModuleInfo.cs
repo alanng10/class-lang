53e4b8ff4b995e1924fdf96c80feb539460230e2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-07 22:16:35 +0800

    Tool

--- Tool/Z.Infra.Class/ModuleInfo.cs
+++ Tool/Z.Infra.Class/ModuleInfo.cs
@@ -2,8 +2,8 @@ namespace Z.Infra.Class;
 
 public class ModuleInfo : Any
 {
-    public virtual string RefString([CallerFilePath] string filePath = null)
+    public virtual string RefString(string o)
     {
-        return null;
+        return o;
     }
 }
