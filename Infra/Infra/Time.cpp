598bb738804f12ea343d8d0ea5312fb398e866b0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-08 07:08:38 +0800

    Infra

--- Infra/Infra/Time.cpp
+++ Infra/Infra/Time.cpp
@@ -202,10 +202,7 @@ Int Time_OffsetUtcGet(Int o)
     return a;
 }
 
-Int Time_OffsetUtcSet(Int o, Int value)
-{
-    return true;
-}
+FieldDefaultSet(Time, OffsetUtc)
 
 Int Time_LocalTimeGet(Int o)
 {
@@ -218,10 +215,7 @@ Int Time_LocalTimeGet(Int o)
     return a;
 }
 
-Int Time_LocalTimeSet(Int o, Int value)
-{
-    return true;
-}
+FieldDefaultSet(Time, LocalTime)
 
 Int Time_Current(Int o)
 {
@@ -268,10 +262,7 @@ Int Time_YearDayGet(Int o)
     return a;
 }
 
-Int Time_YearDaySet(Int o, Int value)
-{
-    return true;
-}
+FieldDefaultSet(Time, YearDay)
 
 Int Time_WeekDayGet(Int o)
 {
@@ -286,10 +277,7 @@ Int Time_WeekDayGet(Int o)
     return a;
 }
 
-Int Time_WeekDaySet(Int o, Int value)
-{
-    return true;
-}
+FieldDefaultSet(Time, WeekDay)
 
 Int Time_YearDayCountGet(Int o)
 {
@@ -304,379 +292,170 @@ Int Time_YearDayCountGet(Int o)
     return a;
 }
 
-Int Time_YearDayCountSet(Int o, Int value)
-{
-    return true;
-}
+FieldDefaultSet(Time, YearDayCount)
 
 Int Time_MonthDayCountGet(Int o)
 {
     Time* m;
-
     m = CP(o);
-
-
-
     QDate date;
-
     date = m->Intern->date();
-
-
-
     int u;
-
     u = date.daysInMonth();
-
-
-
     Int a;
-
     a = u;
-
-
     return a;
 }
 
-Int Time_MonthDayCountSet(Int o, Int value)
-{
-    return true;
-}
+FieldDefaultSet(Time, MonthDayCount)
 
 Int Time_AddYear(Int o, Int offset)
 {
     Time* m;
-
     m = CP(o);
-
-
-
     int ua;
-
     ua = offset;
-
-
-
-
     QDateTime u;
-
     u = m->Intern->addYears(ua);
-
-
-
-
     (*(m->Intern)) = u;
-
-
-
-
     return true;
 }
 
 Int Time_AddMonth(Int o, Int offset)
 {
     Time* m;
-
     m = CP(o);
-
-
-
-
     int ua;
-
     ua = offset;
-
-
-
-
     QDateTime u;
-
     u = m->Intern->addMonths(ua);
-
-
-
-
     (*(m->Intern)) = u;
-
-
-
-
     return true;
 }
 
 Int Time_AddDay(Int o, Int offset)
 {
     Time* m;
-
     m = CP(o);
-
-
-
-
     qint64 ua;
-
     ua = offset;
-
-
-
-
     QDateTime u;
-
     u = m->Intern->addDays(ua);
-
-
-
-
     (*(m->Intern)) = u;
-
-
-
-
     return true;
 }
 
 Int Time_AddHour(Int o, Int offset)
 {
     qint64 u;
-
     u = offset;
-
     u = u * 60 * 60 * 1000;
-
-
-
     Int oa;
-
     oa = u;
-
-
     return Time_AddMillisecond(o, oa);
 }
 
 Int Time_AddMinute(Int o, Int offset)
 {
     qint64 u;
-
     u = offset;
-
     u = u * 60 * 1000;
-
-
-
     Int oa;
-
     oa = u;
-
-
     return Time_AddMillisecond(o, oa);
 }
 
 Int Time_AddSecond(Int o, Int offset)
 {
     qint64 u;
-
     u = offset;
-
     u = u * 1000;
-
-
-
     Int oa;
-
     oa = u;
-
-
     return Time_AddMillisecond(o, oa);
 }
 
 Int Time_AddMillisecond(Int o, Int offset)
 {
     Time* m;
-
     m = CP(o);
-
-
-
     qint64 offsetU;
-
     offsetU = offset;
-
-
-
     QDateTime u;
-
     u = m->Intern->addMSecs(offsetU);
-
-
-
-
     (*(m->Intern)) = u;
-
-
-
-
     return true;
 }
 
 Int Time_MillisecondTo(Int o, Int other)
 {
     Time* m;
-
     m = CP(o);
-
-
-
     Time* ma;
-
     ma = CP(other);
-
-
-
-
     qint64 u;
-
     u = m->Intern->msecsTo(*(ma->Intern));
-
-
-
     Int a;
-
     a = u;
-
-
     return a;
 }
 
 Int Time_DayTo(Int o, Int other)
 {
     Time* m;
-
     m = CP(o);
-
-
-
     Time* ma;
-
     ma = CP(other);
-
-
-
-
     QDate da;
-
     da = m->Intern->date();
-
-
     QDate db;
-
     db = ma->Intern->date();
-
-
-
-
     qint64 u;
-
     u = da.daysTo(db);
-
-
-
-
     Int a;
-
     a = u;
-
-
     return a;
 }
 
 Int Time_LeapYear(Int year)
 {
     int yearU;
-
     yearU = year;
-
-
-
-
     bool bu;
-
     bu = QDate::isLeapYear(yearU);
-
-
-
-
     Bool a;
-
     a = bu;
-
-
     return a;
 }
 
 Int Time_ValidDate(Int year, Int month, Int day)
 {
     int yearU;
-
     int monthU;
-
     int dayU;
-
-
     yearU = year;
-
     monthU = month;
-
     dayU = day;
-
-
-
-
     bool bu;
-
     bu = QDate::isValid(yearU, monthU, dayU);
-
-
-
     Bool a;
-
     a = bu;
-
-
-
     return a;
 }
 
 Int Time_ValidTime(Int hour, Int minute, Int second, Int millisecond)
 {
     int hourU;
-
     int minuteU;
-
     int secondU;
-
     int millisecondU;
-
-
     hourU = hour;
-
     minuteU = minute;
-
     secondU = second;
-
     millisecondU = millisecond;
-
-
-
-
     bool bu;
-
     bu = QTime::isValid(hourU, minuteU, secondU, millisecondU);
-
-
-
     Bool a;
-
     a = bu;
-
-
     return a;
 }
