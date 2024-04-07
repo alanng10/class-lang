cdb1f5201b7b1f6a585c7655bc0024918a9a96c8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-07 19:19:28 +0800

    Tool

--- Tool/Z.Infra.Class/String.cs
+++ Tool/Z.Infra.Class/String.cs
@@ -2,22 +2,17 @@ namespace Z.Infra.Class;
 
 public class String
 {
-    public static int G_Count(string o)
+    public int G_Count(string o)
     {
         return o.Length;
     }
 
-    public static int C_Char(string o, int index)
+    public int C_Char(string o, int index)
     {
-        if (index < 0 | !(index < o.Length))
-        {
-            return -1;
-        }
-
         return o[index];
     }
 
-    public static string C_SubString(string o, Range range)
+    public string C_SubString(string o, Range range)
     {
         return o.Substring(range.Index, range.Count);
     }
