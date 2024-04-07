bb0ce0409a17ef32028e1367f26bfda88f8bbadb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-08 06:38:47 +0800

    Infra

--- Infra/Infra/TransformIntern.hpp
+++ Infra/Infra/TransformIntern.hpp
@@ -1,20 +1,13 @@
 #pragma once
 
-
 #include <QtGlobal>
-
-
 
 class TransformIntern
 {
 public:
-
     qreal* Value() { return &(m_matrix[0][0]); }
 
-
-
 private:
-
     qreal m_matrix[3][3];
 
     mutable uint m_type : 5;
