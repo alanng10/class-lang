cc7beaff0b3dd897a44534361b9c15c55b6e1759
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-08 07:17:32 +0800

    Infra

--- Infra/Infra/PostIntern.cpp
+++ Infra/Infra/PostIntern.cpp
@@ -1,26 +1,13 @@
 #include "PostIntern.hpp"
 
-
-
-
-
-
 Bool PostIntern::Init()
 {
     return true;
 }
 
-
-
-
-
 void PostIntern::customEvent(QEvent *event)
 {
     Int post;
-
     post = this->Post;
-
-
-
     Post_ExecuteHandle(post);
 }
--- Infra/Infra/PostIntern.hpp
+++ Infra/Infra/PostIntern.hpp
@@ -1,36 +1,18 @@
 #pragma once
 
-
-
-
-
 #include <QObject>
 #include <QEvent>
 
-
-
 #include "Probate.hpp"
-
-
-
-
-
 
 class PostIntern : public QObject
 {
     Q_OBJECT
 
-
 public:
-
     Bool Init();
-
-
     Int Post;
 
-
 protected:
-
     void customEvent(QEvent* event) override;
 };
-
