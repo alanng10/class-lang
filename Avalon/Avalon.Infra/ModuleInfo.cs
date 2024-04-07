6ed3a2e79d8a4fd0c877de44d5d9ab5a726db4cc
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-07 15:49:01 +0800

    Avalon

--- Avalon/Avalon.Infra/ModuleInfo.cs
+++ Avalon/Avalon.Infra/ModuleInfo.cs
@@ -2,8 +2,8 @@ namespace Avalon.Infra;
 
 public class ModuleInfo : Any
 {
-    public virtual string RefString()
+    public virtual string RefString([CallerFilePath] string filePath = null)
     {
-        return null;
+        return "";
     }
 }
