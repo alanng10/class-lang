fe48eb5349e8d435a634aa2dd17a573dac760c69
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-08 07:12:39 +0800

    Infra

--- Infra/Infra/Share.hpp
+++ Infra/Infra/Share.hpp
@@ -1,32 +1,13 @@
 #pragma once
 
-
-
-
-
 #include <QThreadStorage>
 
-
-
-
-
 #include "Probate.hpp"
-
-
-
-
 
 struct Share
 {
     Int Stat;
-
-
     Int ThreadStorage;
 };
-
-
-
-
-
 
 #define CP(a) ((Share*)(a))
