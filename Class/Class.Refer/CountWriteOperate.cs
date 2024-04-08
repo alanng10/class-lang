23fb17d5f3bd0624b6eff1576a312b3e39fb5ecd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-08 13:12:15 +0800

    Class

--- Class/Class.Refer/CountWriteOperate.cs
+++ Class/Class.Refer/CountWriteOperate.cs
@@ -2,15 +2,7 @@ namespace Class.Refer;
 
 public class CountWriteOperate : WriteOperate
 {
-    public override bool Init()
-    {
-        base.Init();
-        this.InfraInfra = InfraInfra.This;
-        return true;
-    }
-
     public virtual Write Write { get; set; }
-    protected virtual InfraInfra InfraInfra { get; set; }
 
     public override bool ExecuteByte(int value)
     {
