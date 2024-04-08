fe8d9137e338e72ca9e48f1229b86c078c40f5e6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-08 23:55:46 +0800

    Class

--- Class/Class.Token/CountCreateOperate.cs
+++ Class/Class.Token/CountCreateOperate.cs
@@ -7,14 +7,8 @@ public class CountCreateOperate : CreateOperate
     public override bool ExecuteToken()
     {
         int index;
-        int codeIndex;
         index = this.Create.TokenIndex;
-        codeIndex = this.Create.CodeTokenIndex;
-
         index = index + 1;
-        codeIndex = codeIndex + 1;
-
-        this.Create.CodeTokenIndex = codeIndex;
         this.Create.TokenIndex = index;
         return true;
     }
@@ -22,14 +16,8 @@ public class CountCreateOperate : CreateOperate
     public override bool ExecuteComment()
     {
         int index;
-        int codeIndex;
         index = this.Create.CommentIndex;
-        codeIndex = this.Create.CodeCommentIndex;
-
         index = index + 1;
-        codeIndex = codeIndex + 1;
-
-        this.Create.CodeCommentIndex = codeIndex;
         this.Create.CommentIndex = index;
         return true;
     }
