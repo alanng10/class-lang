07f86ce39e7367be04d4f3b7f8658b2e4410708f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-09 16:08:47 +0800

    Script

--- Script/Avalon/CleanModule.cmd
+++ Script/Avalon/CleanModule.cmd
@@ -4,4 +4,4 @@ echo Clean Module
 set AvalonModuleOutFold=.\Out\net6.0
 del /F /Q %AvalonModuleOutFold%\Avalon.* 2>NUL
 del /F /Q %AvalonModuleOutFold%\MakeProject.* 2>NUL
-rmdir /S /Q %AvalonModuleOutFold%\Avalon.Intern.- 2>NUL
+rmdir /S /Q %AvalonModuleOutFold%\Avalon.Intern.data 2>NUL
