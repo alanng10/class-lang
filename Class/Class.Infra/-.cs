7f4245c52667025b1a21c880d011a737c794827c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-09 17:21:05 +0800

    Class

--- Class/Class.Infra/-.cs
+++ Class/Class.Infra/-.cs
@@ -1,8 +1,9 @@
 global using Any = Avalon.Infra.Any;
 global using InfraRange = Avalon.Infra.Range;
 global using Data = Avalon.Infra.Data;
+global using Compare = Avalon.Infra.Compare;
+global using StringCompare = Avalon.Infra.StringCompare;
 global using InfraInfra = Avalon.Infra.Infra;
-global using List = Avalon.List.List;
 global using Array = Avalon.List.Array;
 global using Table = Avalon.List.Table;
 global using Text = Avalon.Text.Text;
---
+++ Class/Class.Infra/ModuleRefCompare.cs
@@ -0,0 +1,38 @@
+namespace Class.Infra;
+
+public class ModuleRefCompare : Compare
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.StringCompare = new StringCompare();
+        this.StringCompare.Init();
+        return true;
+    }
+
+    protected virtual StringCompare StringCompare { get; set; }
+
+    public override int Execute(object left, object right)
+    {
+        if (left == null | right == null)
+        {
+            return 0;
+        }
+
+        ModuleRef leftA;
+        ModuleRef rightA;
+        leftA = (ModuleRef)left;
+        rightA = (ModuleRef)right;
+
+
+        int a;
+        a = this.StringCompare.Execute(leftA.Name, rightA.Name);
+
+        if (!(a == 0))
+        {
+            return a;
+        }
+
+        return leftA.Ver.CompareTo(rightA.Ver);
+    }
+}
