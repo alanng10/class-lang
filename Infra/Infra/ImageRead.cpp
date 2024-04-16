1c96d42c5c2de694bc4fee086c5f7a42c9d00b3a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 15:13:27 +0800

    Infra

--- Infra/Infra/ImageRead.cpp
+++ Infra/Infra/ImageRead.cpp
@@ -5,38 +5,19 @@ CppClassNew(ImageRead)
 Int ImageRead_Init(Int o)
 {
     ImageRead* m;
-
     m = CP(o);
-
-
-
-
     QImageReader* u;
-
-
-    u = new QImageReader();
-
-
-
+    u = new QImageReader;
     m->Intern = u;
-
-
-
     return true;
 }
 
 Int ImageRead_Final(Int o)
 {
     ImageRead* m;
-
     m = CP(o);
 
-
-
     delete m->Intern;
-
-
-
     return true;
 }
 
@@ -46,100 +27,43 @@ CppField(ImageRead, Image)
 Int ImageRead_Execute(Int o)
 {
     ImageRead* m;
-
     m = CP(o);
-
-
-
-
     Int stream;
-
     stream = m->Stream;
-
-
-
-
     Int image;
-
     image = m->Image;
 
-
-
-
     Int ua;
-
     ua = Stream_Intern(stream);
-
-
-
     QIODevice* ub;
-
     ub = (QIODevice*)ua;
-
-
-
     m->Intern->setDevice(ub);
-
-
-
-
 
     QImage oo;
 
-
-
     bool bu;
-
-
     bu = m->Intern->read(&oo);
 
-
+    m->Intern->setDevice(null);
 
     Bool bo;
-
     bo = bu;
-
-
     if (!bo)
     {
         return false;
     }
 
-
-
-
-
-    m->Intern->setDevice(null);
-
-
-
-
-
     QImage::Format format;
-
     format = QImage::Format_ARGB32;
-
-
 
     if (!(oo.format() == format))
     {
         oo.convertTo(format);
     }
 
-
-
-
     Int oa;
-
     oa = CastInt(&oo);
 
-
-
     Image_SetReadIntern(image, oa);
-
-
-
-
-
     return true;
 }
