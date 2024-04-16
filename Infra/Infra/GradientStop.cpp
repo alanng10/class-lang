8d380caf9ad14484798e294ccd8ef460f9a61022
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 13:48:08 +0800

    Infra

--- Infra/Infra/GradientStop.cpp
+++ Infra/Infra/GradientStop.cpp
@@ -5,80 +5,35 @@ CppClassNew(GradientStop)
 Int GradientStop_Init(Int o)
 {
     GradientStop* m;
-
     m = CP(o);
-
-
-
     Int count;
-
     count = m->Count;
-
-
-
     Int pointCount;
-
     pointCount = sizeof(GradientStopPoint);
-
-
-
     Int byteCount;
-
     byteCount = count * pointCount;
 
-
-
     Int dataValue;
-
     dataValue = New(byteCount);
 
-
-
-
     m->Data = Data_New();
-
-
     Data_Init(m->Data);
-
-
     Data_CountSet(m->Data, byteCount);
-
-
     Data_ValueSet(m->Data, dataValue);
-
-
-
-
     return true;
 }
 
 Int GradientStop_Final(Int o)
 {
     GradientStop* m;
-
     m = CP(o);
-
-
-
-
     Int dataValue;
-
     dataValue = Data_ValueGet(m->Data);
 
-
-
     Data_Final(m->Data);
-
-
     Data_Delete(m->Data);
 
-
-
     Delete(dataValue);
-
-
-
-
     return true;
 }
 
@@ -92,25 +47,12 @@ Int GradientStop_PointGet(Int o, Int index, Int pos, Int color)
     GradientStopPoint* oa;
     oa = (GradientStopPoint*)oo;
 
-
-
     Int* posU;
-
     Int* colorU;
-
-
     posU = (Int*)pos;
-
     colorU = (Int*)color;
-
-
-
     *posU = oa->Pos;
-
     *colorU = oa->Color;
-
-
-
     return true;
 }
 
@@ -122,38 +64,19 @@ Int GradientStop_PointSet(Int o, Int index, Int pos, Int color)
     GradientStopPoint* oa;
     oa = (GradientStopPoint*)oo;
 
-
-
     oa->Pos = pos;
-
-
     oa->Color = color;
-
-
-
-
     return true;
 }
 
 Int GradientStop_PointAddress(Int o, Int index)
 {
     GradientStop* m;
-
     m = CP(o);
-
-
-
-
     Int dataValue;
-
     dataValue = Data_ValueGet(m->Data);
-
-
-
     GradientStopPoint* oo;
-
     oo = (GradientStopPoint*)(dataValue);
-
 
     GradientStopPoint* oa;
     oa = oo + index;
