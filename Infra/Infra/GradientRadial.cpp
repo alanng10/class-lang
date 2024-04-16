619fba9f805cfe0d513951c14145c83dc23a7b20
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 13:45:02 +0800

    Infra

--- Infra/Infra/GradientRadial.cpp
+++ Infra/Infra/GradientRadial.cpp
@@ -5,113 +5,44 @@ CppClassNew(GradientRadial)
 Int GradientRadial_Init(Int o)
 {
     GradientRadial* m;
-
     m = CP(o);
-
-
-
-
     Int centerPos;
-
     centerPos = m->CenterPos;
-
-
-
     Int centerRadius;
-
     centerRadius = m->CenterRadius;
-
-
-
     Int focusPos;
-
     focusPos = m->FocusPos;
-
-
-
     Int focusRadius;
-
     focusRadius = m->FocusRadius;
 
-
-
-
-
-
     Int centerLeft;
-
     Int centerUp;
-
-
     centerLeft = Pos_LeftGet(centerPos);
-
     centerUp = Pos_UpGet(centerPos);
-
-
-
-
     Int focusLeft;
-
     Int focusUp;
-
-
     focusLeft = Pos_LeftGet(focusPos);
-
     focusUp = Pos_UpGet(focusPos);
 
-
-
-
     InternQReal(centerLeft)
-
     InternQReal(centerUp)
-
-
     InternQReal(focusLeft)
-
     InternQReal(focusUp)
-
-
-
     InternQReal(centerRadius)
-
     InternQReal(focusRadius)
 
-
-
-
-
-
     QRadialGradient* radialGradient;
-
-
     radialGradient = new QRadialGradient(centerLeftU, centerUpU, centerRadiusU, focusLeftU, focusUpU, focusRadiusU);
-
-
-
-
     m->Intern = radialGradient;
-
-
-
-
     return true;
 }
 
 Int GradientRadial_Final(Int o)
 {
     GradientRadial* m;
-
     m = CP(o);
 
-
-
-
     delete m->Intern;
-
-
-
-
     return true;
 }
 
@@ -123,16 +54,8 @@ CppField(GradientRadial, FocusRadius)
 Int GradientRadial_Intern(Int o)
 {
     GradientRadial* m;
-
     m = CP(o);
-
-
-
-    Int u;
-
-    u = CastInt(m->Intern);
-
-
-
-    return u;
+    Int a;
+    a = CastInt(m->Intern);
+    return a;
 }
