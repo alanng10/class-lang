2f17e190b4142d2b059010f39f4893446be3ddd7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 23:22:19 +0800

    Infra

--- Infra/Infra/Network.cpp
+++ Infra/Infra/Network.cpp
@@ -158,247 +158,90 @@ Int Network_Close(Int o)
     return true;
 }
 
-
-
-
-
-
 Int Network_ServerSet(Int o, Int socket)
 {
     Network* m;
-
     m = CP(o);
-
-
-
-
-
-
     QTcpSocket* uu;
-
     uu = (QTcpSocket*)socket;
-
-
-
-
     QIODevice* ua;
-
     ua = uu;
-
-
-
-
     Int oa;
-
     oa = CastInt(ua);
-
-
-
-
     m->OpenSocket = oa;
 
-
-
-
-
     NetworkHandle* handle;
-
     handle = new NetworkHandle;
-
-
     handle->Network = o;
-
-
     handle->Init();
-
-
-
-
     m->Handle = handle;
 
-
-
-
-
-
     Int stream;
-
-
     stream = m->Stream;
-
-
-
-
-
     Int streamValue;
-
     streamValue = m->OpenSocket;
 
-
-
-
-
     Int share;
-
-
     share = Infra_Share();
-
-
-
     Int stat;
-
     stat = Share_Stat(share);
-
-
-
-
     Int kind;
-
     kind = Stat_StreamKindNetwork(stat);
 
-
-
-
     Bool canRead;
-
     canRead = true;
-
-
-
     Bool canWrite;
-
     canWrite = true;
 
-
-
-
     Stream_KindSet(stream, kind);
-
-
     Stream_ValueSet(stream, streamValue);
-
-
-
     Stream_CanReadSet(stream, canRead);
-
-
     Stream_CanWriteSet(stream, canWrite);
-
-
-
-
     return true;
 }
-
-
-
-
-
 
 Int Network_Abort(Int o)
 {
     Network* m;
-
     m = CP(o);
-
-
-
     Int socket;
-
     socket = m->OpenSocket;
-
-
-
     QIODevice* uu;
-
     uu = (QIODevice*)socket;
-
-
-
     QTcpSocket* u;
-
     u = (QTcpSocket*)uu;
 
-
-
     u->abort();
-
-
-
-
     return true;
 }
-
-
-
-
-
 
 Int Network_GetOpenSocket(Int o)
 {
     Network* m;
-
     m = CP(o);
-
-
-
     return m->OpenSocket;
 }
-
-
-
-
 
 Int Network_ReadyCountGet(Int o)
 {
     Network* m;
-
     m = CP(o);
-
-
-
     Int socket;
-
     socket = m->OpenSocket;
-
-
-
     QIODevice* uu;
-
     uu = (QIODevice*)socket;
-
-
-
     QTcpSocket* u;
-
     u = (QTcpSocket*)uu;
 
-
-
     qint64 ua;
-
     ua = u->bytesAvailable();
 
-
-
     Int a;
-
     a = ua;
-
-
-
     return a;
 }
 
-
-
-
-
-Int Network_ReadyCountSet(Int o, Int value)
-{
-    return true;
-}
-
+FieldDefaultSet(Network, ReadyCount)
 CppField(Network, CaseChangedState)
 CppField(Network, ErrorState)
 CppField(Network, ReadyReadState)
@@ -406,135 +249,60 @@ CppField(Network, ReadyReadState)
 Int Network_CaseChanged(Int o)
 {
     Network* m;
-
     m = CP(o);
-
-
-
-
     Int state;
-
     state = m->CaseChangedState;
-
-
-
     Int aa;
-
     aa = State_MaideGet(state);
-
-
-
     Int arg;
-
     arg = State_ArgGet(state);
 
-
-
-
     Network_CaseChanged_Maide maide;
-
     maide = (Network_CaseChanged_Maide)aa;
-
-
-
     if (!(maide == null))
     {
         maide(o, arg);
     }
-
-
-
-
     return true;
 }
 
 Int Network_Error(Int o)
 {
     Network* m;
-
     m = CP(o);
-
-
-
-
     Int state;
-
     state = m->ErrorState;
-
-
-
     Int aa;
-
     aa = State_MaideGet(state);
-
-
-
     Int arg;
-
     arg = State_ArgGet(state);
 
-
-
-
     Network_Error_Maide maide;
-
     maide = (Network_Error_Maide)aa;
-
-
-
     if (!(maide == null))
     {
         maide(o, arg);
     }
-
-
-
-
     return true;
 }
 
 Int Network_ReadyRead(Int o)
 {
     Network* m;
-
     m = CP(o);
-
-
-
-
     Int state;
-
     state = m->ReadyReadState;
-
-
-
     Int aa;
-
     aa = State_MaideGet(state);
-
-
-
     Int arg;
-
     arg = State_ArgGet(state);
 
-
-
-
     Network_ReadyRead_Maide maide;
-
     maide = (Network_ReadyRead_Maide)aa;
-
-
-
     if (!(maide == null))
     {
         maide(o, arg);
     }
-
-
-
-
     return true;
 }
 
