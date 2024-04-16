d0832d4fcd26640e586244958e96249540838d11
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 15:16:15 +0800

    Infra

--- Infra/Infra/ImageWrite.cpp
+++ Infra/Infra/ImageWrite.cpp
@@ -12,43 +12,22 @@ const char* ImageWrite_Var_Format[] =
 Int ImageWrite_Init(Int o)
 {
     ImageWrite* m;
-
     m = CP(o);
-
-
-
 
     m->Quality = 100;
 
-
-
-
     QImageWriter* u;
-
-
     u = new QImageWriter;
-
-
-
     m->Intern = u;
-
-
-
     return true;
 }
 
 Int ImageWrite_Final(Int o)
 {
     ImageWrite* m;
-
     m = CP(o);
 
-
-
     delete m->Intern;
-
-
-
     return true;
 }
 
@@ -60,108 +39,47 @@ CppField(ImageWrite, Quality)
 Int ImageWrite_Execute(Int o)
 {
     ImageWrite* m;
-
     m = CP(o);
 
-
-
-
     Int format;
-
     format = m->Format;
-
-
-
     if (format == null)
     {
         return false;
     }
 
-
-
     Int streamU;
-
     streamU = Stream_Intern(m->Stream);
-
-
-
     Int imageU;
-
     imageU = Image_Intern(m->Image);
-
-
-
-
     Int formatU;
-
     formatU = format - 1;
-
-
-
     int qualityU;
-
     qualityU = m->Quality;
 
-
-
-
-
     QIODevice* oa;
-
     oa = (QIODevice*)streamU;
 
-
-
-
-
     const char* ooa;
-
     ooa = ImageWrite_Var_Format[formatU];
 
-
-
     QByteArray ob;
-
     ob = QByteArray(ooa);
 
-
-
-
     QImage* oc;
-
     oc = (QImage*)imageU;
 
-
-
-
-
-
-
     m->Intern->setDevice(oa);
-
-
-
     m->Intern->setFormat(ob);
-
-
-
     m->Intern->setQuality(qualityU);
 
-
-
-
     bool bu;
-
     bu = m->Intern->write(*oc);
 
-
-
+    m->Intern->setDevice(null);
 
     Bool a;
-
     a = bu;
-
-
     return a;
 }
 
