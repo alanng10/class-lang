4724366ce657c86a7a482a1c388b60c35beea1e1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 23:43:27 +0800

    Infra

--- Infra/Infra/Pen.cpp
+++ Infra/Infra/Pen.cpp
@@ -5,136 +5,54 @@ CppClassNew(Pen)
 Int Pen_Init(Int o)
 {
     Pen* m;
-
     m = CP(o);
-
-
-
-
     Int kind;
-
     kind = m->Kind;
-
-
-
-
     Int width;
-
     width = m->Width;
-
-
-
-
     Int brush;
-
     brush = m->Brush;
-
-
-
-
     Int cap;
-
     cap = m->Cap;
-
-
-
-
     Int join;
-
     join = m->Join;
-
-
-
-
-
 
     if ((kind == null) | (brush == null) | (cap == null) | (join == null))
     {
         return true;
     }
 
-
-
-
-
     Qt::PenStyle styleU;
-
     styleU = (Qt::PenStyle)kind;
 
-
-
-
     int widthU;
-
     widthU = (int)width;
-
-
-
     qreal widthUu;
-
     widthUu = widthU;
 
-
-
-
     Int ua;
-
     ua = Brush_Intern(brush);
-
-
-
     QBrush* brushU;
-
     brushU = (QBrush*)ua;
 
-
-
-
-
     Qt::PenCapStyle capStyleU;
-
     capStyleU = (Qt::PenCapStyle)(cap - 1);
 
-
-
-
     Qt::PenJoinStyle joinStyleU;
-
     joinStyleU = (Qt::PenJoinStyle)(join - 1);
 
-
-
-
-
     QPen* u;
-
-
     u = new QPen(*brushU, widthUu, styleU, capStyleU, joinStyleU);
-
-
-
     m->Intern = u;
-
-
-
-
-
     return true;
 }
 
 Int Pen_Final(Int o)
 {
     Pen* m;
-
     m = CP(o);
 
-
-
     delete m->Intern;
-
-
-
-
     return true;
 }
 
@@ -147,16 +65,8 @@ CppField(Pen, Join)
 Int Pen_Intern(Int o)
 {
     Pen* m;
-
     m = CP(o);
-
-
-
-    Int u;
-
-    u = CastInt(m->Intern);
-
-
-
-    return u;
+    Int a;
+    a = CastInt(m->Intern);
+    return a;
 }
