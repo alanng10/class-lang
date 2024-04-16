d5568c3865a475ba0f888c9602ea1240b5f90401
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 23:45:41 +0800

    Infra

--- Infra/Infra/Phore.cpp
+++ Infra/Infra/Phore.cpp
@@ -1,60 +1,25 @@
 #include "Phore.hpp"
 
-
-
-
 CppClassNew(Phore)
-
-
-
-
-
 
 Int Phore_Init(Int o)
 {
     Phore* m;
-
     m = CP(o);
-
-
-
-
     Int initCount;
-
     initCount = m->InitCount;
-
-
-
-
     int ua;
-
     ua = initCount;
-
-
-
-
     m->Intern = new QSemaphore(ua);
     return true;
 }
 
-
-
-
-
-
 Int Phore_Final(Int o)
 {
     Phore* m;
-
     m = CP(o);
 
-
-
     delete m->Intern;
-
-
-
-
     return true;
 }
 
@@ -63,68 +28,28 @@ CppField(Phore, InitCount)
 Int Phore_CountGet(Int o)
 {
     Phore* m;
-
     m = CP(o);
-
-
-
     int u;
-
     u = m->Intern->available();
-
-
-
     Int a;
-
     a = u;
-
-
     return a;
 }
 
-
-
-
-
-Int Phore_CountSet(Int o, Int value)
-{
-    return true;
-}
-
-
-
-
+FieldDefaultSet(Phore, Count)
 
 Int Phore_Acquire(Int o)
 {
     Phore* m;
-
     m = CP(o);
-
-
-
     m->Intern->acquire();
-
-
-
     return true;
 }
-
-
-
-
 
 Int Phore_Release(Int o)
 {
     Phore* m;
-
     m = CP(o);
-
-
-
     m->Intern->release();
-
-
-
     return true;
 }
