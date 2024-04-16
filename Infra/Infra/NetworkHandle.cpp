7a36698415d0d7d2c856999b8520fdd972e12515
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 23:31:34 +0800

    Infra

--- Infra/Infra/NetworkHandle.cpp
+++ Infra/Infra/NetworkHandle.cpp
@@ -1,102 +1,43 @@
 #include "NetworkHandle.hpp"
 
-
-
-
-
-
 Bool NetworkHandle::Init()
 {
     Int network;
-
     network = this->Network;
-
-
     Int socket;
-
     socket = Network_GetOpenSocket(network);
-
-
-
     QIODevice* uu;
-
     uu = (QIODevice*)socket;
-
-
-
     QTcpSocket* u;
-
     u = (QTcpSocket*)uu;
 
-
-
-
     connect(u, &QTcpSocket::stateChanged, this, &NetworkHandle::CaseChangedHandle);
-
-
-
     connect(u, &QTcpSocket::errorOccurred, this, &NetworkHandle::ErrorHandle);
-
-
-
     connect(uu, &QIODevice::readyRead, this, &NetworkHandle::ReadyReadHandle);
-
-
-
-
-
     return true;
 }
-
-
-
-
 
 void NetworkHandle::CaseChangedHandle(QAbstractSocket::SocketState socketState)
 {
     Int network;
-
     network = this->Network;
-
-
-
-
     if (socketState == QAbstractSocket::ConnectedState)
     {
         Network_ConnectedOpen(network);
     }
-
-
-
-
     Network_CaseChanged(network);
 }
-
-
-
 
 void NetworkHandle::ErrorHandle(QAbstractSocket::SocketError socketError)
 {
     Int network;
-
     network = this->Network;
-
-
-
-
     Network_Error(network);
 }
-
-
-
 
 void NetworkHandle::ReadyReadHandle()
 {
     Int network;
-
     network = this->Network;
-
-
-
     Network_ReadyRead(network);
 }
