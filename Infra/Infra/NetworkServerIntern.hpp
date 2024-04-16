6618765cc6ca560bc4883bfea23610e8cd7b82c1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 23:36:13 +0800

    Infra

--- Infra/Infra/NetworkServerIntern.hpp
+++ Infra/Infra/NetworkServerIntern.hpp
@@ -1,32 +1,16 @@
 #pragma once
 
-
-
-
-
 #include <QTcpServer>
 
-
-
 #include "Probate.hpp"
-
-
-
-
-
 
 class NetworkServerIntern : public QTcpServer
 {
     Q_OBJECT
 
-
 public:
-
     Bool Init();
-
-
     Int NetworkServer;
-
 
 private slots:
 
