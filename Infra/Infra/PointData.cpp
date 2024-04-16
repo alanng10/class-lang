2e7f54f05a09219cc2c5e6820deafbc5bc544449
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-16 23:47:39 +0800

    Infra

--- Infra/Infra/PointData.cpp
+++ Infra/Infra/PointData.cpp
@@ -1,102 +1,44 @@
 #include "PointData.hpp"
 
-
-
-
-
 Int PointData_PointGet(Int address, Int result)
 {
     QPoint* u;
-
     u = (QPoint*)(address);
 
-
-
-
     QPoint point;
-
     point = *u;
 
-
-
     SInt leftU;
-
     SInt upU;
-
     leftU = point.x();
-
     upU = point.y();
 
-
-
     Int left;
-
     Int up;
-
-
     left = leftU;
-
     up = upU;
 
-
-
     Pos_LeftSet(result, left);
-
-
     Pos_UpSet(result, up);
-
-
-
     return true;
 }
-
-
-
-
-
 
 Int PointData_PointSet(Int address, Int pos)
 {
     Int left;
-
     Int up;
-
-
     left = Pos_LeftGet(pos);
-
     up = Pos_UpGet(pos);
 
-
-
-
     int leftU;
-
     int upU;
-
-
     leftU = left;
-
     upU = up;
-
-
-
 
     QPoint point(leftU, upU);
 
-
-
-
     QPoint* u;
-
     u = (QPoint*)(address);
-
-
-
-
     *u = point;
-
-
-
-
     return true;
 }
--- Infra/Infra/PointData.hpp
+++ Infra/Infra/PointData.hpp
@@ -1,12 +1,5 @@
 #pragma once
 
-
-
-
-
 #include <QPoint>
-
-
-
 
 #include "Probate.hpp"
