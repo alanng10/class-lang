259a6df08530bb76d578859d832d10b4098a0aec
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-17 20:04:25 +0800

    Infra

--- Infra/Infra/ProcessIntern.hpp
+++ Infra/Infra/ProcessIntern.hpp
@@ -1,38 +1,19 @@
 #pragma once
 
-
-
-
-
 #include <QProcess>
 
-
-
 #include "Probate.hpp"
-
-
-
-
-
 
 class ProcessIntern : public QProcess
 {
     Q_OBJECT
 
-
 public:
-
     Bool Init();
-
-
     Int Process;
-
 
 private slots:
 
     void StartedHandle();
-
-
     void FinshedHandle(int exitCode, QProcess::ExitStatus exitStatus);
 };
-
