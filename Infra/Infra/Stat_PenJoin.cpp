793c88e8e5021bc7eeec63ea09cd106a20ac1380
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-18 15:01:00 +0800

    Infra

--- Infra/Infra/Stat_PenJoin.cpp
+++ Infra/Infra/Stat_PenJoin.cpp
@@ -1,42 +1,23 @@
 #include "Stat.hpp"
 
-
-
-
 Int Stat_Var_PenJoinMiter = Qt::MiterJoin + 1;
-
 Int Stat_Var_PenJoinBevel = Qt::BevelJoin + 1;
-
 Int Stat_Var_PenJoinRound = Qt::RoundJoin + 1;
-
 Int Stat_Var_PenJoinSvgMiter = Qt::SvgMiterJoin + 1;
-
-
-
-
-
 
 Int Stat_PenJoinMiter(Int o)
 {
     return Stat_Var_PenJoinMiter;
 }
-
-
 Int Stat_PenJoinBevel(Int o)
 {
     return Stat_Var_PenJoinBevel;
 }
-
-
 Int Stat_PenJoinRound(Int o)
 {
     return Stat_Var_PenJoinRound;
 }
-
-
 Int Stat_PenJoinSvgMiter(Int o)
 {
     return Stat_Var_PenJoinSvgMiter;
 }
-
-
