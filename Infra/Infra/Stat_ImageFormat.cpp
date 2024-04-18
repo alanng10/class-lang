c3b426f6e382f6d86e13a9c577a0e6c569944e12
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-18 14:51:57 +0800

    Infra

--- Infra/Infra/Stat_ImageFormat.cpp
+++ Infra/Infra/Stat_ImageFormat.cpp
@@ -1,28 +1,15 @@
 #include "Stat.hpp"
 
 
-
-
-
-
-
-
-
 Int Stat_ImageFormatBmp(Int o)
 {
     return 1;
 }
-
-
 Int Stat_ImageFormatJpg(Int o)
 {
     return 2;
 }
-
-
 Int Stat_ImageFormatPng(Int o)
 {
     return 3;
 }
-
-
