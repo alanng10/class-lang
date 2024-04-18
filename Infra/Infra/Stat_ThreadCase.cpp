94fce33d47db01f1064d623b64d318a2f572669e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-18 15:14:38 +0800

    Infra

--- Infra/Infra/Stat_ThreadCase.cpp
+++ Infra/Infra/Stat_ThreadCase.cpp
@@ -1,40 +1,23 @@
 #include "Stat.hpp"
 
 
-
-
-
-
-
-
-
 Int Stat_ThreadCaseReady(Int o)
 {
     return 1;
 }
-
-
 Int Stat_ThreadCaseExecute(Int o)
 {
     return 2;
 }
-
-
 Int Stat_ThreadCasePause(Int o)
 {
     return 3;
 }
-
-
 Int Stat_ThreadCaseFinish(Int o)
 {
     return 4;
 }
-
-
 Int Stat_ThreadCaseTerminate(Int o)
 {
     return 5;
 }
-
-
