03b877dcc991170ea1d3d72d9b80be9e5a8e8e22
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-18 14:57:11 +0800

    Infra

--- Infra/Infra/Stat_NetworkStatus.cpp
+++ Infra/Infra/Stat_NetworkStatus.cpp
@@ -1,210 +1,128 @@
 #include "Stat.hpp"
 
-
-
-
 Int Stat_Var_NetworkStatusNoError = 0;
-
 Int Stat_Var_NetworkStatusUnknownSocketError = QAbstractSocket::UnknownSocketError + 2;
-
 Int Stat_Var_NetworkStatusConnectionRefusedError = QAbstractSocket::ConnectionRefusedError + 2;
-
 Int Stat_Var_NetworkStatusRemoteHostClosedError = QAbstractSocket::RemoteHostClosedError + 2;
-
 Int Stat_Var_NetworkStatusHostNotFoundError = QAbstractSocket::HostNotFoundError + 2;
-
 Int Stat_Var_NetworkStatusSocketAccessError = QAbstractSocket::SocketAccessError + 2;
-
 Int Stat_Var_NetworkStatusSocketResourceError = QAbstractSocket::SocketResourceError + 2;
-
 Int Stat_Var_NetworkStatusSocketTimeoutError = QAbstractSocket::SocketTimeoutError + 2;
-
 Int Stat_Var_NetworkStatusDatagramTooLargeError = QAbstractSocket::DatagramTooLargeError + 2;
-
 Int Stat_Var_NetworkStatusNetworkError = QAbstractSocket::NetworkError + 2;
-
 Int Stat_Var_NetworkStatusAddressInUseError = QAbstractSocket::AddressInUseError + 2;
-
 Int Stat_Var_NetworkStatusSocketAddressNotAvailableError = QAbstractSocket::SocketAddressNotAvailableError + 2;
-
 Int Stat_Var_NetworkStatusUnsupportedSocketOperationError = QAbstractSocket::UnsupportedSocketOperationError + 2;
-
 Int Stat_Var_NetworkStatusUnfinishedSocketOperationError = QAbstractSocket::UnfinishedSocketOperationError + 2;
-
 Int Stat_Var_NetworkStatusProxyAuthenticationRequiredError = QAbstractSocket::ProxyAuthenticationRequiredError + 2;
-
 Int Stat_Var_NetworkStatusSslHandshakeFailedError = QAbstractSocket::SslHandshakeFailedError + 2;
-
 Int Stat_Var_NetworkStatusProxyConnectionRefusedError = QAbstractSocket::ProxyConnectionRefusedError + 2;
-
 Int Stat_Var_NetworkStatusProxyConnectionClosedError = QAbstractSocket::ProxyConnectionClosedError + 2;
-
 Int Stat_Var_NetworkStatusProxyConnectionTimeoutError = QAbstractSocket::ProxyConnectionTimeoutError + 2;
-
 Int Stat_Var_NetworkStatusProxyNotFoundError = QAbstractSocket::ProxyNotFoundError + 2;
-
 Int Stat_Var_NetworkStatusProxyProtocolError = QAbstractSocket::ProxyProtocolError + 2;
-
 Int Stat_Var_NetworkStatusOperationError = QAbstractSocket::OperationError + 2;
-
 Int Stat_Var_NetworkStatusSslInternalError = QAbstractSocket::SslInternalError + 2;
-
 Int Stat_Var_NetworkStatusSslInvalidUserDataError = QAbstractSocket::SslInvalidUserDataError + 2;
-
 Int Stat_Var_NetworkStatusTemporaryError = QAbstractSocket::TemporaryError + 2;
-
-
-
-
-
 
 Int Stat_NetworkStatusNoError(Int o)
 {
     return Stat_Var_NetworkStatusNoError;
 }
-
-
 Int Stat_NetworkStatusUnknownSocketError(Int o)
 {
     return Stat_Var_NetworkStatusUnknownSocketError;
 }
-
-
 Int Stat_NetworkStatusConnectionRefusedError(Int o)
 {
     return Stat_Var_NetworkStatusConnectionRefusedError;
 }
-
-
 Int Stat_NetworkStatusRemoteHostClosedError(Int o)
 {
     return Stat_Var_NetworkStatusRemoteHostClosedError;
 }
-
-
 Int Stat_NetworkStatusHostNotFoundError(Int o)
 {
     return Stat_Var_NetworkStatusHostNotFoundError;
 }
-
-
 Int Stat_NetworkStatusSocketAccessError(Int o)
 {
     return Stat_Var_NetworkStatusSocketAccessError;
 }
-
-
 Int Stat_NetworkStatusSocketResourceError(Int o)
 {
     return Stat_Var_NetworkStatusSocketResourceError;
 }
-
-
 Int Stat_NetworkStatusSocketTimeoutError(Int o)
 {
     return Stat_Var_NetworkStatusSocketTimeoutError;
 }
-
-
 Int Stat_NetworkStatusDatagramTooLargeError(Int o)
 {
     return Stat_Var_NetworkStatusDatagramTooLargeError;
 }
-
-
 Int Stat_NetworkStatusNetworkError(Int o)
 {
     return Stat_Var_NetworkStatusNetworkError;
 }
-
-
 Int Stat_NetworkStatusAddressInUseError(Int o)
 {
     return Stat_Var_NetworkStatusAddressInUseError;
 }
-
-
 Int Stat_NetworkStatusSocketAddressNotAvailableError(Int o)
 {
     return Stat_Var_NetworkStatusSocketAddressNotAvailableError;
 }
-
-
 Int Stat_NetworkStatusUnsupportedSocketOperationError(Int o)
 {
     return Stat_Var_NetworkStatusUnsupportedSocketOperationError;
 }
-
-
 Int Stat_NetworkStatusUnfinishedSocketOperationError(Int o)
 {
     return Stat_Var_NetworkStatusUnfinishedSocketOperationError;
 }
-
-
 Int Stat_NetworkStatusProxyAuthenticationRequiredError(Int o)
 {
     return Stat_Var_NetworkStatusProxyAuthenticationRequiredError;
 }
-
-
 Int Stat_NetworkStatusSslHandshakeFailedError(Int o)
 {
     return Stat_Var_NetworkStatusSslHandshakeFailedError;
 }
-
-
 Int Stat_NetworkStatusProxyConnectionRefusedError(Int o)
 {
     return Stat_Var_NetworkStatusProxyConnectionRefusedError;
 }
-
-
 Int Stat_NetworkStatusProxyConnectionClosedError(Int o)
 {
     return Stat_Var_NetworkStatusProxyConnectionClosedError;
 }
-
-
 Int Stat_NetworkStatusProxyConnectionTimeoutError(Int o)
 {
     return Stat_Var_NetworkStatusProxyConnectionTimeoutError;
 }
-
-
 Int Stat_NetworkStatusProxyNotFoundError(Int o)
 {
     return Stat_Var_NetworkStatusProxyNotFoundError;
 }
-
-
 Int Stat_NetworkStatusProxyProtocolError(Int o)
 {
     return Stat_Var_NetworkStatusProxyProtocolError;
 }
-
-
 Int Stat_NetworkStatusOperationError(Int o)
 {
     return Stat_Var_NetworkStatusOperationError;
 }
-
-
 Int Stat_NetworkStatusSslInternalError(Int o)
 {
     return Stat_Var_NetworkStatusSslInternalError;
 }
-
-
 Int Stat_NetworkStatusSslInvalidUserDataError(Int o)
 {
     return Stat_Var_NetworkStatusSslInvalidUserDataError;
 }
-
-
 Int Stat_NetworkStatusTemporaryError(Int o)
 {
     return Stat_Var_NetworkStatusTemporaryError;
 }
-
-
