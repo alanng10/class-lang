1be7b6142e1145d87ba51f00eef90c0c711a25a2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-18 14:36:06 +0800

    Tool

--- Tool/Z.Tool.Infra.StatBrushKind/Exe.cs
+++ Tool/Z.Tool.Infra.StatBrushKind/Exe.cs
@@ -1,25 +1,14 @@
 namespace Z.Tool.Infra.StatBrushKind;
 
-
-
-
 class Exe : Any
 {
     static int Main()
     {
         Gen gen;
-
         gen = new Gen();
-
         gen.Init();
-
-
-
         int o;
-
         o = gen.Execute();
-
-
         return o;
     }
 }
--- Tool/Z.Tool.Infra.StatBrushKind/Gen.cs
+++ Tool/Z.Tool.Infra.StatBrushKind/Gen.cs
@@ -1,32 +1,14 @@
 namespace Z.Tool.Infra.StatBrushKind;
 
-
-
-
-
 class Gen : StatGen
 {
     public override int Execute()
     {
         this.ClassName = "BrushKind";
-
-
-
         this.ValuePostfix = "Pattern";
-
-        
-
-
         this.ItemListFileName = "ItemListBrushKind.txt";
-
-
-
-
         int o;
-        
         o = base.Execute();
-
-
         return o;
     }
 }
