c093d9c4ba603af48a8c8c1f2bebe030effa082b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-18 15:08:17 +0800

    Infra

--- Infra/Infra/Stat_StreamKind.cpp
+++ Infra/Infra/Stat_StreamKind.cpp
@@ -1,28 +1,15 @@
 #include "Stat.hpp"
 
 
-
-
-
-
-
-
-
 Int Stat_StreamKindMemory(Int o)
 {
     return 1;
 }
-
-
 Int Stat_StreamKindStorage(Int o)
 {
     return 2;
 }
-
-
 Int Stat_StreamKindNetwork(Int o)
 {
     return 3;
 }
-
-
