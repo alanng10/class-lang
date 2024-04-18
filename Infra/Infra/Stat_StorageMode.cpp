8ecf8eaaec40df7b949b0fb6e4e2d2d76442a0a7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-18 15:04:37 +0800

    Infra

--- Infra/Infra/Stat_StorageMode.cpp
+++ Infra/Infra/Stat_StorageMode.cpp
@@ -1,42 +1,23 @@
 #include "Stat.hpp"
 
-
-
-
 Int Stat_Var_StorageModeRead = QIODeviceBase::ReadOnly;
-
 Int Stat_Var_StorageModeWrite = QIODeviceBase::WriteOnly;
-
 Int Stat_Var_StorageModeNew = QIODeviceBase::NewOnly;
-
 Int Stat_Var_StorageModeExisting = QIODeviceBase::ExistingOnly;
-
-
-
-
-
 
 Int Stat_StorageModeRead(Int o)
 {
     return Stat_Var_StorageModeRead;
 }
-
-
 Int Stat_StorageModeWrite(Int o)
 {
     return Stat_Var_StorageModeWrite;
 }
-
-
 Int Stat_StorageModeNew(Int o)
 {
     return Stat_Var_StorageModeNew;
 }
-
-
 Int Stat_StorageModeExisting(Int o)
 {
     return Stat_Var_StorageModeExisting;
 }
-
-
