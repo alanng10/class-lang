581cfb723e000c5e55e509a3954ff93d78639e61
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-19 12:57:56 +0800

    Infra

--- Infra/Infra/ThreadIntern.cpp
+++ Infra/Infra/ThreadIntern.cpp
@@ -5,191 +5,75 @@ Bool ThreadIntern::Init()
     return true;
 }
 
-
-
-
-
 void ThreadIntern::run()
 {
     Qt::HANDLE uu;
-
-
     uu = QThread::currentThreadId();
 
-
-
-
     Int threadId;
-
     threadId = CastInt(uu);
 
-
-
-
     Int thread;
-
     thread = this->Thread;
 
-
-
-
-
     Int handle;
-
     handle = Thread_OS_OpenHandle(threadId);
-
-
-
 
     Thread_HandleSet(thread, handle);
 
-
-
-
     Thread_OS_Set();
 
-
-
-
-
     Int share;
-
     share = Infra_Share();
-
-
-
-
     Int stat;
-
     stat = Share_Stat(share);
 
-
-
-
     Int executeCase;
-
     executeCase = Stat_ThreadCaseExecute(stat);
-
-
-
-
     Thread_CaseSet(thread, executeCase);
-
-
-
-
-
-    Int ua;
-
-    ua = Thread_GetInternHandleSemaphore(thread);
-
-
-
-
-    QSemaphore* handleSemaphore;
-
-
-    handleSemaphore = (QSemaphore*)ua;
-
-
-
-    handleSemaphore->release();
-
-
-
-
-
-
 
     Thread_StoreSetThread(thread);
 
-
-
-
-
-
     Main_CurrentThreadSignalHandleSet();
 
+    Int ua;
+    ua = Thread_GetInternHandleSemaphore(thread);
 
-
+    QSemaphore* handleSemaphore;
+    handleSemaphore = (QSemaphore*)ua;
+    handleSemaphore->release();
 
     Int state;
-
     state = Thread_ExecuteStateGet(thread);
-
-
-
     Int aa;
-
     aa = State_MaideGet(state);
-
-
     Int ab;
-
     ab = State_ArgGet(state);
 
-
-
-
     Thread_Execute_Maide maide;
-
     maide = (Thread_Execute_Maide)aa;
 
-
-
     Int status;
-
     status = 0;
-
 
     if (!(maide == null))
     {
         status = maide(thread, ab);
     }
 
-
-
-
-
     Thread_StatusSet(thread, status);
 
-
-
-
-
-
-
-
     Int finishCase;
-
     finishCase = Stat_ThreadCaseFinish(stat);
-
-
-
-
-
+    
     Int uc;
-
     uc = Thread_GetInternCaseMutex(thread);
-
-
-
-
     QMutex* caseMutex;
-
-
     caseMutex = (QMutex*)uc;
-
-
-
 
     caseMutex->lock();
 
-
-
     Thread_CaseSet(thread, finishCase);
-
-
 
     caseMutex->unlock();
 }
@@ -199,7 +83,11 @@ void ThreadIntern::run()
 
 
 
-int ThreadIntern::ExecuteEventLoop()
+Int ThreadIntern::ExecuteEventLoop()
 {
-    return this->exec();
+    int u;
+    u = this->exec();
+    Int a;
+    a = u;
+    return a;
 }
--- Infra/Infra/ThreadIntern.hpp
+++ Infra/Infra/ThreadIntern.hpp
@@ -1,40 +1,24 @@
 #pragma once
 
-
-
-
-
 #include <QThread>
 #include <QSemaphore>
 #include <QMutex>
 #include <QThreadStorage>
 
-
-
-
 #include "Probate.hpp"
-
-
-
-
-
 
 class ThreadIntern : public QThread
 {
     Q_OBJECT
 
-
 public:
     Bool Init();
-    
+
     Int Thread;
 
-
-    int ExecuteEventLoop();
-
+    Int ExecuteEventLoop();
 
 protected:
 
     void run() override;
 };
-
