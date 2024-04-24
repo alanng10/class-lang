1351f87bf22ade901408d4562d9256db26041c70
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-24 20:51:09 +0800

    Script

--- Script/Deploy.cmd
+++ Script/Deploy.cmd
@@ -9,9 +9,8 @@ copy /Y %DotNetModuleOutFold%\Avalon.*.dll %DeployModuleFold% 1>NUL 2>NUL
 copy /Y %DotNetModuleOutFold%\System.*.ref %DeployModuleFold% 1>NUL 2>NUL
 copy /Y %DotNetModuleOutFold%\Class.*.dll %DeployModuleFold% 1>NUL 2>NUL
 copy /Y %DotNetModuleOutFold%\Class.*.ref %DeployModuleFold% 1>NUL 2>NUL
-copy /Y %DotNetModuleOutFold%\ClassExe.dll %DeployModuleFold% 1>NUL 2>NUL
-copy /Y %DotNetModuleOutFold%\ClassExe.deps.json %DeployModuleFold% 1>NUL 2>NUL
-copy /Y %DotNetModuleOutFold%\ClassExe.runtimeconfig.json %DeployModuleFold% 1>NUL 2>NUL
+copy /Y %DotNetModuleOutFold%\Class.*-ExeCon.deps.json %DeployModuleFold% 1>NUL 2>NUL
+copy /Y %DotNetModuleOutFold%\Class.*-ExeCon.runtimeconfig.json %DeployModuleFold% 1>NUL 2>NUL
 copy /Y %DotNetModuleOutFold%\class.exe %DeployModuleFold% 1>NUL 2>NUL
 
 pushd %DotNetModuleOutFold%
