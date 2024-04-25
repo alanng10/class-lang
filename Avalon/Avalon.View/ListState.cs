0c8002b69ee901c915d559eb0f991dd9162cd851
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-25 14:54:32 +0800

    Avalon

--- Avalon/Avalon.View/ListState.cs
+++ Avalon/Avalon.View/ListState.cs
@@ -1,45 +1,18 @@
 namespace Avalon.View;
 
-
-
-
-
-
 public class ListState : State
 {
     public virtual List List { get; set; }
 
-
-
-
     public override bool Execute()
     {
         Change change;
-
         change = (Change)this.Arg;
-
-
-
-
         CompComp a;
-
         a = change.Comp;
-
-
-
-
         Comp item;
-
         item = (Comp)a;
-
-
-
-
         this.List.ItemChange(item);
-
-        
-
-
         return true;
     }
 }
