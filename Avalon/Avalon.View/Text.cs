3cb43ca8af57b94fce1e547a010cd5744aea8dee
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-25 15:04:06 +0800

    Avalon

--- Avalon/Avalon.View/Text.cs
+++ Avalon/Avalon.View/Text.cs
@@ -1,192 +1,84 @@
 namespace Avalon.View;
 
-
-
-
-
-
 public class Text : View
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
         this.TextInfra = TextInfra.This;
 
-
-
-
         this.ValueField = this.CreateValueField();
-
-
-
-
         this.ForeField = this.CreateForeField();
-
-
-
-
         this.FontField = this.CreateFontField();
-
-
-
-
         this.DestField = this.CreateDestField();
-
-
-
-
         this.AlignField = this.CreateAlignField();
 
-
-
-
-
-
         this.Value = this.CreateValue();
-
-
-
-
         this.Fore = this.CreateFore();
-
-
-
-
         this.Font = this.CreateFont();
-
-
-
-
         this.Dest = this.CreateDest();
-
-
-
-
         this.Align = this.CreateAlign();
-
-
-
-
-
         return true;
     }
 
-
-
-
-
     protected virtual TextInfra TextInfra { get; set; }
-
-
-
-
 
     protected virtual Field CreateValueField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
 
-
-
-
     protected virtual Field CreateForeField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
-
-
-
 
     protected virtual Field CreateFontField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
 
-
-
-
     protected virtual Field CreateDestField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
-
-
-
 
     protected virtual Field CreateAlignField()
     {
         return this.ViewInfra.FieldCreate(this);
     }
 
-
-
-
-
-
     protected virtual TextText CreateValue()
     {
         return this.TextInfra.TextCreateString("");
     }
-
-
-
-
 
     protected virtual DrawPen CreateFore()
     {
         return this.DrawInfra.BlackPen;
     }
 
-
-
-
     protected virtual DrawFont CreateFont()
     {
         return this.DrawInfra.Font;
     }
 
-
-
-
     protected virtual Rect CreateDest()
     {
         Rect a;
-
         a = new Rect();
-
         a.Init();
-
-
         return a;
     }
-
-
-
-
 
     protected virtual DrawTextAlign CreateAlign()
     {
         DrawTextAlignList a;
-
         a = DrawTextAlignList.This;
-
-
         return a.LeftUp;
     }
 
-
-
-
-
-
     public virtual Field ValueField { get; set; }
-
-
-
 
     public virtual TextText Value
     {
@@ -201,29 +93,13 @@ public class Text : View
         }
     }
 
-
-
-
-
     protected virtual bool ChangeValue(Change change)
     {
         this.Trigger(this.ValueField);
-
-
-
         return true;
     }
 
-
-
-
-
-
-
     public virtual Field ForeField { get; set; }
-
-
-
 
     public virtual DrawPen Fore
     {
@@ -238,30 +114,13 @@ public class Text : View
         }
     }
 
-
-
-
-
     protected virtual bool ChangeFore(Change change)
     {
         this.Trigger(this.ForeField);
-
-
-
         return true;
     }
 
-
-
-
-
-
-
-
     public virtual Field FontField { get; set; }
-
-
-
 
     public virtual DrawFont Font
     {
@@ -276,29 +135,13 @@ public class Text : View
         }
     }
 
-
-
-
-
     protected virtual bool ChangeFont(Change change)
     {
         this.Trigger(this.FontField);
-
-
-
         return true;
     }
 
-
-
-
-
-
-
     public virtual Field DestField { get; set; }
-
-
-
 
     public virtual Rect Dest
     {
@@ -313,28 +156,13 @@ public class Text : View
         }
     }
 
-
-
-
-
     protected virtual bool ChangeDest(Change change)
     {
         this.Trigger(this.DestField);
-
-
-
         return true;
     }
 
-
-
-
-
-
     public virtual Field AlignField { get; set; }
-
-
-
 
     public virtual DrawTextAlign Align
     {
@@ -349,171 +177,75 @@ public class Text : View
         }
     }
 
-
-
-
-
     protected virtual bool ChangeAlign(Change change)
     {
         this.Trigger(this.AlignField);
-
-
-
         return true;
     }
-
-
-
-
-
 
     public override bool Change(Field field, Change change)
     {
         base.Change(field, change);
-
-
-
-
         if (this.ValueField == field)
         {
             this.ChangeValue(change);
         }
-
-
         if (this.ForeField == field)
         {
             this.ChangeFore(change);
         }
-
-
         if (this.FontField == field)
         {
             this.ChangeFont(change);
         }
-
-
         if (this.DestField == field)
         {
             this.ChangeDest(change);
         }
-
-
         if (this.AlignField == field)
         {
             this.ChangeAlign(change);
         }
-
-
-
         return true;
     }
-
-
-
-
-
-
-
-
 
     protected override bool CheckDrawChild()
     {
         return !(this.Value == null);
     }
 
-
-
-
-
-
-
-
     protected override bool ExecuteChildDraw(DrawDraw draw)
     {
         TextText text;
-
         text = this.Value;
 
-
-
         DrawRect dest;
-
         dest = this.DrawRectA;
-
-
-
         dest.Pos.Left = this.Dest.Pos.Left;
-
-
         dest.Pos.Up = this.Dest.Pos.Up;
-
-
         dest.Size.Width = this.Dest.Size.Width;
-
-
         dest.Size.Height = this.Dest.Size.Height;
 
-
-
         DrawTextAlign align;
-
         align = this.Align;
-
-
-
-
         DrawPen fore;
-
         fore = this.Fore;
-
-
         DrawFont font;
-
         font = this.Font;
-
-
-
-
+        
         DrawPen oa;
-
         oa = draw.Pen;
-
-
-
         DrawFont ob;
-
         ob = draw.Font;
-
-
-
         draw.Pen = fore;
-
-
         draw.Font = font;
-
-
 
         draw.ExecuteText(text, dest, align, false);
 
-
-
         draw.Font = ob;
-
-
         draw.Pen = oa;
-
-
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     public override View Child
     {
