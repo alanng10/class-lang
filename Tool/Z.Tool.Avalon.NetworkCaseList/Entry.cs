e4bc5fe0d5086c29f83ee5bc5cee386d60fe65c1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 01:16:12 +0800

    Tool

---
+++ Tool/Z.Tool.Avalon.NetworkCaseList/Entry.cs
@@ -0,0 +1,31 @@
+namespace Z.Tool.Avalon.NetworkCaseList;
+
+class Entry : EntryEntry
+{
+    public override int Execute()
+    {
+        Main main;
+        main = new Main();
+        main.Init();
+
+        Gen gen;
+        gen = new Gen();
+        gen.Init();
+        int o;
+        o = gen.Execute();
+
+        main.Final();
+        return o;
+    }
+
+    [STAThread]
+    static int Main()
+    {
+        EntryEntry a;
+        a = new Entry();
+        a.Init();
+        int o;
+        o = a.Execute();
+        return o;
+    }
+}
--- Tool/Z.Tool.Avalon.NetworkCaseList/Gen.cs
+++ Tool/Z.Tool.Avalon.NetworkCaseList/Gen.cs
@@ -1,4 +1,4 @@
-namespace Z.Tool.NetworkCaseListSourceGen;
+namespace Z.Tool.Avalon.NetworkCaseList;
 
 
 
