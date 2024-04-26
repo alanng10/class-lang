7c8b549aa629d8456dd343dee0fed9a6c03439e9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 01:59:11 +0800

    Tool

--- Tool/Z.Infra.StatItemListSourceGen/Gen.cs
+++ Tool/Z.Infra.StatItemListSourceGen/Gen.cs
@@ -1,117 +1,55 @@
 namespace Z.Infra.StatItemListSourceGen;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
         this.AddMethodFileName = "AddMethodStatItem.txt";
-
-
-
         this.InitMethodFileName = "InitMethodStatItem.txt";
-
-
-
-
         return true;
     }
-
-
-
 
     public override int Execute()
     {
         this.OutputFilePath = this.GetOutputFilePath();
-
-
-
         return base.Execute();
     }
 
-
-
-
-
     protected virtual string StatItemClassName { get; set; }
-
-
-
 
     protected virtual string GetStatItemListFileName()
     {
         return "../../Tool/Z.Tool.Infra.Stat" + this.StatItemClassName + "/ItemList" + this.StatItemClassName + ".txt";
     }
 
-
-
-
     protected virtual string GetOutputFilePath()
     {
         return "../../Avalon/" + this.Namespace + "/" + this.ClassName + ".cs";
     }
 
-
-
-
-
-
     protected override TableEntry GetItemEntry(string line)
     {
         string name;
-
         name = line;
-
-
         string value;
-
         value = line;
 
-
-
         int uu;
-
         uu = line.IndexOf(' ');
-
-
-
         if (!(uu < 0))
         {
             name = line.Substring(0, uu);
-
-
             value = line.Substring(uu + 1);
         }
 
-
-
-
         TableEntry a;
-
         a = new TableEntry();
-
         a.Init();
-
         a.Index = name;
-
         a.Value = value;
-
-
-
         return a;
     }
-
-    
-
-
 
     protected override bool AppendInitFieldAddItem(StringBuilder sb, string index, object value)
     {
@@ -119,11 +57,6 @@ public class Gen : SourceGen
             .Append("(")
             .Append("Extern.Stat_" + StatItemClassName + index).Append("(").Append("stat").Append(")")
             .Append(")");
-
-
-
-
-
         return true;
     }
 }
