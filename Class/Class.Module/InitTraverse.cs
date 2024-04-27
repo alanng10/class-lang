74941ac832b4143eabe00a6da07e707cf062d739
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 11:09:27 +0800

    Class

--- Class/Class.Module/InitTraverse.cs
+++ Class/Class.Module/InitTraverse.cs
@@ -7,38 +7,10 @@ public class InitTraverse : Traverse
 {
     protected override bool ExecuteNode(NodeNode node)
     {
-        Info check;
+        Info info;
+        info = this.Create.CreateInfo();
 
-
-
-        check = this.Create.CreateCheck();
-
-
-
-
-
-        TableEntry entry;
-
-
-        entry = new TableEntry();
-
-
-        entry.Init();
-
-
-        entry.Index = node;
-
-
-        entry.Value = check;
-
-
-
-
-        this.Create.Check.Add(entry);
-
-
-
-
+        node.NodeAny = info;
         return true;
     }
 }
