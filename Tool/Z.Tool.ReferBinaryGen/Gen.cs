6f403d8737bf4c148b361c4a7711426bd1371434
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 10:01:38 +0800

    Tool

--- Tool/Z.Tool.ReferBinaryGen/Gen.cs
+++ Tool/Z.Tool.ReferBinaryGen/Gen.cs
@@ -99,7 +99,7 @@ public class Gen : Any
         this.ExecuteTypeModule(typeof(Port));
         this.ExecuteTypeModule(typeof(Token));
         this.ExecuteTypeModule(typeof(Node));
-        this.ExecuteTypeModule(typeof(Check));
+        this.ExecuteTypeModule(typeof(ReferInfo));
         this.ExecuteTypeModule(typeof(ModuleResult));
         this.ExecuteTypeModule(typeof(Task));
 
