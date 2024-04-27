74ddaeac4eec6d60779b3ffc5a2f3927f8fb570b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 11:04:05 +0800

    Class

--- Class/Class.Module/SystemClass.cs
+++ Class/Class.Module/SystemClass.cs
@@ -1,86 +1,10 @@
 namespace Class.Module;
 
-
-
-
-
 public class SystemClass : Any
 {
     public virtual ClassClass Any { get; set; }
-
-
-
-
     public virtual ClassClass Bool { get; set; }
-
-
-
-
     public virtual ClassClass Int { get; set; }
-
-
-
-
     public virtual ClassClass String { get; set; }
-
-
-
-
-
-
-    public override bool Init()
-    {
-        base.Init();
-
-
-        
-        this.Any = this.NewClass("Any");
-
-
-
-
-        this.Bool = this.NewClass("Bool");
-
-
-
-
-        this.Int = this.NewClass("Int");
-
-
-
-
-        this.String = this.NewClass("String");
-
-
-
-
-        return true;
-    }
-
-    private ClassClass NewClass(string name)
-    {
-        ClassClass c;
-
-
-        c = new ClassClass();
-
-
-        c.Init();
-
-
-        c.Name = name;
-
-
-
-
-
-        ClassClass ret;
-
-
-        ret = c;
-
-
-
-        return ret;
-    }
+    public virtual ClassClass ModuleInfo { get; set; }
 }
