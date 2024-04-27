6b3d00b9380122ba0013fb8a2e6e65667006ff29
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 18:39:48 +0800

    Class

--- Class/Class.Infra/Field.cs
+++ Class/Class.Infra/Field.cs
@@ -1,26 +1,15 @@
 namespace Class.Infra;
 
-
-
 public class Field : Any
 {
     public virtual string Name { get; set; }
-
     public virtual Class Class { get; set; }
-
-    public virtual SystemInfo SystemInfo { get; set; }
-
     public virtual Count Count { get; set; }
-
     public virtual Field Virtual { get; set; }
-
     public virtual Table Get { get; set; }
-
     public virtual Table Set { get; set; }
-
     public virtual Class Parent { get; set; }
-
-    public virtual object Any { get; set; }
-
     public virtual int Index { get; set; }
+    public virtual object Any { get; set; }
+    public virtual SystemInfo SystemInfo { get; set; }
 }
