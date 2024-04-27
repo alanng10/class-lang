33bd479cae97b97d4fd7a7781d96edacb1d252cf
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 20:16:13 +0800

    Class

--- Class/Class.Infra/Maide.cs
+++ Class/Class.Infra/Maide.cs
@@ -1,26 +1,15 @@
 namespace Class.Infra;
 
-
-
 public class Maide : Any
 {
     public virtual string Name { get; set; }
-
     public virtual Class Class { get; set; }
-
-    public virtual SystemInfo SystemInfo { get; set; }
-
     public virtual Count Count { get; set; }
-
     public virtual Maide Virtual { get; set; }
-
     public virtual Table Param { get; set; }
-
     public virtual Table Call { get; set; }
-
     public virtual Class Parent { get; set; }
-
-    public virtual object Any { get; set; }
-
     public virtual int Index { get; set; }
+    public virtual object Any { get; set; }
+    public virtual SystemInfo SystemInfo { get; set; }
 }
--- Class/Class.Infra/Var.cs
+++ Class/Class.Infra/Var.cs
@@ -3,10 +3,7 @@ namespace Class.Infra;
 public class Var : Any
 {
     public virtual string Name { get; set; }
-
     public virtual Class Class { get; set; }
-
-    public virtual SystemInfo SystemInfo { get; set; }
-
     public virtual object Any { get; set; }
+    public virtual SystemInfo SystemInfo { get; set; }
 }
