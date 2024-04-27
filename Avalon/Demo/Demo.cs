bad820cb111b6caa995c2d81d5bcfee271dc28ea
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-27 13:34:40 +0800

    Avalon

--- Avalon/Demo/Demo.cs
+++ Avalon/Demo/Demo.cs
@@ -503,7 +503,7 @@ class Demo : Any
         argList.SetItem(7, argF);
 
         Text varBase;
-        varBase = this.TextInfra.TextCreateString("G H , j h\n\n");
+        varBase = this.TextCreateString("G H , j h\n\n");
 
         TextFormat format;
         format = new TextFormat();
@@ -535,6 +535,15 @@ class Demo : Any
         return true;
     }
 
+    private Text TextCreateString(string o)
+    {
+        Range range;
+        range = new Range();
+        range.Init();
+        range.Count = o.Length;
+        return this.TextInfra.TextCreateString(o, range);
+    }
+
     private bool ExecuteIntParse()
     {
         TextIntParse a;
@@ -542,20 +551,20 @@ class Demo : Any
         a.Init();
 
         Text ooo;
-        ooo = this.TextInfra.TextCreateString("43695");
+        ooo = this.TextCreateString("43695");
         long ooa;
         ooa = a.Execute(ooo, 10, false);
         this.Console.Out.Write("Demo.ExecuteIntParse ooa: " + ooa.ToString() + "\n");
 
-        ooo = this.TextInfra.TextCreateString("9E532F");
+        ooo = this.TextCreateString("9E532F");
         ooa = a.Execute(ooo, 16, true);
         this.Console.Out.Write("Demo.ExecuteIntParse ooa: 0h" + ooa.ToString("x15") + "\n");
 
-        ooo = this.TextInfra.TextCreateString("0000000000009294ef0d");
+        ooo = this.TextCreateString("0000000000009294ef0d");
         ooa = a.Execute(ooo, 16, false);
         this.Console.Out.Write("Demo.ExecuteIntParse ooa: 0h" + ooa.ToString("x16") + "\n");
 
-        ooo = this.TextInfra.TextCreateString("1000000000000000");
+        ooo = this.TextCreateString("1000000000000000");
         ooa = a.Execute(ooo, 16, true);
         this.Console.Out.Write("Demo.ExecuteIntParse ooa: 0h" + ooa.ToString("x16") + "\n");
         return true;
@@ -1042,7 +1051,7 @@ class Demo : Any
         oa = "G L 的哈gd@行 o #";
 
         Text text;
-        text = this.TextInfra.TextCreateString(oa);
+        text = this.TextCreateString(oa);
 
         ViewC viewC;
         viewC = new ViewC();
