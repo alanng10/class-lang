cc474b14a3254fc23e7317faa654ad368df65f1a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-28 23:59:48 +0800

    Script

--- Script/VSCode/Make.cmd
+++ Script/VSCode/Make.cmd
@@ -1,8 +1,11 @@
 @echo off
 
 set VSCodeExtFold=VSCode\class-lang-vscode-ext
+set VSCodeOutFold=.\Out\VSCode
+mkdir %VSCodeOutFold% 1>NUL 2>NUL
+
 copy /Y .\LICENSE %VSCodeExtFold%\LICENSE.txt 1>NUL 2>NUL
 
 pushd %VSCodeExtFold%
-cmd /C "vsce package"
+cmd /C "vsce package -o ..\..\%VSCodeOutFold%\class.vsix"
 popd
