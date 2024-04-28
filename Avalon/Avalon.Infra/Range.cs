f5197d2bb75601a21beffdc8ecfc63d2f2cb542c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-28 16:01:23 +0800

    Avalon

--- Avalon/Avalon.Infra/Range.cs
+++ Avalon/Avalon.Infra/Range.cs
@@ -2,6 +2,8 @@ namespace Avalon.Infra;
 
 public class Range : Any
 {
-    public virtual int Index { get; set; }
-    public virtual int Count { get; set; }
+    public virtual int Index { get { return __D_Index; } set { __D_Index = value; } }
+    protected int __D_Index;
+    public virtual int Count { get { return __D_Count; } set { __D_Count = value; } }
+    protected int __D_Count;
 }
