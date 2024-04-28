a53e43643a1662d5c4c7f86d66bf4b129d45154a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-28 16:04:01 +0800

    Avalon

--- Avalon/Avalon.Infra/DataRange.cs
+++ Avalon/Avalon.Infra/DataRange.cs
@@ -2,6 +2,8 @@ namespace Avalon.Infra;
 
 public class DataRange : Any
 {
-    public virtual long Index { get; set; }
-    public virtual long Count { get; set; }
+    public virtual long Index { get { return __D_Index; } set { __D_Index = value; } }
+    protected long __D_Index;
+    public virtual long Count { get { return __D_Count; } set { __D_Count = value; } }
+    protected long __D_Count;
 }
