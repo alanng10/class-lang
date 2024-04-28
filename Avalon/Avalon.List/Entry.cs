9b561804869a520c4a7cc37541ba7f50c545a0fe
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-28 17:23:16 +0800

    Avalon

--- Avalon/Avalon.List/Entry.cs
+++ Avalon/Avalon.List/Entry.cs
@@ -2,6 +2,8 @@ namespace Avalon.List;
 
 public class Entry : Any
 {
-    public virtual object Index { get; set; }
-    public virtual object Value { get; set; }
+    public virtual object Index { get { return __D_Index; } set { __D_Index = value; } }
+    protected object __D_Index;
+    public virtual object Value { get { return __D_Value; } set { __D_Value = value; } }
+    protected object __D_Value;
 }
