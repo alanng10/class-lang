0ba50071be7d1aee960ea6abb69290162de74457
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-28 16:03:12 +0800

    Avalon

--- Avalon/Avalon.Infra/State.cs
+++ Avalon/Avalon.Infra/State.cs
@@ -2,8 +2,10 @@ namespace Avalon.Infra;
 
 public class State : Any
 {
-    public virtual object Arg { get; set; }
-    public virtual object Result { get; set; }
+    public virtual object Arg { get { return __D_Arg; } set { __D_Arg = value; } }
+    protected object __D_Arg;
+    public virtual object Result { get { return __D_Result; } set { __D_Result = value; } }
+    protected object __D_Result;
 
     public virtual bool Execute()
     {
