02e14e31ef255281426cd4ba3262c841b7fbe6f3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-29 10:18:14 +0800

    Class

--- Class/Class.Module/Info.cs
+++ Class/Class.Module/Info.cs
@@ -8,9 +8,6 @@ public class Info : Any
     public virtual Var Var { get; set; }
     public virtual ClassClass TargetClass { get; set; }
     public virtual ClassClass OperateClass { get; set; }
-    public virtual ClassClass NewClass { get; set; }
-    public virtual ClassClass ShareClass { get; set; }
-    public virtual ClassClass CastClass { get; set; }
     public virtual Field GetField { get; set; }
     public virtual Field SetField { get; set; }
     public virtual Maide CallMaide { get; set; }
