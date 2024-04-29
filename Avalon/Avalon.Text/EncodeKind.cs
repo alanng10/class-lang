370b92bb4ff951529c8a9d1ed71760351c7f4835
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-29 17:55:02 +0800

    Avalon

--- Avalon/Avalon.Text/EncodeKind.cs
+++ Avalon/Avalon.Text/EncodeKind.cs
@@ -2,6 +2,7 @@ namespace Avalon.Text;
 
 public class EncodeKind : Any
 {
-    public virtual int Index { get; set; }
+    public virtual int Index { get { return __D_Index; } set { __D_Index = value; } }
+    protected int __D_Index;
     internal virtual ulong Intern { get; set; }
 }
