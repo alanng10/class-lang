d4d03cd598a996bbf0185145984203b766c03fdf
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-29 16:20:13 +0800

    Avalon

--- Avalon/Avalon.Math/Compose.cs
+++ Avalon/Avalon.Math/Compose.cs
@@ -2,6 +2,8 @@ namespace Avalon.Math;
 
 public class Compose : Any
 {
-    public virtual long Significand { get; set; }
-    public virtual long Exponent { get; set; }
+    public virtual long Significand { get { return __D_Significand; } set { __D_Significand = value; } }
+    protected long __D_Significand;
+    public virtual long Exponent { get { return __D_Exponent; } set { __D_Exponent = value; } }
+    protected long __D_Exponent;
 }
