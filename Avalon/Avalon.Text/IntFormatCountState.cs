95115398fd82de3a7c7beed00fb65a5d90756af4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-04-30 10:45:53 +0800

    Avalon

--- Avalon/Avalon.Text/IntFormatCountState.cs
+++ Avalon/Avalon.Text/IntFormatCountState.cs
@@ -4,6 +4,12 @@ public class IntFormatCountState : FormatCountState
 {
     public override bool Execute()
     {
+        FormatArg arg;
+        arg = (FormatArg)this.Arg;
+
+        int count;
+        count = this.Format.IntDigitCount(arg.ValueInt, arg.Base);
+        this.Result = count;
         return true;
     }
 }
