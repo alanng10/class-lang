9f512130e53ca0e97da3c7f98d283b077afdb880
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-01 16:39:30 +0800

    Tool

---
+++ Tool/Z.Infra.Class/BoolValue.cs
@@ -0,0 +1,8 @@
+namespace Z.Infra.Class;
+
+public class BoolValue
+{
+    public static readonly uint False = 0x10000000;
+    public static readonly uint True = 0x10000001;
+    public static readonly uint Null = 0;
+}
