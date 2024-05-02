86d3ca79509d598f85b1f1f7e105a39afd72e130
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-02 15:01:03 +0800

    Avalon

--- Avalon/Avalon.List/-.cs
+++ Avalon/Avalon.List/-.cs
@@ -1,6 +1,6 @@
-global using Compare = Avalon.Infra.Compare;
 global using Any = Avalon.Infra.Any;
 global using Range = Avalon.Infra.Range;
+global using Compare = Avalon.Infra.Compare;
 global using InfraInfra = Avalon.Infra.Infra;
 global using SystemArray = System.Array;
 global using IComparerObject = System.Collections.Generic.IComparer<object>;
