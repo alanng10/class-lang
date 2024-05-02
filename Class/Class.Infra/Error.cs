b3372e68e31279e6a874f576b7b193b98df7e45a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-02 16:40:31 +0800

    Class

--- Class/Class.Infra/Error.cs
+++ Class/Class.Infra/Error.cs
@@ -1,26 +1,12 @@
 namespace Class.Infra;
 
-
-
-
-
-
 public class Error : Any
 {
     public virtual Stage Stage { get; set; }
 
-
-
-
     public virtual ErrorKind Kind { get; set; }
 
-
-
-
     public virtual Range Range { get; set; }
-
-
-
 
     public virtual Source Source { get; set; }
 }
