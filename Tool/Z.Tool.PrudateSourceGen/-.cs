d8d48ef4ab18f8656ab7f607e31b7f9c90afb9f1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-03 17:04:07 +0800

    Tool

--- Tool/Z.Tool.PrudateSourceGen/-.cs
+++ Tool/Z.Tool.PrudateSourceGen/-.cs
@@ -1,11 +1,9 @@
 global using Any = Avalon.Infra.Any;
 global using List = Avalon.List.List;
 global using Array = Avalon.List.Array;
-global using Table = Avalon.List.Table;
 global using Iter = Avalon.List.Iter;
 global using Main = Avalon.Main.Main;
 global using EntryEntry = Avalon.Entry.Entry;
-global using ExeExe = Avalon.Entry.Entry;
 global using ToolInfra = Z.Infra.Infra.Infra;
 global using StringSplitOption = System.StringSplitOptions;
 global using StringBuilder = System.Text.StringBuilder;
