02fe24e98e0965c4ffb5ae8dae4d7d7abdddc7df
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-03 14:07:56 +0800

    Tool

--- Tool/Z.Tool.Infra.StatImageFormat/-.cs
+++ Tool/Z.Tool.Infra.StatImageFormat/-.cs
@@ -1,3 +1,4 @@
-global using Any = Avalon.Infra.Any;
-global using Iter = Avalon.List.Iter;
+global using Main = Avalon.Main.Main;
+global using EntryEntry = Avalon.Entry.Entry;
 global using StatGen = Z.Infra.StatSourceGen.Gen;
+global using STAThreadAttribute = System.STAThreadAttribute;
