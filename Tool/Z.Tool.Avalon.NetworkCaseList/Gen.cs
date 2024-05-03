515e8a6c9a49ade219fe36a4828634d94cc7a83e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-03 12:40:48 +0800

    Tool

--- Tool/Z.Tool.Avalon.NetworkCaseList/Gen.cs
+++ Tool/Z.Tool.Avalon.NetworkCaseList/Gen.cs
@@ -1,69 +1,22 @@
 namespace Z.Tool.Avalon.NetworkCaseList;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Avalon.Network";
-
-
         this.ClassName = "CaseList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "Case";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
         this.StatItemClassName = "NetworkCase";
-        
-
-
-
         this.ItemListFileName = this.GetStatItemListFileName();
-
-
-
-
-
         this.AddMethodFileName = "AddMethodNetworkCase.txt";
-
-
-
         this.InitMethodFileName = "InitMethod.txt";
-
-
-
-
-
         return true;
     }
-
-
-
-
 
     protected override bool AppendInitFieldAddItem(StringBuilder sb, string index, object value)
     {
