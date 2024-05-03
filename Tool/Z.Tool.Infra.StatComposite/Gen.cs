1129d6c5f0a60c6f69a0ec0badefe8083c079661
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-03 13:18:21 +0800

    Tool

--- Tool/Z.Tool.Infra.StatComposite/Gen.cs
+++ Tool/Z.Tool.Infra.StatComposite/Gen.cs
@@ -1,40 +1,16 @@
 namespace Z.Tool.StatCompositeSourceGen;
 
-
-
-
-
 class Gen : StatGen
 {
     public override int Execute()
     {
         this.ClassName = "Composite";
-
-
-
         this.ScopeName = "QPainter";
-
-
-
         this.ValuePrefix = "CompositionMode_";
-
-
-        
         this.ValueOffset = " + 1";
-        
-
-
-
-        this.ItemListFileName = "ItemListComposite.txt";
-
-
-
-
-        int o;
-        
+        this.ItemListFileName = "ToolData/ItemListComposite.txt";
+        int o;        
         o = base.Execute();
-
-
         return o;
     }
 }
