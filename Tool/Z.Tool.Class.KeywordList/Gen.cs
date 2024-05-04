5ad8af0bd9757a633fcf6c6dad2438d95b0f6798
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 14:31:37 +0800

    Tool

--- Tool/Z.Tool.Class.KeywordList/Gen.cs
+++ Tool/Z.Tool.Class.KeywordList/Gen.cs
@@ -1,160 +1,63 @@
 namespace Z.Tool.Class.KeywordList;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Class.Infra";
-
-
         this.ClassName = "KeywordList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "Keyword";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
-
         this.ItemListFileName = "ToolData/ItemListKeyword.txt";
-
-
-
         this.AddMethodFileName = "ToolData/AddMethodKeyword.txt";
-
-
-
         this.OutputFilePath = "../../Class/Class.Infra/KeywordList.cs";
-
-
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     protected override TableEntry GetItemEntry(string line)
     {
         string index;
-
         index = line;
 
-
-
-
-
         string aa;
-
         aa = "Item";
 
-
-
-
         string k;
-
         k = index;
-
-
-
         if (k.StartsWith(aa))
         {
             k = k.Substring(aa.Length);
         }
 
-
-
         k = k.ToLower();
 
-
-
-
-
-        string text;
-        
+        string text;        
         text = k;
 
-
-
-
-
         Value value;
-
         value = new Value();
-
         value.Init();
-
         value.Text = text;
 
-
-
-
-
         TableEntry entry;
-
         entry = new TableEntry();
-
         entry.Init();
-
         entry.Index = index;
-
         entry.Value = value;
-
-
-
-
         return entry;
     }
-
-
-
-
 
     protected override bool AppendInitFieldAddItem(StringBuilder sb, string index, object value)
     {
         Value a;
-
         a = (Value)value;
-
-
-
-
         sb.Append("AddItem")
             .Append("(")
             .Append("\"").Append(a.Text).Append("\"")
             .Append(")");
-            
-        
-
-
-
         return true;
     }
 }
