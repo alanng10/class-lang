5335ef6761ae4122e3201505f701dfa9c876aeb5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 14:52:54 +0800

    Tool

--- Tool/Z.Tool.Class.TaskKindList/Gen.cs
+++ Tool/Z.Tool.Class.TaskKindList/Gen.cs
@@ -1,57 +1,19 @@
 namespace Z.Tool.Class.TaskKindList;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Class.Console";
-
-
         this.ClassName = "TaskKindList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "TaskKind";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
-
         this.ItemListFileName = "ToolData/ItemListTaskKind.txt";
-
-
-
         this.AddMethodFileName = "ToolData/AddMethodTaskKind.txt";
-
-
-
         this.OutputFilePath = "../../Class/Class.Console/TaskKindList.cs";
-
-
-
-
-
         return true;
     }
 }
