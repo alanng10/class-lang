8e01dfd1d3c831bbc61567af127b7ebe22f23d9b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 14:34:59 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/Class.cs
+++ Tool/Z.Tool.Class.NodeList/Class.cs
@@ -1,17 +1,8 @@
 namespace Z.Tool.Class.NodeList;
 
-
-
-
 public class Class : Any
 {
     public virtual string Name { get; set; }
-
-
-
     public virtual string Base { get; set; }
-
-
-
     public virtual Array Field { get; set; }
 }
--- Tool/Z.Tool.Class.NodeList/CreateOperateStateGen.cs
+++ Tool/Z.Tool.Class.NodeList/CreateOperateStateGen.cs
@@ -1,151 +1,60 @@
 namespace Z.Tool.Class.NodeList;
 
-
-
-
-
-
 public class CreateOperateStateGen : Any
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
         this.ToolInfra = ToolInfra.This;
-
-
-
-
         return true;
     }
 
-
-
-
     public virtual Array ClassArray { get; set; }
-
-
-
-
     protected virtual ToolInfra ToolInfra { get; set; }
-
-
-
     protected virtual string OutputFoldPath { get; set; }
-
-
-
     protected virtual string SourceFileName { get; set; }
-
-
-
-
-
 
     public virtual int Execute()
     {
         this.SourceFileName = "ToolData/CreateOperateStateSource.txt";
-
-
-
         this.OutputFoldPath = "../../Class/Class.Node";
 
-
-
-
-
         string kk;
-
         kk = this.ToolInfra.StorageTextRead(this.SourceFileName);
 
-
-
-
         int count;
-
         count = this.ClassArray.Count;
-
-
         int i;
-
         i = 0;
-
-
         while (i < count)
         {
             Class varClass;
-
             varClass = (Class)this.ClassArray.Get(i);
 
-
-            
             string kind;
-
             kind = varClass.Name;
 
-
-
-
             string fieldSetListString;
-
             fieldSetListString = this.GetFieldSetListString(varClass.Field);
 
-
-
-
             StringBuilder sb;
-
-
             sb = new StringBuilder();
-
-
-
             sb.Append(kk);
-
-
-
             sb.Replace("#NodeKind#", kind);
-
-
             sb.Replace("#FieldSetList#", fieldSetListString);
 
-
-
-
             string k;
-
             k = sb.ToString();
 
-
-
-
             string path;
-
             path = this.GetOutputFilePath(kind);
-
-
-
 
             this.ToolInfra.StorageTextWrite(path, k);
 
-
-
             i = i + 1;
         }
-
-
-
-
-
         return 0;
     }
-
-
-
-
 
     protected virtual string GetFieldSetListString(Array fieldList)
     {
@@ -170,15 +79,9 @@ public class CreateOperateStateGen : Any
         return k;
     }
 
-
-
-
-
-
     protected virtual bool AppendFieldSet(StringBuilder sb, Field field, int index)
     {
         string className;
-
         className = field.Class;
 
 
@@ -305,44 +208,24 @@ public class CreateOperateStateGen : Any
         return k;
     }
 
-
-
-
-
     protected virtual string GetCastClassName(string className)
     {
         string k;
-
         k = className;
-
-
         if (k == "String")
         {
             k = "string";
         }
-
-
         return k;
     }
-
-
-
-
 
     protected virtual string GetOutputFilePath(string kind)
     {
         string fileName;
-
         fileName = "Z_CreateOperateState_" + kind + ".cs";
 
-
-
         string filePath;
-
         filePath = this.OutputFoldPath + "/" + fileName;
-
-
-
         return filePath;
     }
 }
