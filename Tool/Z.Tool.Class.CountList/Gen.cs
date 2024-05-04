b9d5ad79f2e2b25275c22141cc8697305430dc66
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 14:26:49 +0800

    Tool

--- Tool/Z.Tool.Class.CountList/Gen.cs
+++ Tool/Z.Tool.Class.CountList/Gen.cs
@@ -1,57 +1,19 @@
 namespace Z.Tool.Class.CountList;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Class.Infra";
-
-
         this.ClassName = "CountList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "Count";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
-
         this.ItemListFileName = "ToolData/ItemListCount.txt";
-
-
-
         this.AddMethodFileName = "ToolData/AddMethodCount.txt";
-
-
-
         this.OutputFilePath = "../../Class/Class.Infra/CountList.cs";
-
-
-
-
-
         return true;
     }
 }
