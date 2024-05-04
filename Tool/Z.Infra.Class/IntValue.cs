7022c9f8029a6431120a54d6312e75187a9344ba
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 13:30:44 +0800

    Tool

--- Tool/Z.Infra.Class/IntValue.cs
+++ Tool/Z.Infra.Class/IntValue.cs
@@ -3,4 +3,6 @@ namespace Z.Infra.Class;
 public class IntValue
 {
     public static readonly ulong Null = 0;
+
+    public static readonly ulong Zero = 0x100000000000000;
 }
---
+++ Tool/Z.Infra.Class/ValueShare.cs
@@ -0,0 +1,10 @@
+namespace Z.Infra.Class;
+
+public class ValueShare
+{
+    public static uint Bool { get; } = BoolValue.False;
+
+    public static ulong Int { get; } = IntValue.Zero;
+
+    public static string String { get; } = "";
+}
