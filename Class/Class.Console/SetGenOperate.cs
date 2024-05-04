fb2ad082b537c925d652d3a4a58501f37e417bd8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 15:19:53 +0800

    Class

---
+++ Class/Class.Console/SetGenOperate.cs
@@ -0,0 +1,43 @@
+namespace Class.Console;
+
+public class SetGenOperate : GenOperate
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.TextInfra = TextInfra.This;
+        return true;
+    }
+
+    protected virtual TextInfra TextInfra { get; set; }
+
+    public override bool ExecuteString(string o)
+    {
+        TextInfra textInfra;
+        textInfra = TextInfra.This;
+        GenArg arg;
+        arg = this.Traverse.Arg;
+        
+        int index;
+        index = arg.Index;
+        Data data;
+        data = arg.Data;
+
+        int count;
+        count = o.Length;
+        int i;
+        i = 0;
+        while (i < count)
+        {
+            char c;
+            c = o[i];
+
+            textInfra.DataCharSet(data, index, c);
+            i = i + 1;
+        }
+
+        index = index + count;
+        arg.Index = index;
+        return true;
+    }
+}
