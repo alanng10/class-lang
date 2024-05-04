43ba5d0df33d419a17dad0478f704f5b93190dde
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 14:25:45 +0800

    Tool

--- Tool/Z.Tool.Avalon.TextEncodeKindList/Gen.cs
+++ Tool/Z.Tool.Avalon.TextEncodeKindList/Gen.cs
@@ -1,52 +1,18 @@
 namespace Z.Tool.Avalon.TextEncodeKindList;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Avalon.Text";
-
-
         this.ClassName = "EncodeKindList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "EncodeKind";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
         this.StatItemClassName = "TextEncodeKind";
-        
-
-
-
         this.ItemListFileName = this.GetStatItemListFileName();
-
-
-
-
         return true;
     }
 }
