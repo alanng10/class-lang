fe4f3ce9835a9f7e7bfb7f8e072487a29b6169fe
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-04 14:29:00 +0800

    Tool

--- Tool/Z.Tool.Class.DelimitList/Gen.cs
+++ Tool/Z.Tool.Class.DelimitList/Gen.cs
@@ -1,143 +1,54 @@
 namespace Z.Tool.Class.DelimitList;
 
-
-
-
-
-
 public class Gen : SourceGen
 {
     public override bool Init()
     {
         base.Init();
-
-
-
-
-
         this.Namespace = "Class.Infra";
-
-
         this.ClassName = "DelimitList";
-
-
         this.BaseClassName = "Any";
-
-
         this.ItemClassName = "Delimit";
-
-
         this.ArrayClassName = "Array";
-
-
-
-
         this.Export = true;
-
-
-
-
-
         this.ItemListFileName = "ToolData/ItemListDelimit.txt";
-
-
-
         this.AddMethodFileName = "ToolData/AddMethodDelimit.txt";
-
-
-
         this.OutputFilePath = "../../Class/Class.Infra/DelimitList.cs";
-
-
-
-
-
         return true;
     }
-
-
-
-
-
-
 
     protected override TableEntry GetItemEntry(string line)
     {
         string[] ua;
-
-
         ua = line.Split(' ');
 
-
-
-
         string index;
-
         index = ua[0];
 
-
-
-
-
         string text;
-
         text = ua[1];
 
-
-
-
-
-
         Value value;
-
         value = new Value();
-
         value.Init();
-
         value.Text = text;
 
-
-
-
-
         TableEntry entry;
-
         entry = new TableEntry();
-
         entry.Init();
-
         entry.Index = index;
-
         entry.Value = value;
-
-
-
-
         return entry;
     }
-
-
-
-
 
     protected override bool AppendInitFieldAddItem(StringBuilder sb, string index, object value)
     {
         Value a;
-
         a = (Value)value;
-
-
-
-
         sb.Append("AddItem")
             .Append("(")
             .Append("\"").Append(a.Text).Append("\"")
             .Append(")");
-            
-        
-
-
-
         return true;
     }
 }
--- Tool/Z.Tool.Class.DelimitList/Value.cs
+++ Tool/Z.Tool.Class.DelimitList/Value.cs
@@ -1,8 +1,5 @@
 namespace Z.Tool.Class.DelimitList;
 
-
-
-
 class Value : Any
 {
     public virtual string Text { get; set; }
