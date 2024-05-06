4939aed321ddafe47358bfe18b80bb77860fa0d5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-06 23:41:33 +0800

    Class

--- Class/Class.Console/ShareGen.cs
+++ Class/Class.Console/ShareGen.cs
@@ -2,10 +2,38 @@ namespace Class.Console;
 
 public class ShareGen : Any
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.InfraInfra = InfraInfra.This;
+        this.StorageInfra = StorageInfra.This;
+
+        this.InitSourceTemplate();
+        return true;
+    }
+
     public virtual ClassClass Class { get; set; }
+    public virtual string Source { get; set; }
+    protected virtual InfraInfra InfraInfra { get; set; }
+    protected virtual StorageInfra StorageInfra { get; set; }
+    protected virtual string SourceTemplate { get; set; }
+
+    protected virtual bool InitSourceTemplate()
+    {
+        string k;
+        k = this.InfraInfra.PathCombine;
+
+        this.SourceTemplate = this.StorageInfra.TextRead("Class.Console.data" + k + "ClassShare.txt");
+        return true;
+    }
 
     public virtual bool Execute()
     {
+        string o;
+        o = this.SourceTemplate;
+        o = o.Replace("#ClassName#", this.Class.Name);
+        
+        this.Source = o;
         return true;
     }
 }
