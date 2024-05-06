f6e31807d30f22bed2d727fffa017031a5be1af0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-06 23:16:39 +0800

    Class

---
+++ Class/Class.Console/ShareGen.cs
@@ -0,0 +1,11 @@
+namespace Class.Console;
+
+public class ShareGen : Any
+{
+    public virtual ClassClass Class { get; set; }
+
+    public virtual bool Execute()
+    {
+        return true;
+    }
+}
