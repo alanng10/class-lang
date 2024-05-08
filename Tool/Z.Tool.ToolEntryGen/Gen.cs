5ac48a7f4252692469a8a8f6b557ceb14db57ec5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-09 01:16:44 +0800

    Tool

--- Tool/Z.Tool.ToolEntryGen/Gen.cs
+++ Tool/Z.Tool.ToolEntryGen/Gen.cs
@@ -11,12 +11,30 @@ class Gen : Any
 
     protected virtual ToolInfra ToolInfra { get; set; }
     protected virtual string SourceTemplate { get; set; }
-    
+
     public virtual int Execute()
     {
         this.SourceTemplate = this.ToolInfra.StorageTextRead("ToolData/Entry.txt");
 
+        this.ExecuteOne("Avalon.BrushKindList");
+        this.ExecuteOne("Avalon.CompositeList");
+        this.ExecuteOne("Avalon.GradientKindList");
+        this.ExecuteOne("Avalon.GradientSpreadList");
+        this.ExecuteOne("Avalon.ImageFormatList");
+        this.ExecuteOne("Avalon.NetworkCaseList");
+        this.ExecuteOne("Avalon.NetworkPortKindList");
+        this.ExecuteOne("Avalon.NetworkStatusList");
+        this.ExecuteOne("Avalon.PenCapList");
+        this.ExecuteOne("Avalon.PenJoinList");
+        this.ExecuteOne("Avalon.PenKindList");
+        this.ExecuteOne("Avalon.TextAlignList");
+        this.ExecuteOne("Avalon.TextEncodeKindList");
         this.ExecuteOne("Class.CountList");
+        this.ExecuteOne("Class.DelimitList");
+        this.ExecuteOne("Class.ErrorKindList");
+        this.ExecuteOne("Class.KeywordList");
+        this.ExecuteOne("Class.NodeList");
+        this.ExecuteOne("Class.TaskKindList");
         return 0;
     }
 
