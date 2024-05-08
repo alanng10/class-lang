2ac0175beb1ecf2bbb07d86af822b8d6be122d5c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-09 02:05:46 +0800

    Tool

--- Tool/Z.Tool.Avalon.NetworkCaseList/-.cs
+++ Tool/Z.Tool.Avalon.NetworkCaseList/-.cs
@@ -1,4 +1,3 @@
-global using Main = Avalon.Main.Main;
 global using EntryEntry = Avalon.Entry.Entry;
 global using SourceGen = Z.Infra.StatItemListSourceGen.Gen;
 global using STAThreadAttribute = System.STAThreadAttribute;
--- Tool/Z.Tool.Avalon.TextAlignList/-.cs
+++ Tool/Z.Tool.Avalon.TextAlignList/-.cs
@@ -3,7 +3,6 @@ global using StringCompare = Avalon.Infra.StringCompare;
 global using Array = Avalon.List.Array;
 global using Table = Avalon.List.Table;
 global using TableEntry = Avalon.List.Entry;
-global using Main = Avalon.Main.Main;
 global using EntryEntry = Avalon.Entry.Entry;
 global using SourceGen = Z.Infra.ListSourceGen.Gen;
 global using STAThreadAttribute = System.STAThreadAttribute;
