9c5e3160fbd64dd8e57e39cdc6c41c40da317e65
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-08 22:01:45 +0800

    Avalon

---
+++ Avalon/Avalon.Infra/StringData.cs
@@ -0,0 +1,21 @@
+namespace Avalon.Infra;
+
+public class StringData : Data
+{
+    public new virtual string Value { get; set; }
+
+    public virtual int GetChar(int index)
+    {
+        if (!this.Contain(index))
+        {
+            return -1;
+        }
+
+        return this.Value[index];
+    }
+
+    public virtual bool ContainChar(int index)
+    {
+        return this.InfraInfra.CheckIndex(this.Value.Length, index);
+    }
+}
