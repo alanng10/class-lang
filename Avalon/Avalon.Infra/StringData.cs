dc4aa3e3308d3599cdc266694ebb001885e48741
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-08 22:22:08 +0800

    Avalon

--- Avalon/Avalon.Infra/StringData.cs
+++ Avalon/Avalon.Infra/StringData.cs
@@ -11,7 +11,12 @@ public class StringData : Data
             return -1;
         }
 
-        return this.Value[index];
+        char oc;
+        oc = this.Value[index];
+        
+        int a;
+        a = oc;
+        return a;
     }
 
     public virtual bool ContainChar(int index)
