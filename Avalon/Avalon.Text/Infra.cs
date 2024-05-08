fa0fd4c759676748acefe50f203d5f51f2ffc5e7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-08 21:46:08 +0800

    Class

--- Avalon/Avalon.Text/Infra.cs
+++ Avalon/Avalon.Text/Infra.cs
@@ -268,4 +268,167 @@ public class Infra : Any
 
         return o.Data(text.Data, text.Range);
     }
+
+    public virtual int StringIndex(string o, Range range, string other, Range otherRange)
+    {
+        InfraInfra infraInfra;
+        infraInfra = this.InfraInfra;
+
+        int thisIndex;
+        int thisCount;
+        thisIndex = 0;
+        thisCount = 0;
+        bool ba;
+        ba = (range == null);
+        if (ba)
+        {
+            thisIndex = 0;
+            thisCount = o.Length;
+        }
+        if (!ba)
+        {
+            thisIndex = range.Index;
+            thisCount = range.Count;
+            if (!infraInfra.CheckRange(o.Length, thisIndex, thisCount))
+            {
+                return -1;
+            }
+        }
+
+        int otherIndex;
+        int otherCount;
+        otherIndex = 0;
+        otherCount = 0;
+        bool bb;
+        bb = (otherRange == null);
+        if (bb)
+        {
+            otherIndex = 0;
+            otherCount = other.Length;
+        }
+        if (!bb)
+        {
+            otherIndex = otherRange.Index;
+            otherCount = otherRange.Count;
+            if (!infraInfra.CheckRange(other.Length, otherIndex, otherCount))
+            {
+                return -1;
+            }
+        }
+
+        if (thisCount < otherCount)
+        {
+            return -1;
+        }
+
+        int count;
+        count = thisCount - otherCount + 1;
+        int i;
+        i = 0;
+        while (i < count)
+        {
+            int index;
+            index = thisIndex + i;
+
+            bool b;
+            b = false;
+
+            int countA;
+            countA = otherCount;
+            int iA;
+            iA = 0;
+            while (!b & iA < countA)
+            {
+                char oca;
+                char ocb;
+                oca = o[index + iA];
+                ocb = other[otherIndex + iA];
+
+                if (!(oca == ocb))
+                {
+                    b = true;
+                }
+                iA = iA + 1;
+            }
+
+            if (!b)
+            {
+                return i;
+            }
+            i = i + 1;
+        }
+
+        return -1;
+    }
+
+    public virtual bool StringEqualString(string o, Range range, string other, Range otherRange)
+    {
+        InfraInfra infraInfra;
+        infraInfra = this.InfraInfra;
+
+        int thisIndex;
+        int thisCount;
+        thisIndex = 0;
+        thisCount = 0;
+        bool ba;
+        ba = (range == null);
+        if (ba)
+        {
+            thisIndex = 0;
+            thisCount = o.Length;
+        }
+        if (!ba)
+        {
+            thisIndex = range.Index;
+            thisCount = range.Count;
+            if (!infraInfra.CheckRange(o.Length, thisIndex, thisCount))
+            {
+                return false;
+            }
+        }
+
+        int otherIndex;
+        int otherCount;
+        otherIndex = 0;
+        otherCount = 0;
+        bool bb;
+        bb = (otherRange == null);
+        if (bb)
+        {
+            otherIndex = 0;
+            otherCount = other.Length;
+        }
+        if (!bb)
+        {
+            otherIndex = otherRange.Index;
+            otherCount = otherRange.Count;
+            if (!infraInfra.CheckRange(other.Length, otherIndex, otherCount))
+            {
+                return false;
+            }
+        }
+
+        if (!(thisCount == otherCount))
+        {
+            return false;
+        }
+
+        int count;
+        count = thisCount;
+        int i;
+        i = 0;
+        while (i < count)
+        {
+            char oca;
+            char ocb;
+            oca = o[thisIndex + i];
+            ocb = other[otherIndex + i];
+            if (!(oca == ocb))
+            {
+                return false;
+            }
+            i = i + 1;
+        }
+        return true;
+    }
 }
