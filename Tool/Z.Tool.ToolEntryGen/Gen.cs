2d5bc3b709f3f2cef369e979f6262ee023e6d7a2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-09 01:10:54 +0800

    Tool

--- Tool/Z.Tool.ToolEntryGen/Gen.cs
+++ Tool/Z.Tool.ToolEntryGen/Gen.cs
@@ -10,14 +10,25 @@ class Gen : Any
     }
 
     protected virtual ToolInfra ToolInfra { get; set; }
-
+    protected virtual string SourceTemplate { get; set; }
+    
     public virtual int Execute()
     {
+        this.SourceTemplate = this.ToolInfra.StorageTextRead("ToolData/Entry.txt");
+
+        this.ExecuteOne("Class.CountList");
         return 0;
     }
 
     protected virtual bool ExecuteOne(string toolName)
     {
+        string a;
+        a = this.SourceTemplate.Replace("#Name#", toolName);
+
+        string k;
+        k = "../../Tool/Z.Tool." + toolName + "/Entry.cs";
+
+        this.ToolInfra.StorageTextWrite(k, a);
         return true;
     }
 }
