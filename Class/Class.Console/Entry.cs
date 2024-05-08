6721f0b532950818fce7d71adb83144af4112094
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-09 02:13:32 +0800

    Class

--- Class/Class.Console/Entry.cs
+++ Class/Class.Console/Entry.cs
@@ -2,18 +2,14 @@ namespace Class.Console;
 
 public class Entry : EntryEntry
 {
-    public override int Execute()
+    protected override int ExecuteMain()
     {
-        Main main;
-        main = new Main();
-        main.Init();
         Console console;
         console = new Console();
         console.Init();
         console.Arg = this.Arg;
         int a;
         a = console.Execute();
-        main.Final();
         return a;
     }
 }
