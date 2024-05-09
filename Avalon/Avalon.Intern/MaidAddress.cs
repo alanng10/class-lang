9444e6e2e49285e37f7b9e944f91c3ecb738e3cd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-09 22:06:31 +0800

    Avalon

--- Avalon/Avalon.Intern/MaidAddress.cs
+++ Avalon/Avalon.Intern/MaidAddress.cs
@@ -1,36 +1,18 @@
 namespace Avalon.Intern;
 
-
-
-
-
 public class MaideAddress : object
 {
     public virtual bool Init()
     {
         this.InternIntern = Intern.This;
 
-
-
         this.Value = this.InternIntern.MaidePointer(this.Delegate);
-        
-
-
-
         return true;
     }
 
-
-
+    public virtual ulong Value { get; set; }
+    
+    public virtual SystemDelegate Delegate { get; set; }
     
     protected virtual Intern InternIntern { get; set; }
-
-
-
-
-    public virtual ulong Value { get; set; }
-
-
-
-    public virtual SystemDelegate Delegate { get; set; }
 }
