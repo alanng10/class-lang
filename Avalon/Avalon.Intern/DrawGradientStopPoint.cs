3fd011787cfed12879c1b14282a4c81987436332
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-09 22:07:33 +0800

    Avalon

--- Avalon/Avalon.Intern/DrawGradientStopPoint.cs
+++ Avalon/Avalon.Intern/DrawGradientStopPoint.cs
@@ -1,20 +1,13 @@
 namespace Avalon.Intern;
 
-
-
-
-
 public class DrawGradientStopPoint : object
 {
     public virtual bool Init()
     {
         return true;
     }
-
-
     
     public virtual ulong Pos { get; set; }
-
 
     public virtual ulong Color { get; set; }
 }
