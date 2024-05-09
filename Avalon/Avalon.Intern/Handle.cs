2266cb6645ff60b7c32d41f36299e1068499db80
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-09 22:09:08 +0800

    Avalon

--- Avalon/Avalon.Intern/Handle.cs
+++ Avalon/Avalon.Intern/Handle.cs
@@ -1,79 +1,34 @@
 namespace Avalon.Intern;
 
-
-
-
-
-
 public class Handle : object
 {
     public virtual bool Init()
     {
         this.InternIntern = Intern.This;
-        
-
-
-        
+                
         this.GCHandle = SystemGCHandle.Alloc(this.Any);
-
-
-
-
         return true;
     }
-
-
-
-
 
     public virtual bool Final()
     {
         this.GCHandle.Free();
-
-
-
         return true;
     }
 
-
-
-
-
     public virtual object Any { get; set; }
-
-
-
-
 
     protected virtual Intern InternIntern { get; set; }
 
-
-
-
-
+    private SystemGCHandle GCHandle;
 
     public virtual ulong ULong()
     {
         SystemIntPtr u;
-
         u = SystemGCHandle.ToIntPtr(this.GCHandle);
 
-
-
-
         ulong o;
-
-
         o = (ulong)u;
-
-
-
         return o;
     }
-
-
-
-
-
-    private SystemGCHandle GCHandle;
 }
