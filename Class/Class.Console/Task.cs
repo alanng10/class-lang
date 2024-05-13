329e3c96aa99c5674117c103e09c4247db0ad7f6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-13 11:54:41 +0800

    Class

--- Class/Class.Console/Task.cs
+++ Class/Class.Console/Task.cs
@@ -6,6 +6,7 @@ public class Task : Any
     public virtual string Node { get; set; }
     public virtual string Path { get; set; }
     public virtual string Source { get; set; }
+    public virtual string Dest { get; set; }
     public virtual bool Print { get; set; }
     public virtual Out Out { get; set; }
     public virtual Out Err { get; set; }
