a3021e4730fc05a03a5e181fc8694e01cf1c164e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-16 15:10:09 +0800

    Tool

---
+++ Tool/Z.Tool.Avalon.StorageStatusList/Gen.cs
@@ -0,0 +1,21 @@
+namespace Z.Tool.Avalon.StorageStatusList;
+
+public class Gen : SourceGen
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Namespace = "Avalon.Storage";
+        this.ClassName = "StatusList";
+        this.BaseClassName = "Any";
+        this.AnyClassName = "Any";
+        this.ItemClassName = "Status";
+        this.ArrayClassName = "Array";
+        this.Export = true;
+        this.StatItemClassName = "StorageStatus";
+        this.ItemListFileName = this.GetStatItemListFileName();
+        this.AddMethodFileName = "ToolData/AddMethodStorageStatus.txt";
+        this.InitMethodFileName = "ToolData/InitMethod.txt";
+        return true;
+    }
+}
