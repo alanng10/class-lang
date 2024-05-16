6b836fa038497baa799ca77b9ad124a965b38bc4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-17 01:56:29 +0800

    Class

---
+++ Class/Class.Port/CountReadOperate.cs
@@ -0,0 +1,60 @@
+namespace Class.Port;
+
+public class CountReadOperate : ReadOperate
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.ListInfra = ListInfra.This;
+        this.String = "";
+        this.Array = this.ListInfra.ArrayCreate(0);
+        return true;
+    }
+
+    public virtual Read Read { get; set; }
+    protected virtual ListInfra ListInfra { get; set; }
+    protected virtual string String { get; set; }
+    protected virtual Array Array { get; set; }
+
+    public override string ExecuteString(Text text)
+    {
+        ReadArg arg;
+        arg = this.Read.Arg;
+        arg.StringIndex = arg.StringIndex + 1;
+        arg.StringTextIndex = arg.StringTextIndex + text.Range.Count;
+        return this.String;
+    }
+
+    public override Array ExecuteArray(int count)
+    {
+        ReadArg arg;
+        arg = this.Read.Arg;
+        arg.ArrayIndex = arg.ArrayIndex + 1;
+        return this.Array;
+    }
+
+    public override ModuleRef ExecuteModuleRef()
+    {
+        return null;
+    }
+
+    public override Import ExecuteImport()
+    {
+        return null;
+    }
+
+    public override ImportClass ExecuteImportClass()
+    {
+        return null;
+    }
+
+    public override Export ExecuteExport()
+    {
+        return null;
+    }
+
+    public override Storage ExecuteStorage()
+    {
+        return null;
+    }
+}
--- Class/Class.Port/ReadArg.cs
+++ Class/Class.Port/ReadArg.cs
@@ -3,6 +3,7 @@ namespace Class.Port;
 public class ReadArg : Any
 {
     public virtual int StringIndex { get; set; }
+    public virtual int StringTextIndex { get; set; }
     public virtual int ArrayIndex { get; set; }
     public virtual int ModuleRefIndex { get; set; }
     public virtual int ImportIndex { get; set; }
