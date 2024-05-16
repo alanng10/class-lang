88d8140087b018af21f71889201f50d4ff6a3f50
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-16 15:01:01 +0800

    Infra

--- Infra/Infra/Stat_Composite.cpp
+++ Infra/Infra/Stat_Composite.cpp
@@ -11,19 +11,6 @@ Int Stat_Var_CompositeSourceOut = QPainter::CompositionMode_SourceOut + 1;
 Int Stat_Var_CompositeDestinationOut = QPainter::CompositionMode_DestinationOut + 1;
 Int Stat_Var_CompositeSourceAtop = QPainter::CompositionMode_SourceAtop + 1;
 Int Stat_Var_CompositeDestinationAtop = QPainter::CompositionMode_DestinationAtop + 1;
-Int Stat_Var_CompositeXor = QPainter::CompositionMode_Xor + 1;
-Int Stat_Var_CompositePlus = QPainter::CompositionMode_Plus + 1;
-Int Stat_Var_CompositeMultiply = QPainter::CompositionMode_Multiply + 1;
-Int Stat_Var_CompositeScreen = QPainter::CompositionMode_Screen + 1;
-Int Stat_Var_CompositeOverlay = QPainter::CompositionMode_Overlay + 1;
-Int Stat_Var_CompositeDarken = QPainter::CompositionMode_Darken + 1;
-Int Stat_Var_CompositeLighten = QPainter::CompositionMode_Lighten + 1;
-Int Stat_Var_CompositeColorDodge = QPainter::CompositionMode_ColorDodge + 1;
-Int Stat_Var_CompositeColorBurn = QPainter::CompositionMode_ColorBurn + 1;
-Int Stat_Var_CompositeHardLight = QPainter::CompositionMode_HardLight + 1;
-Int Stat_Var_CompositeSoftLight = QPainter::CompositionMode_SoftLight + 1;
-Int Stat_Var_CompositeDifference = QPainter::CompositionMode_Difference + 1;
-Int Stat_Var_CompositeExclusion = QPainter::CompositionMode_Exclusion + 1;
 
 Int Stat_CompositeSourceOver(Int o)
 {
@@ -68,56 +55,4 @@ Int Stat_CompositeSourceAtop(Int o)
 Int Stat_CompositeDestinationAtop(Int o)
 {
     return Stat_Var_CompositeDestinationAtop;
-}
-Int Stat_CompositeXor(Int o)
-{
-    return Stat_Var_CompositeXor;
-}
-Int Stat_CompositePlus(Int o)
-{
-    return Stat_Var_CompositePlus;
-}
-Int Stat_CompositeMultiply(Int o)
-{
-    return Stat_Var_CompositeMultiply;
-}
-Int Stat_CompositeScreen(Int o)
-{
-    return Stat_Var_CompositeScreen;
-}
-Int Stat_CompositeOverlay(Int o)
-{
-    return Stat_Var_CompositeOverlay;
-}
-Int Stat_CompositeDarken(Int o)
-{
-    return Stat_Var_CompositeDarken;
-}
-Int Stat_CompositeLighten(Int o)
-{
-    return Stat_Var_CompositeLighten;
-}
-Int Stat_CompositeColorDodge(Int o)
-{
-    return Stat_Var_CompositeColorDodge;
-}
-Int Stat_CompositeColorBurn(Int o)
-{
-    return Stat_Var_CompositeColorBurn;
-}
-Int Stat_CompositeHardLight(Int o)
-{
-    return Stat_Var_CompositeHardLight;
-}
-Int Stat_CompositeSoftLight(Int o)
-{
-    return Stat_Var_CompositeSoftLight;
-}
-Int Stat_CompositeDifference(Int o)
-{
-    return Stat_Var_CompositeDifference;
-}
-Int Stat_CompositeExclusion(Int o)
-{
-    return Stat_Var_CompositeExclusion;
 }
