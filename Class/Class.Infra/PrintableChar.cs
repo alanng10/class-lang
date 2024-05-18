7f50b3df6e857251ba2344e4eb081bebdb8479a2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-18 14:30:20 +0800

    Class

--- Class/Class.Infra/PrintableChar.cs
+++ Class/Class.Infra/PrintableChar.cs
@@ -14,52 +14,16 @@ public class PrintableChar : Any
         return share;
     }
 
-    public override bool Init()
-    {
-        base.Init();
-
-        this.AsciiData = new Data();
-        this.AsciiData.Count = 0x80;
-        this.AsciiData.Init();
-
-        this.SetRange('!', '~');
-        return true;
-    }
-
-    protected virtual Data AsciiData { get; set; }
-
     public virtual bool Get(int index)
-    {        
-        int a;
-        a = this.AsciiData.Get(index);
-
-        if (a == -1)
-        {
-            return false;
-        }
-
-        return !(a == 0);
-    }
-
-    protected virtual bool SetRange(int first, int last)
     {
-        Data data;
-        data = this.AsciiData;
+        int first;
+        int last;
+        first = '!';
+        last = '~';
 
-        int count;
-        count = last - first + 1;
-        int i;
-        i = 0;
-        while (i < count)
-        {
-            int n;
-            n = first + i;
+        bool a;
+        a = !(index < first | last < index);
 
-            data.Set(n, 1);
-
-            i = i + 1;
-        }
-
-        return true;
+        return a;
     }
 }
