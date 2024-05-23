7cf35e58993fbb8f4be335f760134936793a8e47
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-23 18:06:47 +0800

    Avalon

--- Avalon/Avalon.Infra/StringCompare.cs
+++ Avalon/Avalon.Infra/StringCompare.cs
@@ -2,19 +2,50 @@ namespace Avalon.Infra;
 
 public class StringCompare : Compare
 {
+    public virtual IntCompare CharCompare { get; set; }
+
     public override int Execute(object left, object right)
     {
-        if (left == null | right == null)
-        {
-            return 0;
-        }
-
         string leftString;
         string rightString;
         leftString = (string)left;
         rightString = (string)right;
-        int a;
-        a = string.CompareOrdinal(leftString, rightString);
-        return a;
+
+        int leftCount;
+        leftCount = leftString.Length;
+
+        int rightCount;
+        rightCount = rightString.Length;
+
+        IntCompare charCompare;
+        charCompare = this.CharCompare;
+
+        int count;
+        count = leftCount;
+        if (rightCount < count)
+        {
+            count = rightCount;
+        }
+
+        int i;
+        i = 0;
+        while (i < count)
+        {
+            char oca;
+            char ocb;
+            oca = leftString[i];
+            ocb = rightString[i];
+
+            int oo;
+            oo = charCompare.Execute(oca, ocb);
+            if (!(oo == 0))
+            {
+                return oo;
+            }
+
+            i = i + 1;
+        }
+
+        return leftCount - rightCount;
     }
 }
