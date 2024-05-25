2ca2506ff03eb0b5645f97bdb20ddec25eb319c9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-05-25 15:53:05 +0800

    Class

--- Class/Class.Port/ImportClass.cs
+++ Class/Class.Port/ImportClass.cs
@@ -2,7 +2,7 @@ namespace Class.Port;
 
 public class ImportClass : Any
 {
-    public virtual string Name { get; set; }
-
     public virtual string Class { get; set; }
+    
+    public virtual string Name { get; set; }
 }
