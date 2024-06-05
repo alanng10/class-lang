520fea700702bae46b983aa0b61a2ac39cc8fe9a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-05 11:33:21 +0800

    Avalon

---
+++ Avalon/Demo/NetworkPeerStatusState.cs
@@ -0,0 +1,35 @@
+namespace Demo;
+
+class NetworkPeerStatusState : State
+{
+    public ThreadNetworkServerState ServerState { get; set; }
+    private int Status { get; set; }
+
+    public override bool Execute()
+    {
+        bool b;
+        b = this.ExecuteAll();
+        if (!b)
+        {
+            this.ServerState.ExitNetwork(this.Status);
+        }
+        return true;
+    }
+
+    private bool ExecuteAll()
+    {
+        NetworkStatusList statusList;
+        statusList = this.ServerState.Demo.NetworkStatusList;
+
+        Network network;
+        network = this.ServerState.Demo.Peer;
+
+        if (!(network.Status == statusList.NoError))
+        {
+            this.Status = 160;
+            return false;
+        }
+
+        return true;
+    }
+}
