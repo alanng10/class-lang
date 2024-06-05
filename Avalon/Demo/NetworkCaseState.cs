442cedf33d9fc24a5232d481aa23b18b08ef9555
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-05 11:31:24 +0800

    Avalon

---
+++ Avalon/Demo/NetworkCaseState.cs
@@ -0,0 +1,40 @@
+namespace Demo;
+
+class NetworkCaseState : State
+{
+    public ThreadNetworkState NetworkState { get; set; }
+    private int Status { get; set; }
+
+    public override bool Execute()
+    {
+        NetworkCaseList caseList;
+        caseList = this.NetworkState.Demo.NetworkCaseList;
+
+        Network network;
+        network = this.NetworkState.Network;
+
+        if (network.Case == caseList.Connected)
+        {
+            NetworkReadyState ka;
+            ka = this.NetworkState.ReadyState;
+
+            Data data;
+            data = ka.Data;
+
+            DataRange range;
+            range = ka.Range;
+
+            data.Set(0, 58);
+
+            range.Count = 1;
+
+            network.Stream.Write(data, range);
+        }
+
+        if (network.Case == caseList.Unconnected)
+        {
+        }
+
+        return true;
+    }
+}
