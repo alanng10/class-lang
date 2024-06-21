38f4c8d7f987d79af90dde94d442c4ea38d5cec7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-21 16:10:32 +0800

    Class

--- Class/Class.Infra/StoragePathCheck.cs
+++ Class/Class.Infra/StoragePathCheck.cs
@@ -5,6 +5,7 @@ public class StoragePathCheck : Any
     public override bool Init()
     {
         base.Init();
+        this.InfraInfra = InfraInfra.This;
         this.TextInfra = TextInfra.This;
 
         IntCompare charCompare;
@@ -14,20 +15,124 @@ public class StoragePathCheck : Any
         this.TextCompare.CharCompare = charCompare;
         this.TextCompare.Init();
 
+        this.Combine = this.TextInfra.TextCreateStringData(this.InfraInfra.PathCombine, null);
         this.BackSlash = this.TextInfra.TextCreateStringData("\\", null);
+        this.Dot = this.TextInfra.TextCreateStringData(".", null);
+        this.DotDot = this.TextInfra.TextCreateStringData("..", null);
         return true;
     }
 
+    protected virtual InfraInfra InfraInfra { get; set; }
     protected virtual TextInfra TextInfra { get; set; }
     protected virtual TextCompare TextCompare { get; set; }
+    protected virtual Text Combine { get; set; }
     protected virtual Text BackSlash { get; set; }
+    protected virtual Text Dot { get; set; }
+    protected virtual Text DotDot { get; set; }
 
-    public virtual bool IsValid(Text text)
+
+    public virtual bool IsValidSourcePath(Text text)
     {
         int k;
         k = this.TextInfra.Index(text, this.BackSlash, this.TextCompare);
 
         if (!(k == -1))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public virtual bool IsValidDestPath(Text text)
+    {
+        TextInfra textInfra;
+        textInfra = this.TextInfra;
+
+        if (!this.IsValidSourcePath(text))
+        {
+            return false;
+        }
+
+        Compare compare;
+        compare = this.TextCompare;
+        Text combine;
+        combine = this.Combine;
+        Text dot;
+        dot = this.Dot;
+        Text dotDot;
+        dotDot = this.DotDot;
+
+        int combineCount;
+        combineCount = combine.Range.Count;
+
+        InfraRange textRange;
+        textRange = text.Range;
+
+        int kaa;
+        int kab;
+        kaa = textRange.Index;
+        kab = textRange.Count;
+
+        bool b;
+        b = false;
+
+        int kk;
+        kk = textInfra.Index(text, combine, compare);
+        while (!b & !(kk == -1))
+        {
+            int e;
+            e = textRange.Count;
+
+            textRange.Count = kk;
+
+            if (!b)
+            {
+                if (textInfra.Equal(text, dot, compare))
+                {
+                    b = true;
+                }
+            }
+
+            if (!b)
+            {
+                if (textInfra.Equal(text, dotDot, compare))
+                {
+                    b = true;
+                }
+            }
+
+            textRange.Count = e;
+            
+            int ka;
+            ka = kk + combineCount;
+
+            textRange.Index = textRange.Index + ka;
+            textRange.Count = textRange.Count - ka;
+
+            kk = textInfra.Index(text, combine, compare);
+        }
+
+        if (!b)
+        {
+            if (textInfra.Equal(text, dot, compare))
+            {
+                b = true;
+            }
+        }
+
+        if (!b)
+        {
+            if (textInfra.Equal(text, dotDot, compare))
+            {
+                b = true;
+            }
+        }
+
+        textRange.Index = kaa;
+        textRange.Count = kab;
+
+        if (b)
         {
             return false;
         }
