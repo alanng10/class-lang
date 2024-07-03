33c86566dfa848b3ae3af34feabbffdfc2336a53
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-03 15:32:23 +0800

    Tool

---
+++ Tool/Z.Tool.Class.NodeList/TraverseGen.cs
@@ -0,0 +1,60 @@
+namespace Z.Tool.Class.NodeList;
+
+public class TraverseGen : Any
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.ToolInfra = ToolInfra.This;
+
+        this.PathSource = this.GetPath("Source");
+        this.PathNode = this.GetPath("Node");
+        this.PathDerive = this.GetPath("Derive");
+        this.PathExecuteNode = this.GetPath("ExecuteNode");
+        this.PathArray = this.GetPath("Array");
+        this.PathField = this.GetPath("Field");
+        return true;
+    }
+
+    public virtual Table ClassTable { get; set; }
+    protected virtual ToolInfra ToolInfra { get; set; }
+    protected virtual string PathOutput { get; set; }
+    protected virtual string PathSource { get; set; }
+    protected virtual string PathNode { get; set; }
+    protected virtual string PathDerive { get; set; }
+    protected virtual string PathExecuteNode { get; set; }
+    protected virtual string PathArray { get; set; }
+    protected virtual string PathField { get; set; }
+
+    public virtual bool Execute()
+    {
+        string textSource;
+        textSource = this.ToolInfra.StorageTextRead(this.PathSource);
+
+        string nodeList;
+        nodeList = this.NodeList();
+
+        string k;
+        k = textSource.Replace("#NodeList#", nodeList);
+    
+        this.ToolInfra.StorageTextWrite(this.PathOutput, k);
+        return true;
+    }
+
+    protected virtual string NodeList()
+    {
+        Table table;
+        table = this.ClassTable;
+
+        Iter iter;
+        iter = table.IterCreate();
+        table.IterSet(iter);
+
+        return null;
+    }
+
+    private string GetPath(string name)
+    {
+        return "ToolData/Traverse" + name + ".txt";
+    }
+}
