eeaeabaffaf9bfeef51ed737eb1e122a6d1d30de
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-03 17:33:48 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/Class.cs
+++ Tool/Z.Tool.Class.NodeList/Class.cs
@@ -4,7 +4,7 @@ public class Class : Any
 {
     public virtual string Name { get; set; }
     public virtual string Base { get; set; }
-    public virtual Array Field { get; set; }
+    public virtual Table Field { get; set; }
     
     public virtual Table Derive { get; set; }
 }
