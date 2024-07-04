f56fb9055c9fc10df49fb55deb8795517fa8eae2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-04 11:34:29 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/Field.cs
+++ Tool/Z.Tool.Class.NodeList/Field.cs
@@ -5,4 +5,6 @@ public class Field : Any
     public virtual string Class { get; set; }
     public virtual string ItemClass { get; set; }
     public virtual string Name { get; set; }
+
+    public virtual bool AnyBool { get; set; }
 }
