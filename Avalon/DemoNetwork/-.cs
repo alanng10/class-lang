bfd5d456b6ea6f0b38280db89fe33feb9f089c2e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-17 17:08:13 +0800

    Avalon

--- Avalon/DemoNetwork/-.cs
+++ Avalon/DemoNetwork/-.cs
@@ -1,5 +1,5 @@
 global using Any = Avalon.Infra.Any;
-global using Console = Avalon.Console.Console;
+global using Console = Avalon.Single.Console;
 global using EntryEntry = Avalon.Entry.Entry;
 global using STAThreadAttribute = System.STAThreadAttribute;
 global using ThreadNetworkState = Demo.ThreadNetworkState;
