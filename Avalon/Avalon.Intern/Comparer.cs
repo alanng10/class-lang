13400437944ead3812406e122969296017fbe030
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-20 21:19:32 +0800

    Avalon

--- Avalon/Avalon.Intern/Comparer.cs
+++ Avalon/Avalon.Intern/Comparer.cs
@@ -2,6 +2,11 @@ namespace Avalon.Intern;
 
 public class Comparer : IComparerObject
 {
+    public virtual bool Init()
+    {
+        return true;
+    }
+
     public virtual int Compare(object x, object y)
     {
         return 0;
