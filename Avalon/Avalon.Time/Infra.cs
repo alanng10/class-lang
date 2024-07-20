7bea077aa1883438414382cba45f9d678ea61790
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-20 21:40:06 +0800

    Avalon

--- Avalon/Avalon.Time/Infra.cs
+++ Avalon/Avalon.Time/Infra.cs
@@ -34,19 +34,6 @@ class Infra : Any
         this.MillisecSystemTickCount = TimeSpan.TicksPerMillisecond;
 
         this.DaySecCount = 24 * 60 * 60;
-
-        this.SystemTickMin = DateTime.MinValue.Ticks;
-        this.SystemTickMax = DateTime.MaxValue.Ticks;
-
-        double k;
-        k = this.DaySystemTickCount;
-
-        long ka;
-        ka = 1 << 30;
-
-        k = k / ka;
-
-        this.SystemTickPerTick = k;
         return true;
     }
 
@@ -63,10 +50,4 @@ class Infra : Any
     public virtual long MillisecSystemTickCount { get; set; }
 
     public virtual int DaySecCount { get; set; }
-
-    public virtual long SystemTickMin { get; set; }
-
-    public virtual long SystemTickMax { get; set; }
-
-    public virtual double SystemTickPerTick { get; set; }
 }
