437eac490cb351d09bae789c22141359026fb321
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-23 18:26:35 +0800

    Avalon

--- Avalon/Avalon.Text/FormatCountState.cs
+++ Avalon/Avalon.Text/FormatCountState.cs
@@ -2,6 +2,15 @@ namespace Avalon.Text;
 
 public class FormatCountState : State
 {
-    public new virtual int Result { get; set; }
+    public override bool Init()
+    {
+        base.Init();
+        Value k;
+        k = new Value();
+        k.Init();
+        this.Result = k;
+        return true;
+    }
+
     public virtual Format Format { get; set; }
 }
