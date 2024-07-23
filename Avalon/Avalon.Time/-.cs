06af5607134f0cd7302f02ef89d4c95889df3874
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-23 13:47:13 +0800

    Avalon

--- Avalon/Avalon.Time/-.cs
+++ Avalon/Avalon.Time/-.cs
@@ -3,7 +3,6 @@ global using Extern = Avalon.Intern.Extern;
 global using IntervalElapseMaide = Avalon.Intern.Extern.Interval_Elapse_Maide;
 global using MaideAddress = Avalon.Intern.MaideAddress;
 global using Handle = Avalon.Intern.Handle;
-global using InternTime = Avalon.Intern.Time;
 global using InternInfra = Avalon.Intern.Infra;
 global using Any = Avalon.Infra.Any;
 global using EventEvent = Avalon.Event.Event;
