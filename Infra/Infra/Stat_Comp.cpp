eb2f24cdbae81bba32f17bdd229ae1def65b3e85
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-23 20:28:59 +0800

    Infra

---
+++ Infra/Infra/Stat_Comp.cpp
@@ -0,0 +1,58 @@
+#include "Stat.hpp"
+
+Int Stat_Var_CompSourceOver = QPainter::CompositionMode_SourceOver + 1;
+Int Stat_Var_CompDestinationOver = QPainter::CompositionMode_DestinationOver + 1;
+Int Stat_Var_CompClear = QPainter::CompositionMode_Clear + 1;
+Int Stat_Var_CompSource = QPainter::CompositionMode_Source + 1;
+Int Stat_Var_CompDestination = QPainter::CompositionMode_Destination + 1;
+Int Stat_Var_CompSourceIn = QPainter::CompositionMode_SourceIn + 1;
+Int Stat_Var_CompDestinationIn = QPainter::CompositionMode_DestinationIn + 1;
+Int Stat_Var_CompSourceOut = QPainter::CompositionMode_SourceOut + 1;
+Int Stat_Var_CompDestinationOut = QPainter::CompositionMode_DestinationOut + 1;
+Int Stat_Var_CompSourceAtop = QPainter::CompositionMode_SourceAtop + 1;
+Int Stat_Var_CompDestinationAtop = QPainter::CompositionMode_DestinationAtop + 1;
+
+Int Stat_CompSourceOver(Int o)
+{
+    return Stat_Var_CompSourceOver;
+}
+Int Stat_CompDestinationOver(Int o)
+{
+    return Stat_Var_CompDestinationOver;
+}
+Int Stat_CompClear(Int o)
+{
+    return Stat_Var_CompClear;
+}
+Int Stat_CompSource(Int o)
+{
+    return Stat_Var_CompSource;
+}
+Int Stat_CompDestination(Int o)
+{
+    return Stat_Var_CompDestination;
+}
+Int Stat_CompSourceIn(Int o)
+{
+    return Stat_Var_CompSourceIn;
+}
+Int Stat_CompDestinationIn(Int o)
+{
+    return Stat_Var_CompDestinationIn;
+}
+Int Stat_CompSourceOut(Int o)
+{
+    return Stat_Var_CompSourceOut;
+}
+Int Stat_CompDestinationOut(Int o)
+{
+    return Stat_Var_CompDestinationOut;
+}
+Int Stat_CompSourceAtop(Int o)
+{
+    return Stat_Var_CompSourceAtop;
+}
+Int Stat_CompDestinationAtop(Int o)
+{
+    return Stat_Var_CompDestinationAtop;
+}
