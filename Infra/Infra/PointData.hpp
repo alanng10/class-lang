c7219cfb718dc9f63b23331f037cddcbee086735
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-24 19:04:23 +0800

    Infra

--- Infra/Infra/PointData.hpp
+++ Infra/Infra/PointData.hpp
@@ -10,3 +10,24 @@ a##_u = CastDoubleToInt(a);\
 Int a##A;\
 a##A = ValueGetFromInternValue(a##_u);\
 
+
+#define PosValue(prefix) \
+Int prefix##Left;\
+prefix##Left = Pos_LeftGet(prefix##Pos);\
+Int prefix##Up;\
+prefix##Up = Pos_UpGet(prefix##Pos);\
+
+
+#define InternValue(a) \
+Int a##_u;\
+a##_u = InternValueGet(a);\
+qreal a##U;\
+a##U = CastIntToDouble(a##_u);\
+
+
+#define InternPosValue(prefix) \
+InternValue(prefix##Left);\
+InternValue(prefix##Up);\
+
+
+#define InternPos(prefix) QPointF prefix##PosU(prefix##LeftU, prefix##UpU);
