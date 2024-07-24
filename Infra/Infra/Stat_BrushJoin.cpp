ab7d2d50745aab158f140084b5b87a2579998901
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-24 20:23:51 +0800

    Infra

---
+++ Infra/Infra/Stat_BrushJoin.cpp
@@ -0,0 +1,23 @@
+#include "Stat.hpp"
+
+Int Stat_Var_BrushJoinMiter = Qt::MiterJoin + 1;
+Int Stat_Var_BrushJoinBevel = Qt::BevelJoin + 1;
+Int Stat_Var_BrushJoinRound = Qt::RoundJoin + 1;
+Int Stat_Var_BrushJoinSvgMiter = Qt::SvgMiterJoin + 1;
+
+Int Stat_BrushJoinMiter(Int o)
+{
+    return Stat_Var_BrushJoinMiter;
+}
+Int Stat_BrushJoinBevel(Int o)
+{
+    return Stat_Var_BrushJoinBevel;
+}
+Int Stat_BrushJoinRound(Int o)
+{
+    return Stat_Var_BrushJoinRound;
+}
+Int Stat_BrushJoinSvgMiter(Int o)
+{
+    return Stat_Var_BrushJoinSvgMiter;
+}
