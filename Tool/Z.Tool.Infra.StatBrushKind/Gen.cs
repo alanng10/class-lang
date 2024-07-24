f05f352c5ee1607c71e51ece7a4cc5626f744de9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-24 14:18:26 +0800

    Tool

--- Tool/Z.Tool.Infra.StatBrushKind/Gen.cs
+++ Tool/Z.Tool.Infra.StatBrushKind/Gen.cs
@@ -5,10 +5,20 @@ class Gen : StatGen
     public override int Execute()
     {
         this.ClassName = "BrushKind";
-        this.ValuePostfix = "Pattern";
         this.ItemListFileName = "ToolData/ItemListBrushKind.txt";
+        this.MethodFileName = "ToolData/MaideBrushKind.txt";
         int o;
         o = base.Execute();
         return o;
+    }
+
+    protected override string GetItemMethod(string method, Iter iter, int index)
+    {
+        return base.GetItemMethod(method, iter, index + 1);
+    }
+
+    protected override string GetShareVarList()
+    {
+        return "";
     }
 }
