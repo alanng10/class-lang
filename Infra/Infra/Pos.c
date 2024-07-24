cc36b0eecda507710819b7c7e371f998f1363767
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-24 20:34:52 +0800

    Infra

--- Infra/Infra/Pos.c
+++ Infra/Infra/Pos.c
@@ -12,38 +12,5 @@ Int Pos_Final(Int o)
     return true;
 }
 
-FieldGet(Pos, Left)
-
-Int Pos_LeftSet(Int o, Int value)
-{
-    Pos* m;
-    m = CastPointer(o);
-
-    SInt oa;
-    oa = value;
-    oa = oa << 4;
-    oa = oa >> 4;
-
-    Int ob;
-    ob = oa;
-    m->Left = ob;
-    return true;
-}
-
-FieldGet(Pos, Up)
-
-Int Pos_UpSet(Int o, Int value)
-{
-    Pos* m;
-    m = CastPointer(o);
-
-    SInt oa;
-    oa = value;
-    oa = oa << 4;
-    oa = oa >> 4;
-
-    Int ob;
-    ob = oa;
-    m->Up = ob;
-    return true;
-}
+Field(Pos, Left)
+Field(Pos, Up)
