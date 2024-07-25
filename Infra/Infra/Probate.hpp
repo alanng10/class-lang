d62894211588694c1a9eb5eec8f6c9d1d08ac61f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-25 12:59:03 +0800

    Infra

--- Infra/Infra/Probate.hpp
+++ Infra/Infra/Probate.hpp
@@ -57,3 +57,60 @@ Int varClass##_##name##Set(Int o, Int value)\
 #define CppField(varClass, name) \
 CppFieldGet(varClass, name)\
 CppFieldSet(varClass, name)
+
+
+#define PosValue(prefix) \
+Int prefix##Left;\
+prefix##Left = Pos_LeftGet(prefix##Pos);\
+Int prefix##Up;\
+prefix##Up = Pos_UpGet(prefix##Pos);\
+
+
+#define SizeValue(prefix) \
+Int prefix##Width;\
+prefix##Width = Size_WidthGet(prefix##Size);\
+Int prefix##Height;\
+prefix##Height = Size_HeightGet(prefix##Size);\
+
+
+#define RectValue(prefix) \
+Int prefix##Pos;\
+prefix##Pos = Rect_PosGet(prefix##Rect);\
+Int prefix##Size;\
+prefix##Size = Rect_SizeGet(prefix##Rect);\
+PosValue(prefix);\
+SizeValue(prefix);\
+
+
+#define InternValue(a) \
+Int a##_u;\
+a##_u = Math_GetInternValue(0, a);\
+qreal a##U;\
+a##U = CastIntToDouble(a##_u);\
+
+
+#define InternPosValue(prefix) \
+InternValue(prefix##Left);\
+InternValue(prefix##Up);\
+
+
+#define InternSizeValue(prefix) \
+InternValue(prefix##Width);\
+InternValue(prefix##Height);\
+
+
+#define InternRectValue(prefix) \
+InternPosValue(prefix);\
+InternSizeValue(prefix);\
+
+
+#define InternPos(prefix) QPointF prefix##PosU(prefix##LeftU, prefix##UpU);
+
+#define InternRect(prefix) QRectF prefix##RectU(prefix##LeftU, prefix##UpU, prefix##WidthU, prefix##HeightU);
+
+#define ValueFromInternValue(a) \
+Int a##_u;\
+a##_u = CastDoubleToInt(a);\
+Int a##A;\
+a##A = Math_GetValueFromInternValue(0, a##_u);\
+
