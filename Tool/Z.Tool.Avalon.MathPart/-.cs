9fd35225754ed0754ded976bdb19e4d0edea1a4e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-25 16:45:27 +0800

    Tool

--- Tool/Z.Tool.Avalon.MathPart/-.cs
+++ Tool/Z.Tool.Avalon.MathPart/-.cs
@@ -1,4 +1,10 @@
 global using Any = Avalon.Infra.Any;
+global using IntCompare = Avalon.Infra.IntCompare;
+global using StringCompare = Avalon.Infra.StringCompare;
+global using Array = Avalon.List.Array;
+global using Table = Avalon.List.Table;
+global using Iter = Avalon.List.Iter;
+global using ListInfra = Avalon.List.Infra;
 global using EntryEntry = Avalon.Entry.Entry;
 global using ToolInfra = Z.Infra.Infra.Infra;
 global using STAThreadAttribute = System.STAThreadAttribute;
--- Tool/Z.Tool.Avalon.MathPart/Gen.cs
+++ Tool/Z.Tool.Avalon.MathPart/Gen.cs
@@ -2,8 +2,73 @@ namespace Z.Tool.Avalon.MathPart;
 
 public class Gen : Any
 {
-    public virtual bool Init()
+    public override bool Init()
     {
+        base.Init();
+        this.ListInfra = ListInfra.This;
+        this.ToolInfra = ToolInfra.This;
+        return true;
+    }
+
+    protected virtual ListInfra ListInfra { get; set; }
+    protected virtual ToolInfra ToolInfra { get; set; }
+    protected virtual Table TrigoTable { get; set; }
+
+    public virtual int Execute()
+    {
+        this.SetTrigoTable();
+
+        
+
+
+        return 0;
+    }
+
+    protected virtual bool SetTrigoTable()
+    {
+        ListInfra listInfra;
+        listInfra = this.ListInfra;
+        ToolInfra toolInfra;
+        toolInfra = this.ToolInfra;
+
+        string ka;
+        ka = toolInfra.StorageTextRead("ToolData/TrigoList.txt");
+
+        Array k;
+        k = toolInfra.SplitLineList(ka);
+        
+        IntCompare kaa;
+        kaa = new IntCompare();
+        kaa.Init();
+
+        StringCompare compare;
+        compare = new StringCompare();
+        compare.CharCompare = kaa;
+        compare.Init();
+
+        Table table;
+        table = new Table();
+        table.Compare = compare;
+        table.Init();
+
+        int count;
+        count = k.Count;
+        int i;
+        i = 0;
+        while (i < count)
+        {
+            string aa;
+            aa = (string)k.GetAt(i);
+
+            if (table.Valid(aa))
+            {
+                return false;
+            }
+
+            listInfra.TableAdd(table, aa, aa);
+
+            i = i + 1;
+        }
         return true;
     }
 }
