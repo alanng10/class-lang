7619dc97f8c069036d8377dd50787157ff4ff78d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-25 12:59:33 +0800

    Infra

--- Infra/Infra/Transform.hpp
+++ Infra/Infra/Transform.hpp
@@ -12,10 +12,3 @@ struct Transform
 };
 
 #define CP(a) ((Transform*)(a))
-
-#define InternValue(a) \
-Int a##_uu;\
-a##_uu = Math_GetInternValue(0, a);\
-qreal a##U;\
-a##U = CastIntToDouble(a##_uu);\
-
