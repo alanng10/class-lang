5c98e5a3b9ac017ab07640c70245c83e862a8858
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-26 11:08:54 +0800

    Tool

---
+++ Tool/Z.Tool.MathGen/InfraPartGen.cs
@@ -0,0 +1,14 @@
+namespace Z.Tool.MathGen;
+
+class InfraPartGen : PartGen
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.PartFilePath = "ToolData/Math/InfraPart.txt";
+        this.MaideFilePath = "ToolData/Math/InfraMaide.txt";
+        this.MaideTwoFilePath = "ToolData/Math/InfraMaideTwo.txt";
+        this.OutputFilePath = "../../Infra/Infra/MathPart.cpp";
+        return true;
+    }
+}
