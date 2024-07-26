25f171a008fdbb49b9d966085da8c6f40bc5957b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-26 14:46:18 +0800

    Avalon

---
+++ Avalon/Avalon.Math/Infra.cs
@@ -0,0 +1,26 @@
+namespace Avalon.Math;
+
+public class Infra : Any
+{
+    public static Infra This { get; } = ShareCreate();
+
+    private static Infra ShareCreate()
+    {
+        Infra share;
+        share = new Infra();
+        Any a;
+        a = share;
+        a.Init();
+        return share;
+    }
+
+    public virtual long Int(Math math, Comp comp, long value)
+    {
+        comp.Significand = value;
+        comp.Exponent = 0;
+
+        long a;
+        a = math.Value(comp);
+        return a;
+    }
+}
