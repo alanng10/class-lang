8ca32208fbf5b6c982e001cdb4430a10bf0404fd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-27 19:54:04 +0800

    Avalon

---
+++ Avalon/Avalon.Draw/PointList.cs
@@ -0,0 +1,52 @@
+namespace Avalon.Draw;
+
+public class PointList : Any
+{
+    public override bool Init()
+    {
+        base.Init();
+
+        int count;
+        count = this.Count;
+        ulong countU;
+        countU = (ulong)count;
+
+        ulong share;
+        share = Extern.Infra_Share();
+
+        ulong stat;
+        stat = Extern.Share_Stat(share);
+
+        ulong ua;
+        ua = Extern.Stat_PointDataCount(stat);
+
+        ulong dataCount;
+        dataCount = countU * ua;
+
+        ulong dataValue;
+        dataValue = Extern.New(dataCount);
+
+        this.InternDataValue = dataValue;
+
+        ulong intern;
+        intern = Extern.Data_New();
+        Extern.Data_Init(intern);
+        Extern.Data_CountSet(intern, dataCount);
+        Extern.Data_ValueSet(intern, dataValue);
+        this.Intern = intern;
+        return true;
+    }
+
+    public virtual bool Final()
+    {
+        Extern.Data_Final(this.Intern);
+        Extern.Data_Delete(this.Intern);
+
+        Extern.Delete(this.InternDataValue);
+        return true;
+    }
+
+    public virtual int Count { get; set; }
+    internal virtual ulong Intern { get; set; }
+    private ulong InternDataValue { get; set; }
+}
