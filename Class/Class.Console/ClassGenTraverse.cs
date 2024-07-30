adcc50f78e9fd0d1358f8ff13709e8d3e3584723
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-30 18:04:48 +0800

    Class

--- Class/Class.Console/ClassGenTraverse.cs
+++ Class/Class.Console/ClassGenTraverse.cs
@@ -4,6 +4,11 @@ public class ClassGenTraverse : Traverse
 {
     public virtual ClassGen Gen { get; set; }
 
+    protected virtual bool TextIndent()
+    {
+        return this.Gen.TextIndent();
+    }
+
     protected virtual bool Text(string text)
     {
         return this.Gen.Text(text);
