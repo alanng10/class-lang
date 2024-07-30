1a5cf570fa53152156acccbec768192a6d7d4208
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-30 17:57:40 +0800

    Class

--- Class/Class.Console/ClassGenTraverse.cs
+++ Class/Class.Console/ClassGenTraverse.cs
@@ -3,4 +3,25 @@ namespace Class.Console;
 public class ClassGenTraverse : Traverse
 {
     public virtual ClassGen Gen { get; set; }
+
+    protected virtual bool Text(string text)
+    {
+        ClassGenOperate o;
+        o = this.Gen.Operate;
+        
+        int count;
+        count = text.Length;
+        int i;
+        i = 0;
+        while (i < count)
+        {
+            char oc;
+            oc = text[i];
+
+            o.ExecuteChar(oc);
+
+            i = i + 1;
+        }
+        return true;
+    }
 }
