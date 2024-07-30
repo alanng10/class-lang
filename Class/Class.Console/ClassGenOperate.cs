17d47097293090bdebbb9a5bf24d6ef74843de6a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-30 16:48:27 +0800

    Class

--- Class/Class.Console/ClassGenOperate.cs
+++ Class/Class.Console/ClassGenOperate.cs
@@ -4,17 +4,7 @@ public class ClassGenOperate : Any
 {
     public virtual ClassGen Gen { get; set; }
 
-    public virtual bool ExecuteText(string o)
-    {
-        return false;
-    }
-
-    public virtual bool ExecuteChar(char o)
-    {
-        return false;
-    }
-
-    public virtual bool ExecuteIntFormat(long o)
+    public virtual bool ExecuteByte(int o)
     {
         return false;
     }
