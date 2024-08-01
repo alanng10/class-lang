d5e0071575f0b4e64ae2c43c955e778fbe026e81
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-01 15:04:06 +0800

    Avalon

--- Avalon/Avalon.Infra/StringJoin.cs
+++ Avalon/Avalon.Infra/StringJoin.cs
@@ -5,12 +5,10 @@ public class StringJoin : Any
     public override bool Init()
     {
         base.Init();
-        this.InfraInfra = Infra.This;
         this.Builder = new StringBuilder();
         return true;
     }
 
-    protected virtual Infra InfraInfra { get; set; }
     private StringBuilder Builder { get; set; }
 
     public virtual string Result()
@@ -24,35 +22,9 @@ public class StringJoin : Any
         return true;
     }
 
-    public virtual bool Append(Data a)
+    public virtual bool Append(char oc)
     {
-        Infra infraInfra;
-        infraInfra = this.InfraInfra;
-
-        int ka;
-        ka = sizeof(char);
-
-        long k;
-        k = a.Count;
-        k = k / ka;
-
-        int count;
-        count = (int)k;
-        int i;
-        i = 0;
-        while (i < count)
-        {
-            long index;
-            index = i;
-            index = index * ka;
-
-            char oc;
-            oc = infraInfra.DataCharGet(a, index);
-
-            this.Builder.Append(oc);
-
-            i = i + 1;
-        }
+        this.Builder.Append(oc);
         return true;
     }
 }
