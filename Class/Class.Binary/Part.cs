8548e913d0a352ef94ab5439597aba2ca23f60a7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-02 11:06:21 +0800

    Class

--- Class/Class.Binary/Part.cs
+++ Class/Class.Binary/Part.cs
@@ -2,6 +2,12 @@ namespace Class.Binary;
 
 public class Part : Any
 {
+    public virtual int FieldIndex { get; set; }
+    public virtual int FieldCount { get; set; }
+
+    public virtual int MaideIndex { get; set; }
+    public virtual int MaideCount { get; set; }
+
     public virtual Array Field { get; set; }
 
     public virtual Array Maide { get; set; }
