94f271e4090e2d997280c75e90d59705110d151c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-04 07:21:59 +0800

    Class

--- Class/Class.Console/ClassGenTraverse.cs
+++ Class/Class.Console/ClassGenTraverse.cs
@@ -208,19 +208,19 @@ public class ClassGenTraverse : Traverse
         ClassGen gen;
         gen = this.Gen;
 
-        string argA;
-        string argB;
-        argA = gen.VarA;
-        argB = gen.VarB;
+        string varA;
+        string varB;
+        varA = gen.VarA;
+        varB = gen.VarB;
 
-        gen.EvalValueGet(2, argA);
-        gen.EvalValueGet(1, argB);
+        gen.EvalValueGet(2, varA);
+        gen.EvalValueGet(1, varB);
 
-        gen.OperateDelimit(argA, argA, argB, gen.DelimitEqual);
+        gen.OperateDelimit(varA, varA, varB, gen.DelimitEqual);
 
-        gen.VarMaskSet(argA, gen.RefKindBoolMask);
+        gen.VarMaskSet(varA, gen.RefKindBoolMask);
 
-        gen.EvalValueSet(2, argA);
+        gen.EvalValueSet(2, varA);
 
         gen.EvalIndexPosSet(-1);
 
