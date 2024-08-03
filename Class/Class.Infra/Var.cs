5a6dfc831a7abc921465d4c3c883e982fdccc48c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-03 17:50:47 +0800

    Class

--- Class/Class.Infra/Var.cs
+++ Class/Class.Infra/Var.cs
@@ -4,5 +4,6 @@ public class Var : Any
 {
     public virtual string Name { get; set; }
     public virtual Class Class { get; set; }
+    public virtual int Index { get ;set; }
     public virtual object Any { get; set; }
 }
