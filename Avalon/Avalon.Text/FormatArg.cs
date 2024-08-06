2b1778e34287a6b932f809e46cc2d99156002b62
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-07 01:15:05 +0800

    Avalon

--- Avalon/Avalon.Text/FormatArg.cs
+++ Avalon/Avalon.Text/FormatArg.cs
@@ -2,16 +2,11 @@ namespace Avalon.Text;
 
 public class FormatArg : Any
 {
-    public virtual int Pos { get { return __D_Pos; } set { __D_Pos = value; } }
-    protected int __D_Pos;
-    public virtual int Kind { get { return __D_Kind; } set { __D_Kind = value; } }
-    protected int __D_Kind;
-    public virtual bool ValueBool { get { return __D_ValueBool; } set { __D_ValueBool = value; } }
-    protected bool __D_ValueBool;
-    public virtual long ValueInt { get { return __D_ValueInt; } set { __D_ValueInt = value; } }
-    protected long __D_ValueInt;
-    public virtual Text ValueText { get { return __D_ValueText; } set { __D_ValueText = value; } }
-    protected Text __D_ValueText;
+    public virtual int Pos { get; set; }
+    public virtual int Kind { get; set; }
+    public virtual bool ValueBool { get; set; }
+    public virtual long ValueInt { get; set; }
+    public virtual Text ValueText { get; set; }
     public virtual bool AlignLeft { get; set; }
     public virtual int FieldWidth { get; set; }
     public virtual int MaxWidth { get; set; }
