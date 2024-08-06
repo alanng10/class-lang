51737adcad7de4049cabb88ffadd394715a7a6fd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-07 01:04:20 +0800

    Tool

---
+++ Tool/Z.Tool.Avalon.StorageStatusList/Gen.cs
@@ -0,0 +1,27 @@
+namespace Z.Tool.Avalon.StorageStatusList;
+
+public class Gen : AvalonSourceGen
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Namespace = "Avalon.Storage";
+        this.ClassName = "StatusList";
+        this.BaseClassName = "Any";
+        this.AnyClassName = "Any";
+        this.ItemClassName = "Status";
+        this.ArrayClassName = "Array";
+        this.Export = true;
+        this.StatItemClassName = "StorageStatus";
+        this.ItemListFileName = this.GetStatItemListFileName();
+        this.AddMethodFileName = "ToolData/Avalon/AddMaideStorageStatus.txt";
+        this.InitMethodFileName = "ToolData/Avalon/InitMaide.txt";
+        return true;
+    }
+
+    protected override bool AppendInitFieldAddItem(StringBuilder sb, string index, object value)
+    {
+        sb.Append("AddItem").Append("(").Append(")");
+        return true;
+    }
+}
