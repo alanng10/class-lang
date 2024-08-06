17b7543198235b8759a5be055227bb2920500267
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-07 01:00:39 +0800

    Avalon

--- Avalon/Avalon.Storage/StatusList.cs
+++ Avalon/Avalon.Storage/StatusList.cs
@@ -57,12 +57,15 @@ public class StatusList : Any
 
     protected virtual Status AddItem()
     {
+        int index;
+        index = this.Index;
+
         Status item;
         item = new Status();
         item.Init();
-        item.Index = this.Index;
-        this.Array.SetAt(item.Index, item);
-        this.Index = this.Index + 1;
+        item.Index = index;
+        this.Array.SetAt(index, item);
+        this.Index = index + 1;
         return item;
     }
 
