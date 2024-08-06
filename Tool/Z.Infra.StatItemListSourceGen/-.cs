eb89caec5642cb9b7ad0d6c96263a1fcdb78fec7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-07 00:35:48 +0800

    Tool

--- Tool/Z.Infra.StatItemListSourceGen/-.cs
+++ Tool/Z.Infra.StatItemListSourceGen/-.cs
@@ -1,4 +1,5 @@
 global using Any = Avalon.Infra.Any;
 global using TableEntry = Avalon.List.Entry;
 global using SourceGen = Z.Infra.ListSourceGen.Gen;
+global using AvalonSourceGen = Z.Infra.ListSourceGen.AvalonGen;
 global using StringBuilder = System.Text.StringBuilder;
