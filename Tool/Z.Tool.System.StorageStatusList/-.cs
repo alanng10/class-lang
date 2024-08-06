85eb63456db5c7c1a158aa19b679d8484ae60c80
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-07 00:51:05 +0800

    Tool

--- Tool/Z.Tool.System.StorageStatusList/-.cs
+++ Tool/Z.Tool.System.StorageStatusList/-.cs
@@ -1,4 +1,5 @@
 global using EntryEntry = Avalon.Entry.Entry;
 global using SourceGen = Z.Infra.StatItemListSourceGen.Gen;
+global using AvalonSourceGen = Z.Infra.StatItemListSourceGen.AvalonGen;
 global using STAThreadAttribute = System.STAThreadAttribute;
 global using StringBuilder = System.Text.StringBuilder;
