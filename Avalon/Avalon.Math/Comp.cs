10605cf833121603561be2c1d4266f871eafeafd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-06 15:40:36 +0800

    Avalon

--- Avalon/Avalon.Math/Comp.cs
+++ Avalon/Avalon.Math/Comp.cs
@@ -2,8 +2,6 @@ namespace Avalon.Math;
 
 public class Comp : Any
 {
-    public virtual long Significand { get { return __D_Significand; } set { __D_Significand = value; } }
-    protected long __D_Significand;
-    public virtual long Exponent { get { return __D_Exponent; } set { __D_Exponent = value; } }
-    protected long __D_Exponent;
+    public virtual long Significand { get; set; }
+    public virtual long Exponent { get; set; }
 }
