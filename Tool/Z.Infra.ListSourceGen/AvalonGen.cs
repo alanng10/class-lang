2924bc1cfb23209a5e02bfd55cd454dc9c6ef813
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-07 18:27:52 +0800

    Tool

--- Tool/Z.Infra.ListSourceGen/AvalonGen.cs
+++ Tool/Z.Infra.ListSourceGen/AvalonGen.cs
@@ -6,8 +6,8 @@ public class AvalonGen : Gen
     {
         base.Init();
         this.ClassFileName = "ToolData/Avalon/Class.txt";
-        this.InitMethodFileName = "ToolData/Avalon/InitMethod.txt";
-        this.AddMethodFileName = "ToolData/Avalon/AddMethod.txt";
+        this.InitMethodFileName = "ToolData/Avalon/InitMaide.txt";
+        this.AddMethodFileName = "ToolData/Avalon/AddMaide.txt";
         this.ArrayCompListFileName = "ToolData/Avalon/ArrayCompList.txt";
         this.ItemListFileName = "ToolData/Avalon/ItemList.txt";
         return true;
