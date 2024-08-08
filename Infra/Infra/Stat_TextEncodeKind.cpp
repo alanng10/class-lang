15dfd28aeb58ac2f059df043a3fc1917c646308a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-08 21:06:17 +0800

    Infra

--- Infra/Infra/Stat_TextEncodeKind.cpp
+++ Infra/Infra/Stat_TextEncodeKind.cpp
@@ -1,18 +1,15 @@
 #include "Stat.hpp"
 
-Int Stat_Var_TextEncodeKindUtf8 = QStringConverter::Utf8 + 1;
-Int Stat_Var_TextEncodeKindUtf16 = QStringConverter::Utf16 + 1;
-Int Stat_Var_TextEncodeKindUtf32 = QStringConverter::Utf32 + 1;
 
 Int Stat_TextEncodeKindUtf8(Int o)
 {
-    return Stat_Var_TextEncodeKindUtf8;
+    return 1;
 }
 Int Stat_TextEncodeKindUtf16(Int o)
 {
-    return Stat_Var_TextEncodeKindUtf16;
+    return 2;
 }
 Int Stat_TextEncodeKindUtf32(Int o)
 {
-    return Stat_Var_TextEncodeKindUtf32;
+    return 3;
 }
