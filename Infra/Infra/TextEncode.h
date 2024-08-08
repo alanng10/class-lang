e6c2786349abd0eed36f646fa5ac1bbd433db673
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-09 00:03:23 +0800

    Infra

--- Infra/Infra/TextEncode.h
+++ Infra/Infra/TextEncode.h
@@ -16,3 +16,10 @@ Int TextEncode_ExecuteCount16To8(Int o, Int data);
 Int TextEncode_ExecuteCount16To32(Int o, Int data);
 Int TextEncode_ExecuteCount8To16(Int o, Int data);
 Int TextEncode_ExecuteCount8To32(Int o, Int data);
+
+Int TextEncode_ExecuteResult32To8(Int o, Int result, Int data);
+Int TextEncode_ExecuteResult32To16(Int o, Int result, Int data);
+Int TextEncode_ExecuteResult16To8(Int o, Int result, Int data);
+Int TextEncode_ExecuteResult16To32(Int o, Int result, Int data);
+Int TextEncode_ExecuteResult8To16(Int o, Int result, Int data);
+Int TextEncode_ExecuteResult8To32(Int o, Int result, Int data);
