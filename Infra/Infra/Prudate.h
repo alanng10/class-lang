19c49b5fa62b5bf7921807aacc3d8d0beef9ef85
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-08 23:30:21 +0800

    Infra

--- Infra/Infra/Prudate.h
+++ Infra/Infra/Prudate.h
@@ -135,8 +135,8 @@ Infra_Api Int Array_ItemGet(Int o, Int index);
 Infra_Api Int Array_ItemSet(Int o, Int index, Int value);
 
 InfraApiNew(TextEncode)
-Infra_Api Int TextEncode_ExecuteCount(Int o, Int innKnud, Int outKind, Int data);
-Infra_Api Int TextEncode_ExecuteResult(Int o, Int result, Int innKnud, Int outKind, Int data);
+Infra_Api Int TextEncode_ExecuteCount(Int o, Int innKind, Int outKind, Int data);
+Infra_Api Int TextEncode_ExecuteResult(Int o, Int result, Int innKind, Int outKind, Int data);
 
 InfraApiNew(Format)
 Infra_Api Int Format_ExecuteCount(Int o, Int varBase, Int argList);
