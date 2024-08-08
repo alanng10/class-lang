42892cf7e1bac4033ba15e6dd4422a3a4ba9f537
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-08 21:04:59 +0800

    Tool

--- Tool/Z.Tool.Infra.StatTextEncodeKind/Gen.cs
+++ Tool/Z.Tool.Infra.StatTextEncodeKind/Gen.cs
@@ -1,38 +1,32 @@
 namespace Z.Tool.Infra.StatTextEncodeKind;
 
-
-
-
-
 class Gen : StatGen
 {
     public override int Execute()
     {
         this.ClassName = "TextEncodeKind";
 
+        this.ScopeName = "";
 
-
-
-        this.ScopeName = "QStringConverter";
-
-
-
-
-        this.ValueOffset = " + 1";
-
-
-
+        this.ScopeSeparator = "";
 
         this.ItemListFileName = "ToolData/ItemListTextEncodeKind.txt";
 
-
-
+        this.MethodFileName = "ToolData/MaideTextEncodeKind.txt";
 
         int o;
-        
         o = base.Execute();
 
-
         return o;
+    }
+
+    protected override string GetItemMethod(string method, Iter iter, int index)
+    {
+        return base.GetItemMethod(method, iter, index + 1);
+    }
+
+    protected override string GetShareVarList()
+    {
+        return "";
     }
 }
