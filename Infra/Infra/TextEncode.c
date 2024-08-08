36ecdabc4bbcef678f2c2a9c8562fb697a915bc1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-08 23:45:18 +0800

    Infra

--- Infra/Infra/TextEncode.c
+++ Infra/Infra/TextEncode.c
@@ -2,6 +2,13 @@
 
 InfraClassNew(TextEncode)
 
+TextEncode_CountMaide CountMaideArray[3][3] =
+{
+    { null, TextEncode_ExecuteCount8To16, TextEncode_ExecuteCount8To32},
+    { TextEncode_ExecuteCount16To8, null, TextEncode_ExecuteCount16To32},
+    { TextEncode_ExecuteCount32To8, TextEncode_ExecuteCount32To16, null}
+};
+
 Int TextEncode_Init(Int o)
 {
     return true;
@@ -12,13 +19,43 @@ Int TextEncode_Final(Int o)
     return true;
 }
 
-Int TextEncode_ExecuteCount(Int o, Int innKind, Int outKind, Int count)
+Int TextEncode_ExecuteCount(Int o, Int innKind, Int outKind, Int data)
 {
     return 0;
 }
 
-Int TextEncode_ExecuteResult(Int o, Int result, Int innKind, Int outKind, Int count)
+Int TextEncode_ExecuteResult(Int o, Int result, Int innKind, Int outKind, Int data)
 {
     return 0;
 }
 
+
+Int TextEncode_ExecuteCount32To8(Int o, Int data)
+{
+    return 0;
+}
+
+Int TextEncode_ExecuteCount32To16(Int o, Int data)
+{
+    return 0;
+}
+
+Int TextEncode_ExecuteCount16To8(Int o, Int data)
+{
+    return 0;
+}
+
+Int TextEncode_ExecuteCount16To32(Int o, Int data)
+{
+    return 0;
+}
+
+Int TextEncode_ExecuteCount8To16(Int o, Int data)
+{
+    return 0;
+}
+
+Int TextEncode_ExecuteCount8To32(Int o, Int data)
+{
+    return 0;
+}
--- Infra/Infra/TextEncode.h
+++ Infra/Infra/TextEncode.h
@@ -6,3 +6,13 @@ typedef struct
 {
 }
 TextEncode;
+
+typedef Int (*TextEncode_CountMaide)(Int o, Int data);
+typedef Int (*TextEncode_ResultMaide)(Int o, Int result, Int data);
+
+Int TextEncode_ExecuteCount32To8(Int o, Int data);
+Int TextEncode_ExecuteCount32To16(Int o, Int data);
+Int TextEncode_ExecuteCount16To8(Int o, Int data);
+Int TextEncode_ExecuteCount16To32(Int o, Int data);
+Int TextEncode_ExecuteCount8To16(Int o, Int data);
+Int TextEncode_ExecuteCount8To32(Int o, Int data);
