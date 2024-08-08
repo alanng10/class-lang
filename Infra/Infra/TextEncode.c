712e893fd1be495db9ce0957677a0d5874e196db
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-09 00:07:11 +0800

    Infra

--- Infra/Infra/TextEncode.c
+++ Infra/Infra/TextEncode.c
@@ -9,6 +9,14 @@ TextEncode_CountMaide CountMaideArray[3][3] =
     { TextEncode_ExecuteCount32To8, TextEncode_ExecuteCount32To16, null}
 };
 
+TextEncode_ResultMaide ResultMaideArray[3][3] =
+{
+    { null, TextEncode_ExecuteResult8To16, TextEncode_ExecuteResult8To32},
+    { TextEncode_ExecuteResult16To8, null, TextEncode_ExecuteResult16To32},
+    { TextEncode_ExecuteResult32To8, TextEncode_ExecuteResult32To16, null}
+};
+
+
 Int TextEncode_Init(Int o)
 {
     return true;
@@ -149,4 +157,94 @@ Int TextEncode_ExecuteCount8To16(Int o, Int data)
 Int TextEncode_ExecuteCount8To32(Int o, Int data)
 {
     return 0;
+}
+
+Int TextEncode_ExecuteResult32To8(Int o, Int result, Int data)
+{
+    Int dataCount;
+    Int dataValue;
+    dataCount = Data_CountGet(data);
+    dataValue = Data_ValueGet(data);
+
+    Int resultValue;
+    resultValue = Data_ValueGet(result);
+
+    Char* p;
+    p = CastPonter(dataValue);
+
+    Byte* dest;
+    dest = CastPointer(resultValue);
+
+    Int ka;
+    ka = 4;
+
+    Int k;
+    k = 0;
+
+    Int count;
+    count = dataCount / ka;
+    Int i;
+    i = 0;
+    while (i < count)
+    {
+        Char oc;
+        oc = p[i];
+
+        if (oc < 0x80)
+        {
+            Byte oba;
+            oba = oc;
+
+            dest[k] = oba;
+
+            k = k + 1;
+        }
+
+        if (!(oc < 0x80) & oc < 0x800)
+        {
+            k = k + 2;
+        }
+
+        if (!(oc < 0x800) & oc < 0x10000)
+        {
+            k = k + 3;
+        }
+
+        if (!(oc < 0x10000) & oc < 0x110000)
+        {
+            k = k + 4;
+        }
+
+        i = i + 1;
+    }
+
+    Int a;
+    a = k;
+    return a;
+}
+
+
+Int TextEncode_ExecuteResult32To16(Int o, Int result, Int data)
+{
+
+}
+
+Int TextEncode_ExecuteResult16To8(Int o, Int result, Int data)
+{
+
+}
+
+Int TextEncode_ExecuteResult16To32(Int o, Int result, Int data)
+{
+
+}
+
+Int TextEncode_ExecuteResult8To16(Int o, Int result, Int data)
+{
+
+}
+
+Int TextEncode_ExecuteResult8To32(Int o, Int result, Int data)
+{
+
 }
