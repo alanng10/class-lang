f0429384712c992a761fc553830eed522fdcb7cb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-10 02:54:31 +0800

    Infra

--- Infra/Infra/TextEncode.c
+++ Infra/Infra/TextEncode.c
@@ -216,31 +216,8 @@ Int TextEncode_ExecuteResult16To32(Int o, Int result, Int data)
 
 Int TextEncode_ExecuteResult8To16(Int o, Int result, Int data)
 {
-    Int dataCount;
-    Int dataValue;
-    dataCount = Data_CountGet(data);
-    dataValue = Data_ValueGet(data);
-
-    Int resultValue;
-    resultValue = Data_ValueGet(result);
-
-    Byte* p;
-    p = CastPointer(dataValue);
-
-    Int16* dest;
-    dest = CastPointer(resultValue);
-
-    Int k;
-    k = 0;
-
-    Bool b;
-    b = true;
-
-    Int count;
-    count = dataCount;
-
-    Int i;
-    i = 0;
+    StartDest(Int16);
+    Start(Byte);
     while (b & (i < count))
     {
         Char oc;
