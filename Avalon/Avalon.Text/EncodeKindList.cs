6a17820710fcf310bae9812bf0e134d3b9471ef9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-10 22:47:29 +0800

    Avalon

--- Avalon/Avalon.Text/EncodeKindList.cs
+++ Avalon/Avalon.Text/EncodeKindList.cs
@@ -28,23 +28,13 @@ public class EncodeKindList : Any
 
         this.Utf8 = this.AddItem(Extern.Stat_TextEncodeKindUtf8(stat));
         this.Utf16 = this.AddItem(Extern.Stat_TextEncodeKindUtf16(stat));
-        this.Utf16LE = this.AddItem(Extern.Stat_TextEncodeKindUtf16LE(stat));
-        this.Utf16BE = this.AddItem(Extern.Stat_TextEncodeKindUtf16BE(stat));
         this.Utf32 = this.AddItem(Extern.Stat_TextEncodeKindUtf32(stat));
-        this.Utf32LE = this.AddItem(Extern.Stat_TextEncodeKindUtf32LE(stat));
-        this.Utf32BE = this.AddItem(Extern.Stat_TextEncodeKindUtf32BE(stat));
-        this.Latin1 = this.AddItem(Extern.Stat_TextEncodeKindLatin1(stat));
         return true;
     }
 
     public virtual EncodeKind Utf8 { get; set; }
     public virtual EncodeKind Utf16 { get; set; }
-    public virtual EncodeKind Utf16LE { get; set; }
-    public virtual EncodeKind Utf16BE { get; set; }
     public virtual EncodeKind Utf32 { get; set; }
-    public virtual EncodeKind Utf32LE { get; set; }
-    public virtual EncodeKind Utf32BE { get; set; }
-    public virtual EncodeKind Latin1 { get; set; }
 
     protected virtual EncodeKind AddItem(ulong o)
     {
@@ -68,7 +58,7 @@ public class EncodeKindList : Any
 
     protected virtual Array Array { get; set; }
 
-    protected virtual int ArrayCount { get { return 8; } set { } }
+    protected virtual int ArrayCount { get { return 3; } set { } }
 
     public virtual int Count { get; set; }
     
