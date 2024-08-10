972f7ea26f9e7539a2dce7e69a9fc03e7293a47f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-10 22:59:30 +0800

    Avalon

--- Avalon/Avalon.Text/Encode.cs
+++ Avalon/Avalon.Text/Encode.cs
@@ -8,108 +8,66 @@ public class Encode : Any
         this.InternIntern = InternIntern.This;
         this.InfraInfra = InfraInfra.This;
         this.TextInfra = Infra.This;
-
-        this.InternData = Extern.Data_New();
-        Extern.Data_Init(this.InternData);
-        this.InternString = Extern.String_New();
-        Extern.String_Init(this.InternString);
-
-        ulong ua;
-        ua = this.Kind.Intern;
-        bool ba;
-        ba = this.WriteBom;
-        ulong ub;
-        ub = (ulong)(ba ? 1 : 0);
-
-        this.Intern = Extern.TextEncode_New();
-        Extern.TextEncode_KindSet(this.Intern, ua);
-        Extern.TextEncode_WriteBomSet(this.Intern, ub);
-        Extern.TextEncode_Init(this.Intern);
         return true;
     }
-
-    public virtual bool Final()
-    {
-        Extern.TextEncode_Final(this.Intern);
-        Extern.TextEncode_Delete(this.Intern);
-
-        Extern.String_Final(this.InternString);
-        Extern.String_Delete(this.InternString);
-
-        Extern.Data_Final(this.InternData);
-        Extern.Data_Delete(this.InternData);
-        return true;
-    }
-
-    public virtual EncodeKind Kind { get; set; }
-    public virtual bool WriteBom { get; set; }
 
     private InternIntern InternIntern { get; set; }
     protected virtual InfraInfra InfraInfra { get; set; }
     protected virtual Infra TextInfra { get; set; }
     private ulong Intern { get; set; }
-    private ulong InternString { get; set; }
-    private ulong InternData { get; set; }
 
-    public virtual int TextCountMax(long dataCount)
+    public virtual long ExecuteCount(EncodeKind innKind, EncodeKind outKind, Data data, RangeInt dataRange)
     {
-        ulong ua;
-        ua = (ulong)dataCount;
-        ulong u;
-        u = Extern.TextEncode_StringCountMax(this.Intern, ua);
-        int a;
-        a = (int)u;
-        return a;
-    }
+        long dataIndex;
+        long dataCount;
+        dataIndex = dataRange.Index;
+        dataCount = dataRange.Count;
 
-    public virtual int Text(Text text, Data data, RangeInt range)
-    {
-        if (!this.InfraInfra.ValidRangeInt(data.Count, range.Index, range.Count))
+        if (!this.InfraInfra.ValidRangeInt(data.Count, dataIndex, dataCount))
         {
             return -1;
         }
 
-        long dataCount;
-        dataCount = range.Count;
-        ulong ua;
-        ua = (ulong)dataCount;
-        ulong dataU;
-        dataU = this.InternData;
-        Extern.Data_CountSet(dataU, ua);
+        ulong dataIndexU;
+        ulong dataCountU;
+        dataIndexU = (ulong)dataIndex;
+        dataCountU = (ulong)dataCount;
 
-        int a;
-        a = this.InternIntern.TextEncodeString(this.Intern, text.Data.Value, text.Range.Index, dataU, data.Value, range.Index);
-        return a;
-    }
-
-    public virtual long DataCountMax(int textCount)
-    {
-        ulong ua;
-        ua = (ulong)textCount;
         ulong u;
-        u = Extern.TextEncode_DataCountMax(this.Intern, ua);
+        u = this.InternIntern.TextEncodeCount(innKind.Intern, outKind.Intern, data.Value, dataIndexU, dataCountU);
+
         long a;
         a = (long)u;
         return a;
     }
 
-    public virtual long Data(Data data, long index, Text text)
+    public virtual bool ExecuteResult(Text result, EncodeKind innKind, EncodeKind outKind, Data data, RangeInt dataRange)
     {
-        if (!this.TextInfra.ValidRange(text))
+        if (!this.TextInfra.ValidRange(result))
         {
-            return -1;
+            return false;
         }
 
-        int textCount;
-        textCount = text.Range.Count;
-        ulong ua;
-        ua = (ulong)textCount;
-        ulong textU;
-        textU = this.InternString;
-        Extern.String_CountSet(textU, ua);
+        long dataIndex;
+        long dataCount;
+        dataIndex = dataRange.Index;
+        dataCount = dataRange.Count;
 
-        long a;
-        a = this.InternIntern.TextEncodeData(this.Intern, data.Value, index, textU, text.Data.Value, text.Range.Index);
-        return a;
+        if (!this.InfraInfra.ValidRangeInt(data.Count, dataIndex, dataCount))
+        {
+            return false;
+        }
+
+        ulong dataIndexU;
+        ulong dataCountU;
+        dataIndexU = (ulong)dataIndex;
+        dataCountU = (ulong)dataCount;
+
+        ulong resultIndex;
+        resultIndex = (ulong)result.Range.Index;
+
+        this.InternIntern.TextEncodeResult(result.Data.Value, resultIndex, innKind.Intern, outKind.Intern, data.Value, dataIndexU, dataCountU);
+
+        return true;
     }
 }
