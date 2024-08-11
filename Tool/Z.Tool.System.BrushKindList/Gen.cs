03ebc98c612f3e2828bb0cdf702ce0c75df0d4d5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-11 21:17:25 +0800

    Tool

---
+++ Tool/Z.Tool.System.BrushKindList/Gen.cs
@@ -0,0 +1,19 @@
+namespace Z.Tool.System.BrushKindList;
+
+public class Gen : SourceGen
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Namespace = "System.Draw";
+        this.ClassName = "BrushKindList";
+        this.BaseClassName = "Any";
+        this.AnyClassName = "Any";
+        this.ItemClassName = "BrushKind";
+        this.ArrayClassName = "Array";
+        this.Export = true;
+        this.StatItemClassName = "BrushKind";
+        this.ItemListFileName = this.GetStatItemListFileName();
+        return true;
+    }
+}
