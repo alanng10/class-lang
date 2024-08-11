b6146d40861daabe9937d7684654bc57f2c0335f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-11 23:13:25 +0800

    Tool

---
+++ Tool/Z.Tool.System.GradientSpreadList/Gen.cs
@@ -0,0 +1,19 @@
+namespace Z.Tool.System.GradientSpreadList;
+
+public class Gen : SourceGen
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Namespace = "System.Draw";
+        this.ClassName = "GradientSpreadList";
+        this.BaseClassName = "Any";
+        this.AnyClassName = "Any";
+        this.ItemClassName = "GradientSpread";
+        this.ArrayClassName = "Array";
+        this.Export = true;
+        this.StatItemClassName = "GradientSpread";
+        this.ItemListFileName = this.GetStatItemListFileName();
+        return true;
+    }
+}
