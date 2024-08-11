eb5286e5d9a461c29c6a15e8d0943e51d430e919
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-11 17:29:18 +0800

    Infra

--- Infra/Infra/Draw.hpp
+++ Infra/Infra/Draw.hpp
@@ -32,7 +32,7 @@ struct Draw
 
 #define CP(a) ((Draw*)(a))
 
-#define TextCountMax 4096
+#define TextCountMax 1024
 
 Int Draw_TextSet(Int o, Int textData, Int textCount);
 Int Draw_QStringSetRaw(Int result, Int data, Int count);
