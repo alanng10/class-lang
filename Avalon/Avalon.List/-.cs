7bacbbc3d825f82de1bfaae3e1a420232c194e23
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-13 22:13:52 +0800

    Avalon

--- Avalon/Avalon.List/-.cs
+++ Avalon/Avalon.List/-.cs
@@ -1,6 +1,4 @@
-global using InternComparer = Avalon.Intern.Comparer;
 global using Any = Avalon.Infra.Any;
 global using Range = Avalon.Infra.Range;
 global using Compare = Avalon.Infra.Compare;
 global using InfraInfra = Avalon.Infra.Infra;
-global using SystemArray = System.Array;
