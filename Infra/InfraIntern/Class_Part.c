3728a82f18089f42dffdffae8257233dbbd0a91b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-13 12:18:35 +0800

    Infra

--- Infra/InfraIntern/Class_Part.c
+++ Infra/InfraIntern/Class_Part.c
@@ -1,7 +1,9 @@
 #include "Class.h"
 
-Int Intern_Intern_MaideCall[2] = 
+Int Intern_Intern_MaideCall[4] = 
 { 
     CastInt(Intern_Any_Init),
-    CastInt(Intern_Intern_RefCompare)
+    CastInt(Intern_Intern_RefCompare),
+    CastInt(Intern_Intern_StringChar),
+    CastInt(Intern_Intern_StringCount)
 };
--- Infra/InfraIntern/Probate_Part.h
+++ Infra/InfraIntern/Probate_Part.h
@@ -3,3 +3,5 @@
 #include "Prusate.h"
 
 Int Intern_Intern_RefCompare(Eval* eval, Int frame);
+Int Intern_Intern_StringChar(Eval* eval, Int frame);
+Int Intern_Intern_StringCount(Eval* eval, Int frame);
