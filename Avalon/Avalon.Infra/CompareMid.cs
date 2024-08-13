ad433d68672f0f492870e7045d193b3633b5ec15
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-13 14:51:42 +0800

    Avalon

--- Avalon/Avalon.Infra/CompareMid.cs
+++ Avalon/Avalon.Infra/CompareMid.cs
@@ -4,6 +4,21 @@ public class CompareMid : Any
 {
     public virtual int Execute(int left, int right)
     {
-        return left - right;
+        long k;
+        k = left;
+        k = k - right;
+
+        int a;
+        a = 0;
+        if (k < 0)
+        {
+            a = -1;
+        }
+        if (0 < k)
+        {
+            a = 1;
+        }
+        
+        return a;
     }
 }
