2657de1dbd51a511bd6ace6b2c09bffcc3c731cd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-13 12:04:42 +0800

    Infra

--- Infra/InfraIntern/Class_Part.c
+++ Infra/InfraIntern/Class_Part.c
@@ -1,3 +1,7 @@
 #include "Class.h"
 
-Int Intern_Intern_MaideCall[1] = { CastInt(Intern_Any_Init) };
+Int Intern_Intern_MaideCall[2] = 
+{ 
+    CastInt(Intern_Any_Init),
+    CastInt(Intern_Intern_RefCompare)
+};
--- Infra/InfraIntern/Probate.h
+++ Infra/InfraIntern/Probate.h
@@ -6,4 +6,4 @@ extern Int Intern_Intern_MaideCall[];
 
 Int Intern_Any_Init(Eval* eval, Int frame);
 
-Int Intern_RefCompare(Eval* eval, Int frame);
+Int Intern_Intern_RefCompare(Eval* eval, Int frame);
