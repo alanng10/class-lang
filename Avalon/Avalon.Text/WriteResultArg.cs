fe56e527a9992634b8b8f0b5b2f3cdaa4f16540b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-15 02:57:14 +0800

    Avalon

---
+++ Avalon/Avalon.Text/WriteResultArg.cs
@@ -0,0 +1,7 @@
+namespace Avalon.Text;
+
+public class WriteResultArg : Any
+{
+    public virtual WriteArg Arg { get; set; }
+    public virtual Text Result { get; set; }
+}
--- Avalon/Avalon.Text/WriteResultState.cs
+++ Avalon/Avalon.Text/WriteResultState.cs
@@ -2,6 +2,15 @@ namespace Avalon.Text;
 
 public class WriteResultState : State
 {
+    public override bool Init()
+    {
+        base.Init();
+        WriteResultArg k;
+        k = new WriteResultArg();
+        k.Init();
+        this.Arg = k;
+        return true;
+    }
+
     public virtual Write Format { get; set; }
-    public virtual Text ArgResult { get; set; }
 }
