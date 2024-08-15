2776571420dd9fee265cfe935b0cdf4d39146ed8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-15 15:01:58 +0800

    Avalon

--- Avalon/Avalon.Text/WriteArg.cs
+++ Avalon/Avalon.Text/WriteArg.cs
@@ -2,6 +2,14 @@ namespace Avalon.Text;
 
 public class WriteArg : Any
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.Value = new Value();
+        this.Value.Init();
+        return true;
+    }
+
     public virtual long Pos { get; set; }
     public virtual long Kind { get; set; }
     public virtual Value Value { get; set; }
