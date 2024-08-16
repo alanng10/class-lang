b07592d3116521b040ae28d9257abe64205dceae
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-16 16:51:46 +0800

    Avalon

--- Avalon/Avalon.Text/Code.cs
+++ Avalon/Avalon.Text/Code.cs
@@ -2,6 +2,18 @@ namespace Avalon.Text;
 
 public class Code : Any
 {
+    public static Code This { get; } = ShareCreate();
+
+    private static Code ShareCreate()
+    {
+        Code share;
+        share = new Code();
+        Any a;
+        a = share;
+        a.Init();
+        return share;
+    }
+
     public override bool Init()
     {
         base.Init();
