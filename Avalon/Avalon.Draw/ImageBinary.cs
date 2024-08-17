7b86065f8875e9beb54a32507627b88277ddb937
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 05:21:35 +0800

    Avalon

--- Avalon/Avalon.Draw/ImageBinary.cs
+++ Avalon/Avalon.Draw/ImageBinary.cs
@@ -1,6 +1,6 @@
 namespace Avalon.Draw;
 
-public class ImageFormat : Any
+public class ImageBinary : Any
 {
     public virtual long Index { get; set; }
     internal virtual ulong Intern { get; set; }
