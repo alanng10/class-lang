6744a4d7a2b4e25d606706103c82fa83cc5f4c43
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 04:40:18 +0800

    Tool

--- Tool/Z.Tool.Avalon.StorageStatusList/Gen.cs
+++ Tool/Z.Tool.Avalon.StorageStatusList/Gen.cs
@@ -5,17 +5,17 @@ public class Gen : SourceGen
     public override bool Init()
     {
         base.Init();
-        this.Namespace = "Avalon.Storage";
-        this.ClassName = "StatusList";
-        this.BaseClassName = "Any";
-        this.AnyClassName = "Any";
-        this.ItemClassName = "Status";
-        this.ArrayClassName = "Array";
+        this.Namespace = this.S("Avalon.Storage");
+        this.ClassName = this.S("StatusList");
+        this.BaseClassName = this.S("Any");
+        this.AnyClassName = this.S("Any");
+        this.ItemClassName = this.S("Status");
+        this.ArrayClassName = this.S("Array");
         this.Export = true;
-        this.StatItemClassName = "StorageStatus";
+        this.StatItemClassName = this.S("StorageStatus");
         this.ItemListFileName = this.GetStatItemListFileName();
-        this.AddMethodFileName = "ToolData/Avalon/AddMaideStorageStatus.txt";
-        this.InitMethodFileName = "ToolData/Avalon/InitMaide.txt";
+        this.AddMethodFileName = this.S("ToolData/Avalon/AddMaideStorageStatus.txt");
+        this.InitMethodFileName = this.S("ToolData/Avalon/InitMaide.txt");
         return true;
     }
 
