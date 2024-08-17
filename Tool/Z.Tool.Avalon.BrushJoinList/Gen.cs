23b9587da0198fe88d1e8d251101305704107394
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 04:22:29 +0800

    Tool

--- Tool/Z.Tool.Avalon.BrushJoinList/Gen.cs
+++ Tool/Z.Tool.Avalon.BrushJoinList/Gen.cs
@@ -5,14 +5,14 @@ public class Gen : SourceGen
     public override bool Init()
     {
         base.Init();
-        this.Namespace = "Avalon.Draw";
-        this.ClassName = "BrushJoinList";
-        this.BaseClassName = "Any";
-        this.AnyClassName = "Any";
-        this.ItemClassName = "BrushJoin";
-        this.ArrayClassName = "Array";
+        this.Namespace = this.S("Avalon.Draw");
+        this.ClassName = this.S("BrushJoinList");
+        this.BaseClassName = this.S("Any");
+        this.AnyClassName = this.S("Any");
+        this.ItemClassName = this.S("BrushJoin");
+        this.ArrayClassName = this.S("Array");
         this.Export = true;
-        this.StatItemClassName = "BrushJoin";
+        this.StatItemClassName = this.S("BrushJoin");
         this.ItemListFileName = this.GetStatItemListFileName();
         return true;
     }
