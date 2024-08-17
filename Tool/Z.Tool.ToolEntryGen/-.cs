92008a98baa8c3ffe8652f83cff71d6d86713f92
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 03:41:00 +0800

    Tool

--- Tool/Z.Tool.ToolEntryGen/-.cs
+++ Tool/Z.Tool.ToolEntryGen/-.cs
@@ -1,4 +1,7 @@
 global using Any = Avalon.Infra.Any;
+global using String = Avalon.Infra.String;
+global using Text = Avalon.Text.Text;
+global using TextInfra = Avalon.Text.Infra;
 global using EntryEntry = Avalon.Entry.Entry;
 global using ToolInfra = Z.Infra.Infra.Infra;
 global using STAThreadAttribute = System.STAThreadAttribute;
--- Tool/Z.Tool.ToolEntryGen/Gen.cs
+++ Tool/Z.Tool.ToolEntryGen/Gen.cs
@@ -5,54 +5,57 @@ class Gen : Any
     public override bool Init()
     {
         base.Init();
+        this.TextInfra = TextInfra.This;
         this.ToolInfra = ToolInfra.This;
         return true;
     }
 
+    protected virtual TextInfra TextInfra { get; set; }
     protected virtual ToolInfra ToolInfra { get; set; }
-    protected virtual string SourceTemplate { get; set; }
+    protected virtual String SourceTemplate { get; set; }
 
     public virtual int Execute()
     {
         this.SourceTemplate = this.ToolInfra.StorageTextRead(this.S("ToolData/Entry.txt"));
 
         this.ExecuteOne("Avalon.TextEncodeKindList");
-        this.ExecuteOne("Avalon.BrushKindList");
-        this.ExecuteOne("Avalon.BrushLineList");
+        this.ExecuteOne("Avalon.StorageStatusList");
+        this.ExecuteOne("Avalon.NetworkCaseList");
+        this.ExecuteOne("Avalon.NetworkPortKindList");
+        this.ExecuteOne("Avalon.NetworkStatusList");
         this.ExecuteOne("Avalon.BrushCapList");
         this.ExecuteOne("Avalon.BrushJoinList");
+        this.ExecuteOne("Avalon.BrushKindList");
+        this.ExecuteOne("Avalon.BrushLineList");
         this.ExecuteOne("Avalon.DrawCompList");
         this.ExecuteOne("Avalon.GradientKindList");
         this.ExecuteOne("Avalon.GradientSpreadList");
         this.ExecuteOne("Avalon.ImageFormatList");
         this.ExecuteOne("Avalon.TextAlignList");
-        this.ExecuteOne("Avalon.NetworkCaseList");
-        this.ExecuteOne("Avalon.NetworkPortKindList");
-        this.ExecuteOne("Avalon.NetworkStatusList");
         this.ExecuteOne("Class.CountList");
         this.ExecuteOne("Class.DelimitList");
         this.ExecuteOne("Class.ErrorKindList");
         this.ExecuteOne("Class.KeywordList");
         this.ExecuteOne("Class.NodeList");
         this.ExecuteOne("Class.TaskKindList");
-        this.ExecuteOne("Infra.StatBrushKind");
-        this.ExecuteOne("Infra.StatComp");
-        this.ExecuteOne("Infra.StatGradientKind");
-        this.ExecuteOne("Infra.StatGradientSpread");
-        this.ExecuteOne("Infra.StatImageFormat");
+        this.ExecuteOne("Infra.StatThreadCase");
+        this.ExecuteOne("Infra.StatStreamKind");
         this.ExecuteOne("Infra.StatNetworkCase");
         this.ExecuteOne("Infra.StatNetworkPortKind");
         this.ExecuteOne("Infra.StatNetworkStatus");
-        this.ExecuteOne("Infra.StatPenCap");
-        this.ExecuteOne("Infra.StatPenJoin");
-        this.ExecuteOne("Infra.StatPenKind");
         this.ExecuteOne("Infra.StatStorageMode");
         this.ExecuteOne("Infra.StatStorageStatus");
-        this.ExecuteOne("Infra.StatStreamKind");
+        this.ExecuteOne("Infra.StatComp");
+        this.ExecuteOne("Infra.StatBrushCap");
+        this.ExecuteOne("Infra.StatBrushJoin");
+        this.ExecuteOne("Infra.StatBrushKind");
+        this.ExecuteOne("Infra.StatBrushLine");
+        this.ExecuteOne("Infra.StatGradientKind");
+        this.ExecuteOne("Infra.StatGradientSpread");
+        this.ExecuteOne("Infra.StatImageFormat");
         this.ExecuteOne("Infra.StatTextAlign");
         this.ExecuteOne("Infra.StatTextEncodeKind");
         this.ExecuteOne("Infra.StatTextWrap");
-        this.ExecuteOne("Infra.StatThreadCase");
         this.ExecuteOne("MathGen");
         this.ExecuteOne("PrudateGen");
         this.ExecuteOne("ReferBinaryGen");
@@ -62,13 +65,74 @@ class Gen : Any
 
     protected virtual bool ExecuteOne(string toolName)
     {
-        string a;
-        a = this.SourceTemplate.Replace("#Name#", toolName);
+        String ka;
+        ka = this.S(toolName);
 
-        string k;
-        k = "../../Tool/Z.Tool." + toolName + "/Entry.cs";
+        Text k;
+        k = this.TextCreate(this.SourceTemplate);
+        k = this.Replace(k, "#Name#", ka);
 
-        this.ToolInfra.StorageTextWrite(k, a);
+        String a;
+        a = this.StringCreate(k);
+
+        String path;
+        path = this.AddClear().AddS("../../Tool/Z.Tool.").Add(ka).AddS("/Entry.cs").AddResult();
+
+        this.ToolInfra.StorageTextWrite(path, a);
         return true;
+    }
+
+    protected virtual Text Replace(Text text, string delimit, String join)
+    {
+        return this.TextReplace(text, this.TextCreate(this.S(delimit)), this.TextCreate(join));
+    }
+
+    protected virtual Text TextCreate(String o)
+    {
+        return this.TextInfra.TextCreateStringData(o, null);
+    }
+
+    protected virtual String StringCreate(Text text)
+    {
+        return this.TextInfra.StringCreate(text);
+    }
+
+    protected virtual Text TextReplace(Text text, Text delimit, Text join)
+    {
+        return this.ToolInfra.TextReplace(text, delimit, join);
+    }
+
+    public virtual Gen Add(String a)
+    {
+        this.ToolInfra.Add(a);
+        return this;
+    }
+
+    public virtual Gen AddS(string o)
+    {
+        this.ToolInfra.AddS(o);
+        return this;
+    }
+
+    public virtual Gen AddClear()
+    {
+        this.ToolInfra.AddClear();
+        return this;
+    }
+
+    public virtual String AddResult()
+    {
+        return this.ToolInfra.AddResult();
+    }
+
+    public virtual Gen AddIndent(long indent)
+    {
+        this.ToolInfra.AddIndent(indent);
+        return this;
+    }
+
+    protected virtual String S(string o)
+    {
+        return this.ToolInfra.S(o);
     }
 }
