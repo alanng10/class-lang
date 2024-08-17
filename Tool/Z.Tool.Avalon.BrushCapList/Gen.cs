6f6da1c1152a73a2f28ed3ef0b3eeb6bc37aecf2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 04:20:21 +0800

    Tool

--- Tool/Z.Tool.Avalon.BrushCapList/Gen.cs
+++ Tool/Z.Tool.Avalon.BrushCapList/Gen.cs
@@ -5,14 +5,14 @@ public class Gen : SourceGen
     public override bool Init()
     {
         base.Init();
-        this.Namespace = "Avalon.Draw";
-        this.ClassName = "BrushCapList";
-        this.BaseClassName = "Any";
-        this.AnyClassName = "Any";
-        this.ItemClassName = "BrushCap";
-        this.ArrayClassName = "Array";
+        this.Namespace = this.S("Avalon.Draw");
+        this.ClassName = this.S("BrushCapList");
+        this.BaseClassName = this.S("Any");
+        this.AnyClassName = this.S("Any");
+        this.ItemClassName = this.S("BrushCap");
+        this.ArrayClassName = this.S("Array");
         this.Export = true;
-        this.StatItemClassName = "BrushCap";
+        this.StatItemClassName = this.S("BrushCap");
         this.ItemListFileName = this.GetStatItemListFileName();
         return true;
     }
