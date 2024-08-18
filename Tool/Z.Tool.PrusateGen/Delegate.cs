9fc87d60312225a588b69956e2334d2414b23d30
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 14:49:19 +0800

    Tool

--- Tool/Z.Tool.PrusateGen/Delegate.cs
+++ Tool/Z.Tool.PrusateGen/Delegate.cs
@@ -2,7 +2,7 @@ namespace Z.Tool.PrusateGen;
 
 public class Delegate : Any
 {
-    public virtual string Name { get; set; }
+    public virtual String Name { get; set; }
 
     public virtual Array Param { get; set; }
 }
