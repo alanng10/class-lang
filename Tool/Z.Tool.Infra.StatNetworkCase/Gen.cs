cbcb1c0575f1aa755827c23ba5a54077c11b2868
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 10:53:10 +0800

    Tool

--- Tool/Z.Tool.Infra.StatNetworkCase/Gen.cs
+++ Tool/Z.Tool.Infra.StatNetworkCase/Gen.cs
@@ -1,40 +1,15 @@
 namespace Z.Tool.Infra.StatNetworkCase;
 
-
-
-
-
 class Gen : StatGen
 {
-    public override int Execute()
+    public override long Execute()
     {
         this.ClassName = "NetworkCase";
-
-
-
         this.ScopeName = "QAbstractSocket";
-
-
-
         this.ValuePostfix = "State";
-
-
-
         this.ValueOffset = " + 1";
-
-
-
-
         this.ItemListFileName = "ToolData/ItemListNetworkCase.txt";
 
-
-
-
-        int o;
-        
-        o = base.Execute();
-
-
-        return o;
+        return base.Execute();
     }
 }
