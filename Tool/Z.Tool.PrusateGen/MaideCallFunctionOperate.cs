93c583bd1b8a582f1179be6050125baa113b4d46
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 14:24:48 +0800

    Tool

--- Tool/Z.Tool.PrusateGen/MaideCallFunctionOperate.cs
+++ Tool/Z.Tool.PrusateGen/MaideCallFunctionOperate.cs
@@ -1,8 +1,5 @@
 namespace Z.Tool.PrusateGen;
 
-
-
-
 class MaideCallFunctionOperate : FunctionOperate
 {
     public virtual Maide Method { get; set; }
@@ -19,54 +16,36 @@ class MaideCallFunctionOperate : FunctionOperate
         return true;
     }
 
-
-
-
-    public override bool ExecuteParam(StringBuilder sb)
+    public override bool ExecuteParam()
     {
         Array param;
-
         param = this.Method.Param;
 
-
-
         bool b;
-
         b = this.Method.Static;
-
 
         if (b)
         {
             bool ba;
-
             ba = (param.Count == 0);
-
 
             if (!ba)
             {
-                string oa;
-
+                String oa;
                 oa = this.Gen.GetParamItem(param, 0);
 
+                this.Gen.AddVarDeclare(oa);
 
-                this.Gen.AppendVarDeclare(sb, oa);
-
-
-                this.Gen.AppendParamWithOffset(sb, param, 1);
+                this.Gen.AddParamWithOffset(param, 1);
             }
         }
 
-
         if (!b)
         {
-            this.Gen.AppendVarDeclare(sb, "o");
+            this.Gen.AddVarDeclare(this.S("o"));
 
-
-            this.Gen.AppendParamWithOffset(sb, param, 0);
+            this.Gen.AddParamWithOffset(param, 0);
         }
-
-
-
         return true;
     }
 }
