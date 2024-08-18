84e1266618481afdc1751826db15d49228b5f187
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-18 11:45:17 +0800

    Tool

--- Tool/Z.Tool.Infra.StatThreadCase/Gen.cs
+++ Tool/Z.Tool.Infra.StatThreadCase/Gen.cs
@@ -1,55 +1,25 @@
 namespace Z.Tool.Infra.StatThreadCase;
 
-
-
-
-
 class Gen : StatGen
 {
-    public override int Execute()
+    public override long Execute()
     {
-        this.ClassName = "ThreadCase";
+        this.ClassName = this.S("ThreadCase");
+        this.ScopeName = this.S("");
+        this.ScopeSeparator = this.S("");
+        this.ItemListFileName = this.S("ToolData/Infra/ItemListThreadCase.txt");
+        this.MethodFileName = this.S("ToolData/Infra/MaideThreadCase.txt");
 
-
-
-        this.ScopeName = "";
-
-
-
-        this.ScopeSeparator = "";
-        
-
-
-
-        this.ItemListFileName = "ToolData/ItemListThreadCase.txt";
-
-
-        this.MethodFileName = "ToolData/MethodThreadCase.txt";
-
-
-
-
-        int o;
-        
-        o = base.Execute();
-
-
-        return o;
+        return base.Execute();
     }
 
-
-
-
-    protected override string GetItemMethod(string method, Iter iter, int index)
+    protected override String GetItemMethod(String method, Iter iter, long index)
     {
         return base.GetItemMethod(method, iter, index + 1);
     }
 
-
-
-
-    protected override string GetShareVarList()
+    protected override String GetShareVarList()
     {
-        return "";
+        return this.S("");
     }
 }
