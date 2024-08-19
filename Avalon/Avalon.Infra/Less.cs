c14b8a8fb651630fe8e3cb3aecbd240c320e14d1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-20 05:36:09 +0800

    Avalon

--- Avalon/Avalon.Infra/Less.cs
+++ Avalon/Avalon.Infra/Less.cs
@@ -2,7 +2,7 @@ namespace Avalon.Infra;
 
 public class Less : Any
 {
-    public virtual long Execute(object left, object right)
+    public virtual long Execute(object lite, object rite)
     {
         return 0;
     }
