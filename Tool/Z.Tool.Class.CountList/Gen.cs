568134774af7ee99cce168401a20c500a3a49975
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-20 09:35:59 +0800

    Tool

--- Tool/Z.Tool.Class.CountList/Gen.cs
+++ Tool/Z.Tool.Class.CountList/Gen.cs
@@ -5,12 +5,12 @@ public class Gen : SourceGen
     public override bool Init()
     {
         base.Init();
-        this.Module = "Class.Infra";
-        this.ClassName = "CountList";
-        this.BaseClassName = "Any";
-        this.AnyClassName = "Any";
-        this.ItemClassName = "Count";
-        this.ArrayClassName = "Array";
+        this.Module = this.S("Class.Infra");
+        this.ClassName = this.S("CountList");
+        this.BaseClassName = this.S("Any");
+        this.AnyClassName = this.S("Any");
+        this.ItemClassName = this.S("Count");
+        this.ArrayClassName = this.S("Array");
         this.Export = true;
         this.ItemListFileName = "ToolData/ItemListCount.txt";
         this.AddMethodFileName = "ToolData/AddMethodCount.txt";
