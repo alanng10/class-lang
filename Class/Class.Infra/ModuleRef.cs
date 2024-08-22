9f708d4a613117580eee2afa0b48c7a7168677ab
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-08-22 14:26:27 +0800

    Class

--- Class/Class.Infra/ModuleRef.cs
+++ Class/Class.Infra/ModuleRef.cs
@@ -2,6 +2,6 @@ namespace Class.Infra;
 
 public class ModuleRef : Any
 {
-    public virtual string Name { get; set; }
+    public virtual String Name { get; set; }
     public virtual long Version { get; set; }
 }
