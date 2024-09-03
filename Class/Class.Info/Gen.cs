9d02408c778ca9f688e80cfefd5670c65cbb41f7
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-03 10:51:21 +0800

    Class

--- Class/Class.Info/Gen.cs
+++ Class/Class.Info/Gen.cs
@@ -671,69 +671,11 @@ public class Gen : Any
         return k;
     }
 
-    protected virtual bool AppendIndent(int count)
+    protected virtual Array FoldList(String foldPath)
     {
-        int i;
-        i = 0;
-        while (i < count)
-        {
-            this.Append("    ");
-            i = i + 1;
-        }
-        return true;
-    }
-
-    protected virtual bool Append(string text)
-    {
-        this.InfraInfra.StringJoinString(this.StringJoin, text);
-        return true;
-    }
-
-    protected virtual Array FoldList(string foldPath)
-    {
-        string[] u;
-        u = Directory.GetDirectories(foldPath);
-
-        int count;
-        count = u.Length;
-        int i;
-        i = 0;
-        while (i < count)
-        {
-            string path;
-            path = u[i];
-
-            string name;
-            name = Path.GetFileName(path);
-
-            u[i] = name;
-            i = i + 1;
-        }
-
-        Array array;
-        array = this.ListInfra.ArrayCreate(count);
-
-        i = 0;
-        while (i < count)
-        {
-            string k;
-            k = u[i];
-
-            array.SetAt(i, k);
-            i = i + 1;
-        }
-
-        Range range;
-        range = new Range();
-        range.Init();
-        range.Count = count;
-
-        StringLess less;
-        less = this.InfraInfra.StringLessCreate();
-
-        array.Sort(range, less);
-
-        return array;
+        Array a;
+        a = this.StorageArrange.FoldList(foldPath);
+        return a;
     }
 
     protected virtual Text Replace(Text text, string limit, String join)
