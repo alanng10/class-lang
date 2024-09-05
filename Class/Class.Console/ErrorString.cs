5d684d8323e8fc5d2db9157196995ce905394c61
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-06 07:24:52 +0800

    Class

--- Class/Class.Console/ErrorString.cs
+++ Class/Class.Console/ErrorString.cs
@@ -6,43 +6,55 @@ public class ErrorString : ClassInfraGen
     {
         base.Init();
         this.BorderLine = this.StringComp.CreateChar('-', 50);
+        this.SKind = this.S("Kind");
+        this.SName = this.S("Name");
+        this.SRange = this.S("Range");
+        this.SSource = this.S("Source");
         this.SSpace = this.S(" ");
         this.SComma = this.S(",");
+        this.SColon = this.S(":");
         this.SBraceRoundLite = this.S("(");
         this.SBraceRoundRite = this.S(")");
         return true;
     }
 
     protected virtual String BorderLine { get; set; }
+    protected virtual String SKind { get; set; }
+    protected virtual String SName { get; set; }
+    protected virtual String SRange { get; set; }
+    protected virtual String SSource { get; set; }
     protected virtual String SSpace { get; set; }
     protected virtual String SComma { get; set; }
+    protected virtual String SColon { get; set; }
     protected virtual String SBraceRoundLite { get; set; }
     protected virtual String SBraceRoundRite { get; set; }
 
     public virtual String Execute(Error error)
     {
+        this.AddClear();
+
         this.AddBorder();
 
-        this.AppendField(h, "Kind", this.KindString(error));
+        this.AddField(this.SKind, this.KindString(error));
 
         bool b;
         b = (error.Source == null);
 
         if (b)
         {
-            string kk;
+            String kk;
             kk = error.Name;
             
             if (!(kk == null))
             {
-                this.AppendField(h, "Name", kk);
+                this.AddField(this.SName, kk);
             }
         }
         if (!b)
         {
-            this.AppendField(h, "Range", this.RangeString(error));
+            this.AddField(this.SRange, this.RangeString(error));
 
-            this.AppendField(h, "Source", this.SourceString(error));
+            this.AddField(this.SSource, this.SourceString(error));
         }
 
         this.AddBorder();
@@ -59,13 +71,9 @@ public class ErrorString : ClassInfraGen
         return true;
     }
 
-    protected virtual bool AppendField(StringJoin h, string word, string value)
+    protected virtual bool AddField(String word, String value)
     {
-        this.Append(h, word);
-        this.Append(h, ":");
-        this.Append(h, " ");
-        this.Append(h, value);
-        this.Append(h, this.NewLine);
+        this.Add(word).Add(this.SColon).Add(this.SSpace).Add(value).AddLine();
         return true;
     }
 
