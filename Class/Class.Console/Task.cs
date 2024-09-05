1c4313f878bc8c3d96c8e4356a7ea4811b9d5374
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-06 07:26:01 +0800

    Class

--- Class/Class.Console/Task.cs
+++ Class/Class.Console/Task.cs
@@ -3,10 +3,10 @@ namespace Class.Console;
 public class Task : Any
 {
     public virtual TaskKind Kind { get; set; }
-    public virtual string Node { get; set; }
-    public virtual string Path { get; set; }
-    public virtual string Source { get; set; }
-    public virtual string Dest { get; set; }
+    public virtual String Node { get; set; }
+    public virtual String Path { get; set; }
+    public virtual String Source { get; set; }
+    public virtual String Dest { get; set; }
     public virtual bool ArgBool { get; set; }
     public virtual bool Print { get; set; }
     public virtual Out Out { get; set; }
