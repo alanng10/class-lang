eaa1ce51b49ac53e5bdc59d12c0c56da5e4dbe1c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-06 15:35:19 +0800

    Class

--- Class/Class.Info/-.cs
+++ Class/Class.Info/-.cs
@@ -1,13 +1,10 @@
 global using Any = Avalon.Infra.Any;
 global using String = Avalon.Infra.String;
-global using CharForm = Avalon.Infra.CharForm;
 global using Array = Avalon.List.Array;
 global using Table = Avalon.List.Table;
 global using Iter = Avalon.List.Iter;
 global using ListInfra = Avalon.List.Infra;
 global using Text = Avalon.Text.Text;
-global using Write = Avalon.Text.Write;
-global using WriteArg = Avalon.Text.WriteArg;
 global using TextInfra = Avalon.Text.Infra;
 global using Time = Avalon.Time.Time;
 global using StorageArrange = Avalon.Storage.Comp;
