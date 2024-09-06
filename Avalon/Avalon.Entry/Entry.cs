186d11e679536639d9bfda4c3c0df1c211399c32
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-06 13:17:43 +0800

    Avalon

--- Avalon/Avalon.Entry/Entry.cs
+++ Avalon/Avalon.Entry/Entry.cs
@@ -78,6 +78,11 @@ public class Entry : Any
         string[] ao;
         ao = this.InternArg;
 
+        if (ao == null)
+        {
+            return true;
+        }
+
         long count;
         count = ao.LongLength;
         Array array;
