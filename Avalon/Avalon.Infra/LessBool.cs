3fc6fd2cb85d31974712465bb4a27b0b70c3a87a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-09 11:19:39 +0800

    Avalon

--- Avalon/Avalon.Infra/LessBool.cs
+++ Avalon/Avalon.Infra/LessBool.cs
@@ -2,17 +2,17 @@ namespace Avalon.Infra;
 
 public class LessBool : Any
 {
-    public virtual long Execute(bool left, bool right)
+    public virtual long Execute(bool lite, bool rite)
     {
-        long leftA;
-        long rightA;
-        leftA = this.BoolInt(left);
-        rightA = this.BoolInt(right);
-        
-        return leftA - rightA;
+        long liteA;
+        long riteA;
+        liteA = this.IntBool(lite);
+        riteA = this.IntBool(rite);
+
+        return liteA - riteA;
     }
 
-    protected virtual long BoolInt(bool o)
+    protected virtual long IntBool(bool o)
     {
         long a;
         a = 0;
