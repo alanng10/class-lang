5b198fbb25039f8a5bc21467efad1347e5b492cb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-20 22:51:54 +0800

    Class

---
+++ Class/Class.Infra/Ver.cs
@@ -0,0 +1,8 @@
+namespace Class.Infra;
+
+public class Ver : Any
+{
+    public virtual long Major { get; set; }
+    public virtual long Minor { get; set; }
+    public virtual long Vise { get; set; }
+}
