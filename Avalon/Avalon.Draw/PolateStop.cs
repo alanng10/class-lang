3c99a27125ffeb3055b911251a2ece3bd3186496
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-22 18:44:09 +0800

    Avalon

--- Avalon/Avalon.Draw/PolateStop.cs
+++ Avalon/Avalon.Draw/PolateStop.cs
@@ -8,8 +8,8 @@ public class PolateStop : Any
 
         this.InternIntern = InternIntern.This;
         this.DrawInfra = Infra.This;
-        this.InternDrawGradientStopPoint = new InternDrawGradientStopPoint();
-        this.InternDrawGradientStopPoint.Init();
+        this.InternDrawPolateStopPoint = new InternDrawPolateStopPoint();
+        this.InternDrawPolateStopPoint.Init();
 
         long count;
         count = this.Count;
@@ -33,7 +33,7 @@ public class PolateStop : Any
     private InternIntern InternIntern { get; set; }
     protected virtual Infra DrawInfra { get; set; }
     internal virtual ulong Intern { get; set; }
-    private InternDrawGradientStopPoint InternDrawGradientStopPoint { get; set; }
+    private InternDrawPolateStopPoint InternDrawPolateStopPoint { get; set; }
 
     public virtual bool PointSet(long index, PolateStopPoint point)
     {
@@ -49,8 +49,8 @@ public class PolateStop : Any
 
     public virtual bool PointGet(long index, PolateStopPoint result)
     {
-        InternDrawGradientStopPoint u;
-        u = this.InternDrawGradientStopPoint;
+        InternDrawPolateStopPoint u;
+        u = this.InternDrawPolateStopPoint;
 
         ulong indexU;
         indexU = (ulong)index;
