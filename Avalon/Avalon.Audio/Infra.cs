c1000dd2f81eecc323b8f35a6041510b1d947bf4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 14:19:07 +0800

    Avalon

---
+++ Avalon/Avalon.Audio/Infra.cs
@@ -0,0 +1,26 @@
+namespace Avalon.Audio;
+
+public class Infra : Any
+{
+    public static Infra This { get; } = ShareCreate();
+
+    private static Infra ShareCreate()
+    {
+        Infra share;
+        share = new Infra();
+        Any a;
+        a = share;
+        a.Init();
+        return share;
+    }
+
+    public override bool Init()
+    {
+        base.Init();
+
+        this.PixelByteCount = 4;
+        return true;
+    }
+
+    public virtual long PixelByteCount { get; set; }
+}
