93d5ee6d638aa48ce9c23c47c9add7cf4487d42c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 15:06:01 +0800

    Avalon

--- Avalon/Avalon.Tune/-.cs
+++ Avalon/Avalon.Tune/-.cs
@@ -1,5 +1,2 @@
-global using InternIntern = Avalon.Intern.Intern;
-global using InternInfra = Avalon.Intern.Infra;
-global using Extern = Avalon.Intern.Extern;
 global using Any = Avalon.Infra.Any;
 global using AudioAudio = Avalon.Audio.Audio;
