c6a48d5eaab1ae3ae7419bf994f21e9a7aef91f8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 15:05:19 +0800

    Avalon

--- Avalon/Avalon.Tune/Tune.cs
+++ Avalon/Avalon.Tune/Tune.cs
@@ -2,6 +2,24 @@ namespace Avalon.Tune;
 
 public class Tune : Any
 {
+    public override bool Init()
+    {
+        base.Init();
+        // this.Intern = Extern.Tune_New();
+        // Extern.Tune_Init(this.Intern);
+        return true;
+    }
+
+    public virtual bool Final()
+    {
+        // Extern.Tune_Final(this.Intern);
+        // Extern.Tune_Delete(this.Intern);
+        return true;
+    }
+
+    public virtual ulong Out { get; set; }
+    private ulong Intern { get; set; }
+
     public virtual bool ExecuteAudio(AudioAudio audio)
     {
         return true;
