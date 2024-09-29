57268d446122654b686659fdc2b8c43d77e91f0d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 14:14:47 +0800

    Avalon

--- Avalon/Avalon.Audio/Audio.cs
+++ Avalon/Avalon.Audio/Audio.cs
@@ -2,9 +2,33 @@ namespace Avalon.Audio;
 
 public class Audio : Any
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.InternData = Extern.Data_New();
+        Extern.Data_Init(this.InternData);
+
+        this.Intern = Extern.Audio_New();
+        Extern.Audio_DataSet(this.Intern, this.InternData);
+        Extern.Audio_Init(this.Intern);
+        this.Ident = this.Intern;
+        return true;
+    }
+
+    public virtual bool Final()
+    {
+        Extern.Image_Final(this.Intern);
+        Extern.Image_Delete(this.Intern);
+
+        Extern.Data_Final(this.InternData);
+        Extern.Data_Delete(this.InternData);
+        return true;
+    }
+
     public virtual long Count { get; set; }
     public virtual long Time { get; set; }
     private ulong Intern { get; set; }
+    private ulong InternData { get; set; }
 
     public virtual bool DataCreate()
     {
