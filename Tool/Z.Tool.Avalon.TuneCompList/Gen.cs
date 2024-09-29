265e126febe4527cbec0cfe778663dc00cd8c19e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 22:07:12 +0800

    Tool

--- Tool/Z.Tool.Avalon.TuneCompList/Gen.cs
+++ Tool/Z.Tool.Avalon.TuneCompList/Gen.cs
@@ -5,16 +5,16 @@ public class Gen : SourceGen
     public override bool Init()
     {
         base.Init();
-        this.Module = this.S("Class.Infra");
-        this.ClassName = this.S("CountList");
+        this.Module = this.S("Avalon.Tune");
+        this.ClassName = this.S("CompList");
         this.BaseClassName = this.S("Any");
         this.AnyClassName = this.S("Any");
-        this.ItemClassName = this.S("Count");
+        this.ItemClassName = this.S("Comp");
         this.ArrayClassName = this.S("Array");
         this.Export = true;
-        this.ItemListFileName = this.S("ToolData/Class/ItemListCount.txt");
-        this.AddMethodFileName = this.S("ToolData/Class/AddMaideCount.txt");
-        this.OutputFilePath = this.S("../../Class/Class.Infra/CountList.cs");
+        this.ItemListFileName = this.S("ToolData/Avalon/ItemListTuneComp.txt");
+        this.AddMethodFileName = this.S("ToolData/Avalon/AddMaideTuneComp.txt");
+        this.OutputFilePath = this.S("../../Avalon/Avalon.Tune/CompList.cs");
         return true;
     }
 }
