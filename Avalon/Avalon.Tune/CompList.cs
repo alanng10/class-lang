c258e38e7007604e850a070e41c88afc25e7ab84
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 22:09:56 +0800

    Avalon

---
+++ Avalon/Avalon.Tune/CompList.cs
@@ -0,0 +1,63 @@
+namespace Avalon.Tune;
+
+public class CompList : Any
+{
+    public static CompList This { get; } = ShareCreate();
+
+    private static CompList ShareCreate()
+    {
+        CompList share;
+        share = new CompList();
+        Any a;
+        a = share;
+        a.Init();
+        return share;
+    }
+
+    public override bool Init()
+    {
+        base.Init();
+        this.InitArray();
+        this.Count = this.Array.Count;
+        this.Index = 0;
+
+        this.Add = this.AddItem();
+        this.Set = this.AddItem();
+        return true;
+    }
+
+    public virtual Comp Add { get; set; }
+    public virtual Comp Set { get; set; }
+
+    protected virtual Comp AddItem()
+    {
+        Comp item;
+        item = new Comp();
+        item.Init();
+        item.Index = this.Index;
+        this.Array.SetAt(item.Index, item);
+        this.Index = this.Index + 1;
+        return item;
+    }
+
+    protected virtual bool InitArray()
+    {
+        this.Array = new Array();
+        this.Array.Count = this.ArrayCount;
+        this.Array.Init();
+        return true;
+    }
+
+    protected virtual Array Array { get; set; }
+
+    protected virtual long ArrayCount { get { return 2; } set { } }
+
+    public virtual long Count { get; set; }
+    
+    protected virtual long Index { get; set; }
+
+    public virtual Comp Get(long index)
+    {
+        return (Comp)this.Array.GetAt(index);
+    }
+}
