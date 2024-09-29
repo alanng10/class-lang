74cd7feb6d8180afcdb79001d348f5acf6498990
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 23:47:13 +0800

    Avalon

--- Avalon/Avalon.Tune/-.cs
+++ Avalon/Avalon.Tune/-.cs
@@ -1,4 +1,7 @@
 global using Any = Avalon.Infra.Any;
 global using Range = Avalon.Infra.Range;
 global using Array = Avalon.List.Array;
+global using MathMath = Avalon.Math.Math;
+global using MathComp = Avalon.Math.Comp;
+global using MathInfra = Avalon.Math.Infra;
 global using AudioAudio = Avalon.Audio.Audio;
