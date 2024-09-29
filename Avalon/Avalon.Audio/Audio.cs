79b173d3c64e0fbedb2aeae9980a1ca00d090389
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-29 14:11:56 +0800

    Avalon

--- Avalon/Avalon.Audio/Audio.cs
+++ Avalon/Avalon.Audio/Audio.cs
@@ -3,6 +3,24 @@ namespace Avalon.Audio;
 public class Audio : Any
 {
     public virtual long Count { get; set; }
-
     public virtual long Time { get; set; }
+    private ulong Intern { get; set; }
+
+    public virtual bool DataCreate()
+    {
+        long count;
+        count = this.Count;
+        long time;
+        time = this.Time;
+
+        ulong countU;
+        countU = (ulong)count;
+        ulong timeU;
+        timeU = (ulong)time;
+        Extern.Audio_CountSet(this.Intern, countU);
+        Extern.Audio_TimeSet(this.Intern, timeU);
+
+        Extern.Audio_DataCreate(this.Intern);
+        return true;
+    }
 }
