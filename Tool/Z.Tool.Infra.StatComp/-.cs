27336aa0c722f642f5f020397d60b306f371189a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-30 14:50:20 +0800

    Tool

--- Tool/Z.Tool.Infra.StatComp/-.cs
+++ Tool/Z.Tool.Infra.StatComp/-.cs
@@ -1,3 +1,6 @@
+global using String = Avalon.Infra.String;
+global using ListEntry = Avalon.List.Entry;
+global using Text = Avalon.Text.Text;
 global using EntryEntry = Avalon.Entry.Entry;
 global using StatGen = Z.Infra.StatSourceGen.Gen;
 global using STAThreadAttribute = System.STAThreadAttribute;
--- Tool/Z.Tool.Infra.StatComp/Gen.cs
+++ Tool/Z.Tool.Infra.StatComp/Gen.cs
@@ -12,4 +12,20 @@ class Gen : StatGen
 
         return base.Execute();
     }
+
+    protected override ListEntry GetItemEntry(String line)
+    {
+        Text ka;
+        ka = this.Replace(this.TA(line), "Dest", this.S("Destination"));
+
+        String k;
+        k = this.StringCreate(ka);
+
+        ListEntry a;
+        a = new ListEntry();
+        a.Init();
+        a.Index = line;
+        a.Value = k;
+        return a;
+    }
 }
