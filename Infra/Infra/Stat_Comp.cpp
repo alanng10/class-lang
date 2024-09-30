5b77d1d803b22f82ecd00c510dd97953c7f5d6b6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-09-30 14:52:47 +0800

    Infra

--- Infra/Infra/Stat_Comp.cpp
+++ Infra/Infra/Stat_Comp.cpp
@@ -1,48 +1,43 @@
 #include "Stat.hpp"
 
 Int Stat_Var_CompSourceOver = QPainter::CompositionMode_SourceOver + 1;
-Int Stat_Var_CompDestinationOver = QPainter::CompositionMode_DestinationOver + 1;
-Int Stat_Var_CompClear = QPainter::CompositionMode_Clear + 1;
+Int Stat_Var_CompDestOver = QPainter::CompositionMode_DestinationOver + 1;
 Int Stat_Var_CompSource = QPainter::CompositionMode_Source + 1;
-Int Stat_Var_CompDestination = QPainter::CompositionMode_Destination + 1;
+Int Stat_Var_CompDest = QPainter::CompositionMode_Destination + 1;
 Int Stat_Var_CompSourceIn = QPainter::CompositionMode_SourceIn + 1;
-Int Stat_Var_CompDestinationIn = QPainter::CompositionMode_DestinationIn + 1;
+Int Stat_Var_CompDestIn = QPainter::CompositionMode_DestinationIn + 1;
 Int Stat_Var_CompSourceOut = QPainter::CompositionMode_SourceOut + 1;
-Int Stat_Var_CompDestinationOut = QPainter::CompositionMode_DestinationOut + 1;
+Int Stat_Var_CompDestOut = QPainter::CompositionMode_DestinationOut + 1;
 
 Int Stat_CompSourceOver(Int o)
 {
     return Stat_Var_CompSourceOver;
 }
-Int Stat_CompDestinationOver(Int o)
+Int Stat_CompDestOver(Int o)
 {
-    return Stat_Var_CompDestinationOver;
-}
-Int Stat_CompClear(Int o)
-{
-    return Stat_Var_CompClear;
+    return Stat_Var_CompDestOver;
 }
 Int Stat_CompSource(Int o)
 {
     return Stat_Var_CompSource;
 }
-Int Stat_CompDestination(Int o)
+Int Stat_CompDest(Int o)
 {
-    return Stat_Var_CompDestination;
+    return Stat_Var_CompDest;
 }
 Int Stat_CompSourceIn(Int o)
 {
     return Stat_Var_CompSourceIn;
 }
-Int Stat_CompDestinationIn(Int o)
+Int Stat_CompDestIn(Int o)
 {
-    return Stat_Var_CompDestinationIn;
+    return Stat_Var_CompDestIn;
 }
 Int Stat_CompSourceOut(Int o)
 {
     return Stat_Var_CompSourceOut;
 }
-Int Stat_CompDestinationOut(Int o)
+Int Stat_CompDestOut(Int o)
 {
-    return Stat_Var_CompDestinationOut;
+    return Stat_Var_CompDestOut;
 }
