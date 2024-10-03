107aaa6a77afe0ba927f2c92c51e200685a260db
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-03 16:22:04 +0800

    Avalon

--- Avalon/Avalon.View/Frame.cs
+++ Avalon/Avalon.View/Frame.cs
@@ -2,4 +2,17 @@ namespace Avalon.View;
 
 public class Frame : FrameFrame
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.ViewInfra = Infra.This;
+        return true;
+    }
+
+    protected virtual Infra ViewInfra { get; set; }
+
+    protected virtual Field CreateViewField()
+    {
+        return this.ViewInfra.FieldCreate(this);
+    }
 }
