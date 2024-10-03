f5e4175bae1379e886185d561ca1cc10eedfb5c3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-03 19:41:51 +0800

    Mirai

--- Mirai/Mirai.Draw/-.cs
+++ Mirai/Mirai.Draw/-.cs
@@ -1,4 +1,6 @@
+global using InternIntern = Avalon.Intern.Intern;
 global using Extern = Avalon.Intern.Extern;
+global using InternInfra = Avalon.Intern.Infra;
 global using Any = Avalon.Infra.Any;
 global using Array = Avalon.List.Array;
 global using Color = Avalon.Video.Color;
