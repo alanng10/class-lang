45b74c02427797279168ee8a7416e6dfc9973618
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-04 14:44:16 +0800

    Mirai

---
+++ Mirai/Mirai.Media/Infra.cs
@@ -0,0 +1,29 @@
+namespace Mirai.Media;
+
+class Infra : Any
+{
+    public static Infra This { get; } = ShareCreate();
+
+    private static Infra ShareCreate()
+    {
+        Infra share;
+        share = new Infra();
+        Any a;
+        a = share;
+        a.Init();
+        return share;
+    }
+
+    public override bool Init()
+    {
+        base.Init();
+        VideoOutFrameMaide maideA;
+        maideA = new VideoOutFrameMaide(VideoOut.InternFrameEvent);
+        this.VideoOutFrameMaideAddress = new MaideAddress();
+        this.VideoOutFrameMaideAddress.Delegate = maideA;
+        this.VideoOutFrameMaideAddress.Init();
+        return true;
+    }
+
+    internal virtual MaideAddress VideoOutFrameMaideAddress { get; set; }
+}
