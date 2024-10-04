aa725af71ebd322591db68d32bfb1b9413696139
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-04 14:47:08 +0800

    Mirai

--- Mirai/Mirai.Media/-.cs
+++ Mirai/Mirai.Media/-.cs
@@ -5,6 +5,6 @@ global using VideoOutFrameMaide = Avalon.Intern.Extern.VideoOut_Frame_Maide;
 global using MaideAddress = Avalon.Intern.MaideAddress;
 global using Handle = Avalon.Intern.Handle;
 global using Any = Avalon.Infra.Any;
+global using String = Avalon.Infra.String;
 global using State = Avalon.Infra.State;
-global using StreamStream = Avalon.Stream.Stream;
 global using VideoVideo = Avalon.Video.Video;
