f2e46dac6c68b854acfb0b612d00c19bd69e2418
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-04 20:09:10 +0800

    Script

--- Script/Saber/MakeModule.cmd
+++ Script/Saber/MakeModule.cmd
@@ -2,10 +2,10 @@
 
 echo Make Module
 set DotNetModuleOutFold=.\Out\net8.0
-pushd Class\ClassExe
+pushd Class\SaberExe
 dotnet build -v quiet
 popd
 pushd %DotNetModuleOutFold%
-del /F /Q class.exe 2>NUL
-rename Class.Console-ExeCon.exe class.exe
+del /F /Q saber.exe 2>NUL
+rename Saber.Console-ExeCon.exe saber.exe
 popd
