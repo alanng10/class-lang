10014f38b253b76d09c356d00256bd7fbcbd1fa9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-05 18:04:30 +0800

    Mirai

--- Mirai/Mirai.Draw/Pos.cs
+++ Mirai/Mirai.Draw/Pos.cs
@@ -4,5 +4,4 @@ public class Pos : Any
 {
     public virtual long Col { get; set; }
     public virtual long Row { get; set; }
-    public virtual long Hor { get; set; }
 }
--- Mirai/Mirai.Draw/Size.cs
+++ Mirai/Mirai.Draw/Size.cs
@@ -4,5 +4,4 @@ public class Size : Any
 {
     public virtual long Wed { get; set; }
     public virtual long Het { get; set; }
-    public virtual long Dep { get; set; }
 }
