100c271aa8df083cc2e2fe9518b4de42c2490f3e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-06 14:53:34 +0800

    Avalon

--- Avalon/Demo/NetworkPeer.cs
+++ Avalon/Demo/NetworkPeer.cs
@@ -2,7 +2,23 @@ namespace Demo;
 
 class NetworkPeer : NetworkNetwork
 {
+    public override bool Init()
+    {
+        this.Data = new Data();
+        this.Data.Count = 10;
+        this.Data.Init();
+
+        this.Range = new Range();
+        this.Range.Init();
+        return true;
+    }
+
+    public Demo Demo { get; set; }
     public ThreadNetworkHostState ThreadState { get; set; }
+
+    private Data Data { get; set; }
+    private Range Range { get; set; }
+    private long Stage { get; set; }
     private long StatusCode { get; set; }
 
     public override bool StatusEvent()
