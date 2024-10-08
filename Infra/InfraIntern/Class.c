bf6fee2106af545e2f9d526ff00d962d47f32f94
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-08 14:51:49 +0800

    Infra

--- Infra/InfraIntern/Class.c
+++ Infra/InfraIntern/Class.c
@@ -20,3 +20,25 @@ Int Intern_Intern_ClassAny[4] =
 };
 
 Int Intern_Intern_Class = CastInt(Intern_Intern_ClassAny);
+
+Int Intern_Extern_StateFieldGet[0] = { };
+Int Intern_Extern_StateFieldSet[0] = { };
+Int Intern_Extern_StateMaideCall[0] = { };
+
+Int Intern_Extern_State[3] = 
+{
+    CastInt(Intern_Extern_StateFieldGet), CastInt(Intern_Extern_StateFieldSet), CastInt(Intern_Extern_StateMaideCall)
+};
+
+Int Intern_Extern_Base[2] =
+{ 
+    0,
+    CastInt(Intern_Extern_State)
+};
+
+Int Intern_Extern_ClassAny[4] =
+{
+    CastInt(Intern_Extern_Base), 0x10000000000000, 0, 0
+};
+
+Int Intern_Extern_Class = CastInt(Intern_Extern_ClassAny);
