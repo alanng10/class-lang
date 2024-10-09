cdcd4caa8bb9f6d7dbdca9cce52c88316407f988
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-09 12:10:29 +0800

    Infra

--- Infra/InfraIntern/New.c
+++ Infra/InfraIntern/New.c
@@ -1,6 +1,31 @@
 #include "New.h"
 
+Int Intern_New_Phore;
+
 Int Intern_New(Int kind, Int info, Eval* eval)
 {
+    Phore_Open(Intern_New_Phore);
+
+    Int kk;
+
+    if (kind == 0)
+    {
+        Int* cc;
+        cc = CastPointer(info);
+
+        kk = cc[2];
+    }
+
+    Int intCount;
+    intCount = kk + 3;
+
+    Int dataCount;
+    dataCount = intCount * Constant_IntByteCount();
+
+    
+
+
+    Phore_Close(Intern_New_Phore);
+
     return 0;
 }
