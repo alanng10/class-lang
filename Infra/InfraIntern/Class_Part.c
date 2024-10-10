51b0330624ff6cfe01aec9770fc5b5c062a84188
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-10 16:54:29 +0800

    Infra

--- Infra/InfraIntern/Class_Part.c
+++ Infra/InfraIntern/Class_Part.c
@@ -4,15 +4,15 @@ Int Intern_Intern_MaideCall[13] =
 { 
     CastInt(Intern_Intern_Init),
     CastInt(Intern_Intern_RefLess),
+    CastInt(Intern_Intern_StringNew),
+    CastInt(Intern_Intern_StringValueGet),
+    CastInt(Intern_Intern_StringValueSet),
+    CastInt(Intern_Intern_StringCountGet),
+    CastInt(Intern_Intern_StringCountSet),
     CastInt(Intern_Intern_DataNew),
     CastInt(Intern_Intern_DataGet),
     CastInt(Intern_Intern_DataSet),
     CastInt(Intern_Intern_ArrayNew),
     CastInt(Intern_Intern_ArrayGet),
-    CastInt(Intern_Intern_ArraySet),
-    CastInt(Intern_Intern_StringNew),
-    CastInt(Intern_Intern_StringValueGet),
-    CastInt(Intern_Intern_StringValueSet),
-    CastInt(Intern_Intern_StringCountGet),
-    CastInt(Intern_Intern_StringCountSet)
+    CastInt(Intern_Intern_ArraySet)
 };
--- Infra/InfraIntern/Pronate_Part.h
+++ Infra/InfraIntern/Pronate_Part.h
@@ -3,14 +3,14 @@
 #include "Prusate.h"
 
 Int Intern_Intern_RefLess(Eval* eval, Int frame);
+Int Intern_Intern_StringNew(Eval* eval, Int frame);
+Int Intern_Intern_StringValueGet(Eval* eval, Int frame);
+Int Intern_Intern_StringValueSet(Eval* eval, Int frame);
+Int Intern_Intern_StringCountGet(Eval* eval, Int frame);
+Int Intern_Intern_StringCountSet(Eval* eval, Int frame);
 Int Intern_Intern_DataNew(Eval* eval, Int frame);
 Int Intern_Intern_DataGet(Eval* eval, Int frame);
 Int Intern_Intern_DataSet(Eval* eval, Int frame);
 Int Intern_Intern_ArrayNew(Eval* eval, Int frame);
 Int Intern_Intern_ArrayGet(Eval* eval, Int frame);
 Int Intern_Intern_ArraySet(Eval* eval, Int frame);
-Int Intern_Intern_StringNew(Eval* eval, Int frame);
-Int Intern_Intern_StringValueGet(Eval* eval, Int frame);
-Int Intern_Intern_StringValueSet(Eval* eval, Int frame);
-Int Intern_Intern_StringCountGet(Eval* eval, Int frame);
-Int Intern_Intern_StringCountSet(Eval* eval, Int frame);
