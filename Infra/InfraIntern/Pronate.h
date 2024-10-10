efab9ffdfb93b08f13bcc06949157ead3399c02c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-10 11:47:30 +0800

    Infra

--- Infra/InfraIntern/Pronate.h
+++ Infra/InfraIntern/Pronate.h
@@ -6,7 +6,14 @@
 
 #define RefMaskMemoryClear (0x000fffffffffffff)
 
+#define RefKindNull (0x0ULL)
 #define RefKindAny (0x1ULL)
+#define RefKindBool (0x2ULL)
+#define RefKindInt (0x3ULL)
+#define RefKindString (0x4ULL)
+#define RefKindStringValue (0x5ULL)
+#define RefKindData (0x6ULL)
+#define RefKindArray (0x7ULL)
 
 #define RefKindMaskAny (RefKindAny << 60)
 
