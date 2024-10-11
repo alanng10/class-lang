a01c7baf64974ae6bfe4e4d19398e1f653f83e5b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-11 23:57:35 +0800

    Tool

--- Tool/Z.Tool.PrusateGen/SystemExternGen.cs
+++ Tool/Z.Tool.PrusateGen/SystemExternGen.cs
@@ -4,13 +4,13 @@ class SystemExternGen : ExternGen
 {
     public override bool Init()
     {
+        base.Init();
+
         this.PrusateFileName = this.S("ToolData/Prusate/SystemExtern.txt");
 
         this.OutputFilePath = this.S("../../System/System.Infra/Extern.cla");
 
         this.IntTypeName = this.S("Int");
-
-        this.InitClassNewMethodArray();
         return true;
     }
 }
