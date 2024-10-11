2dd6717eef225625c814fb0816f4fc083833aec4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-11 22:26:44 +0800

    Infra

---
+++ Infra/InfraIntern/Prusate_Part.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Prusate.h"
+
+Intern_Api Int Intern_Intern_Memory(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_StringNew(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_StringValueGet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_StringValueSet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_StringCountGet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_StringCountSet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_DataNew(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_DataGet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_DataSet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_ArrayNew(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_ArrayGet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_ArraySet(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_RefLess(Eval* eval, Int frame);
+Intern_Api Int Intern_Intern_ThisThread(Eval* eval, Int frame);
