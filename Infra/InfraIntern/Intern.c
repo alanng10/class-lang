e0da09f73b87ad3f1bc6c54377cabb848ae70601
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-12 14:59:23 +0800

    Infra

--- Infra/InfraIntern/Intern.c
+++ Infra/InfraIntern/Intern.c
@@ -308,56 +308,7 @@ Int Intern_Intern_InitThread(Eval* eval, Int frame)
 
     RefKindClear(thread);
 
-    Int dataCount;
-    dataCount = sizeof(ThreadData);
-
-    Int p;
-    p = New(dataCount);
-
-    Int* array;
-    array = CastPointer(ThreadArray);
-
-    Intern_New_Open();
-
-    Bool b;
-    b = false;
-
-    Int index;
-    index = 0;
-
-    Int count;
-    count = ThreadCountMax;
-
-    Int i;
-    i = 0;
-    while ((!b) & (i < count))
-    {
-        if (array[i] == null)
-        {
-            index = i;
-            b = true;
-        }
-
-        i = i + 1;
-    }
-
-    if (!b)
-    {
-        Exit(30);
-    }
-
-    ThreadData* kk;
-    kk = CastPointer(p);
-
-    kk->Index = index;
-    kk->ThreadAny = threadAny;
-    kk->Thread = thread;
-    
-    array[index] = p;
-
-    Thread_IdentSet(thread, index);
-
-    Intern_New_Close();
+    Intern_InitThread(thread, threadAny);
 
     Int ke;
     ke = BoolTrue;
--- Infra/InfraIntern/Thread.c
+++ Infra/InfraIntern/Thread.c
@@ -4,3 +4,60 @@
 Int Var_ThreadArray[ThreadCountMax];
 
 Int ThreadArray = CastInt(&Var_ThreadArray);
+
+
+Bool Intern_InitThread(Int thread, Int threadAny)
+{
+    Int dataCount;
+    dataCount = sizeof(ThreadData);
+
+    Int p;
+    p = New(dataCount);
+
+    Int* array;
+    array = CastPointer(ThreadArray);
+
+    Intern_New_Open();
+
+    Bool b;
+    b = false;
+
+    Int index;
+    index = 0;
+
+    Int count;
+    count = ThreadCountMax;
+
+    Int i;
+    i = 0;
+    while ((!b) & (i < count))
+    {
+        if (array[i] == null)
+        {
+            index = i;
+            b = true;
+        }
+
+        i = i + 1;
+    }
+
+    if (!b)
+    {
+        Exit(30);
+    }
+
+    ThreadData* kk;
+    kk = CastPointer(p);
+
+    kk->Index = index;
+    kk->ThreadAny = threadAny;
+    kk->Thread = thread;
+
+    array[index] = p;
+
+    Thread_IdentSet(thread, index);
+
+    Intern_New_Close();
+
+    return true;
+}
