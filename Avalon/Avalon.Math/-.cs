9ea889db408e36156ff907897764a61c74640e15
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-13 21:28:52 +0800

    Avalon

--- Avalon/Avalon.Math/-.cs
+++ Avalon/Avalon.Math/-.cs
@@ -1,4 +1,2 @@
-global using InternIntern = Avalon.Intern.Intern;
 global using Extern = Avalon.Intern.Extern;
-global using InternMathComp = Avalon.Intern.MathComp;
 global using Any = Avalon.Infra.Any;
