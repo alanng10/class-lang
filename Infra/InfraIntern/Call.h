0123186a9e69e84f0db0902dec31b6fd6b7c3aef
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-15 20:09:19 +0800

    Infra

--- Infra/InfraIntern/Call.h
+++ Infra/InfraIntern/Call.h
@@ -2,8 +2,8 @@
 
 #include "Pronate.h"
 
-#define VarSetDeref(dest, var, pos) (dest = *(((Int*)var) + pos))
+#define VarSetDeref(dest, var, pos) (dest = *(((Int*)var) + pos));
 
-#define MaskClear(name, mask) (name = name & mask)
+#define MaskClear(name, mask) (name = name & mask);
 
-#define MaskSet(name, mask) (name = name | mask)
+#define MaskSet(name, mask) (name = name | mask);
