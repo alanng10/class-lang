c64db16495559b03f884807146021161b3fd035a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-15 20:58:42 +0800

    Saber

--- Saber/Saber.Console/ClassGen.cs
+++ Saber/Saber.Console/ClassGen.cs
@@ -857,6 +857,54 @@ public class ClassGen : ClassBase
         return true;
     }
 
+    public virtual bool CondSet(String dest, String cond, String trueValue, String falseValue)
+    {
+        this.TextIndent();
+
+        this.Text(dest);
+
+        this.Text(this.Space);
+        this.Text(this.LimitAre);
+        this.Text(this.Space);
+
+        this.Text(this.LimitBraceRoundLite);
+
+        this.Text(cond);
+
+        this.Text(this.Space);
+        this.Text(this.LimitMul);
+        this.Text(this.Space);
+
+        this.Text(trueValue);
+
+        this.Text(this.LimitBraceRoundRite);
+
+        this.Text(this.Space);
+        this.Text(this.LimitAdd);
+        this.Text(this.Space);
+
+        this.Text(this.LimitBraceRoundLite);
+
+        this.Text(this.LimitBraceRoundLite);
+
+        this.Text(this.LimitNot);
+        this.Text(cond);
+
+        this.Text(this.LimitBraceRoundRite);
+
+        this.Text(this.Space);
+        this.Text(this.LimitMul);
+        this.Text(this.Space);
+
+        this.Text(falseValue);
+
+        this.Text(this.LimitBraceRoundRite);
+
+        this.Text(this.LimitSemicolon);
+        this.Text(this.NewLine);
+        return true;
+    }
+
     public virtual bool VarSet(String dest, String value)
     {
         this.TextIndent();
