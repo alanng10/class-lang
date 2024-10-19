c4b3ea81f57a0cb17830e33eb405de6a2eb23974
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-19 22:25:29 +0800

    Mirai

--- Mirai/Demo/TypeState.cs
+++ Mirai/Demo/TypeState.cs
@@ -161,14 +161,35 @@ class TypeState : State
 
         if (a == d.AlphaE & o)
         {
-            this.Demo.Play.PosSet(2 * 60 * 1000);
-
             this.Demo.Play.Execute();
         }
 
         if (a == d.AlphaR & o)
         {
             this.Demo.Play.Pause();
+        }
+
+        if (a == d.AlphaG & o)
+        {
+            long kaaa;
+            //global::System.Console.Write("Demo Play Pos Get Before\n");
+            kaaa = this.Demo.Play.Pos;
+            //global::System.Console.Write("Demo Play Pos Get After, value: " + kaaa + "\n");
+
+            long kkaa;
+            kkaa = kaaa + 10 * 1000;
+            //global::System.Console.Write("Demo Play New Pos, value: " + kkaa + "\n");
+
+            long time;
+            //global::System.Console.Write("Demo Play Time Get Before\n");
+            time = this.Demo.Play.Time;
+            //global::System.Console.Write("Demo Play Time Get After, value: " + time + "\n");
+            if (!(time < kkaa))
+            {
+                //global::System.Console.Write("Demo Play Pos Set Before\n");
+                this.Demo.Play.Pos = kkaa;
+                //global::System.Console.Write("Demo Play Pos Set After\n");
+            }
         }
 
         if (a == d.AlphaN & o)
