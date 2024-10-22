e9a9c87056c5693d6e1270c321c6d49258683335
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-22 21:06:34 +0800

    Saber

--- Saber/Saber.Console/Console.cs
+++ Saber/Saber.Console/Console.cs
@@ -20,6 +20,8 @@ public class Console : ClassBase
 
         this.ClassGen = this.CreateClassGen();
 
+        this.ModuleGen = this.CreateModuleGen();
+
         this.ErrorString = new ErrorString();
         this.ErrorString.Init();
 
@@ -72,6 +74,7 @@ public class Console : ClassBase
     protected virtual PortRead PortRead { get; set; }
     protected virtual PortLoad PortLoad { get; set; }
     protected virtual ClassGen ClassGen { get; set; }
+    protected virtual ModuleGen ModuleGen { get; set; }
     protected virtual NameCheck NameCheck { get; set; }
     protected virtual Out Out { get; set; }
     protected virtual Out Err { get; set; }
@@ -144,6 +147,14 @@ public class Console : ClassBase
     {
         ClassGen a;
         a = new ClassGen();
+        a.Init();
+        return a;
+    }
+
+    protected virtual ModuleGen CreateModuleGen()
+    {
+        ModuleGen a;
+        a = new ModuleGen();
         a.Init();
         return a;
     }
