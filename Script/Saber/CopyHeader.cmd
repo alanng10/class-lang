fc65c6f0fd85d59696c087aefea5a572292707d5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-23 10:56:50 +0800

    Script

--- Script/Saber/CopyHeader.cmd
+++ Script/Saber/CopyHeader.cmd
@@ -3,5 +3,5 @@
 echo Copy Header
 set SaberConsoleDataFold=.\Saber\Saber.Console\Saber.Console.data
 
-copy /Y Infra\Infra\Prusate.h %SaberConsoleDataFold%
-copy /Y Infra\InfraIntern\Prusate.h %SaberConsoleDataFold%
+copy /Y Infra\Infra\Prusate.h %SaberConsoleDataFold%\Infra_Prusate.h
+copy /Y Infra\InfraIntern\Prusate.h %SaberConsoleDataFold%\InfraIntern_Prusate.h
