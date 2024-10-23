c99072a53ce4a9e908c3736e39216aa17c212fb3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-23 17:22:13 +0800

    Infra

--- Infra/InfraIntern/Share.c
+++ Infra/InfraIntern/Share.c
@@ -2,5 +2,37 @@
 
 Int Intern_Share(Int info, Eval* eval)
 {
-    
+    Int* p;
+    p = CastPointer(info);
+
+    Int phore;
+    phore = p[4];
+
+    Phore_Open(phore);
+
+    Int share;
+    share = p[3];
+
+    if (!(share == null))
+    {
+        eval->S[eval->N] = share;
+
+        eval->N = eval->N + 1;
+
+        Phore_Close(phore);
+        return 0;
+    }
+
+    Intern_New(1, info, eval);
+
+    Int k;
+    k = eval->S[eval->N - 1];
+
+    p[3] = k;
+
+    Intern_Call(eval, 1, 3, 0);
+
+    Phore_Close(phore);
+
+    return 0;
 }
