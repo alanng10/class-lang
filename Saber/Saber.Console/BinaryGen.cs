366d4b0a96b0977d6890a1956fc6ed7b94faf2e8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-24 12:00:25 +0800

    Saber

--- Saber/Saber.Console/BinaryGen.cs
+++ Saber/Saber.Console/BinaryGen.cs
@@ -12,8 +12,8 @@ public class BinaryGen : Any
 
     public virtual ClassModule Module { get; set; }
     public virtual BinaryBinary Result { get; set; }
-    protected virtual ListInfra ListInfra { get; set; }
-    protected virtual ClassInfra ClassInfra { get; set; }
+    public virtual ListInfra ListInfra { get; set; }
+    public virtual ClassInfra ClassInfra { get; set; }
     public virtual Table IndexTable { get; set; }
 
     public virtual bool Execute()
