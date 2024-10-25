05b2b31bbbc0363c774dd996c9224aa8d7796012
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-25 11:41:04 +0800

    Saber

---
+++ Saber/Saber.Console/ProjectGen.cs
@@ -0,0 +1,60 @@
+namespace Saber.Console;
+
+public class ProjectGen : ClassBase
+{
+    public virtual ClassGen Gen { get; set; }
+    public virtual Table ModuleTable { get; set; }
+    public virtual String Result { get; set; }
+
+    public virtual bool Execute()
+    {
+        ClassGen gen;
+        gen = this.Gen;
+
+        gen.Arg = new GenArg();
+        gen.Arg.Init();
+
+        gen.Operate = gen.CountOperate;
+
+        gen.ResetStageIndex();
+        this.ExecuteStage();
+
+        long nn;
+        nn = gen.Arg.Index;
+        nn = nn * sizeof(uint);
+        Data data;
+        data = new Data();
+        data.Count = nn;
+        data.Init();
+        gen.Arg.Data = data;
+
+        gen.Operate = gen.SetOperate;
+
+        gen.ResetStageIndex();
+        this.ExecuteStage();
+
+        gen.Operate = null;
+        gen.Arg = null;
+
+        String o;
+        o = this.StringComp.CreateData(data, null);
+
+        this.Result = o;
+        return true;
+    }
+
+    public virtual bool ExecuteStage()
+    {
+        return true;
+    }
+
+    public virtual bool ModuleImportString(ClassModule module)
+    {
+        ClassGen gen;
+        gen = this.Gen;
+
+        gen.Text(module.Ref.Name);
+        gen.Text(this.ClassInfra.Hyphen);
+        return true;
+    }
+}
