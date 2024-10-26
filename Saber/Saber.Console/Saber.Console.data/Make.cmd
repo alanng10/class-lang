0e7a09bace2762fe546c8afb6e2cbc24c179fdc0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-26 15:27:58 +0800

    Saber

--- Saber/Saber.Console/Saber.Console.data/Make.cmd
+++ Saber/Saber.Console/Saber.Console.data/Make.cmd
@@ -4,8 +4,8 @@ call Var
 
 set Module=%~1
 
-call CleanModule %Module%
+call CleanOutFold %Module%
 call MakeModuleProject %Module%
 call MakeModule %Module%
 call MoveBinary %Module%
-call CleanModule %Module%
+call CleanOutFold %Module%
