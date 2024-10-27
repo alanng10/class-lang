827da19608cc1184befaa51769656cf5340c92db
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-27 12:19:30 +0800

    Script

--- Script/Infra/UpdateDeployIntern.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call Script\Infra\Var
-
-set InfraInternOutFold=.\Out\InfraIntern-Windows-Release\release
-set InfraDeployFold=.\Out\InfraDeploy
-mkdir %InfraDeployFold% 1>NUL 2>NUL
-copy /Y %InfraInternOutFold%\InfraIntern.dll %InfraDeployFold%
