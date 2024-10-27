e74470bf8d1c11245a231be1b14df397b1855f40
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-27 14:23:47 +0800

    Avalon

--- Avalon/Avalon.Infra/-.cs
+++ Avalon/Avalon.Infra/-.cs
@@ -1,3 +1,1 @@
-global using InternIntern = Avalon.Intern.Intern;
-global using InternInfra = Avalon.Intern.Infra;
 global using ReferenceEqualityComparer = System.Collections.Generic.ReferenceEqualityComparer;
