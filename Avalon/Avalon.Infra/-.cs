c8e6bfd32efb4954a1a3a50b7a7e5b956c1909a1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-28 21:55:52 +0800

    Avalon

--- Avalon/Avalon.Infra/-.cs
+++ Avalon/Avalon.Infra/-.cs
@@ -1,1 +1,7 @@
 global using ReferenceEqualityComparer = System.Collections.Generic.ReferenceEqualityComparer;
+global using SystemDelegate = System.Delegate;
+global using SystemIntPtr = System.IntPtr;
+global using SystemThreadStaticAttribute = System.ThreadStaticAttribute;
+global using SystemGCHandle = System.Runtime.InteropServices.GCHandle;
+global using Marshal = System.Runtime.InteropServices.Marshal;
+global using DllImportAttribute = System.Runtime.InteropServices.DllImportAttribute;
