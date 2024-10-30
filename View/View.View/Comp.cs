cea666b539d319434aed626ca1d6e7c270dac609
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-10-30 12:58:42 +0800

    View

---
+++ View/View.View/Comp.cs
@@ -0,0 +1,13 @@
+namespace View.View;
+
+public class Comp : CompComp
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.ViewInfra = Infra.This;
+        return true;
+    }
+
+    protected virtual Infra ViewInfra { get; set; }
+}
