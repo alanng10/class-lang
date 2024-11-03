1d8974a49c36bd52ce4965ab9135c52dc2236caa
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-03 11:03:02 +0800

    Avalon

--- Avalon/Avalon.Thread/This.cs
+++ Avalon/Avalon.Thread/This.cs
@@ -2,14 +2,6 @@ namespace Avalon.Thread;
 
 public class This : Any
 {
-    public virtual bool Wait(long time)
-    {
-        ulong u;
-        u = (ulong)time;
-        Extern.Thread_Sleep(u);
-        return true;
-    }
-
     public virtual Thread Thread
     {
         get
