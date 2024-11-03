021d23eb8bf025b3a6314ede1340b1075f9b618c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-03 11:22:22 +0800

    Tool

--- Tool/Z.Tool.MathGen/SystemGen.cs
+++ Tool/Z.Tool.MathGen/SystemGen.cs
@@ -9,7 +9,7 @@ class SystemGen : PartGen
         this.MaideFilePath = this.S("ToolData/Math/SystemMaide.txt");
         this.MaideTwoFilePath = this.S("ToolData/Math/SystemMaideTwo.txt");
         this.MaideOperateFilePath = this.S("ToolData/Math/SystemMaideTwo.txt");
-        this.OutputFilePath = this.S("../../System/System.Math/Math.cla");
+        this.OutputFilePath = this.S("../../System/System.Math/Math.cl");
         return true;
     }
 }
