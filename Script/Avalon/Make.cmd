f6cd42b9a9ce616b112b2f8b4a1d3841e5c0570d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-07 01:11:31 +0800

    Script

--- Script/Avalon/Make.cmd
+++ Script/Avalon/Make.cmd
@@ -4,8 +4,8 @@ call Script\Avalon\CleanModule
 echo:
 call Script\Avalon\CleanDemo
 echo:
-call Script\Avalon\CopyInfra
-echo:
 call Script\Avalon\MakeModule
 echo:
 call Script\Avalon\MakeDemo
+echo:
+call Script\Avalon\CopyInfra
