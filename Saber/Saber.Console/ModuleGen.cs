3b426c15fc774a18b7532b718dfe9e592182c1b5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-08 11:07:54 +0800

    Saber

--- Saber/Saber.Console/ModuleGen.cs
+++ Saber/Saber.Console/ModuleGen.cs
@@ -51,143 +51,23 @@ public class ModuleGen : ClassBase
         gen.Include(gen.IncludeValueModule);
         gen.Text(gen.NewLine);
 
-        this.ExecuteExternClassAnyList();
-        gen.Text(gen.NewLine);
-
-        this.ExecuteClassList();
-        gen.Text(gen.NewLine);
-
-        this.ExecuteModule();
+        this.ExecuteModuleVar();
         return true;
     }
 
-    public virtual bool ExecuteModule()
+    public virtual bool ExecuteModuleVar()
     {
         ClassGen gen;
         gen = this.Gen;
 
-        long count;
-        count = this.Module.Class.Count;
-
-        gen.Text(gen.ClassInt);
+        gen.Text(gen.InternModuleStruct);
         gen.Text(gen.Space);
 
         gen.ModuleVarName(this.Module);
-        gen.Text(gen.LimitBraceSquareLite);
-        gen.TextInt(2);
-        gen.Text(gen.LimitBraceSquareRite);
 
-        gen.Text(gen.Space);
-        gen.Text(gen.LimitAre);
-        gen.Text(gen.NewLine);
-
-        gen.Text(gen.LimitBraceLite);
-        gen.Text(gen.NewLine);
-
-        gen.IndentCount = gen.IndentCount + 1;
-
-        gen.TextIndent();
-        gen.Text(gen.CastInt);
-        gen.Text(gen.LimitBraceRoundLite);
-        this.ClassListName(this.Module);
-        gen.Text(gen.LimitBraceRoundRite);
-        gen.Text(gen.LimitComma);
-        gen.Text(gen.NewLine);
-
-        gen.TextIndent();
-        gen.TextInt(count);
-        gen.Text(gen.NewLine);
-
-        gen.IndentCount = gen.IndentCount - 1;
-
-        gen.Text(gen.LimitBraceRite);
         gen.Text(gen.LimitSemicolon);
         gen.Text(gen.NewLine);
         return true;
     }
 
-    public virtual bool ExecuteExternClassAnyList()
-    {
-        ClassGen gen;
-        gen = this.Gen;
-        Iter iter;
-        iter = gen.TableIter;
-
-        this.Module.Class.IterSet(iter);
-
-        while (iter.Next())
-        {
-            ClassClass varClass;
-            varClass = (ClassClass)iter.Value;
-
-            gen.ExecuteExternClassAny(varClass);
-        }
-        iter.Clear();
-        return true;
-    }
-
-    public virtual bool ExecuteClassList()
-    {
-        ClassGen gen;
-        gen = this.Gen;
-
-        long count;
-        count = this.Module.Class.Count;
-
-        gen.Text(gen.ClassInt);
-        gen.Text(gen.Space);
-
-        this.ClassListName(this.Module);
-        gen.Text(gen.LimitBraceSquareLite);
-        gen.TextInt(count);
-        gen.Text(gen.LimitBraceSquareRite);
-
-        gen.Text(gen.Space);
-        gen.Text(gen.LimitAre);
-        gen.Text(gen.NewLine);
-
-        gen.Text(gen.LimitBraceLite);
-        gen.Text(gen.NewLine);
-
-        gen.IndentCount = gen.IndentCount + 1;
-
-        Iter iter;
-        iter = gen.TableIter;
-
-        this.Module.Class.IterSet(iter);
-
-        while (iter.Next())
-        {
-            ClassClass varClass;
-            varClass = (ClassClass)iter.Value;
-
-            gen.TextIndent();
-            gen.Text(gen.CastInt);
-            gen.Text(gen.LimitBraceRoundLite);
-            gen.ClassAnyName(varClass);
-            gen.Text(gen.LimitBraceRoundRite);
-            gen.Text(gen.LimitComma);
-            gen.Text(gen.NewLine);
-        }
-        iter.Clear();
-
-        gen.IndentCount = gen.IndentCount - 1;
-
-        gen.Text(gen.LimitBraceRite);
-        gen.Text(gen.LimitSemicolon);
-        gen.Text(gen.NewLine);
-        return true;
-    }
-
-    public virtual bool ClassListName(ClassModule module)
-    {
-        ClassGen gen;
-        gen = this.Gen;
-
-        gen.ModuleRef(module.Ref);
-        gen.Text(gen.NameCombine);
-        gen.Text(gen.ClassWord);
-        gen.Text(gen.ListWord);
-        return true;
-    }
 }
