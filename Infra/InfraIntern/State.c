ab692e859367fcecabf1c02f93d968afca88ab28
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-08 15:36:04 +0800

    Infra

--- Infra/InfraIntern/State.c
+++ Infra/InfraIntern/State.c
@@ -2,5 +2,48 @@
 
 Int Intern_State_Thread_Execute(Int thread, Int arg)
 {
-    
+    Int index;
+    index = Thread_IdentGet(thread);
+
+    Int* array;
+    array = CastPointer(ThreadArray);
+
+    Int kk;
+    kk = array[index];
+
+    Int count;
+    count = EvalStackCount;
+
+    Int kaa;
+    kaa = count * Constant_IntByteCount();
+
+    Int p;
+    p = New(kaa);
+
+    Int* stack;
+    stack = CastPointer(p);
+
+    Int kab;
+    kab = sizeof(Eval);
+
+    Int pa;
+    pa = New(kab);
+
+    Eval* eval;
+    eval = CastPointer(pa);
+
+    eval->S = stack;
+    eval->Thread = kk;
+
+    Intern_New_Open();
+
+    ThreadData* threadData;
+    threadData = CastPointer(kk);
+
+    threadData->Eval = pa;
+    threadData->Flag = 1;
+
+    Intern_New_Close();
+
+    return 0;
 }
