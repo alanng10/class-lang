7053b1b9b589d195b5f7eb51bdfb6031d0de0a2a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-09 19:25:12 +0800

    Avalon

--- Avalon/Avalon.Storage/Comp.cs
+++ Avalon/Avalon.Storage/Comp.cs
@@ -95,13 +95,13 @@ public class Comp : Any
         ulong pathU;
         pathU = this.InternInfra.StringCreate(path);
 
-        ulong o;
-        o = Extern.StorageComp_FoldCreate(this.Intern, pathU);
+        ulong k;
+        k = Extern.StorageComp_FoldCreate(this.Intern, pathU);
 
         this.InternInfra.StringDelete(pathU);
 
         bool a;
-        a = !(o == 0);
+        a = !(k == 0);
         return a;
     }
 
