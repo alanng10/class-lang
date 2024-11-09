0f70845db213044e448047634460539339d496f9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-09 19:23:57 +0800

    Avalon

--- Avalon/Avalon.Storage/Comp.cs
+++ Avalon/Avalon.Storage/Comp.cs
@@ -46,14 +46,14 @@ public class Comp : Any
         ulong destPathU;
         destPathU = this.InternInfra.StringCreate(destPath);
 
-        ulong o;
-        o = Extern.StorageComp_Rename(this.Intern, pathU, destPathU);
+        ulong k;
+        k = Extern.StorageComp_Rename(this.Intern, pathU, destPathU);
 
         this.InternInfra.StringDelete(destPathU);
         this.InternInfra.StringDelete(pathU);
 
         bool a;
-        a = !(o == 0);
+        a = !(k == 0);
         return a;
     }
 
