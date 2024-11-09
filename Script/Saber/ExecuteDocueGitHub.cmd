7a708d7bc791198c71b1109b8371472bf5e4bf97
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-09 21:13:40 +0800

    Script

--- Script/Saber/ExecuteDocueGitHub.cmd
+++ Script/Saber/ExecuteDocueGitHub.cmd
@@ -3,5 +3,7 @@
 set DocueGitHubOutFold=.\Out\DocueGitHub
 mkdir %DocueGitHubOutFold% 1>NUL 2>NUL
 
-Out\net8.0\saber.exe docue "Docue" "%DocueGitHubOutFold%" -d
+pushd Out\net8.0
+saber.exe docue "..\..\Docue" "..\..\%DocueGitHubOutFold%" -d
 echo Status: %errorlevel%
+popd
