89c6502b6aef0dacfbafa0f1008b1c77d0549a86
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-09 21:15:11 +0800

    Script

--- Script/Saber/ExecuteDocue.cmd
+++ Script/Saber/ExecuteDocue.cmd
@@ -3,5 +3,7 @@
 set DocueOutFold=.\Out\Docue
 mkdir %DocueOutFold% 1>NUL 2>NUL
 
-Out\net8.0\saber.exe docue "Docue" "%DocueOutFold%"
+pushd Out\net8.0
+saber.exe docue "..\..\Docue" "..\..\%DocueOutFold%"
 echo Status: %errorlevel%
+popd
