a2fe41ff2c58ea86c15278d93c738243baa0cf22
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-11 22:38:31 +0800

    Script

--- Script/System/Make.cmd
+++ Script/System/Make.cmd
@@ -15,3 +15,4 @@ call Script\System\MakeModule Time
 call Script\System\MakeModule Storage
 call Script\System\MakeModule Network
 call Script\System\MakeModule Console
+call Script\System\MakeModule Entry
