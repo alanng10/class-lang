0d4cedadb13e7f639dbc861db1314800e285daac
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-11 14:55:37 +0800

    Avalon

--- Avalon/Avalon.Console/ConsoleIntern.cs
+++ Avalon/Avalon.Console/ConsoleIntern.cs
@@ -5,12 +5,10 @@ class ConsoleIntern : Any
     public override bool Init()
     {
         base.Init();
-        this.InternIntern = InternIntern.This;
         this.InternInfra = InternInfra.This;
         return true;
     }
 
-    private InternIntern InternIntern { get; set; }
     private InternInfra InternInfra { get; set; }
     private ulong Intern { get; set; }
 
@@ -32,23 +30,23 @@ class ConsoleIntern : Any
 
     public virtual bool OutWrite(String a)
     {
-        ulong uo;
-        uo = this.InternInfra.StringCreate(a);
+        ulong k;
+        k = this.InternInfra.StringCreate(a);
         
-        Extern.Console_OutWrite(this.Intern, uo);
+        Extern.Console_OutWrite(this.Intern, k);
 
-        this.InternInfra.StringDelete(uo);
+        this.InternInfra.StringDelete(k);
         return true;
     }
 
     public virtual bool ErrWrite(String a)
     {
-        ulong uo;
-        uo = this.InternInfra.StringCreate(a);
+        ulong k;
+        k = this.InternInfra.StringCreate(a);
         
-        Extern.Console_ErrWrite(this.Intern, uo);
+        Extern.Console_ErrWrite(this.Intern, k);
 
-        this.InternInfra.StringDelete(uo);
+        this.InternInfra.StringDelete(k);
         return true;
     }
 
