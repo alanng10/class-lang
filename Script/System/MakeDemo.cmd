1037201ccd952cd1d05c36c92797cdf0615c927f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-12 17:59:49 +0800

    Script

--- Script/System/MakeDemo.cmd
+++ Script/System/MakeDemo.cmd
@@ -1,9 +1,8 @@
 @echo off
 
-set Module=%~1
 set DotNetOutFold=.\Out\net8.0
 
-echo Make Module %Module%
+echo Make Demo
 pushd %DotNetOutFold%
 saber make ../../System/SystemDemo
 echo Status: %errorlevel%
