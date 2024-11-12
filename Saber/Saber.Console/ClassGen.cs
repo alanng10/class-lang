f5897b6c87be1911e6ce9f53f5c8d28d7f5a4dcc
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-12 12:11:38 +0800

    Saber

--- Saber/Saber.Console/ClassGen.cs
+++ Saber/Saber.Console/ClassGen.cs
@@ -2566,11 +2566,25 @@ public class ClassGen : ClassBase
         this.Text(this.LimitAre);
         this.Text(this.Space);
 
+        this.Text(this.CastInt);
+        this.Text(this.LimitBraceRoundLite);
+
+        this.Text(this.LimitBraceRoundLite);
+
+        this.Text(this.LimitBraceRoundLite);
+        this.Text(this.ClassInt);
+        this.Text(this.LimitAsterisk);
+        this.Text(this.LimitBraceRoundRite);
+
         this.Text(value);
+
+        this.Text(this.LimitBraceRoundRite);
 
         this.Text(this.Space);
 
         this.TextPos(pos);
+
+        this.Text(this.LimitBraceRoundRite);
 
         this.Text(this.LimitSemicolon);
         this.Text(this.NewLine);
