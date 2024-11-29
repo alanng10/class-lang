78a15ecf655d3ac55c2207742fbfa49ca73c7c79
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-11-29 08:27:50 +0800

    View

--- View/View.Draw/Color.cs
+++
@@ -1,9 +0,0 @@
-namespace View.Draw;
-
-public class Color : Any
-{
-    public virtual long Alpha { get; set; }
-    public virtual long Red { get; set; }
-    public virtual long Green { get; set; }
-    public virtual long Blue { get; set; }
-}
