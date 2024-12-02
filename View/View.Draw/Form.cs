a3c4927c38f7b5ccc9361fdd86b587d3bb30fa01
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-12-02 11:18:52 +0800

    View

--- View/View.Draw/Form.cs
+++ View/View.Draw/Form.cs
@@ -129,7 +129,7 @@ public class Form : Any
         return a;
     }
 
-    public virtual bool Multiply(Form other)
+    public virtual bool Mul(Form other)
     {
         Extern.Form_Multiply(this.Intern, other.Intern);
         return true;
