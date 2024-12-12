0d248b4488c342956c8b1944b1ca0b68a8cf5e26
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-12-12 18:40:01 +0800

    Saber

---
+++ Saber/SaberDocueExe/Entry.cs
@@ -0,0 +1,16 @@
+namespace SaberExe;
+
+class Entry
+{
+    [STAThread]
+    static int Main(string[] arg)
+    {
+        EntryEntry entry;
+        entry = new ModuleEntry();
+        entry.Init();
+        entry.ArgSet(arg);
+        int o;
+        o = entry.Execute();
+        return o;
+    }
+}
