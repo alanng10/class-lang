b2bf746714e345eba83741d4593e14d34bfc313f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-12-12 18:41:00 +0800

    Saber

--- Saber/SaberDocueExe/-.cs
+++ Saber/SaberDocueExe/-.cs
@@ -1,3 +1,3 @@
 global using EntryEntry = Avalon.Entry.Entry;
-global using ModuleEntry = Saber.Console.Entry;
+global using ModuleEntry = Saber.Docue.Entry;
 global using STAThreadAttribute = System.STAThreadAttribute;
--- Saber/SaberDocueExe/Entry.cs
+++ Saber/SaberDocueExe/Entry.cs
@@ -1,4 +1,4 @@
-namespace SaberExe;
+namespace SaberDocueExe;
 
 class Entry
 {
