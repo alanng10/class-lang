d7842dced376b009dba1831c010b4d82d5bdfab0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-12-12 18:45:21 +0800

    Script

--- Script/Saber/CleanDocue.cmd
+++ Script/Saber/CleanDocue.cmd
@@ -1,4 +1,7 @@
 @echo off
 
-set DocueOutFold=.\Out\Docue
-rmdir /S /Q %DocueOutFold% 2>NUL
+echo Clean Docue
+set DotNetModuleOutFold=.\Out\net8.0
+del /F /Q %DotNetModuleOutFold%\Saber.Docue.* 2>NUL
+del /F /Q %DotNetModuleOutFold%\SaberDocueExe* 2>NUL
+rmdir /S /Q %DotNetModuleOutFold%\Saber.Docue.data 2>NUL
