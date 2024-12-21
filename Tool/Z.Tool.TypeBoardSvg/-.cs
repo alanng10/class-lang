c2aea1df89956318f40c89416837f0d6d7744f4f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-12-21 19:42:17 +0800

    Tool

--- Tool/Z.Tool.TypeBoardSvg/-.cs
+++ Tool/Z.Tool.TypeBoardSvg/-.cs
@@ -2,3 +2,5 @@ global using String = Avalon.Infra.String;
 global using Data = Avalon.Infra.Data;
 global using Text = Avalon.Text.Text;
 global using ToolBase = Z.Infra.Infra.Base;
+global using EntryEntry = Avalon.Entry.Entry;
+global using STAThreadAttribute = System.STAThreadAttribute;
