0eba03759a73b30cc5d41ccbad97f392b128142e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-12-31 15:18:00 +0800

    Tool

---
+++ Tool/Z.Tool.View.AlignList/Gen.cs
@@ -0,0 +1,20 @@
+namespace Z.Tool.View.AlignList;
+
+public class Gen : SourceGen
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Module = this.S("Saber.Infra");
+        this.ClassName = this.S("CountList");
+        this.BaseClassName = this.S("Any");
+        this.AnyClassName = this.S("Any");
+        this.ItemClassName = this.S("Count");
+        this.ArrayClassName = this.S("Array");
+        this.Export = true;
+        this.ItemListFileName = this.S("ToolData/Class/ItemListCount.txt");
+        this.AddMethodFileName = this.S("ToolData/Class/AddMaideCount.txt");
+        this.OutputFilePath = this.S("../../Saber/Saber.Infra/CountList.cs");
+        return true;
+    }
+}
