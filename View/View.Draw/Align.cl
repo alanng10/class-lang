0b354a156de559ecd3fb73ae4a2923905fd7e085
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-12-31 15:43:11 +0800

    View

---
+++ View/View.Draw/Align.cl
@@ -0,0 +1,4 @@
+class Align : Any
+{
+    field prusate Int Index { get { return data; } set { data : value; } }
+}
--- View/View.Draw/Align.cs
+++
@@ -1,6 +0,0 @@
-namespace View.Draw;
-
-public class Align : Any
-{
-    public virtual long Index { get; set; }
-}
