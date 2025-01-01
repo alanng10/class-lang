92a42998650e7b43acd1c1c34b6d3d2a77cb0a5e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-01 21:33:45 +0800

    Tool

--- Tool/Z.Infra.StatItemListSourceGen/MiraiGen.cs
+++
@@ -1,9 +0,0 @@
-namespace Z.Infra.StatItemListSourceGen;
-
-public class MiraiGen : AvalonGen
-{
-    protected override String GetOutputFilePath()
-    {
-        return this.AddClear().AddS("../../Mirai/").Add(this.Module).AddS("/").Add(this.ClassName).AddS(".cs").AddResult();
-    } 
-}
