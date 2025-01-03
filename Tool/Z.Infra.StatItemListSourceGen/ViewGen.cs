8a60dac2af1d30fab07a23032690677048c406af
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-03 09:52:01 +0800

    Tool

--- Tool/Z.Infra.StatItemListSourceGen/ViewGen.cs
+++
@@ -1,9 +0,0 @@
-namespace Z.Infra.StatItemListSourceGen;
-
-public class ViewGen : Gen
-{
-    protected override String GetOutputFilePath()
-    {
-        return this.AddClear().AddS("../../View/").Add(this.Module).Add(this.TextInfra.PathCombine).Add(this.ClassName).AddS(".cl").AddResult();
-    } 
-}
