1d21e197df932ca45542ee9b088d31ff563bdf8a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-16 13:38:42 +0800

    Tool

--- Tool/Z.Tool.NodeListGen/NodeStateGen.cs
+++ Tool/Z.Tool.NodeListGen/NodeStateGen.cs
@@ -2,14 +2,20 @@ namespace Z.Tool.NodeListGen;
 
 public class NodeStateGen : ToolBase
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.OutputFoldPath = this.S("../../Saber/Saber.Node");
+        return true;
+    }
+
     public virtual Table ClassTable { get; set; }
-    private String OutputFoldPath { get; set; }
+    protected virtual String OutputFoldPath { get; set; }
     private String NodeStateSourceFileName { get; set; }
 
     public virtual int Execute()
     {
         this.NodeStateSourceFileName = this.S("ToolData/Saber/NodeStateSource.txt");
-        this.OutputFoldPath = this.S("../../Saber/Saber.Node");
 
         String kk;
         kk = this.ToolInfra.StorageTextRead(this.NodeStateSourceFileName);
@@ -37,20 +43,20 @@ public class NodeStateGen : ToolBase
             a = this.StringCreate(k);
 
             String path;
-            path = this.GetOutputFilePath(kind);
+            path = this.OutputFilePath(kind);
 
             this.ToolInfra.StorageTextWrite(path, a);
         }
         return 0;
     }
 
-    private String GetOutputFilePath(String kind)
+    protected virtual String OutputFilePath(String kind)
     {
         String fileName;
         fileName = this.AddClear().AddS("Z_NodeState_").Add(kind).AddS(".cs").AddResult();
 
         String filePath;
-        filePath = this.AddClear().Add(this.OutputFoldPath).AddS("/").Add(fileName).AddResult();
+        filePath = this.AddClear().Add(this.OutputFoldPath).Add(this.TextInfra.PathCombine).Add(fileName).AddResult();
         return filePath;
     }
 }
