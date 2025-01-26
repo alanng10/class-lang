698452d7e842643e66c3aa41bb1e3f25071fa612
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-26 14:34:12 +0800

    Infra

--- Infra/Infra/VideoOutIntern.cpp
+++ Infra/Infra/VideoOutIntern.cpp
@@ -11,5 +11,15 @@ void VideoOutIntern::FrameEventHandle(const QVideoFrame& frame)
     Int videoOut;
     videoOut = this->VideoOut;
 
+    Int aa;
+    aa = VideoOut_FrameGet(videoOut);
+
+    Int oo;
+    oo = VideoFrame_Intern(aa);
+
+    QVideoFrame* u;
+    u = (QVideoFrame*)oo;
+    *u = frame;
+
     VideoOut_FrameEvent(videoOut);
 }
