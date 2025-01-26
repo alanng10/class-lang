d689094b9ca00b50bdcbae1c01da6152aefad5be
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-26 14:50:40 +0800

    Infra

--- Infra/InfraDemo/Demo.h
+++ Infra/InfraDemo/Demo.h
@@ -1,23 +1,14 @@
 #pragma once
 
-
-
 #include <Infra/Prusate.h>
 
-
-
-
 Int TypeHandle(Int frame, Int index, Int field, Int arg);
-
 
 Int DrawHandle(Int frame, Int arg);
 
 Int VideoOutFrameHandle(Int videoOut, Int arg);
 
 Int ConsoleWriteConstant(const char* o);
-
-
-
 
 Int SetRect(Int rect, Int left, Int up, Int width, Int height);
 
@@ -28,7 +19,5 @@ Int SetSize(Int size, Int width, Int height);
 Int SetRange(Int range, Int index, Int count);
 
 Int MathInt(Int n);
-
-
 
 int main(int argc, char* argv[]);
