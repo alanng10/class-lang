ca31e32c22c6b61b8c785c7031f346d9ab9154b2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-02-08 15:44:53 +0800

    Infra

--- Infra/Infra/FontStore.cpp
+++ Infra/Infra/FontStore.cpp
@@ -11,3 +11,29 @@ Int FontStore_Final(Int o)
 {
     return true;
 }
+
+Int FontStore_Add(Int o, Int data)
+{
+    Int value;
+    value = Data_ValueGet(data);
+    Int count;
+    count = Data_CountGet(data);
+
+    const char* ka;
+    ka = (const char*)value;
+
+    qsizetype kb;
+    kb = count;
+
+    QByteArray k(ka, kb);
+
+    int kk;
+    kk = QFontDatabase::addApplicationFontFromData(k);
+
+    SInt ke;
+    ke = kk;
+
+    Int a;
+    a = ke;
+    return a;
+}
--- Infra/Infra/FontStore.hpp
+++ Infra/Infra/FontStore.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QFontDatabase>
+#include <QByteArray>
 
 #include "Pronate.hpp"
 
