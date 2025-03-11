d0bc048e86866631af998710d73ccc4ff1ec75b4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-03-11 12:32:06 +0800

    Tool

--- Tool/Z.Tool.Infra.StatPolateSpread/Gen.cs
+++ Tool/Z.Tool.Infra.StatPolateSpread/Gen.cs
@@ -12,4 +12,32 @@ class Gen : StatGen
 
         return base.Execute();
     }
+
+    protected override ListEntry GetItemEntry(String line)
+    {
+        Text kk;
+        kk = this.TextCreate(line);
+
+        Array k;
+        k = this.TextLimit(kk, this.TB(this.S(" ")));
+
+        Text kaa;
+        kaa = k.GetAt(0) as Text;
+
+        Text kab;
+        kab = k.GetAt(1) as Text;
+
+        String ka;
+        ka = this.StringCreate(kaa);
+
+        String kb;
+        kb = this.StringCreate(kab);
+
+        ListEntry a;
+        a = new ListEntry();
+        a.Init();
+        a.Index = ka;
+        a.Value = kb;
+        return a;
+    }
 }
