da6c614f004a1e19172f77450ce8a8817b7cc6d6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-07 04:16:49 +0800

    Saber

--- Saber/Saber.Infra/Error.cs
+++ Saber/Saber.Infra/Error.cs
@@ -3,12 +3,8 @@ namespace Saber.Infra;
 public class Error : Any
 {
     public virtual Stage Stage { get; set; }
-
     public virtual ErrorKind Kind { get; set; }
-
     public virtual Range Range { get; set; }
-
     public virtual String Name { get; set; }
-    
     public virtual Source Source { get; set; }
 }
