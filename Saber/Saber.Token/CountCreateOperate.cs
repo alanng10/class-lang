1363aedffd439533945a9551e2ee475c5e16f687
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-06 09:59:31 +0800

    Saber

--- Saber/Saber.Token/CountCreateOperate.cs
+++ Saber/Saber.Token/CountCreateOperate.cs
@@ -9,12 +9,11 @@ public class CountCreateOperate : CreateOperate
         return true;
     }
 
-    public virtual Create Create { get; set; }
     protected virtual InfraInfra InfraInfra { get; set; }
-    protected virtual long CodeTokenStart { get; set; }
-    protected virtual long CodeCommentStart { get; set; }
+    protected virtual long TokenStart { get; set; }
+    protected virtual long CommentStart { get; set; }
 
-    public override bool ExecuteToken()
+    public override bool ExecuteToken(long row, Range range)
     {
         CreateArg arg;
         arg = this.Create.Arg;
@@ -25,7 +24,7 @@ public class CountCreateOperate : CreateOperate
         return true;
     }
 
-    public override bool ExecuteComment()
+    public override bool ExecuteComment(long row, Range range)
     {
         CreateArg arg;
         arg = this.Create.Arg;
@@ -40,40 +39,39 @@ public class CountCreateOperate : CreateOperate
     {
         CreateArg arg;
         arg = this.Create.Arg;
-        this.CodeTokenStart = arg.TokenIndex;
-        this.CodeCommentStart = arg.CommentIndex;
+        this.TokenStart = arg.TokenIndex;
+        this.CommentStart = arg.CommentIndex;
         return true;
     }
 
     public override bool ExecuteCodeEnd(long index)
     {
-        InfraInfra infraInfra;
-        infraInfra = this.InfraInfra;
-
         CreateArg arg;
         arg = this.Create.Arg;
 
         long tokenCount;
         long commentCount;
-        tokenCount = arg.TokenIndex - this.CodeTokenStart;
-        commentCount = arg.CommentIndex - this.CodeCommentStart;
+        tokenCount = arg.TokenIndex - this.TokenStart;
+        commentCount = arg.CommentIndex - this.CommentStart;
 
-        Data codeCountData;
-        codeCountData = arg.CodeCountData;
-        long oa;
-        oa = sizeof(ulong);
-        long ob;
-        ob = index;
-        ob = ob * 2;
-        long oe;
-        oe = ob * oa;
-        long of;
-        of = (ob + 1) * oa;
-        infraInfra.DataIntSet(codeCountData, oe, tokenCount);
-        infraInfra.DataIntSet(codeCountData, of, commentCount);
+        Data data;
+        data = arg.CodeCountData;
+        long kd;
+        kd = sizeof(ulong);
 
-        this.CodeTokenStart = 0;
-        this.CodeCommentStart = 0;
+        long kk;
+        kk = index;
+        kk = kk * 2;
+
+        long ka;
+        ka = kk * kd;
+        this.InfraInfra.DataIntSet(data, ka, tokenCount);
+
+        ka = (kk + 1) * kd;
+        this.InfraInfra.DataIntSet(data, ka, commentCount);
+
+        this.TokenStart = 0;
+        this.CommentStart = 0;
         return true;
     }
 }
