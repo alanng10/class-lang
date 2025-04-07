fc64897ea9c91c00e320b65d3c91f7f1559365d8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-07 12:35:42 +0800

    Tool

--- Tool/Z.Tool.SourceSpace/-.cs
+++ Tool/Z.Tool.SourceSpace/-.cs
@@ -1,4 +1,6 @@
 global using String = Avalon.Infra.String;
+global using Array = Avalon.List.Array;
+global using Text = Avalon.Text.Text;
 global using StorageComp = Avalon.Storage.Comp;
 global using EntryEntry = Avalon.Entry.Entry;
 global using Base = Z.Infra.Infra.Base;
--- Tool/Z.Tool.SourceSpace/SourceSpace.cs
+++ Tool/Z.Tool.SourceSpace/SourceSpace.cs
@@ -6,13 +6,61 @@ public class SourceSpace : Base
     {
         base.Init();
         this.StorageComp = StorageComp.This;
+
+        this.SActual = this.S("Actual");
         return true;
     }
 
+    public virtual String Path { get; set; }
     protected virtual StorageComp StorageComp { get; set; }
+    protected virtual String SActual { get; set; }
 
     public virtual long Execute()
     {
+        
         return 0;
+    }
+
+    protected virtual bool FileList(String path, Array array)
+    {
+        long count;
+        count = array.Count;
+
+        long i;
+        i = 0;
+        while (i < count)
+        {
+            String name;
+            name = array.GetAt(i) as String;
+
+            bool b;
+            b = false;
+            if (this.TextSame(this.TA(name), this.TB(this.SActual)))
+            {
+                b = true;
+            }
+
+            if (!b)
+            {
+                String ka;
+                ka = this.AddClear().Add(path).Add(this.TextInfra.PathCombine).Add(name).AddResult();
+
+                String kk;
+                kk = this.StorageTextRead(ka);
+
+                
+            }
+        }
+    }
+
+    protected virtual bool ExecuteText(String text)
+    {
+        Text ka;
+        ka = this.TextCreate(text);
+
+        Array lineArray;
+        lineArray = this.TextLine(ka);
+
+        return true;
     }
 }
