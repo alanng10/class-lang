4822126c83f2984f880f8816ff78f1734eb8600e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-07 12:51:21 +0800

    Tool

--- Tool/Z.Tool.SourceSpace/SourceSpace.cs
+++ Tool/Z.Tool.SourceSpace/SourceSpace.cs
@@ -8,17 +8,68 @@ public class SourceSpace : Base
         this.StorageComp = StorageComp.This;
 
         this.SActual = this.S("Actual");
+        this.SOut = this.S("Out");
+        this.SBin = this.S("bin");
+        this.SObj = this.S("obj");
         return true;
     }
 
     public virtual String Path { get; set; }
     protected virtual StorageComp StorageComp { get; set; }
     protected virtual String SActual { get; set; }
+    protected virtual String SOut { get; set; }
+    protected virtual String SBin { get; set; }
+    protected virtual String SObj { get; set; }
 
     public virtual long Execute()
     {
         
         return 0;
+    }
+
+    protected virtual bool ExecuteFoldList(String path, Array array)
+    {
+        long count;
+        count = array.Count;
+
+        long i;
+        i = 0;
+        while (i < count)
+        {
+            String name;
+            name = array.GetAt(i) as String;
+
+            bool b;
+            b = false;
+            if (!b)
+            {
+                if (this.NameSame(name, this.SOut))
+                {
+                    b = true;
+                }
+            }
+            if (!b)
+            {
+                if (this.NameSame(name, this.SBin))
+                {
+                    b = true;
+                }
+            }
+            if (!b)
+            {
+                if (this.NameSame(name, this.SObj))
+                {
+                    b = true;
+                }
+            }
+
+            if (!b)
+            {
+                
+            }
+        }
+
+        return true;
     }
 
     protected virtual bool ExecuteFileList(String path, Array array)
@@ -82,5 +133,12 @@ public class SourceSpace : Base
         }
 
         return true;
+    }
+
+    protected virtual bool NameSame(String lite, String rite)
+    {
+        bool a;
+        a = this.TextSame(this.TextForm(this.TA(lite), this.TextInfra.AlphaSiteForm), this.TextForm(this.TB(rite), this.TextInfra.AlphaSiteForm));
+        return a;
     }
 }
