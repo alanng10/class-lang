0b276b7f2db6980e26287c7a89e138303e5dc5a2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-07 12:18:17 +0800

    Tool

--- Tool/Z.Tool.SourceSpace/SourceSpace.cs
+++ Tool/Z.Tool.SourceSpace/SourceSpace.cs
@@ -2,5 +2,17 @@ namespace Z.Tool.SourceSpace;
 
 public class SourceSpace : Base
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.StorageComp = StorageComp.This;
+        return true;
+    }
+
     protected virtual StorageComp StorageComp { get; set; }
+
+    public virtual long Execute()
+    {
+        return 0;
+    }
 }
