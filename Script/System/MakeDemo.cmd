40e6ce7e6f3ae3e4d1dbcac0d802e8cf677e4bc0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-19 16:56:31 +0800

    Script

--- Script/System/MakeDemo.cmd
+++ Script/System/MakeDemo.cmd
@@ -5,8 +5,6 @@ set DotNetOutFold=.\Out\net8.0
 echo Make Demo
 pushd %DotNetOutFold%
 saber make ../../System/SystemDemo
-echo Status: %errorlevel%
 
 saber make ../../System/SystemDemoNetwork
-echo Status: %errorlevel%
 popd
--- Script/System/MakeModule.cmd
+++ Script/System/MakeModule.cmd
@@ -6,5 +6,4 @@ set DotNetOutFold=.\Out\net8.0
 echo Make Module %Module%
 pushd %DotNetOutFold%
 saber make ../../System/System.%Module% -m
-echo Status: %errorlevel%
 popd
