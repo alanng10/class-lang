45c983728993230b36a57efaef41d610db780a5f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-20 00:55:34 +0800

    Infra

--- Infra/Infra/Thread_Linux.c
+++ Infra/Infra/Thread_Linux.c
@@ -10,6 +10,30 @@ Int Thread_OS_CloseHandle(Int handle)
 
 Int Thread_OS_Set()
 {
+    struct sigaction act;
+    act = { };
+
+    sigemptyset(&act.sa_mask);
+    
+    act.sa_flags = 0;
+    act.sa_handler = SignalHandle;
+
+    int k;
+    k = sigaction(SIGUSR1, &act, NULL);
+
+    if (!(k == 0)) 
+    {
+        return false;
+    }
+    
+    k = sigaction(SIGUSR2, &act, NULL);
+    
+    if (!(k == 0)) 
+    {
+        return false;
+    }
+
+    return true;
 }
 
 Int Thread_OS_Pause(Int handle)
@@ -39,12 +63,6 @@ Int Thread_OS_Resume(Int handle)
     return true;
 }
 
-
-// Since I have only 2 threads so using two variables, 
-// array of bools will be more useful for `n` number of threads.
-static int is_th1_ready = 0;
-static int is_th2_ready = 0;
-
 static void SignalHandle(int signal)
 {
     if (signal == SIGUSR1)
@@ -55,33 +73,4 @@ static void SignalHandle(int signal)
     if (signal == SIGUSR2)
     {
     }
-}
-
-static void *thread_job(void *t_id)
-{
-        int i = 0;
-        struct sigaction act;
-
-        pthread_detach(pthread_self());
-
-        sigemptyset(&act.sa_mask);
-        act.sa_flags = 0;
-        act.sa_handler = SignalHandle;
-
-        if (sigaction(SIGUSR1, &act, NULL) == -1) 
-                printf("unable to handle siguser1\n");
-        if (sigaction(SIGUSR2, &act, NULL) == -1) 
-                printf("unable to handle siguser2\n");
-
-        if (t_id == (void *)1)
-            is_th1_ready = 1;
-        if (t_id == (void *)2)
-            is_th2_ready = 1;
-
-        while (1) {
-                printf("thread id: %p, counter: %d\n", t_id, i++);
-                sleep(1);
-        }
-
-        return NULL;
 }
