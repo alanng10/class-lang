a4ab6e783a52444634035dfb20f980788b46cde9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-21 14:55:45 +0800

    Infra

--- Infra/Infra/Thread_Linux.c
+++ Infra/Infra/Thread_Linux.c
@@ -13,12 +13,12 @@ Int Thread_OS_CloseHandle(Int handle)
 Int Thread_OS_Set()
 {
     struct sigaction act;
-    act = { };
+    memset(&act, 0, sizeof(sigaction));
 
     int k;
     k = sigemptyset(&act.sa_mask);
 
-    if (!(k == 0)) 
+    if (!(k == 0))
     {
         return false;
     }
@@ -28,7 +28,7 @@ Int Thread_OS_Set()
 
     k = sigaction(SIGUSR1, &act, NULL);
 
-    if (!(k == 0)) 
+    if (!(k == 0))
     {
         return false;
     }
@@ -70,7 +70,7 @@ Int Thread_OS_Resume(Int handle)
     return true;
 }
 
-static void SignalHandle(int signal)
+void SignalHandle(int signal)
 {
     if (signal == SIGUSR1)
     {
