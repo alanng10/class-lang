f6ee126328f312adb3384fd2abfb5d019c04e087
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-21 16:51:38 +0800

    Script

--- Script/Infra/Execute.sh
+++ Script/Infra/Execute.sh
@@ -4,8 +4,4 @@ InfraDemoPackageOutFold=Out/InfraDemo-Linux-Release
 InfraPackageOutFold=Out/Infra-Linux-Release
 WorkFold=$PWD
 
-pushd $InfraDemoPackageOutFold
-
-PATH="$WorkFold/$InfraPackageOutFold:$PATH" InfraDemo
-
-popd
+LD_LIBRARY_PATH="$WorkFold/$InfraPackageOutFold:$LD_LIBRARY_PATH" $InfraDemoPackageOutFold/InfraDemo
