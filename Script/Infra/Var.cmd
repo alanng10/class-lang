6662b8f9182707c3c66084f8f1917580119023fc
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-21 11:35:59 +0800

    Script

--- Script/Infra/Var.cmd
+++ Script/Infra/Var.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set VarIsSet=0
-set WorkingFold=%cd%
+set WorkFold=%cd%
 
 if "%QtRoot%" == "" (
     set /p QtRoot=<Script\Infra\QtRootPathWindows.txt
@@ -10,5 +10,5 @@ if "%QtRoot%" == "" (
 
 if "%VarIsSet%" == "1" (
     call "%QtRoot%\6.8.1\llvm-mingw_64\bin\qtenv2.bat"1>NUL
-    cd "%WorkingFold%"
+    cd "%WorkFold%"
 )
