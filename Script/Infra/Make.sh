80cc283b3fa26ff3ad593118c4b01831178e6714
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-21 10:49:42 +0800

    Script

---
+++ Script/Infra/Make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Script/Infra/CleanModule
+Script/Infra/CleanDemo
+Script/Infra/CleanIntern
+Script/Infra/CleanExecute
+Script/Infra/MakeModulePackage
+Script/Infra/MakeModule
+Script/Infra/MakeDemoPackage
+Script/Infra/MakeDemo
+Script/Infra/MakeInternPackage
+Script/Infra/MakeIntern
+Script/Infra/MakeExecutePackage
+Script/Infra/MakeExecute
