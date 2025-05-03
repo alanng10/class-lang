b21601931ea117757266584a5e5d6543dad6b83d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-03 09:21:00 +0800

    Avalon

--- Avalon/Avalon.Thread/-.cs
+++ Avalon/Avalon.Thread/-.cs
@@ -1,8 +1,4 @@
 global using InternIntern = Avalon.Infra.Intern;
-global using Extern = Avalon.Infra.Extern;
-global using ThreadExecuteMaide = Avalon.Infra.Extern.Thread_Execute_Maide;
-global using MaideAddress = Avalon.Infra.MaideAddress;
-global using Handle = Avalon.Infra.Handle;
 global using InternInfra = Avalon.Infra.InternInfra;
 global using Any = Avalon.Infra.Any;
 global using State = Avalon.Infra.State;
