41cc5c1d7c721062aaabc3b0c7c10fe3af1076f2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-03 12:08:11 +0800

    Avalon

--- Avalon/Avalon.Time/-.cs
+++ Avalon/Avalon.Time/-.cs
@@ -1,7 +1,2 @@
-global using InternIntern = Avalon.Infra.Intern;
-global using Extern = Avalon.Infra.Extern;
-global using TimeEventElapseMaide = Avalon.Infra.Extern.TimeEvent_Elapse_Maide;
-global using MaideAddress = Avalon.Infra.MaideAddress;
-global using Handle = Avalon.Infra.Handle;
-global using InternInfra = Avalon.Infra.InternInfra;
 global using Any = Avalon.Infra.Any;
+global using SystemTime = System.DateTime;
