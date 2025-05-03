b59ececa449dc6351df84aec1b5838be2abda7d5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-03 09:32:20 +0800

    Avalon

--- Avalon/Avalon.Thread/-.cs
+++ Avalon/Avalon.Thread/-.cs
@@ -2,4 +2,4 @@ global using InternIntern = Avalon.Infra.Intern;
 global using InternInfra = Avalon.Infra.InternInfra;
 global using Any = Avalon.Infra.Any;
 global using State = Avalon.Infra.State;
-global using Value = Avalon.Infra.Value;
+global using SystemThread = System.Threading.Thread;
--- Avalon/Avalon.Thread/Thread.cs
+++ Avalon/Avalon.Thread/Thread.cs
@@ -2,52 +2,11 @@ namespace Avalon.Thread;
 
 public class Thread : Any
 {
-    private ulong PrivateExecuteStateExecute()
-    {
-        this.ExecuteState.Execute();
-
-        object aa;
-        aa = this.ExecuteState.Result;
-
-        ulong oa;
-        oa = 0;
-
-        if (!(aa == null))
-        {
-            if (aa is Value)
-            {
-                Value k;
-                k = (Value)aa;
-
-                long o;
-                o = k.Int;
-
-                oa = (ulong)o;
-            }
-        }
-
-        return oa;
-    }
-
     public override bool Init()
     {
         base.Init();
         this.InternIntern = InternIntern.This;
-        this.InternInfra = InternInfra.This;
-        this.ThreadInfra = Infra.This;
-        this.InternHandle = new Handle();
-        this.InternHandle.Any = this;
-        this.InternHandle.Init();
-
-        MaideAddress oa;
-        oa = this.ThreadInfra.ThreadExecuteMaideAddress;
-        ulong arg;
-        arg = this.InternHandle.ULong();
-        this.InternExecuteState = this.InternInfra.StateCreate(oa, arg);
-
-        this.Intern = Extern.Thread_New();
-        Extern.Thread_Init(this.Intern);
-        Extern.Thread_ExecuteStateSet(this.Intern, this.InternExecuteState);
+        this.Intern = new SystemThread(this.InternExecute);
         return true;
     }
 
@@ -55,96 +14,42 @@ public class Thread : Any
     {
         base.Init();
         this.InternIntern = InternIntern.This;
-        this.InternInfra = InternInfra.This;
-        this.ThreadInfra = Infra.This;
-
-        this.Intern = Extern.Thread_This();
-
         InternIntern.ThisThread = this;
         return true;
     }
 
     public virtual bool Final()
     {
-        Extern.Thread_Final(this.Intern);
-        Extern.Thread_Delete(this.Intern);
-
-        this.InternInfra.StateDelete(this.InternExecuteState);
-
-        this.InternHandle.Final();
-        
         return true;
     }
 
     public virtual State ExecuteState { get; set; }
     public virtual object Any { get; set; }
-    public virtual long Status
-    {
-        get
-        {
-            ulong u;
-            u = Extern.Thread_StatusGet(this.Intern);
-            long a;
-            a = (long)u;
-            return a;
-        }
-        set
-        {
-        }
-    }
     private InternIntern InternIntern { get; set; }
-    private InternInfra InternInfra { get; set; }
-    private Infra ThreadInfra { get; set; }
-    private ulong Intern { get; set; }
-    private ulong InternExecuteState { get; set; }
-    private Handle InternHandle { get; set; }
+    private SystemThread Intern { get; set; }
 
-    internal static ulong InternExecute(ulong thread, ulong arg)
+    private void InternExecute()
     {
-        InternIntern internIntern;
-        internIntern = InternIntern.This;
+        this.PrivateExecuteStateExecute();
+    }
 
-        object ao;
-        ao = internIntern.HandleTarget(arg);
+    private bool PrivateExecuteStateExecute()
+    {
+        InternIntern.ThisThread = this;
 
-        Thread a;        
-        a = (Thread)ao;
-
-        InternIntern.ThisThread = a;
-
-        ulong oa;
-        oa = a.PrivateExecuteStateExecute();
-
-        return oa;
+        this.ExecuteState.Execute();
+        return true;
     }
 
     public virtual bool Execute()
     {
-        Extern.Thread_Execute(this.Intern);
+        this.Intern.Start();
         return true;
     }
 
     public virtual bool Wait()
     {
-        Extern.Thread_Wait(this.Intern);
-        return true;
-    }
-
-    public virtual long ExecuteMain()
-    {
-        ulong u;
-        u = Extern.Thread_ExecuteMain(this.Intern);
-        long a;
-        a = (long)u;
-        return a;
-    }
-
-    public virtual bool Exit(long status)
-    {
-        ulong u;
-        u = (ulong)status;
-
-        Extern.Thread_Exit(this.Intern, u);
+        this.Intern.Join();
         return true;
     }
 }
