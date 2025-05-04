a0d840bd2dad892a6fd2efa78e688678a9aadd2b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-04 13:08:06 +0800

    Avalon

--- Avalon/Avalon.Console/ConsoleInn.cs
+++ Avalon/Avalon.Console/ConsoleInn.cs
@@ -2,10 +2,23 @@ namespace Avalon.Console;
 
 class ConsoleInn : Inn
 {
-    internal virtual ConsoleIntern Intern { get; set; }
+    public override bool Init()
+    {
+        base.Init();
+        this.StringValue = StringValue.This;
+        return true;
+    }
+
+    protected virtual StringValue StringValue { get; set; }
 
     public override String Read()
     {
-        return this.Intern.Read();
+        string ka;
+        ka = SystemConsole.ReadLine();
+
+        String k;
+        k = this.StringValue.Execute(ka);
+
+        return k;
     }
 }
