ea926ca5f1678806d56c7eaba49b84664429b599
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-07 13:13:57 +0800

    Saber

--- Saber/Saber.Node/CreateSetState.cs
+++ Saber/Saber.Node/CreateSetState.cs
@@ -5,10 +5,8 @@ public class CreateSetState : InfraState
     public override bool Init()
     {
         base.Init();
-        CreateSetStateArg aa;
-        aa = new CreateSetStateArg();
-        aa.Init();
-        this.Arg = aa;
+        this.Arg = new CreateSetStateArg();
+        this.Arg.Init();
         return true;
     }
 }
