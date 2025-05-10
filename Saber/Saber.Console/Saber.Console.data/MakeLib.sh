a3f211f94cccdd7d55d8ba16db5fdcd2bdbf9c34
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-10 16:03:54 +0800

    Saber

--- Saber/Saber.Console/Saber.Console.data/MakeLib.sh
+++ Saber/Saber.Console/Saber.Console.data/MakeLib.sh
@@ -9,5 +9,5 @@ SourceFold=./Gen/$ModuleRef
 Import=`cat $SourceFold/Import.txt`
 
 pushd $SourceFold
-gcc -pipe -fno-strict-aliasing -O0 -std=gnu11 -Wall -Wno-ignored-attributes -Wno-bitwise-instead-of-logical -Wno-unused-but-set-variable -Wno-unused-variable -I. -I../.. -Wl,-s -shared -L../../../$ClassPath -L../../../$ClassPath/Module -lInfra -lInfraIntern$Import -o $ModuleRef.so *.c 1>/dev/null 2>../../error.txt
+gcc -pipe -fno-strict-aliasing -O0 -std=gnu11 -Wall -Wno-ignored-attributes -Wno-bitwise-instead-of-logical -Wno-unused-but-set-variable -Wno-unused-variable -I. -I../.. -Wl,-s -shared -L../../../$ClassPath -L../../../$ClassPath/Module -lInfra -lInfraIntern$Import -o lib$ModuleRef.so *.c 1>/dev/null 2>../../error.txt
 popd
