17f2796a99ce76a2b44f320d2fee4872415a4132
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-13 18:14:55 +0800

    Saber

---
+++ Saber/Saber.Console/ModuleRefStringGen.cs
@@ -0,0 +1,54 @@
+namespace Saber.Console;
+
+public class ModuleRefStringGen : TextAdd
+{
+    public virtual ClassGen Gen { get; set; }
+    public virtual ClassModule Module { get; set; }
+    public virtual String Result { get; set; }
+
+    public virtual bool Execute()
+    {
+        ClassGen gen;
+        gen = this.Gen;
+
+        gen.Arg = new GenArg();
+        gen.Arg.Init();
+
+        gen.Operate = gen.CountOperate;
+
+        gen.ResetStageIndex();
+        this.ExecuteStage();
+
+        long nn;
+        nn = gen.Arg.Index;
+        nn = nn * sizeof(uint);
+        Data data;
+        data = new Data();
+        data.Count = nn;
+        data.Init();
+        gen.Arg.Data = data;
+
+        gen.Operate = gen.SetOperate;
+
+        gen.ResetStageIndex();
+        this.ExecuteStage();
+
+        gen.Operate = null;
+        gen.Arg = null;
+
+        String o;
+        o = this.StringComp.CreateData(data, null);
+
+        this.Result = o;
+        return true;
+    }
+
+    public virtual bool ExecuteStage()
+    {
+        ClassGen gen;
+        gen = this.Gen;
+
+        gen.ModuleStructName(this.Module.Ref);
+        return true;
+    }
+}
