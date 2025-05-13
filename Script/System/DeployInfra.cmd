2d6c17a1c8465db57bb3cbda9980ebe7d5a45081
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-13 21:40:29 +0800

    Script

--- Script/System/DeployInfra.cmd
+++ Script/System/DeployInfra.cmd
@@ -5,6 +5,6 @@ echo Deploy Infra
 set ClassOutFold=.\Out\Class
 set InfraDeployFold=.\Out\InfraDeploy
 
-mkdir %ClassOutFold%\Module 1>NUL 2>NUL
+mkdir %ClassOutFold% 1>NUL 2>NUL
 
-xcopy /S /E /Y "%InfraDeployFold%" "%ClassOutFold%\Module" 1>NUL 2>NUL
+xcopy /S /E /Y "%InfraDeployFold%" "%ClassOutFold%" 1>NUL 2>NUL
