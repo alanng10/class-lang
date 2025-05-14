9b5272ee742ce7616e642f18174d21d8b5cdc941
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-14 14:10:11 +0800

    Saber

--- Saber/Saber.Console/Saber.Console.data/MoveLib.sh
+++ Saber/Saber.Console/Saber.Console.data/MoveLib.sh
@@ -6,4 +6,4 @@ ClassPath=$(< ClassPath.txt)
 
 SourceFold=./Gen/$ModuleRef
 
-mv $SourceFold/lib$ModuleRef.so "../$ClassPath/Module/lib$ModuleRef.so" >/dev/null
+mv $SourceFold/lib$ModuleRef.so "../$ClassPath/lib$ModuleRef.so" >/dev/null
