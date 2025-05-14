618ed00908f6fd1955bc4ffed5d2b9294caf5622
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-15 00:49:33 +0800

    Saber

--- Saber/Saber.Infra/Class.cs
+++ Saber/Saber.Infra/Class.cs
@@ -3,22 +3,13 @@ namespace Saber.Infra;
 public class Class : Any
 {
     public virtual String Name { get; set; }
-
     public virtual Class Base { get; set; }
-
     public virtual Table Field { get; set; }
-
     public virtual Table Maide { get; set; }
-
     public virtual Module Module { get; set; }
-
     public virtual long BaseCount { get; set; }
-
     public virtual long FieldStart { get; set; }
-
     public virtual long MaideStart { get; set; }
-
     public virtual long Index { get; set; }
-
     public virtual object Any { get; set; }
 }
