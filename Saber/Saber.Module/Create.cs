8d1b29092a5061db7292e22e77ccaf31b9b245e9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-15 11:16:14 +0800

    Saber

--- Saber/Saber.Module/Create.cs
+++ Saber/Saber.Module/Create.cs
@@ -624,7 +624,7 @@ public class Create : ClassCreate
         }
 
         object ka;
-        ka = this.CompDefine(varClass.Base, a.Name);
+        ka = this.VirtualDefine(varClass.Base, a.Name);
 
         if (ka == null)
         {
@@ -687,7 +687,7 @@ public class Create : ClassCreate
         }
 
         object ka;
-        ka = this.CompDefine(varClass.Base, a.Name);
+        ka = this.VirtualDefine(varClass.Base, a.Name);
 
         if (ka == null)
         {
@@ -1035,7 +1035,7 @@ public class Create : ClassCreate
         return a;
     }
 
-    protected virtual object CompDefine(ClassClass varClass, String name)
+    protected virtual object VirtualDefine(ClassClass varClass, String name)
     {
         return this.ClassInfra.VirtualDefine(varClass, name, this.Module, this.System.Any);
     }
