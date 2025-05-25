152e8a54481d9bbe371eb918c9f40b5ce2f7ca03
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-25 17:07:08 +0800

    Saber

--- Saber/Saber.Module/Info.cs
+++ Saber/Saber.Module/Info.cs
@@ -2,12 +2,12 @@ namespace Saber.Module;
 
 public class Info : Any
 {
-    public virtual ClassClass Class { get; set; }
+    public virtual Class Class { get; set; }
     public virtual Field Field { get; set; }
     public virtual Maide Maide { get; set; }
     public virtual Var Var { get; set; }
-    public virtual ClassClass MarkClass { get; set; }
-    public virtual ClassClass OperateClass { get; set; }
+    public virtual Class MarkClass { get; set; }
+    public virtual Class OperateClass { get; set; }
     public virtual Field GetField { get; set; }
     public virtual Field SetField { get; set; }
     public virtual Maide CallMaide { get; set; }
