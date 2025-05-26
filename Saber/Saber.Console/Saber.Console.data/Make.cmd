24b75efc56228ec34c8c7c4e282fb2f4156c00b9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-26 15:17:28 +0800

    Saber

--- Saber/Saber.Console/Saber.Console.data/Make.cmd
+++ Saber/Saber.Console/Saber.Console.data/Make.cmd
@@ -7,10 +7,5 @@ set Module=%~1
 set /p ClassPath=<ClassPath.txt
 
 call DeleteLib %Module%
-rem call DeleteOutFold %Module%
 call MakeLib %Module%
 call MoveLib %Module%
-rem call MakeModuleProject %Module%
-rem call MakeModule %Module%
-rem call MoveBinary %Module%
-rem call DeleteOutFold %Module%
