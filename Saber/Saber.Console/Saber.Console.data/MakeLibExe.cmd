70385ed108faf44f1788c204678e2096afda674c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-26 15:23:38 +0800

    Saber

--- Saber/Saber.Console/Saber.Console.data/MakeLibExe.cmd
+++ Saber/Saber.Console/Saber.Console.data/MakeLibExe.cmd
@@ -2,7 +2,7 @@
 
 set ModuleRef=%~1
 
-set SourceFold=.\Gen\Exe-%ModuleRef%
+set SourceFold=.\Gen\%ModuleRef%
 
 pushd %SourceFold%
 clang -pipe -fno-strict-aliasing -O0 -std=gnu11 -Wall -Wno-ignored-attributes -Wno-bitwise-instead-of-logical -Wno-unused-but-set-variable -Wno-unused-variable -I. -I../.. -Wl,-s -Wl,-subsystem,console -L../../../%ClassPath% -lInfra -lInfraIntern -l%ModuleRef% -o %ModuleRef%.exe Main.c 1>NUL 2>..\..\ErrorExe.txt
