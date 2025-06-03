fd9327c958750526b23b2e8ac5add0c1ba29634b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-04 04:49:05 +0800

    Infra

---
+++ Infra/Infra/Screen.cpp
@@ -0,0 +1,37 @@
+#include "Screen.hpp"
+
+CppClassNew(Screen)
+
+Int Screen_Init(Int o)
+{
+    return true;
+}
+
+Int Screen_Final(Int o)
+{
+    return true;
+}
+
+Int Screen_Size(Int o)
+{
+    Int share;
+    share = Infra_Share();
+    Int stat;
+    stat = Share_Stat(share);
+
+    Int a;
+    a = Stat_ScreenSize(stat);
+    return a;
+}
+
+Int Screen_Dimend(Int o)
+{
+    Int share;
+    share = Infra_Share();
+    Int stat;
+    stat = Share_Stat(share);
+
+    Int a;
+    a = Stat_ScreenDimend(stat);
+    return a;
+}
