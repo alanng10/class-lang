7185228273a20c706590b5d09fe531d55698ad32
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-12 11:04:14 +0800

    Script

--- Script/Infra/Deploy.cmd
+++ Script/Infra/Deploy.cmd
@@ -9,3 +9,8 @@ call Script\Infra\UpdateDeploy
 pushd %InfraDeployFold%
 %WinDeployQt% Infra.dll
 popd
+
+call Script\Infra\DeployClangFile libc++.dll
+call Script\Infra\DeployClangFile libomp.dll
+call Script\Infra\DeployClangFile libunwind.dll
+call Script\Infra\DeployClangFile libwinpthread-1.dll
