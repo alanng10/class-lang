9cb05b0c53847f1e89adf2005c2a4564456fd401
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-27 10:24:20 +0800

    Saber

--- Saber/Saber.Binary/Maide.cs
+++ Saber/Saber.Binary/Maide.cs
@@ -3,10 +3,7 @@ namespace Saber.Binary;
 public class Maide : Any
 {
     public virtual long Class { get; set; }
-
     public virtual long Count { get; set; }
-
     public virtual String Name { get; set; }
-
     public virtual Array Param { get; set; }
 }
