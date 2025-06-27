f310ad13bb3e4c9cce2b224b78124d6fa441719e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-27 10:25:05 +0800

    Saber

--- Saber/Saber.Binary/Part.cs
+++ Saber/Saber.Binary/Part.cs
@@ -3,10 +3,7 @@ namespace Saber.Binary;
 public class Part : Any
 {
     public virtual long FieldStart { get; set; }
-
     public virtual long MaideStart { get; set; }
-
     public virtual Array Field { get; set; }
-
     public virtual Array Maide { get; set; }
 }
