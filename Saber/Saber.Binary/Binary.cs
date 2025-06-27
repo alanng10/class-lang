08452c27fa86693adda8c3e8099d88143567ee01
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-27 10:22:16 +0800

    Saber

--- Saber/Saber.Binary/Binary.cs
+++ Saber/Saber.Binary/Binary.cs
@@ -3,16 +3,10 @@ namespace Saber.Binary;
 public class Binary : Any
 {
     public virtual ModuleRef Ref { get; set; }
-
     public virtual Array Class { get; set; }
-
     public virtual Array Import { get; set; }
-
     public virtual Array Export { get; set; }
-
     public virtual Array Base { get; set; }
-
     public virtual Array Part { get; set; }
-
     public virtual long Entry { get; set; }
 }
