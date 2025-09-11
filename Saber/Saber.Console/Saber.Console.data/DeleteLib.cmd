673817f47e75045fac42867c1d013287f7f32234
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-11 14:33:38 +0800

    Saber

--- Saber/Saber.Console/Saber.Console.data/DeleteLib.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set ModuleRef=%~1
-
-del /F /Q "..\%ClassPath%\%ModuleRef%.dll" 2>NUL
--- Saber/Saber.Console/Saber.Console.data/MoveLib.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set ModuleRef=%~1
-
-set SourceFold=.\Gen\%ModuleRef%
-
-move %SourceFold%\%ModuleRef%.dll "..\%ClassPath%\%ModuleRef%.dll" >NUL
