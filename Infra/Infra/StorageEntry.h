8231130104c175d3aec4e11aa19ed6e431b8004c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-12 21:31:06 +0800

    Infra

---
+++ Infra/Infra/StorageEntry.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Pronate.h"
+
+typedef struct
+{
+    Int Name;
+    Int Exist;
+    Int Fold;
+    Int Size;
+    Int CreateTime;
+    Int ModifyTime;
+    Int Owner;
+    Int Group;
+    Int Permit;
+}
+StorageEntry;
