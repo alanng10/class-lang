a15c8c5882ff99b28f96258a2eab315adc83ebce
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-13 14:49:51 +0800

    Avalon

--- Avalon/Avalon.Storage/Entry.cs
+++ Avalon/Avalon.Storage/Entry.cs
@@ -6,8 +6,8 @@ public class Entry : Any
     public virtual bool Exist { get; set; }
     public virtual bool Fold { get; set; }
     public virtual long Size { get; set; }
-    public virtual TimeTime CreateTime { get; set; }
-    public virtual TimeTime ModifyTime { get; set; }
+    public virtual long CreateTime { get; set; }
+    public virtual long ModifyTime { get; set; }
     public virtual long Owner { get; set; }
     public virtual long Group { get; set; }
     public virtual Permit Permit { get; set; }
