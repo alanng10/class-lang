1d9b55bc325edbc6401f80ee069de91df14f61ab
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-13 12:48:17 +0800

    Avalon

--- Avalon/Avalon.Storage/Entry.cs
+++ Avalon/Avalon.Storage/Entry.cs
@@ -10,4 +10,5 @@ public class Entry : Any
     public virtual TimeTime ModifyTime { get; set; }
     public virtual long Owner { get; set; }
     public virtual long Group { get; set; }
+    public virtual Permit Permit { get; set; }
 }
