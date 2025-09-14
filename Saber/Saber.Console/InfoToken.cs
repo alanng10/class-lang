01fa2675bf715ded32a485ac682688c65fc162e5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-14 21:29:38 +0800

    Saber

--- Saber/Saber.Console/InfoToken.cs
+++ Saber/Saber.Console/InfoToken.cs
@@ -19,4 +19,35 @@ public class InfoToken : TextAdd
 
     protected virtual PrintChar PrintChar { get; set; }
     protected virtual long Space { get; set; }
+
+    protected virtual bool Start(String name)
+    {
+        this.Add(name).AddLine();
+        this.AddSpace().AddS("{").AddLine();
+
+        this.Space = this.Space + 4;
+        return true;
+    }
+
+    protected virtual InfoToken AddSpace()
+    {
+        long count;
+        count = this.Space;
+        long i;
+        i = 0;
+        while (i < count)
+        {
+            this.AddS(" ");
+
+            i = i + 1;
+        }
+
+        return this;
+    }
+
+    protected virtual bool Null()
+    {
+        this.AddS("null").AddS(",").AddLine();
+        return true;
+    }
 }
