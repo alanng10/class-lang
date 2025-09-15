c6a4232571c08b90928501a3ccd674d7f755c9d4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-15 16:16:29 +0800

    Saber

--- Saber/Saber.Binary/Field.cs
+++ Saber/Saber.Binary/Field.cs
@@ -5,4 +5,5 @@ public class Field : Any
     public virtual long Class { get; set; }
     public virtual long Count { get; set; }
     public virtual String Name { get; set; }
+    public virtual State Get { get; set; }
 }
