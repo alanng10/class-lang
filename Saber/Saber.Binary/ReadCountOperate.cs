9ccc7abc326f570520473afb87cd723202c232a9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-16 17:05:57 +0800

    Saber

--- Saber/Saber.Binary/ReadCountOperate.cs
+++ Saber/Saber.Binary/ReadCountOperate.cs
@@ -16,10 +16,12 @@ public class ReadCountOperate : ReadOperate
         this.Field = this.CreateField();
         this.Maide = this.CreateMaide();
         this.Var = this.CreateVar();
+        this.Entry = this.CreateEntry();
         this.ClassIndex = this.CreateClassIndex();
         this.ModuleRef = this.CreateModuleRef();
         this.String = this.CreateString();
         this.Array = this.CreateArray();
+        this.Data = this.CreateData();
         return true;
     }
 
@@ -79,6 +81,14 @@ public class ReadCountOperate : ReadOperate
         return a;
     }
 
+    protected virtual Entry CreateEntry()
+    {
+        Entry a;
+        a = new Entry();
+        a.Init();
+        return a;
+    }
+
     protected virtual Value CreateClassIndex()
     {
         Value a;
@@ -102,6 +112,15 @@ public class ReadCountOperate : ReadOperate
         return this.ListInfra.ArrayCreate(0);
     }
 
+    protected virtual Data CreateData()
+    {
+        Data a;
+        a = new Data();
+        a.Count = 0;
+        a.Init();
+        return a;
+    }
+
     protected virtual ListInfra ListInfra { get; set; }
     protected virtual TextInfra TextInfra { get; set; }
     protected virtual ClassInfra ClassInfra { get; set; }
@@ -112,10 +131,12 @@ public class ReadCountOperate : ReadOperate
     protected virtual Field Field { get; set; }
     protected virtual Maide Maide { get; set; }
     protected virtual Var Var { get; set; }
+    protected virtual Entry Entry { get; set; }
     protected virtual Value ClassIndex { get; set; }
     protected virtual ModuleRef ModuleRef { get; set; }
     protected virtual String String { get; set; }
     protected virtual Array Array { get; set; }
+    protected virtual Data Data { get; set; }
 
     public override Binary ExecuteBinary()
     {
