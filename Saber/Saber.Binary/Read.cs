f8278f0c2743b46ae0cb38e5759a1c27046cc404
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-16 17:35:17 +0800

    Saber

--- Saber/Saber.Binary/Read.cs
+++ Saber/Saber.Binary/Read.cs
@@ -96,10 +96,12 @@ public class Read : Any
         arg.FieldArray = this.ListInfra.ArrayCreate(arg.FieldIndex);
         arg.MaideArray = this.ListInfra.ArrayCreate(arg.MaideIndex);
         arg.VarArray = this.ListInfra.ArrayCreate(arg.VarIndex);
+        arg.EntryArray = this.ListInfra.ArrayCreate(arg.EntryIndex);
         arg.ClassIndexArray = this.ListInfra.ArrayCreate(arg.ClassIndexIndex);
         arg.ModuleRefArray = this.ListInfra.ArrayCreate(arg.ModuleRefIndex);
         arg.StringArray = this.ListInfra.ArrayCreate(arg.StringIndex);
         arg.ArrayArray = this.ListInfra.ArrayCreate(arg.ArrayIndex);
+        arg.DataArray = this.ListInfra.ArrayCreate(arg.DataIndex);
 
         this.ExecuteCreateBinary();
         this.ExecuteCreateClass();
