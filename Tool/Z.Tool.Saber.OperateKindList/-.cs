133699dfe3821ac882b927821d6609dc0671c86a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-17 12:46:25 +0800

    Tool

--- Tool/Z.Tool.Saber.OperateKindList/-.cs
+++ Tool/Z.Tool.Saber.OperateKindList/-.cs
@@ -4,5 +4,5 @@ global using Array = Avalon.List.Array;
 global using ListEntry = Avalon.List.Entry;
 global using Text = Avalon.Text.Text;
 global using EntryEntry = Avalon.Entry.Entry;
-global using SourceGen = Z.Infra.ListSourceGen.Gen;
+global using SourceGen = Z.Infra.ListSourceGen.AvalonGen;
 global using STAThreadAttribute = System.STAThreadAttribute;
