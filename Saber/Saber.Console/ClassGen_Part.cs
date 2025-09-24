45dcf2d8efdd5508eb6adc4c6e45a5e2fe4f9a3c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-24 22:57:23 +0800

    Saber

--- Saber/Saber.Console/ClassGen_Part.cs
+++ Saber/Saber.Console/ClassGen_Part.cs
@@ -7,31 +7,28 @@ partial class ClassGen
         this.OperateKindList.End.Any = this.InitOperateState(new EndClassGenOperateState());
         this.OperateKindList.Ret.Any = this.InitOperateState(new RetClassGenOperateState());
         this.OperateKindList.Refer.Any = this.InitOperateState(new ReferClassGenOperateState());
+        this.OperateKindList.Are.Any = this.InitOperateState(new AreClassGenOperateState());
         this.OperateKindList.InfStart.Any = this.InitOperateState(new InfStartClassGenOperateState());
         this.OperateKindList.InfEnd.Any = this.InitOperateState(new InfEndClassGenOperateState());
         this.OperateKindList.WhileStart.Any = this.InitOperateState(new WhileStartClassGenOperateState());
         this.OperateKindList.While.Any = this.InitOperateState(new WhileClassGenOperateState());
         this.OperateKindList.WhileEnd.Any = this.InitOperateState(new WhileEndClassGenOperateState());
-        this.OperateKindList.Are.Any = this.InitOperateState(new AreClassGenOperateState());
-        this.OperateKindList.VarMark.Any = this.InitOperateState(new VarMarkClassGenOperateState());
-        this.OperateKindList.SetMark.Any = this.InitOperateState(new SetMarkClassGenOperateState());
         this.OperateKindList.ItemGet.Any = this.InitOperateState(new GetClassGenOperateState());
+        this.OperateKindList.SetMark.Any = this.InitOperateState(new SetMarkClassGenOperateState());
         this.OperateKindList.Call.Any = this.InitOperateState(new CallClassGenOperateState());
         this.OperateKindList.Var.Any = this.InitOperateState(new VarClassGenOperateState());
-        this.OperateKindList.BoolValue.Any = this.InitOperateState(new BoolValueClassGenOperateState());
-        this.OperateKindList.IntValue.Any = this.InitOperateState(new IntValueClassGenOperateState());
-        this.OperateKindList.StringValue.Any = this.InitOperateState(new StringValueClassGenOperateState());
-        this.OperateKindList.ItemThis.Any = this.InitOperateState(new ThisClassGenOperateState());
-        this.OperateKindList.Base.Any = this.InitOperateState(new BaseClassGenOperateState());
-        this.OperateKindList.Null.Any = this.InitOperateState(new NullClassGenOperateState());
+        this.OperateKindList.VarMark.Any = this.InitOperateState(new VarMarkClassGenOperateState());
         this.OperateKindList.New.Any = this.InitOperateState(new NewClassGenOperateState());
         this.OperateKindList.Share.Any = this.InitOperateState(new ShareClassGenOperateState());
         this.OperateKindList.Cast.Any = this.InitOperateState(new CastClassGenOperateState());
+        this.OperateKindList.ItemThis.Any = this.InitOperateState(new ThisClassGenOperateState());
+        this.OperateKindList.Base.Any = this.InitOperateState(new BaseClassGenOperateState());
+        this.OperateKindList.Null.Any = this.InitOperateState(new NullClassGenOperateState());
         this.OperateKindList.Same.Any = this.InitOperateState(new SameClassGenOperateState());
+        this.OperateKindList.Less.Any = this.InitOperateState(new LessClassGenOperateState());
         this.OperateKindList.And.Any = this.InitOperateState(new AndClassGenOperateState());
         this.OperateKindList.Orn.Any = this.InitOperateState(new OrnClassGenOperateState());
         this.OperateKindList.Not.Any = this.InitOperateState(new NotClassGenOperateState());
-        this.OperateKindList.Less.Any = this.InitOperateState(new LessClassGenOperateState());
         this.OperateKindList.Add.Any = this.InitOperateState(new AddClassGenOperateState());
         this.OperateKindList.Sub.Any = this.InitOperateState(new SubClassGenOperateState());
         this.OperateKindList.Mul.Any = this.InitOperateState(new MulClassGenOperateState());
@@ -45,6 +42,9 @@ partial class ClassGen
         this.OperateKindList.BitLite.Any = this.InitOperateState(new BitLiteClassGenOperateState());
         this.OperateKindList.BitRite.Any = this.InitOperateState(new BitRiteClassGenOperateState());
         this.OperateKindList.BitSignRite.Any = this.InitOperateState(new BitSignRiteClassGenOperateState());
+        this.OperateKindList.BoolValue.Any = this.InitOperateState(new BoolValueClassGenOperateState());
+        this.OperateKindList.IntValue.Any = this.InitOperateState(new IntValueClassGenOperateState());
+        this.OperateKindList.StringValue.Any = this.InitOperateState(new StringValueClassGenOperateState());
 
         return true;
     }
