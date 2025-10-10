749376f06ec00cd008403522a503d54bba348230
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-10 16:42:38 +0800

    Saber

--- Saber/Saber.Infra/Var.cs
+++ Saber/Saber.Infra/Var.cs
@@ -4,6 +4,7 @@ public class Var : Any
 {
     public virtual String Name { get; set; }
     public virtual Class Class { get; set; }
-    public virtual long Index { get ;set; }
+    public virtual long Index { get; set; }
+    public virtual long Pos { get; set; }
     public virtual object Any { get; set; }
 }
