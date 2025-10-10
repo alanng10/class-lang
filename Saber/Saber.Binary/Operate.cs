0c9dc44d0e12bba13cf7e6dd487573454d8dfd23
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-10 15:26:08 +0800

    Saber

--- Saber/Saber.Binary/Operate.cs
+++ Saber/Saber.Binary/Operate.cs
@@ -5,4 +5,5 @@ public class Operate : Any
     public virtual long Kind { get; set; }
     public virtual OperateArg ArgA { get; set; }
     public virtual OperateArg ArgB { get; set; }
+    public virtual OperateArg ArgC { get; set; }
 }
