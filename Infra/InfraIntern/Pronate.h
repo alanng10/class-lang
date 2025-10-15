834be8d5f2a5d7f0ddf44cc09b53e32398c07fb4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-15 18:27:07 +0800

    Infra

--- Infra/InfraIntern/Pronate.h
+++ Infra/InfraIntern/Pronate.h
@@ -13,12 +13,6 @@
 
 #define BoolTrue 0x2000000000000001
 
-#define VarSetDeref(dest, var, pos) (dest = *(((Int*)var) + pos));
-
-#define MaskClear(name, mask) (name = name & mask);
-
-#define MaskSet(name, mask) (name = name | mask);
-
 #define RefKindClear(name) name = name & 0x0fffffffffffffff;
 
 #define RefKindSet(name, kind) name = name | (kind << 60);
