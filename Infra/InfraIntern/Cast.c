1acf6f98ef1ca7a946b2bc83757eacc6c9a25182
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-16 12:41:43 +0800

    Infra

--- Infra/InfraIntern/Cast.c
+++ Infra/InfraIntern/Cast.c
@@ -2,4 +2,13 @@
 
 Int Intern_Cast(Int classVar, Eval* eval)
 {
+    Int ka;
+    ka = eval->S[eval->N - 1];
+
+    
+
+
+    eval->S[eval->N - 1] = ka;
+
+    return 0;
 }
--- Infra/InfraIntern/Pronate.h
+++ Infra/InfraIntern/Pronate.h
@@ -13,7 +13,13 @@
 
 #define BoolTrue 0x2000000000000001
 
-#define RefKindClear(name) name = name & 0x0fffffffffffffff;
+#define VarSetDeref(dest, var, pos) (dest = *(((Int*)var) + pos));
+
+#define MaskClear(name, mask) (name = name & mask);
+
+#define MaskSet(name, mask) (name = name | mask);
+
+#define RefKindClear(name) MaskClear(name, 0x0fffffffffffffff);
 
 #define RefKindSet(name, kind) name = name | (kind << 60);
 
