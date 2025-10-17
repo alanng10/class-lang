e22c39e552730371c5004d747d33c00f9aac01d3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-17 15:17:06 +0800

    Script

---
+++ Script/System/Library.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+call ..\Script\Module\Library System.Infra
+call ..\Script\Module\Library System.List
+call ..\Script\Module\Library System.Math
+call ..\Script\Module\Library System.Text
+call ..\Script\Module\Library System.Thread
+call ..\Script\Module\Library System.Stream
+call ..\Script\Module\Library System.Time
+call ..\Script\Module\Library System.Storage
+call ..\Script\Module\Library System.Network
+call ..\Script\Module\Library System.Console
+call ..\Script\Module\Library System.Entry
+call Script\System\MakeDemoLibrary
