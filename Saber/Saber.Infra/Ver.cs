0aff967e52177f2f100df25a96ce9a51f4ca3720
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-18 14:37:31 +0800

    Saber

--- Saber/Saber.Infra/Ver.cs
+++ Saber/Saber.Infra/Ver.cs
@@ -4,5 +4,5 @@ public class Ver : Any
 {
     public virtual long Major { get; set; }
     public virtual long Minor { get; set; }
-    public virtual long Revise { get; set; }
+    public virtual long Melor { get; set; }
 }
