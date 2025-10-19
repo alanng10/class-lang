10e9636f8f5783c96d8646bc5200fe921625f66c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-19 21:25:27 +0800

    Saber

--- Saber/SaberTest/Seer.cs
+++ Saber/SaberTest/Seer.cs
@@ -3,12 +3,8 @@ namespace Saber.Test;
 class Seer : Any
 {
     public virtual String Name { get; set; }
-
     public virtual TaskKind TaskKind { get; set; }
-
     public virtual bool AddKindAfterTaskArg { get; set; }
-
     public virtual bool AddPathAfterTaskArg { get; set; }
-
     public virtual bool SourceFold { get; set; }
 }
