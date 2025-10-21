c96d86f4a069dc48f01fa7e924da2a9b9ff57987
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-21 11:11:54 +0800

    Script

--- Script/System/LibraryDemo.cmd
+++ Script/System/LibraryDemo.cmd
@@ -1,10 +1,4 @@
 @echo off
 
-set DotNetOutFold=.\Out\net8.0
-
-echo Library Demo
-pushd %DotNetOutFold%
-saber library SystemDemo-96207.08.47
-
-saber library SystemDemoNetwork-0.00.00
-popd
+call ..\Script\Module\Library SystemDemo-96207.08.47
+call ..\Script\Module\Library SystemDemoNetwork-0.00.00
