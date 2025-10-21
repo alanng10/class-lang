c100a9bc9ed14ddf2af26a36c430fe1d5a1cfcaf
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-21 11:15:57 +0800

    Script

--- Script/System/Make.cmd
+++ Script/System/Make.cmd
@@ -5,15 +5,15 @@ call Script\System\Clean
 call Script\System\DeployInfra
 call Script\System\DeployIntern
 
-call Script\System\MakeModule Infra
-call Script\System\MakeModule List
-call Script\System\MakeModule Math
-call Script\System\MakeModule Text
-call Script\System\MakeModule Thread
-call Script\System\MakeModule Stream
-call Script\System\MakeModule Time
-call Script\System\MakeModule Storage
-call Script\System\MakeModule Network
-call Script\System\MakeModule Console
-call Script\System\MakeModule Entry
+call Script\System\MakeModule System.Infra
+call Script\System\MakeModule System.List
+call Script\System\MakeModule System.Math
+call Script\System\MakeModule System.Text
+call Script\System\MakeModule System.Thread
+call Script\System\MakeModule System.Stream
+call Script\System\MakeModule System.Time
+call Script\System\MakeModule System.Storage
+call Script\System\MakeModule System.Network
+call Script\System\MakeModule System.Console
+call Script\System\MakeModule System.Entry
 call Script\System\MakeDemo
