28f2e457f2322b4f34657188a52eb1fb716c3825
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-23 16:58:33 +0800

    Script

--- Script/Infra/CleanExecute.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-call Script\Infra\Var
-
-set InfraExecuteOutFold=.\Out\InfraExecute-Windows-Release
-rmdir /S /Q %InfraExecuteOutFold% 2>NUL
--- Script/Infra/CleanExecute.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-InfraExecuteOutFold=Out/InfraExecute-Linux-Release
-rm -rf $InfraExecuteOutFold 2>/dev/null
